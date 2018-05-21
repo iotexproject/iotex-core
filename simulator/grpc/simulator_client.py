a3bda002c9d0686eee192952a78a1221d0e87679
Vedaad Shakib <vedaad799@gmail.com>
2018-05-21 10:16:41 -0700

    Modified gRPC message format

--- simulator/grpc/simulator_client.py
+++ simulator/grpc/simulator_client.py
@@ -24,10 +24,13 @@ import simulator_pb2_grpc
 def run():
     channel = grpc.insecure_channel('localhost:50051')
     stub = simulator_pb2_grpc.SimulatorStub(channel)
-    response = stub.Ping(simulator_pb2.Request(name='you'))
+    response = stub.Ping(simulator_pb2.Request(playerID=1,
+                                               senderID=9,
+                                               messageType=4,
+                                               value="block 44"))
 
     for i in response:
-        print(i.message)
+        print(i)
 
 if __name__ == '__main__':
     run()
