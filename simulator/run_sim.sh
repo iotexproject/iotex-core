5357b9c9ba1db0fb71ca5aac9d6e0b4bfd807546
Vedaad Shakib <vedaad799@gmail.com>
2018-05-24 17:18:57 -0700

    Simulator w/ RDPoS now works for one round

--- simulator/run_sim.sh
+++ simulator/run_sim.sh
@@ -11,11 +11,11 @@ PLAYERS="[(3, 1)]"
 N_VALIDATORS=4
 N_PROPOSERS=1
 N_CONNECTIONS=2
-N_HEARTBEATS_IN_ROUND=3
+N_HEARTBEATS_IN_ROUND=5
 P_TRANSACTIONS=0.1
 MEAN_PROP_TIME=0.1
 SEED=42
-N_ROUNDS=2
+N_ROUNDS=3
 
 python main.py --players="$PLAYERS" --nvalidators=$N_VALIDATORS --nproposers=$N_PROPOSERS --nconnections=$N_CONNECTIONS --nrounds=$N_ROUNDS --nheartbeatsinround=$N_HEARTBEATS_IN_ROUND --ptransactions=$P_TRANSACTIONS --meanproptime=$MEAN_PROP_TIME --seed=$SEED
 
