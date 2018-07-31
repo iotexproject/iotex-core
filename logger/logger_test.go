5723f299377417bf913650e8ba969d990b1c19f5
sggb <330771743@qq.com>
2018-07-31 22:59:16 +0800

    Remove Print and Printf APIs from logger.

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -11,11 +11,11 @@ import "testing"
 func TestLogger(t *testing.T) {
 	defer func() {
 		if r := recover(); r == nil {
-			Print("r == nil")
+			Log().Msg("r == nil")
 		}
 	}()
 
-	Print("test")
+	Log().Msg("test")
 	Debug().Str("test string", "test").Float32("test float", 1.2345).Msg("Test Msg")
 	Info().Str("test string", "test").Float32("test float", 1.2345).Msg("Test Msg")
 	Warn().Str("test string", "test").Float32("test float", 1.2345).Msg("Test Msg")
