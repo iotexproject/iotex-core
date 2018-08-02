285f3c2f020df80a1f253f792186c3dbd09f3cdd
Zhijie Shen <zhijie@iotex.io>
2018-08-01 23:25:20 -0700

    Revive e2e action pool tests

--- e2etest/local_actpool_test.go
+++ e2etest/local_actpool_test.go
@@ -26,49 +26,41 @@ import (
 )
 
 const (
+	// Make sure the key pairs used here match the genesis block
 	// Sender's public/private key pair
-	fromPubKey  = "4104b381ac1ace9c139ecae6850e6e1a48bcd9254cfa2359fc1d8b1098e4914b2ca2b4033218c95e638a38de09d005ce1cc45bd0a555865e8552f9cb7b8c1fe5916285310c0cf502"
-	fromPrivKey = "af149653127da7c46b4dc5ef9adf7a07405fe9962756185ec47849fe6e5093d7d3c21d01"
+	fromPubKey  = "408596b2dfe484c870c38146ca82ab0525a53978286dfd32866a757c080fd8cd4632b60649595ab5342fd560aabbe313cc2c02f0f6aff00e37965d6982abc84de4799f5e59884e06"
+	fromPrivKey = "eb0281546a98eb84aec2cd083a5e41478818275d18aea7fb2d0755d73a4d5b3b37370a00"
 	// Recipient's public/private key pair
-	toPubKey  = "734b0ce05a018f2aefc13c832cca64ba58b10ebbdc5bc3b6a549ab28bc08530e56e74002673aafbc6fc136aab63874318c8a2a5b68c6b53f2b9a7acd54996bdcd70a2fc72241f307"
-	toPrivKey = "a8cf5a40a7b76ed93433f4f92fe9a7140e5c3309769b188c647d1eecf9e1e6eedd0e5600"
+	toPubKey  = "63c098f9fbeafc1353362710ef7ad30e355114bb1531d77c055d61bc555f86a9bcda6206e73309a2c32996624bb41eca7a8db33421dea1b30f67bb6c8e7a37302e10d27617abc905"
+	toPrivKey = "277719b669a950cdd70872c87c7db0a4c4b24936650706ab750d5f0ea77c6301282ec800"
 )
 
 func TestLocalActPool(t *testing.T) {
-	t.Skip()
 	require := require.New(t)
 
 	testutil.CleanupPath(t, testTriePath)
 	testutil.CleanupPath(t, testDBPath)
 
 	cfg, err := newActPoolConfig()
-	require.Nil(err)
+	require.NoError(err)
 
 	blockchain.Gen.TotalSupply = uint64(50 << 22)
 	blockchain.Gen.BlockReward = uint64(0)
 
-	// create node
+	// create server
 	ctx := context.Background()
 	svr := itx.NewServer(cfg)
-	require.Nil(svr.Start(ctx))
+	require.NoError(svr.Start(ctx))
+	require.NotNil(svr.Ap())
 
-	bc := svr.Bc()
-	require.NotNil(bc)
-	t.Log("Create blockchain pass")
-
-	ap := svr.Ap()
-	require.NotNil(ap)
-
-	cfg.Network.IP = "127.0.0.1"
-	cfg.Network.Port = 10001
-	// Narrow down BootstrapNodes options to smooth peer maintenance
-	cfg.Network.BootstrapNodes = []string{"127.0.0.1:10000"}
-	p1 := network.NewOverlay(&cfg.Network)
-	require.NotNil(p1)
-	require.Nil(p1.Start(ctx))
+	// create client
+	cfg.Network.BootstrapNodes = []string{svr.P2p().Self().String()}
+	cli := network.NewOverlay(&cfg.Network)
+	require.NotNil(cli)
+	require.NoError(cli.Start(ctx))
 
 	defer func() {
-		require.Nil(p1.Stop(ctx))
+		require.Nil(cli.Stop(ctx))
 		require.Nil(svr.Stop(ctx))
 		testutil.CleanupPath(t, testTriePath)
 		testutil.CleanupPath(t, testDBPath)
@@ -77,11 +69,14 @@ func TestLocalActPool(t *testing.T) {
 	from := testutil.ConstructAddress(fromPubKey, fromPrivKey)
 	to := testutil.ConstructAddress(toPubKey, toPrivKey)
 
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 5*time.Second, func() (bool, error) {
+		return len(svr.P2p().GetPeers()) == 1 && len(cli.GetPeers()) == 1, nil
+	}))
+
 	// Create three valid actions from "from" to "to"
 	tsf1, _ := signedTransfer(from, to, uint64(1), big.NewInt(1))
 	vote2, _ := signedVote(from, from, uint64(2))
 	tsf3, _ := signedTransfer(from, to, uint64(3), big.NewInt(3))
-
 	// Create three invalid actions from "from" to "to"
 	// Existed Vote
 	vote4, _ := signedVote(from, from, uint64(2))
@@ -91,89 +86,47 @@ func TestLocalActPool(t *testing.T) {
 	// Unsigned Vote
 	vote6, _ := action.NewVote(uint64(6), from.RawAddress, from.RawAddress)
 
-	// Wrap transfers and votes as actions
-	act1 := &pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf1.ConvertToTransferPb()}}
-	act2 := &pb.ActionPb{Action: &pb.ActionPb_Vote{vote2.ConvertToVotePb()}}
-	act3 := &pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf3.ConvertToTransferPb()}}
-	act4 := &pb.ActionPb{Action: &pb.ActionPb_Vote{vote4.ConvertToVotePb()}}
-	act5 := &pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf5.ConvertToTransferPb()}}
-	act6 := &pb.ActionPb{Action: &pb.ActionPb_Vote{vote6.ConvertToVotePb()}}
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf1.ConvertToTransferPb()}}))
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Vote{vote2.ConvertToVotePb()}}))
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf3.ConvertToTransferPb()}}))
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Vote{vote4.ConvertToVotePb()}}))
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Transfer{tsf5.ConvertToTransferPb()}}))
+	require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Vote{vote6.ConvertToVotePb()}}))
 
-	// Wait until actions can be successfully broadcasted
-	err = testutil.WaitUntil(10*time.Millisecond, 2*time.Second, func() (bool, error) {
-		if err := p1.Broadcast(act1); err != nil {
-			return false, err
-		}
-		transfers, _ := ap.PickActs()
-		return len(transfers) == 1, nil
-	})
-	require.Nil(err)
-	err = p1.Broadcast(act2)
-	require.NoError(err)
-	err = p1.Broadcast(act3)
-	require.NoError(err)
-	err = p1.Broadcast(act4)
-	require.NoError(err)
-	err = p1.Broadcast(act5)
-	require.NoError(err)
-	err = p1.Broadcast(act6)
-	require.NoError(err)
-	// Wait until committed blocks contain all the broadcasted actions
-	err = testutil.WaitUntil(10*time.Millisecond, 5*time.Second, func() (bool, error) {
-		// Check whether current committed blocks contain all the valid actions picked from actpool
-		height, _ := bc.TipHeight()
-		var tsfCount int
-		var voteCount int
-		for h := height; h > 0; h-- {
-			blk, _ := bc.GetBlockByHeight(h)
-			if len(blk.Transfers) > 1 {
-				tsfCount += len(blk.Transfers) - 1
-			}
-			if len(blk.Votes) > 0 {
-				voteCount += len(blk.Votes)
-			}
-		}
-		// Excluding coinbase transfers, there should be 2 valid transfers and 1 valid vote in committed blocks
-		return tsfCount == 2 && voteCount == 1, nil
-	})
-	require.Nil(err)
+	// Wait until server receives all the transfers
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 5*time.Second, func() (bool, error) {
+		transfers, votes := svr.Ap().PickActs()
+		// 2 valid transfers and 1 valid vote
+		return len(transfers) == 2 && len(votes) == 1, nil
+	}))
 }
 
 func TestPressureActPool(t *testing.T) {
-	t.Skip()
 	require := require.New(t)
 
 	testutil.CleanupPath(t, testTriePath)
 	testutil.CleanupPath(t, testDBPath)
 
 	cfg, err := newActPoolConfig()
-	require.Nil(err)
+	require.NoError(err)
 
 	blockchain.Gen.TotalSupply = uint64(50 << 22)
 	blockchain.Gen.BlockReward = uint64(0)
 
-	// create node
+	// create server
 	ctx := context.Background()
 	svr := itx.NewServer(cfg)
 	require.Nil(svr.Start(ctx))
+	require.NotNil(svr.Ap())
 
-	bc := svr.Bc()
-	require.NotNil(bc)
-	t.Log("Create blockchain pass")
-
-	ap := svr.Ap()
-	require.NotNil(ap)
-
-	cfg.Network.IP = "127.0.0.1"
-	cfg.Network.Port = 10001
-	// Narrow down BootstrapNodes options to smooth peer maintenance
-	cfg.Network.BootstrapNodes = []string{"127.0.0.1:10000"}
-	p1 := network.NewOverlay(&cfg.Network)
-	require.NotNil(p1)
-	require.Nil(p1.Start(ctx))
+	// create client
+	cfg.Network.BootstrapNodes = []string{svr.P2p().Self().String()}
+	cli := network.NewOverlay(&cfg.Network)
+	require.NotNil(cli)
+	require.Nil(cli.Start(ctx))
 
 	defer func() {
-		require.Nil(p1.Stop(ctx))
+		require.Nil(cli.Stop(ctx))
 		require.Nil(svr.Stop(ctx))
 		testutil.CleanupPath(t, testTriePath)
 		testutil.CleanupPath(t, testDBPath)
@@ -182,40 +135,20 @@ func TestPressureActPool(t *testing.T) {
 	from := testutil.ConstructAddress(fromPubKey, fromPrivKey)
 	to := testutil.ConstructAddress(toPubKey, toPrivKey)
 
-	// Create 1000 valid transfers and broadcast
-	tsf1, _ := signedTransfer(from, to, uint64(1), big.NewInt(1))
-	// Wrap transfers and votes as actions
-	act1 := &pb.ActionPb{Action: &pb.ActionPb_Transfer{Transfer: tsf1.ConvertToTransferPb()}}
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 5*time.Second, func() (bool, error) {
+		return len(svr.P2p().GetPeers()) == 1 && len(cli.GetPeers()) == 1, nil
+	}))
 
-	// Wait until transfers can be successfully broadcasted
-	err = testutil.WaitUntil(10*time.Millisecond, 2*time.Second, func() (bool, error) {
-		if err := p1.Broadcast(act1); err != nil {
-			return false, err
-		}
-		transfers, _ := ap.PickActs()
-		return len(transfers) == 1, nil
-	})
 	require.Nil(err)
-	for i := 2; i <= 1000; i++ {
+	for i := 1; i <= 1000; i++ {
 		tsf, _ := signedTransfer(from, to, uint64(i), big.NewInt(int64(i)))
-		act := &pb.ActionPb{Action: &pb.ActionPb_Transfer{Transfer: tsf.ConvertToTransferPb()}}
-		err := p1.Broadcast(act)
-		require.NoError(err)
+		require.NoError(cli.Broadcast(&pb.ActionPb{Action: &pb.ActionPb_Transfer{Transfer: tsf.ConvertToTransferPb()}}))
 	}
 
 	// Wait until committed blocks contain all broadcasted actions
 	err = testutil.WaitUntil(10*time.Millisecond, 10*time.Second, func() (bool, error) {
-		// Check whether current committed blocks contain all the valid actions picked from actpool
-		height, _ := bc.TipHeight()
-		var tsfCount int
-		for h := height; h > 0; h-- {
-			blk, _ := bc.GetBlockByHeight(h)
-			if len(blk.Transfers) >= 1 {
-				tsfCount += len(blk.Transfers) - 1
-			}
-		}
-		// Excluding coinbase transfers, there should be 256 valid transfers in committed blocks
-		return tsfCount == 256, nil
+		transfers, _ := svr.Ap().PickActs()
+		return len(transfers) == 1000, nil
 	})
 	require.Nil(err)
 }
@@ -249,12 +182,9 @@ func newActPoolConfig() (*config.Config, error) {
 	cfg.Chain.TrieDBPath = testTriePath
 	cfg.Chain.InMemTest = false
 	cfg.Chain.ChainDBPath = testDBPath
-	cfg.Consensus.Scheme = config.StandaloneScheme
-	cfg.Consensus.BlockCreationInterval = time.Second
-	cfg.Network.IP = "127.0.0.1"
-	cfg.Network.Port = 10000
-	cfg.Network.BootstrapNodes = []string{"127.0.0.1:10000", "127.0.0.1:4687"}
-	cfg.ActPool.MaxNumActPerAcct = 256
+	cfg.Consensus.Scheme = config.NOOPScheme
+	cfg.Network.Port = 0
+	cfg.Network.PeerMaintainerInterval = 100 * time.Millisecond
 
 	addr, err := iotxaddress.NewAddress(true, iotxaddress.ChainID)
 	if err != nil {
