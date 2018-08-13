4775ac0c4e4a88287be2fb7978439985b6e295ac
Dustin Xie <dustin.xie@iotex.io>
2018-08-13 15:13:14 -0700

    VM phase 1: implement Contract interface

--- state/contract.go
+++ state/contract.go
@@ -7,6 +7,8 @@
 package state
 
 import (
+	"context"
+
 	"github.com/iotexproject/iotex-core/pkg/hash"
 	"github.com/iotexproject/iotex-core/trie"
 )
@@ -14,14 +16,56 @@ import (
 type (
 	// Contract is a special type of account with code and storage trie.
 	Contract interface {
-		GetState(key hash.Hash32B) hash.Hash32B
-		SetState(key, value hash.Hash32B)
+		GetState(key hash.Hash32B) ([]byte, error)
+		SetState(key hash.Hash32B, value []byte) error
+		SelfState() *State
+		Commit() error
+		RootHash() hash.Hash32B
 	}
 
 	contract struct {
 		State
-		addrHash hash.PKHash  // 20-byte contract address hash
-		root     hash.Hash32B // root of storage trie
-		trie     trie.Trie    // storage trie of the contract
+		trie trie.Trie // storage trie of the contract
 	}
 )
+
+// GetState get the value from contract storage
+func (c *contract) GetState(key hash.Hash32B) ([]byte, error) {
+	v, err := c.trie.Get(key[:])
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+// SetState set the value into contract storage
+func (c *contract) SetState(key hash.Hash32B, value []byte) error {
+	return c.trie.Upsert(key[:], value)
+}
+
+// State returns this contract's state
+func (c *contract) SelfState() *State {
+	return &c.State
+}
+
+// Commit writes the changes into underlying trie
+func (c *contract) Commit() error {
+	c.State.Root = c.trie.RootHash()
+	return c.trie.Commit()
+}
+
+// RootHash returns storage trie's root hash
+func (c *contract) RootHash() hash.Hash32B {
+	return c.State.Root
+}
+
+// newContract returns a Contract instance
+func newContract(tr trie.Trie, state State, root hash.Hash32B) Contract {
+	c := contract{
+		State: state,
+		trie:  tr,
+	}
+	c.State.Root = root
+	c.trie.Start(context.Background())
+	return &c
+}
