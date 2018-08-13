92b8f4cf971c33c430c69dafc40f4d577b4d7f26
Dustin Xie <dustin.xie@iotex.io>
2018-08-12 23:29:40 -0700

    Add contract.go

---
+++ state/contract.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2018 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package state
+
+import (
+	"github.com/iotexproject/iotex-core-internal/pkg/hash"
+	"github.com/iotexproject/iotex-core-internal/trie"
+)
+
+type (
+	// Contract is a special type of account with code and storage trie.
+	Contract interface {
+		GetState(key hash.Hash32B) hash.Hash32B
+		SetState(key, value hash.Hash32B)
+	}
+
+	contract struct {
+		State
+		addrHash hash.PKHash  // 20-byte contract address hash
+		root     hash.Hash32B // root of storage trie
+		trie     trie.Trie    // storage trie of the contract
+	}
+)
