2147811f44bdb423198ea66b77de9d57709bb870
Poppo <330771743@qq.com>
2018-08-14 14:52:41 +0800

    replace remaining std log usage with zerolog

--- tools/addrgen/internal/cmd/generate.go
+++ tools/addrgen/internal/cmd/generate.go
@@ -8,11 +8,11 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/spf13/cobra"
 
 	"github.com/iotexproject/iotex-core/iotxaddress"
+	"github.com/iotexproject/iotex-core/logger"
 )
 
 // generateCmd represents the generate command
@@ -21,7 +21,7 @@ var generateCmd = &cobra.Command{
 	Short: "Generates n number of iotex address key pairs.",
 	Long:  `Generates n number of iotex address key pairs.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("\n%s\n", generate(args))
+		logger.Info().Msgf("\n%s\n", generate(args))
 	},
 }
 
@@ -32,7 +32,7 @@ func generate(args []string) string {
 	for i := 0; i < _addrNum; i++ {
 		addr, err := iotxaddress.NewAddress(iotxaddress.IsTestnet, iotxaddress.ChainID)
 		if err != nil {
-			log.Fatal(err)
+			logger.Fatal().Err(err).Msg("failed to create address")
 		}
 		out += fmt.Sprintf("Public Key: %x\nPrivate Key: %x\nRaw Address: %s\n\n",
 			addr.PublicKey, addr.PrivateKey, addr.RawAddress)
