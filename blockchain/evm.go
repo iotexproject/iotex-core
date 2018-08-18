046637901f743cbf772f9c97e69b046216646cd0
Zhen Li <zhen.li@iotex.io>
2018-08-17 18:30:10 -0700

    Receipt and Log Serialization/Dederialization

--- blockchain/evm.go
+++ blockchain/evm.go
@@ -14,12 +14,15 @@ import (
 	"github.com/CoderZhi/go-ethereum/common"
 	"github.com/CoderZhi/go-ethereum/core/vm"
 	"github.com/CoderZhi/go-ethereum/params"
+	"github.com/golang/protobuf/proto"
+	"github.com/pkg/errors"
+
 	"github.com/iotexproject/iotex-core/blockchain/action"
 	"github.com/iotexproject/iotex-core/iotxaddress"
 	"github.com/iotexproject/iotex-core/logger"
 	"github.com/iotexproject/iotex-core/pkg/hash"
 	"github.com/iotexproject/iotex-core/pkg/keypair"
-	"github.com/pkg/errors"
+	"github.com/iotexproject/iotex-core/proto"
 )
 
 var (
@@ -157,6 +160,97 @@ func GetHashFn(stateDB *EVMStateDBAdapter) func(n uint64) common.Hash {
 
 		return common.Hash{}
 	}
+}
+
+// ConvertToReceiptPb converts a Receipt to protobuf's ReceiptPb
+func (receipt *Receipt) ConvertToReceiptPb() *iproto.ReceiptPb {
+	r := &iproto.ReceiptPb{}
+	r.ReturnValue = receipt.ReturnValue
+	r.Status = receipt.Status
+	r.Hash = receipt.Hash[:]
+	r.GasConsumed = receipt.GasConsumed
+	r.ContractAddress = receipt.ContractAddress
+	r.Logs = []*iproto.LogPb{}
+	for _, log := range receipt.Logs {
+		r.Logs = append(r.Logs, log.ConvertToLogPb())
+	}
+	return r
+}
+
+// ConvertFromReceiptPb converts a protobuf's ReceiptPb to Receipt
+func (receipt *Receipt) ConvertFromReceiptPb(pbReceipt *iproto.ReceiptPb) {
+	receipt.ReturnValue = pbReceipt.GetReturnValue()
+	receipt.Status = pbReceipt.GetStatus()
+	copy(receipt.Hash[:], pbReceipt.GetHash())
+	receipt.GasConsumed = pbReceipt.GetGasConsumed()
+	receipt.ContractAddress = pbReceipt.GetContractAddress()
+	logs := pbReceipt.GetLogs()
+	receipt.Logs = make([]*Log, len(logs))
+	for i, log := range logs {
+		receipt.Logs[i] = &Log{}
+		receipt.Logs[i].ConvertFromLogPb(log)
+	}
+}
+
+// Serialize returns a serialized byte stream for the Receipt
+func (receipt *Receipt) Serialize() ([]byte, error) {
+	return proto.Marshal(receipt.ConvertToReceiptPb())
+}
+
+// Deserialize parse the byte stream into Receipt
+func (receipt *Receipt) Deserialize(buf []byte) error {
+	pbReceipt := &iproto.ReceiptPb{}
+	if err := proto.Unmarshal(buf, pbReceipt); err != nil {
+		return err
+	}
+	receipt.ConvertFromReceiptPb(pbReceipt)
+	return nil
+}
+
+// ConvertToLogPb converts a Log to protobuf's LogPb
+func (log *Log) ConvertToLogPb() *iproto.LogPb {
+	l := &iproto.LogPb{}
+	l.Address = log.Address
+	l.Topics = [][]byte{}
+	for _, topic := range log.Topics {
+		l.Topics = append(l.Topics, topic[:])
+	}
+	l.Data = log.Data
+	l.BlockNumber = log.BlockNumber
+	l.TxnHash = log.TxnHash[:]
+	l.BlockHash = log.BLockHash[:]
+	l.Index = uint32(log.Index)
+	return l
+}
+
+// ConvertFromLogPb converts a protobuf's LogPb to Log
+func (log *Log) ConvertFromLogPb(pbLog *iproto.LogPb) {
+	log.Address = pbLog.GetAddress()
+	pbLogs := pbLog.GetTopics()
+	log.Topics = make([]hash.Hash32B, len(pbLogs))
+	for i, topic := range pbLogs {
+		copy(log.Topics[i][:], topic)
+	}
+	log.Data = pbLog.GetData()
+	log.BlockNumber = pbLog.GetBlockNumber()
+	copy(log.TxnHash[:], pbLog.GetTxnHash())
+	copy(log.BLockHash[:], pbLog.GetBlockHash())
+	log.Index = uint(pbLog.GetIndex())
+}
+
+// Serialize returns a serialized byte stream for the Log
+func (log *Log) Serialize() ([]byte, error) {
+	return proto.Marshal(log.ConvertToLogPb())
+}
+
+// Deserialize parse the byte stream into Log
+func (log *Log) Deserialize(buf []byte) error {
+	pbLog := &iproto.LogPb{}
+	if err := proto.Unmarshal(buf, pbLog); err != nil {
+		return err
+	}
+	log.ConvertFromLogPb(pbLog)
+	return nil
 }
 
 func securityDeposit(ps *EVMParams, stateDB vm.StateDB, gasLimit *uint64) error {
--- blockchain/evm_test.go
+++ blockchain/evm_test.go
@@ -88,3 +88,39 @@ func TestEVM(t *testing.T) {
 	require.NoError(err)
 	require.Nil(bc.CommitBlock(blk))
 }
+
+func TestLogReceipt(t *testing.T) {
+	require := require.New(t)
+	log := Log{Address: "abcde", Data: []byte("12345"), BlockNumber: 5, Index: 6}
+	var topic hash.Hash32B
+	copy(topic[:], hash.Hash256b([]byte("12345")))
+	log.Topics = []hash.Hash32B{topic}
+	copy(log.TxnHash[:], hash.Hash256b([]byte("11111")))
+	copy(log.BLockHash[:], hash.Hash256b([]byte("22222")))
+	s, err := log.Serialize()
+	require.NoError(err)
+	actuallog := Log{}
+	actuallog.Deserialize(s)
+	require.Equal(log.Address, actuallog.Address)
+	require.Equal(log.Topics[0], actuallog.Topics[0])
+	require.Equal(len(log.Topics), len(actuallog.Topics))
+	require.Equal(log.Data, actuallog.Data)
+	require.Equal(log.BlockNumber, actuallog.BlockNumber)
+	require.Equal(log.TxnHash, actuallog.TxnHash)
+	require.Equal(log.BLockHash, actuallog.BLockHash)
+	require.Equal(log.Index, actuallog.Index)
+
+	receipt := Receipt{ReturnValue: []byte("12345"), Status: 5, GasConsumed: 6, ContractAddress: "aaaaa", Logs: []*Log{&log}}
+	copy(receipt.Hash[:], hash.Hash256b([]byte("33333")))
+	s, err = receipt.Serialize()
+	require.NoError(err)
+	actualReceipt := Receipt{}
+	actualReceipt.Deserialize(s)
+	require.Equal(receipt.ReturnValue, actualReceipt.ReturnValue)
+	require.Equal(receipt.Status, actualReceipt.Status)
+	require.Equal(receipt.GasConsumed, actualReceipt.GasConsumed)
+	require.Equal(receipt.ContractAddress, actualReceipt.ContractAddress)
+	require.Equal(receipt.Logs[0], actualReceipt.Logs[0])
+	require.Equal(len(receipt.Logs), len(actualReceipt.Logs))
+	require.Equal(receipt.Hash, actualReceipt.Hash)
+}
