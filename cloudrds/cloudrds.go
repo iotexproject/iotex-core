4eb05b39ecacaa71ffadb01f79d33fca59648813
xiaxiangzhou <xiaxiangzhou@gmail.com>
2018-09-23 00:35:48 -0400

    add cloudrds

--- cloudrds/cloudrds.go
+++ cloudrds/cloudrds.go
@@ -1,1 +1,144 @@
 package cloudrds
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"sync"
+
+	// we need mysql import because it's called in file, (but compile will complain because there is no display)
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/iotexproject/iotex-core/pkg/lifecycle"
+)
+
+var (
+	awsRDSEndpoint = "iotex-explorer-db.ctcedgqcwrb5.us-west-1.rds.amazonaws.com"
+	awsRDSPort     = 4086
+	awsRegion      = "us-west-1"
+	awsRDSUser     = "explorer_admin"
+	awsPass        = "j1cDiH7W7QCB"
+	awsDBName      = "explorer"
+)
+
+// RDSStore is the interface of KV store.
+type RDSStore interface {
+	lifecycle.StartStopper
+
+	// Get DB instance
+	GetDB() *sql.DB
+
+	// ParseRows parse the returned raw rows
+	ParseRows(rows *sql.Rows) ([][]sql.RawBytes, error)
+
+	// Transact wrap the transaction
+	Transact(txFunc func(*sql.Tx) error) error
+}
+
+// rds is RDSStore implementation based aws RDS
+type rds struct {
+	mutex sync.RWMutex
+	db    *sql.DB
+}
+
+// NewAwsRDS instantiates an aws RDS based RDS
+func NewAwsRDS() RDSStore {
+	return &rds{db: nil}
+}
+
+// Start opens the RDS (creates new file if not existing yet)
+func (r *rds) Start(_ context.Context) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.db != nil {
+		return nil
+	}
+
+	connectStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		awsRDSUser, awsPass, awsRDSEndpoint, awsRDSPort, awsDBName,
+	)
+
+	// Use db to perform SQL operations on database
+	db, err := sql.Open("mysql", connectStr)
+	if err != nil {
+		return err
+	}
+	r.db = db
+	return nil
+}
+
+// Stop closes the AWS RDS
+func (r *rds) Stop(_ context.Context) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.db != nil {
+		err := r.db.Close()
+		r.db = nil
+		return err
+	}
+	return nil
+}
+
+// Stop closes the AWS RDS
+func (r *rds) GetDB() *sql.DB {
+	return r.db
+}
+
+// Parse rows
+func (r *rds) ParseRows(rows *sql.Rows) ([][]sql.RawBytes, error) {
+	var parsedRows [][]sql.RawBytes
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(columns)
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+	fmt.Println(values)
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	fmt.Println(scanArgs)
+
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+		parsedRows = append(parsedRows, values)
+	}
+
+	return parsedRows, nil
+}
+
+// Transact wrap the transaction
+func (r *rds) Transact(txFunc func(*sql.Tx) error) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			tx.Rollback() // err is non-nil; don't change it
+		} else {
+			err = tx.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+	err = txFunc(tx)
+	return err
+}
---
+++ cloudrds/cloudrds_test.go
@@ -0,0 +1,241 @@
+// Copyright (c) 2018 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package cloudrds
+
+import (
+	"context"
+	"math/rand"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"database/sql"
+	"errors"
+	"github.com/iotexproject/iotex-core/pkg/hash"
+	"github.com/iotexproject/iotex-core/testutil"
+)
+
+func TestRDSStorePutGet(t *testing.T) {
+	testRDSStorePutGet := func(rdsStore RDSStore, t *testing.T) {
+		require := require.New(t)
+		ctx := context.Background()
+
+		err := rdsStore.Start(ctx)
+		require.Nil(err)
+		defer func() {
+			err = rdsStore.Stop(ctx)
+			require.Nil(err)
+		}()
+
+		db := rdsStore.GetDB()
+
+		nodeAddress := "aaa"
+		userAddress := "bbb"
+		transferHash := hash.ZeroHash32B
+
+		// insert
+		stmt, err := db.Prepare("INSERT transfer_history SET node_address=?,user_address=?,transfer_hash=?")
+		require.Nil(err)
+
+		res, err := stmt.Exec(nodeAddress, userAddress, transferHash[:])
+		require.Nil(err)
+
+		affect, err := res.RowsAffected()
+		require.Nil(err)
+		require.Equal(int64(1), affect)
+
+		// get
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=?")
+		require.Nil(err)
+
+		rows, err := stmt.Query(nodeAddress)
+		require.Nil(err)
+
+		parsedRows, err := rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(1, len(parsedRows))
+		require.Equal(nodeAddress, string(parsedRows[0][0]))
+		require.Equal(userAddress, string(parsedRows[0][1]))
+		require.Equal(string(transferHash[:]), string(parsedRows[0][2]))
+
+		// delete
+		stmt, err = db.Prepare("DELETE FROM transfer_history WHERE node_address=? AND user_address=? AND transfer_hash=?")
+		require.Nil(err)
+
+		res, err = stmt.Exec(nodeAddress, userAddress, transferHash[:])
+		require.Nil(err)
+
+		affect, err = res.RowsAffected()
+		require.Nil(err)
+		require.Equal(int64(1), affect)
+
+		// get
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=?")
+		require.Nil(err)
+
+		rows, err = stmt.Query(nodeAddress)
+		require.Nil(err)
+
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+	}
+
+	path := "/tmp/test-rds-store-" + strconv.Itoa(rand.Int())
+	t.Run("RDS Store", func(t *testing.T) {
+		testutil.CleanupPath(t, path)
+		defer testutil.CleanupPath(t, path)
+		testRDSStorePutGet(NewAwsRDS(), t)
+	})
+}
+
+func TestRDSStoreTransaction(t *testing.T) {
+	testRDSStoreTransaction := func(rdsStore RDSStore, t *testing.T) {
+		require := require.New(t)
+		ctx := context.Background()
+
+		err := rdsStore.Start(ctx)
+		require.Nil(err)
+		defer func() {
+			err = rdsStore.Stop(ctx)
+			require.Nil(err)
+		}()
+
+		db := rdsStore.GetDB()
+
+		nodeAddress := "aaa"
+		userAddress1 := "bbb1"
+		userAddress2 := "bbb2"
+		transferHash := hash.ZeroHash32B
+
+		// get
+		stmt, err := db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err := stmt.Query(nodeAddress, userAddress1)
+		require.Nil(err)
+		parsedRows, err := rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress2)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+
+		/*require.Equal(node_address, string(parsedRows[0][0]))
+		require.Equal(user_adress1, string(parsedRows[0][1]))
+		require.Equal(string(transfer_hash[:]), string(parsedRows[0][2]))*/
+
+		// insert transaction with fail
+		err = rdsStore.Transact(func(tx *sql.Tx) error {
+			insertQuery := "INSERT transfer_history SET node_address=?,user_address=?,transfer_hash=?"
+			if _, err := tx.Exec(insertQuery, nodeAddress, userAddress1, transferHash[:]); err != nil {
+				return err
+			}
+			return errors.New("raise error on purpose")
+			if _, err := tx.Exec(insertQuery, nodeAddress, userAddress2, transferHash[:]); err != nil {
+				return err
+			}
+			return nil
+		})
+		require.NotNil(err)
+
+		// get
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress1)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress2)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+
+		// insert
+		err = rdsStore.Transact(func(tx *sql.Tx) error {
+			insertQuery := "INSERT transfer_history SET node_address=?,user_address=?,transfer_hash=?"
+			if _, err := tx.Exec(insertQuery, nodeAddress, userAddress1, transferHash[:]); err != nil {
+				return err
+			}
+			if _, err := tx.Exec(insertQuery, nodeAddress, userAddress2, transferHash[:]); err != nil {
+				return err
+			}
+			return nil
+		})
+		require.Nil(err)
+
+		// get
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress1)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(1, len(parsedRows))
+		require.Equal(nodeAddress, string(parsedRows[0][0]))
+		require.Equal(userAddress1, string(parsedRows[0][1]))
+		require.Equal(string(transferHash[:]), string(parsedRows[0][2]))
+
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress2)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(1, len(parsedRows))
+		require.Equal(nodeAddress, string(parsedRows[0][0]))
+		require.Equal(userAddress2, string(parsedRows[0][1]))
+		require.Equal(string(transferHash[:]), string(parsedRows[0][2]))
+
+		// delete
+		err = rdsStore.Transact(func(tx *sql.Tx) error {
+			deleteQuery := "DELETE FROM transfer_history WHERE node_address=? AND user_address=? AND transfer_hash=?"
+			if _, err := tx.Exec(deleteQuery, nodeAddress, userAddress1, transferHash[:]); err != nil {
+				return err
+			}
+			if _, err := tx.Exec(deleteQuery, nodeAddress, userAddress2, transferHash[:]); err != nil {
+				return err
+			}
+			return nil
+		})
+		require.Nil(err)
+
+		// get
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress1)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+
+		stmt, err = db.Prepare("SELECT * FROM transfer_history WHERE node_address=? AND user_address=?")
+		require.Nil(err)
+		rows, err = stmt.Query(nodeAddress, userAddress2)
+		require.Nil(err)
+		parsedRows, err = rdsStore.ParseRows(rows)
+		require.Nil(err)
+		require.Equal(0, len(parsedRows))
+	}
+
+	path := "/tmp/test-rds-store-batch-rollback" + strconv.Itoa(rand.Int())
+	t.Run("RDS Store", func(t *testing.T) {
+		testutil.CleanupPath(t, path)
+		defer testutil.CleanupPath(t, path)
+		testRDSStoreTransaction(NewAwsRDS(), t)
+	})
+}
