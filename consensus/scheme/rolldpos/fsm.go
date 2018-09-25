cc392b98c167ab6549a16758a1b72cbea39c2e13
zhi <thecoderzhi@gmail.com>
2018-09-25 12:47:26 -0700

    Add proposer verification

--- consensus/scheme/rolldpos/fsm.go
+++ consensus/scheme/rolldpos/fsm.go
@@ -127,15 +127,13 @@ func (e *consensusEvt) fromProtoMsg(pMsg *iproto.ViewChangeMsg) error {
 
 type proposeBlkEvt struct {
 	consensusEvt
-	block    *blockchain.Block
-	proposer string
+	block *blockchain.Block
 }
 
-func newProposeBlkEvt(block *blockchain.Block, proposer string, c clock.Clock) *proposeBlkEvt {
+func newProposeBlkEvt(block *blockchain.Block, c clock.Clock) *proposeBlkEvt {
 	return &proposeBlkEvt{
 		consensusEvt: *newCEvt(eProposeBlock, c),
 		block:        block,
-		proposer:     proposer,
 	}
 }
 
@@ -143,7 +141,7 @@ func (e *proposeBlkEvt) toProtoMsg() (*iproto.ViewChangeMsg, error) {
 	return &iproto.ViewChangeMsg{
 		Vctype:     iproto.ViewChangeMsg_PROPOSE,
 		Block:      e.block.ConvertToBlockPb(),
-		SenderAddr: e.proposer,
+		SenderAddr: e.block.ProducerAddress(),
 	}, nil
 }
 
@@ -155,7 +153,6 @@ func (e *proposeBlkEvt) fromProtoMsg(pMsg *iproto.ViewChangeMsg) error {
 		e.block = &blockchain.Block{}
 		e.block.ConvertFromBlockPb(pMsg.Block)
 	}
-	e.proposer = pMsg.SenderAddr
 	return nil
 }
 
@@ -477,20 +474,30 @@ func (m *cFSM) handleProposeBlockEvt(evt fsm.Event) (fsm.State, error) {
 			return sInvalid, errors.Wrap(err, "error when calculating the proposer")
 		}
 		blkHash := proposeBlkEvt.block.HashBlock()
-		if proposeBlkEvt.proposer != proposer {
+		producer := proposeBlkEvt.block.ProducerAddress()
+		switch {
+		case producer == "" || producer != proposer:
 			logger.Error().
-				Str("proposer", proposeBlkEvt.proposer).
-				Str("actualProposer", proposer).
+				Str("proposer", producer).
+				Str("expectedProposer", proposer).
 				Uint64("block", proposeBlkEvt.block.Height()).
 				Str("hash", hex.EncodeToString(blkHash[:])).
 				Err(err).
 				Msg("error when validating the block proposer")
 			validated = false
-		} else if proposeBlkEvt.proposer != m.ctx.addr.RawAddress {
+		case !proposeBlkEvt.block.VerifySignature():
+			logger.Error().
+				Str("proposer", producer).
+				Uint64("block", proposeBlkEvt.block.Height()).
+				Str("hash", hex.EncodeToString(blkHash[:])).
+				Err(err).
+				Msg("error when validating the block signature")
+			validated = false
+		case producer != m.ctx.addr.RawAddress:
 			// If the block is self proposed, skip validation
 			if err := m.ctx.chain.ValidateBlock(proposeBlkEvt.block); err != nil {
 				logger.Error().
-					Str("proposer", proposeBlkEvt.proposer).
+					Str("proposer", producer).
 					Uint64("block", proposeBlkEvt.block.Height()).
 					Str("hash", hex.EncodeToString(blkHash[:])).
 					Err(err).
@@ -501,7 +508,7 @@ func (m *cFSM) handleProposeBlockEvt(evt fsm.Event) (fsm.State, error) {
 			if len(proposeBlkEvt.block.Header.DKGPubkey) > 0 && len(proposeBlkEvt.block.Header.DKGBlockSig) > 0 {
 				if err := verifyDKGSignature(proposeBlkEvt.block, m.ctx.epoch.seed); err != nil {
 					logger.Error().
-						Str("proposer", proposeBlkEvt.proposer).
+						Str("proposer", producer).
 						Uint64("block", proposeBlkEvt.block.Height()).
 						Str("hash", hex.EncodeToString(blkHash[:])).
 						Err(err).
@@ -748,7 +755,7 @@ func (m *cFSM) newCEvt(t fsm.EventType) *consensusEvt {
 }
 
 func (m *cFSM) newProposeBlkEvt(blk *blockchain.Block) *proposeBlkEvt {
-	return newProposeBlkEvt(blk, m.ctx.addr.RawAddress, m.ctx.clock)
+	return newProposeBlkEvt(blk, m.ctx.clock)
 }
 
 func (m *cFSM) newPrevoteEvt(blkHash hash.Hash32B, decision bool) *voteEvt {
