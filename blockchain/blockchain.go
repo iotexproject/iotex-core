435b7d845bbb7c4ab7504400811c775b0e049fcb
dustinxie <dahuaxie@gmail.com>
2018-10-30 20:33:55 -0700

    IOTEX-97 Re-enable state root match check in validateBlock() (#257)

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -325,12 +325,6 @@ func (bc *blockchain) startEmptyBlockchain() error {
 	}
 	genesis.Header.stateRoot = root
 	genesis.workingSet = ws
-	// TODO: validateBlock will call runActions again, which is not supposed to rerun. Disable it now
-	/*
-		if err := bc.validateBlock(genesis, false); err != nil {
-			return errors.Wrap(err, "failed to validate Genesis block")
-		}
-	*/
 	// add Genesis block as very first block
 	if err := bc.commitBlock(genesis); err != nil {
 		return errors.Wrap(err, "failed to commit Genesis block")
@@ -379,8 +373,7 @@ func (bc *blockchain) startExistingBlockchain(recoveryHeight uint64) error {
 		if err != nil {
 			return err
 		}
-		// TODO: disable validation before resolve the state root doesn't match issue
-		if _, err := bc.runActions(blk, ws, false); err != nil {
+		if _, err := bc.runActions(blk, ws, true); err != nil {
 			return err
 		}
 		if err := bc.sf.Commit(ws); err != nil {
@@ -808,8 +801,7 @@ func (bc *blockchain) validateBlock(blk *Block, containCoinbase bool) error {
 	if err != nil {
 		return errors.Wrap(err, "Failed to obtain working set from state factory")
 	}
-	// TODO: disable validation before resolve the state root doesn't match issue
-	if _, err := bc.runActions(blk, ws, false); err != nil {
+	if _, err := bc.runActions(blk, ws, true); err != nil {
 		logger.Panic().Err(err).Msgf("Failed to update state on height %d", bc.tipHeight)
 	}
 	// attach working set to be committed to state factory
@@ -847,16 +839,17 @@ func (bc *blockchain) commitBlock(blk *Block) error {
 	return nil
 }
 
-func (bc *blockchain) runActions(blk *Block, ws state.WorkingSet, verify bool) (root hash.Hash32B, err error) {
+func (bc *blockchain) runActions(blk *Block, ws state.WorkingSet, verify bool) (hash.Hash32B, error) {
 	if bc.sf == nil {
-		return root, nil
+		return hash.ZeroHash32B, errors.New("statefactory cannot be nil")
 	}
 	// run executions
 	if _, _, executions := action.ClassifyActions(blk.Actions); len(executions) > 0 {
 		ExecuteContracts(blk, ws, bc)
 	}
 	// update state factory
-	if root, err = ws.RunActions(blk.Height(), blk.Actions); err != nil {
+	root, err := ws.RunActions(blk.Height(), blk.Actions)
+	if err != nil {
 		return root, err
 	}
 	if verify {
