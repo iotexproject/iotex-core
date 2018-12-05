46bb8a26bae31b784c7dde996d736c0e0bae6cb0
Yutong Pei <yutong@iotex.io>
2018-12-05 14:34:23 -0800

    typo

--- action/protocol/multichain/mainchain/datamodel_test.go
+++ action/protocol/multichain/mainchain/datamodel_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 IoTeX,
+// Copyright (c) 2018 IoTeX
 // This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
 // warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
@@ -54,7 +54,7 @@ func TestBlockProofState(t *testing.T) {
 			},
 			{
 				Name:  "abd",
-				Value: byteutil.BytesTo32B([]byte("1000q")),
+				Value: byteutil.BytesTo32B([]byte("1000d")),
 			},
 		},
 		ProducerPublicKey: testaddress.Addrinfo["producer"].PublicKey,
