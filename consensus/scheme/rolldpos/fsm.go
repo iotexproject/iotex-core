ce5594e10ce8656457303e57f9a623ea2ef603b1
sggb <330771743@qq.com>
2018-12-07 10:42:01 +0800

    remove ineffectual assignment

--- consensus/scheme/rolldpos/fsm.go
+++ consensus/scheme/rolldpos/fsm.go
@@ -687,7 +687,6 @@ func (m *cFSM) handleEndorseCommitEvt(evt fsm.Event) (fsm.State, error) {
 	// Commit and broadcast the pending block
 	if err := m.ctx.chain.CommitBlock(pendingBlock); err != nil {
 		if err == db.ErrAlreadyExist {
-			err = nil
 			logger.Info().Uint64("block", pendingBlock.Height()).Msg("Block already exists.")
 		} else {
 			logger.Error().
