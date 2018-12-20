bdb7a4198965f331332203a6af614416e982925c
Dustin Xie <dustin.xie@iotex.io>
2018-12-19 21:06:36 -0800

    Revert "Disable Snapshot() function before solving the memory thrash/leak (#384)"
    This reverts commit c3a6806d0a43193407d06a210eb4da844e5c2c20.

--- action/protocol/execution/evm/evmstatedbadapter_test.go
+++ action/protocol/execution/evm/evmstatedbadapter_test.go
@@ -98,9 +98,6 @@ func TestForEachStorage(t *testing.T) {
 }
 
 func TestSnapshotAndSuicide(t *testing.T) {
-	// TODO: temp disable until we solve the memory thrash/leak issue
-	return
-
 	require := require.New(t)
 	testutil.CleanupPath(t, testTriePath)
 	defer testutil.CleanupPath(t, testTriePath)
