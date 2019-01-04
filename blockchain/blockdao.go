e3159b4771c6a15863a74f3dbc10d26cda4c6804
Zhijie Shen <zjshen14@gmail.com>
2019-01-03 22:46:05 -0800

    Fix golang ci issues

--- blockchain/blockdao.go
+++ blockchain/blockdao.go
@@ -91,7 +91,8 @@ func (dao *blockDAO) Start(ctx context.Context) error {
 
 	// set init height value
 	// TODO: not working with badger, we shouldn't expose detailed db error (e.g., bolt.ErrBucketExists) to application
-	if _, err = dao.kvstore.Get(blockNS, topHeightKey); err != nil && (errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
+	if _, err = dao.kvstore.Get(blockNS, topHeightKey); err != nil &&
+		(errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
 		if err := dao.kvstore.Put(blockNS, topHeightKey, make([]byte, 8)); err != nil {
 			return errors.Wrap(err, "failed to write initial value for top height")
 		}
@@ -99,7 +100,8 @@ func (dao *blockDAO) Start(ctx context.Context) error {
 
 	// TODO: To be deprecated
 	// set init total transfer to be 0
-	if _, err := dao.kvstore.Get(blockNS, totalTransfersKey); err != nil && (errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
+	if _, err := dao.kvstore.Get(blockNS, totalTransfersKey); err != nil &&
+		(errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
 		if err = dao.kvstore.Put(blockNS, totalTransfersKey, make([]byte, 8)); err != nil {
 			return errors.Wrap(err, "failed to write initial value for total transfers")
 		}
@@ -107,7 +109,8 @@ func (dao *blockDAO) Start(ctx context.Context) error {
 
 	// TODO: To be deprecated
 	// set init total vote to be 0
-	if _, err := dao.kvstore.Get(blockNS, totalVotesKey); err != nil && (errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
+	if _, err := dao.kvstore.Get(blockNS, totalVotesKey); err != nil &&
+		(errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
 		if err = dao.kvstore.Put(blockNS, totalVotesKey, make([]byte, 8)); err != nil {
 			return errors.Wrap(err, "failed to write initial value for total votes")
 		}
@@ -115,14 +118,16 @@ func (dao *blockDAO) Start(ctx context.Context) error {
 
 	// TODO: To be deprecated
 	// set init total executions to be 0
-	if _, err := dao.kvstore.Get(blockNS, totalExecutionsKey); err != nil && (errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
+	if _, err := dao.kvstore.Get(blockNS, totalExecutionsKey); err != nil &&
+		(errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
 		if err = dao.kvstore.Put(blockNS, totalExecutionsKey, make([]byte, 8)); err != nil {
 			return errors.Wrap(err, "failed to write initial value for total executions")
 		}
 	}
 
 	// set init total actions to be 0
-	if _, err := dao.kvstore.Get(blockNS, totalActionsKey); err != nil && (errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
+	if _, err := dao.kvstore.Get(blockNS, totalActionsKey); err != nil &&
+		(errors.Cause(err) == db.ErrNotExist || errors.Cause(err) == bolt.ErrBucketNotFound) {
 		if err = dao.kvstore.Put(blockNS, totalActionsKey, make([]byte, 8)); err != nil {
 			return errors.Wrap(err, "failed to write initial value for total actions")
 		}
