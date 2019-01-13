ea85d0e62434f61921da2a0bc6fe59ea5775936b
xiaxiangzhou <xiaxiangzhou@gmail.com>
2019-01-13 11:21:09 -0800

    IOTEX-221 Add sqlite3 local version (#418)
    * add sqlite3 local version

--- db/sql/util.go
+++ db/sql/util.go
@@ -4,7 +4,7 @@
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
-package rds
+package sql
 
 import (
 	"reflect"
@@ -12,24 +12,27 @@ import (
 	"database/sql"
 )
 
-// ParseRows will parse the row
-func ParseRows(rows *sql.Rows, schema interface{}) ([]interface{}, error) {
+// ParseSQLRows will parse the row
+func ParseSQLRows(rows *sql.Rows, schema interface{}) ([]interface{}, error) {
 	var parsedRows []interface{}
-	s := reflect.ValueOf(schema).Elem()
-
-	var fields []interface{}
-	for i := 0; i < s.NumField(); i++ {
-		fields = append(fields, s.Field(i).Addr().Interface())
-	}
 
 	// Fetch rows
 	for rows.Next() {
+		newSchema := reflect.New(reflect.ValueOf(schema).Elem().Type()).Interface()
+
+		s := reflect.ValueOf(newSchema).Elem()
+
+		var fields []interface{}
+		for i := 0; i < s.NumField(); i++ {
+			fields = append(fields, s.Field(i).Addr().Interface())
+		}
+
 		err := rows.Scan(fields...)
 		if err != nil {
 			return nil, err
 		}
 
-		parsedRows = append(parsedRows, schema)
+		parsedRows = append(parsedRows, newSchema)
 	}
 
 	return parsedRows, nil
--- indexservice/indexer_test.go
+++ indexservice/indexer_test.go
@@ -8,8 +8,6 @@ package indexservice
 
 import (
 	"context"
-	"math/rand"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,172 +15,179 @@ import (
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/blockchain/block"
 	"github.com/iotexproject/iotex-core/config"
-	"github.com/iotexproject/iotex-core/db/rds"
+	"github.com/iotexproject/iotex-core/db/sql"
 	"github.com/iotexproject/iotex-core/pkg/version"
 	"github.com/iotexproject/iotex-core/proto"
-	"github.com/iotexproject/iotex-core/testutil"
+	"github.com/iotexproject/iotex-core/test/testaddress"
 )
 
-var (
-	cfg = &config.Default.DB.RDS
-)
+func testSQLite3StorePutGet(store sql.Store, t *testing.T) {
+	require := require.New(t)
+	ctx := context.Background()
 
-func TestIndexService(t *testing.T) {
-	testRDSStorePutGet := func(rdsStore rds.Store, t *testing.T) {
-		t.Skip("Skipping when RDS credentail not provided.")
+	err := store.Start(ctx)
+	require.Nil(err)
+	defer func() {
+		err = store.Stop(ctx)
+		require.Nil(err)
+	}()
 
-		require := require.New(t)
-		ctx := context.Background()
-
-		err := rdsStore.Start(ctx)
-		require.Nil(err)
-		defer func() {
-			err = rdsStore.Stop(ctx)
-			require.Nil(err)
-		}()
-
-		nodeAddr := "aaa"
-		userAddr1 := "bb"
-		userAddr2 := "cc"
-		cfg := config.Default
-		idx := Indexer{
-			cfg:                cfg.Indexer,
-			rds:                rdsStore,
-			hexEncodedNodeAddr: nodeAddr,
-		}
-
-		blk := block.Block{}
-		err = blk.ConvertFromBlockPb(&iproto.BlockPb{
-			Header: &iproto.BlockHeaderPb{
-				Version: version.ProtocolVersion,
-				Height:  123456789,
-			},
-			Actions: []*iproto.ActionPb{
-				{Action: &iproto.ActionPb_Transfer{
-					Transfer: &iproto.TransferPb{Recipient: userAddr2},
-				},
-					Version: version.ProtocolVersion,
-					Sender:  userAddr1,
-					Nonce:   101,
-				},
-				{Action: &iproto.ActionPb_Vote{
-					Vote: &iproto.VotePb{VoteeAddress: userAddr2},
-				},
-					Version: version.ProtocolVersion,
-					Sender:  userAddr1,
-					Nonce:   103,
-				},
-				{Action: &iproto.ActionPb_Execution{
-					Execution: &iproto.ExecutionPb{Contract: userAddr2},
-				},
-					Version: version.ProtocolVersion,
-					Sender:  userAddr1,
-					Nonce:   104,
-				},
-			},
-		})
-		require.NoError(err)
-
-		err = idx.BuildIndex(&blk)
-		require.Nil(err)
-
-		db := rdsStore.GetDB()
-
-		transfers, votes, executions := action.ClassifyActions(blk.Actions)
-
-		// get transfer
-		transferHashes, err := idx.GetTransferHistory(userAddr1)
-		require.Nil(err)
-		require.Equal(1, len(transferHashes))
-		transfer := transfers[0].Hash()
-		require.Equal(transfer, transferHashes[0])
-
-		// get vote
-		voteHashes, err := idx.GetVoteHistory(userAddr1)
-		require.Nil(err)
-		require.Equal(1, len(voteHashes))
-		vote := votes[0].Hash()
-		require.Equal(vote, voteHashes[0])
-
-		// get execution
-		executionHashes, err := idx.GetExecutionHistory(userAddr1)
-		require.Nil(err)
-		require.Equal(1, len(executionHashes))
-		execution := executions[0].Hash()
-		require.Equal(execution, executionHashes[0])
-
-		// get action
-		actionHashes, err := idx.GetActionHistory(userAddr1)
-		require.Nil(err)
-		require.Equal(3, len(actionHashes))
-		action := blk.Actions[0].Hash()
-		require.Equal(action, actionHashes[0])
-
-		// transfer map to block
-		blkHash1, err := idx.GetBlockByTransfer(transfers[0].Hash())
-		require.Nil(err)
-		require.Equal(blkHash1, blk.HashBlock())
-
-		// vote map to block
-		blkHash2, err := idx.GetBlockByVote(votes[0].Hash())
-		require.Nil(err)
-		require.Equal(blkHash2, blk.HashBlock())
-
-		// execution map to block
-		blkHash3, err := idx.GetBlockByExecution(executions[0].Hash())
-		require.Nil(err)
-		require.Equal(blkHash3, blk.HashBlock())
-
-		// action map to block
-		blkHash4, err := idx.GetBlockByAction(blk.Actions[0].Hash())
-		require.Nil(err)
-		require.Equal(blkHash4, blk.HashBlock())
-
-		// delete transfers
-		stmt, err := db.Prepare("DELETE FROM transfer_history WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-		stmt, err = db.Prepare("DELETE FROM transfer_to_block WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-
-		// delete votes
-		stmt, err = db.Prepare("DELETE FROM vote_history WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-		stmt, err = db.Prepare("DELETE FROM vote_to_block WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-
-		// delete executions
-		stmt, err = db.Prepare("DELETE FROM execution_history WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-		stmt, err = db.Prepare("DELETE FROM execution_to_block WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-
-		// delete actions
-		stmt, err = db.Prepare("DELETE FROM action_history WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
-		stmt, err = db.Prepare("DELETE FROM action_to_block WHERE node_address=?")
-		require.Nil(err)
-		_, err = stmt.Exec(nodeAddr)
-		require.Nil(err)
+	nodeAddr := "aaa"
+	addr1 := testaddress.IotxAddrinfo["alfa"]
+	addr2 := testaddress.IotxAddrinfo["bravo"]
+	cfg := config.Default
+	idx := Indexer{
+		cfg:                cfg.Indexer,
+		store:              store,
+		hexEncodedNodeAddr: nodeAddr,
 	}
+	err = idx.CreateTablesIfNotExist()
+	require.Nil(err)
 
-	path := "/tmp/test-indexer-" + strconv.Itoa(rand.Int())
+	blk := block.Block{}
+
+	err = blk.ConvertFromBlockPb(&iproto.BlockPb{
+		Header: &iproto.BlockHeaderPb{
+			Version: version.ProtocolVersion,
+			Height:  123456789,
+		},
+		Actions: []*iproto.ActionPb{
+			{
+				Action: &iproto.ActionPb_Transfer{
+					Transfer: &iproto.TransferPb{Recipient: addr2.RawAddress},
+				},
+				Sender:       addr1.RawAddress,
+				SenderPubKey: addr1.PublicKey[:],
+				Version:      version.ProtocolVersion,
+				Nonce:        101,
+			},
+			{
+				Action: &iproto.ActionPb_Vote{
+					Vote: &iproto.VotePb{VoteeAddress: addr2.RawAddress},
+				},
+				Sender:       addr1.RawAddress,
+				SenderPubKey: addr1.PublicKey[:],
+				Version:      version.ProtocolVersion,
+				Nonce:        103,
+			},
+			{
+				Action: &iproto.ActionPb_Execution{
+					Execution: &iproto.ExecutionPb{Contract: addr2.RawAddress},
+				},
+				Sender:       addr1.RawAddress,
+				SenderPubKey: addr1.PublicKey[:],
+				Version:      version.ProtocolVersion,
+				Nonce:        104,
+			},
+		},
+	})
+	require.NoError(err)
+
+	err = idx.BuildIndex(&blk)
+	require.Nil(err)
+
+	db := store.GetDB()
+
+	transfers, votes, executions := action.ClassifyActions(blk.Actions)
+
+	// get transfer
+	transferHashes, err := idx.GetTransferHistory(addr1.RawAddress)
+	require.Nil(err)
+	require.Equal(1, len(transferHashes))
+	transfer := transfers[0].Hash()
+	require.Equal(transfer, transferHashes[0])
+
+	// get vote
+	voteHashes, err := idx.GetVoteHistory(addr1.RawAddress)
+	require.Nil(err)
+	require.Equal(1, len(voteHashes))
+	vote := votes[0].Hash()
+	require.Equal(vote, voteHashes[0])
+
+	// get execution
+	executionHashes, err := idx.GetExecutionHistory(addr1.RawAddress)
+	require.Nil(err)
+	require.Equal(1, len(executionHashes))
+	execution := executions[0].Hash()
+	require.Equal(execution, executionHashes[0])
+
+	// get action
+	actionHashes, err := idx.GetActionHistory(addr1.RawAddress)
+	require.Nil(err)
+	require.Equal(3, len(actionHashes))
+	action := blk.Actions[0].Hash()
+	require.Equal(action, actionHashes[0])
+
+	// transfer map to block
+	blkHash1, err := idx.GetBlockByTransfer(transfers[0].Hash())
+	require.Nil(err)
+	require.Equal(blkHash1, blk.HashBlock())
+
+	// vote map to block
+	blkHash2, err := idx.GetBlockByVote(votes[0].Hash())
+	require.Nil(err)
+	require.Equal(blkHash2, blk.HashBlock())
+
+	// execution map to block
+	blkHash3, err := idx.GetBlockByExecution(executions[0].Hash())
+	require.Nil(err)
+	require.Equal(blkHash3, blk.HashBlock())
+
+	// action map to block
+	blkHash4, err := idx.GetBlockByAction(blk.Actions[0].Hash())
+	require.Nil(err)
+	require.Equal(blkHash4, blk.HashBlock())
+
+	// delete transfers
+	stmt, err := db.Prepare("DELETE FROM transfer_history WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+	stmt, err = db.Prepare("DELETE FROM transfer_to_block WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+
+	// delete votes
+	stmt, err = db.Prepare("DELETE FROM vote_history WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+	stmt, err = db.Prepare("DELETE FROM vote_to_block WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+
+	// delete executions
+	stmt, err = db.Prepare("DELETE FROM execution_history WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+	stmt, err = db.Prepare("DELETE FROM execution_to_block WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+
+	// delete actions
+	stmt, err = db.Prepare("DELETE FROM action_history WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+	stmt, err = db.Prepare("DELETE FROM action_to_block WHERE node_address=?")
+	require.Nil(err)
+	_, err = stmt.Exec(nodeAddr)
+	require.Nil(err)
+}
+
+func TestIndexServiceOnSqlite3(t *testing.T) {
 	t.Run("Indexer", func(t *testing.T) {
-		testutil.CleanupPath(t, path)
-		defer testutil.CleanupPath(t, path)
-		testRDSStorePutGet(rds.NewAwsRDS(cfg), t)
+		testSQLite3StorePutGet(sql.NewSQLite3(config.Default.DB.SQLITE3), t)
+	})
+}
+
+func TestIndexServiceOnAwsRDS(t *testing.T) {
+	t.Skip("Skipping when RDS credentail not provided.")
+	t.Run("Indexer", func(t *testing.T) {
+		testSQLite3StorePutGet(sql.NewAwsRDS(config.Default.DB.RDS), t)
 	})
 }
