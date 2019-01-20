83347bf61fdea97e365c857bd07d8deb29f390da
lizhefeng <lizhefeng1993@gmail.com>
2019-01-19 16:25:14 -0800

    Add benchmark test for signature verifycation (#462)

---
+++ crypto/benchmark_test.go
@@ -0,0 +1,41 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/CoderZhi/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/blake2b"
+)
+
+func BenchmarkEc283_Verify(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		VerifyEC283Signature(b)
+	}
+}
+
+func BenchmarkSecp256_Verify(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		VerifySECP256Signature(b)
+	}
+}
+
+func VerifyEC283Signature(b *testing.B) {
+	require := require.New(b)
+
+	pk, sk, _ := EC283.NewKeyPair()
+	msg := blake2b.Sum256([]byte{1, 2, 3})
+	sig := EC283.Sign(sk, msg[:])
+	require.True(EC283.Verify(pk, msg[:], sig))
+}
+
+func VerifySECP256Signature(b *testing.B) {
+	require := require.New(b)
+
+	sk, _ := crypto.GenerateKey()
+	pk := crypto.FromECDSAPub(&sk.PublicKey)
+	msg := blake2b.Sum256([]byte{1, 2, 3})
+	sig, err := crypto.Sign(msg[:], sk)
+	require.NoError(err)
+	require.True(crypto.VerifySignature(pk, msg[:], sig[:64]))
+}
