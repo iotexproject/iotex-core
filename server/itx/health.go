b9d7b7a278ee90dd779a9d7ebfddb6e1da5c42d0
Yutong Pei <yutong@iotex.io>
2019-01-22 12:31:02 -0800

    add health check handler (#467)
    * add health check handler

---
+++ server/itx/health.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2018 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package itx
+
+import "net/http"
+
+func registerHealthCheckMux(root *http.ServeMux) {
+	root.Handle("/health/", http.StripPrefix("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})))
+}
