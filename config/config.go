706554df9842e44740f4d565f342a9036f192923
Xiangzhou Xia <xiaxiangzhou@gmail.com>
2019-02-06 09:03:58 -0800

    fix

--- config/config.go
+++ config/config.go
@@ -54,15 +54,15 @@ const (
 	StandaloneScheme = "STANDALONE"
 	// NOOPScheme means that the node does not create only block
 	NOOPScheme = "NOOP"
-	// IndexerTransfer is table identifier for transfer index in indexer
+	// IndexTransfer is table identifier for transfer index in indexer
 	IndexTransfer  = "transfer"
-	// IndexerVote is table identifier for vote index in indexer
+	// IndexVote is table identifier for vote index in indexer
 	IndexVote      = "vote"
-	// IndexerExecution is table identifier for execution index in indexer
+	// IndexExecution is table identifier for execution index in indexer
 	IndexExecution = "execution"
-	// IndexerAction is table identifier for action index in indexer
+	// IndexAction is table identifier for action index in indexer
 	IndexAction    = "action"
-	// IndexerReceipt is table identifier for receipt index in indexer
+	// IndexReceipt is table identifier for receipt index in indexer
 	IndexReceipt   = "receipt"
 )
 
