ef7b00bc205094fb5022fca081f8c7b722d70e48
Yutong Pei <yutong@iotex.io>
2019-02-14 15:49:01 -0800

    add grpc action injector (#538)
    * add grpc injector

---
+++ tools/actioninjector.v2/injector.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/iotexproject/iotex-core/tools/actioninjector.v2/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ tools/actioninjector.v2/internal/client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	"github.com/iotexproject/iotex-core/action"
+	iotexapi "github.com/iotexproject/iotex-core/proto/api"
+)
+
+// Client is the blockchain API client.
+type Client struct {
+	api iotexapi.APIServiceClient
+}
+
+// New creates a new Client.
+func New(serverAddr string) (*Client, error) {
+	conn, err := grpc.Dial(serverAddr)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		api: iotexapi.NewAPIServiceClient(conn),
+	}, nil
+}
+
+// SendAction sends an action to blockchain.
+func (c *Client) SendAction(ctx context.Context, selp action.SealedEnvelope) error {
+	_, err := c.api.SendAction(ctx, &iotexapi.SendActionRequest{Action: selp.Proto()})
+	return err
+}
+
+// GetAccount returns a given account.
+func (c *Client) GetAccount(ctx context.Context, addr string) (*iotexapi.GetAccountResponse, error) {
+	return c.api.GetAccount(ctx, &iotexapi.GetAccountRequest{Address: addr})
+}
