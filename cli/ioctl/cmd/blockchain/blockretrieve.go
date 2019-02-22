2751066185014a2c313ef364a5fea40a4500cf12
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-22 13:11:54 +0530

    blockchain command in progress

--- cli/ioctl/cmd/blockchain/blockretrieve.go
+++ cli/ioctl/cmd/blockchain/blockretrieve.go
@@ -7,78 +7,27 @@
 package blockchain
 
 import (
-	"context"
-	"encoding/hex"
 	"fmt"
-	"strconv"
-
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
-
-	"github.com/iotexproject/iotex-core/pkg/hash"
-	pb "github.com/iotexproject/iotex-core/protogen/iotexapi"
-	pb1 "github.com/iotexproject/iotex-core/protogen/iotextypes"
 )
 
-var serverAddr string
+var blockheaderid int
 
-// blockHeaderRetrieveCmd creates a new `ioctl blockchain blockheader` command
-var blockHeaderRetrieveCmd = &cobra.Command{
+// blockRetrieveCmd represents the account create command
+var blockRetrieveCmd = &cobra.Command{
 	Use:   "blockheader",
-	Short: "Retrieve a block from blockchain",
+	Short: "Create N new accounts and print them",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(blockHeaderRetrieve(args))
+		fmt.Println(blockRetrieve(args))
 	},
 }
 
 func init() {
-	blockHeaderRetrieveCmd.Flags().StringVarP(&serverAddr, "host", "s", "127.0.0.1:14014", "host of api server")
-	BlockchainCmd.AddCommand(blockHeaderRetrieveCmd)
+	blockRetrieveCmd.Flags().IntVarP(&blockheaderid, "blockheaderid", "b", 1, "retrieves a full block from the blockchain")
+	BlockchainCmd.AddCommand(blockRetrieveCmd)
 }
 
-// blockHeaderRetrieve is the actual implementation
-func blockHeaderRetrieve(args []string) (*pb1.BlockHeader, error) {
-	// deduce height or hash method from user input
-	method := "hash"
-	var userHeight uint64
-	var userHash hash.Hash256
-
-	v := args[0]
-	// check if user provided height
-	if h, err := strconv.Atoi(v); err == nil {
-		method = "height"
-		userHeight = uint64(h)
-	}
-
-	// otherwise it's hash string
-	if method == "hash" {
-		userHash, _ = toHash256(v)
-	}
-
-	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
-	if err != nil {
-		return nil, err
-	}
-
-	client := pb.NewAPIServiceClient(conn)
-	req := &pb.GetBlockMetasRequest{}
-	_, err = client.GetBlockMetas(context.Background(), req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO - implement code to get blockheader
-	return new(pb1.BlockHeader), nil
-}
-
-func toHash256(hashString string) (hash.Hash256, error) {
-	bytes, err := hex.DecodeString(hashString)
-	if err != nil {
-		return hash.ZeroHash256, err
-	}
-	var _hash hash.Hash256
-	copy(_hash[:], bytes)
-	return _hash, nil
+func blockRetrieve(args []string) string {
+	return "TODO: Implement it"
 }
