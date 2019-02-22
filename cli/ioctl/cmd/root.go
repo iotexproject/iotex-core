c2e5a2eed102b6782d5fddd319ea17c60b7e4866
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-22 13:11:54 +0530

    blockchain command in progress

--- cli/ioctl/cmd/root.go
+++ cli/ioctl/cmd/root.go
@@ -35,6 +35,7 @@ func Execute() {
 func init() {
 	RootCmd.AddCommand(account.AccountCmd)
 	RootCmd.AddCommand(version.VersionCmd)
+	RootCmd.AddCommand(blockchain.BlockchainCmd)
 	RootCmd.AddCommand(bc.BCCmd)
 	RootCmd.AddCommand(blockchain.BlockchainCmd)
 }
