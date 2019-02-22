db89b0027d685378651864a41e67d57921769583
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-22 14:51:45 +0530

    blockchain cli in progress.. 1

--- cli/ioctl/cmd/blockchain/blockretrieve.go
+++ cli/ioctl/cmd/blockchain/blockretrieve.go
@@ -25,6 +25,7 @@ var blockRetrieveCmd = &cobra.Command{
 }
 
 func init() {
+	// TODO - override default help of cobra, we don't need last value as flag
 	blockRetrieveCmd.Flags().IntVarP(&blockheaderid, "blockheaderid", "", 0, "retrieves a full block from the blockchain")
 	BlockchainCmd.AddCommand(blockRetrieveCmd)
 }
