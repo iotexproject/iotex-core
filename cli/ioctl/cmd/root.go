83ed706d24e823cba04fddd842455e9b1786fbde
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-22 13:11:54 +0530

    blockchain command in progress

--- cli/ioctl/cmd/root.go
+++ cli/ioctl/cmd/root.go
@@ -13,6 +13,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/account"
+	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/blockchain"
 	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/bc"
 	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/version"
 )
@@ -34,5 +35,6 @@ func Execute() {
 func init() {
 	RootCmd.AddCommand(account.AccountCmd)
 	RootCmd.AddCommand(version.VersionCmd)
+	RootCmd.AddCommand(blockchain.BlockchainCmd)
 	RootCmd.AddCommand(bc.BCCmd)
 }
