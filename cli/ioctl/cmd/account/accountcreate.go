79c9c6cb68bf9e1f4d82818cffc6fee26f1579a6
Raullen Chai <raullenchai@gmail.com>
2019-02-21 22:05:07 -0800

    implement "ioctl account create" and "ioctl version" (#574)
    * implment "ioctl account create" and "ioctl version"
    
    * fix typo
    
    * encode addr
    
    * s/2018/2019
    
    * support layer2 cmd
    
    * update

---
+++ cli/ioctl/cmd/account/accountcreate.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2019 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package account
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	"github.com/iotexproject/iotex-core/address"
+	"github.com/iotexproject/iotex-core/pkg/keypair"
+	"github.com/iotexproject/iotex-core/pkg/log"
+)
+
+var numAccounts int
+
+// accountCreateCmd represents the account create command
+var accountCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create N new accounts and print them",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(accountCreate(args))
+	},
+}
+
+func init() {
+	accountCreateCmd.Flags().IntVarP(&numAccounts, "num", "n", 1, "number of accounts to create")
+
+	AccountCmd.AddCommand(accountCreateCmd)
+}
+
+func accountCreate(args []string) string {
+	items := make([]string, numAccounts)
+	for i := 0; i < numAccounts; i++ {
+		private, err := crypto.GenerateKey()
+		if err != nil {
+			log.L().Fatal("failed to create key pair", zap.Error(err))
+		}
+		pkHash := keypair.HashPubKey(&private.PublicKey)
+		addr, _ := address.FromBytes(pkHash[:])
+		pubKeyBytes := keypair.PublicKeyToBytes(&private.PublicKey)
+		priKeyBytes := keypair.PrivateKeyToBytes(private)
+		items[i] = fmt.Sprintf(
+			"{\"Address\": \"%s\", \"PublicKey\": \"%x\", \"PrivateKey\": \"%x\"}",
+			addr.String(),
+			pubKeyBytes,
+			priKeyBytes,
+		)
+	}
+	return "[" + strings.Join(items, ",") + "]"
+}
