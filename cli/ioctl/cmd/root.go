f9a41df52825e836b6c948e0b9812eb5c6ffb482
Raullen Chai <raullenchai@gmail.com>
2019-02-21 22:05:07 -0800

    implement "ioctl account create" and "ioctl version" (#574)
    * implment "ioctl account create" and "ioctl version"
    
    * fix typo
    
    * encode addr
    
    * s/2018/2019
    
    * support layer2 cmd
    
    * update

---
+++ cli/ioctl/cmd/root.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 IoTeX
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/account"
+	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/version"
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "ioctl",
+	Short: "Command-line interface for IoTeX blockchain",
+	Long:  `ioctl is a command-line interface for interacting with IoTeX blockchain.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+func init() {
+	RootCmd.AddCommand(account.AccountCmd)
+	RootCmd.AddCommand(version.VersionCmd)
+}
