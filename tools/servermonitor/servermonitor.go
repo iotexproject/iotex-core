5660c946b16dc947324bc8fa9c58eb08388aec47
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-22 17:42:28 +0530

    fixed rebase error

--- tools/servermonitor/servermonitor.go
+++ tools/servermonitor/servermonitor.go
@@ -38,8 +38,6 @@ func main() {
 	}
 	servers := strings.FieldsFunc(addr, splitFn)
 
-	var client pb.APIServiceClient
-
 	// check health for all given servers
 	for _, bcServer := range servers {
 		// TODO - run for all servers in goroutines ?
