e0e86e72a6c7652f75e40f3ea5b300d67563f800
wanlibing <646770026@qq.com>
2019-02-27 14:52:49 +0800

    Update go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v 'vender\|action'); do
+for d in $(go list ./... | grep -v 'vender' | grep "subchain"); do
     go test -short -v -coverprofile=profile.out -covermode=count "$d" > go_test.txt
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
