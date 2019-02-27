656627bf5e37b58a8a56ab9ca6086561c9af3803
wanlibing <646770026@qq.com>
2019-02-27 11:31:11 +0800

    Update go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+for d in $(go list ./... | grep -v 'vender\|accountpb\|util\|subchainpb\|rewardingpb\|candidatesutil\|chainservice\|cli\|consensus\|triepb\|idl\|p2p\|log\|unit\|protogen\|server\|identityset\|mock\|tools'); do
     go test -short -v -coverprofile=profile.out -covermode=count "$d" 
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
