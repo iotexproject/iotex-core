55eeafc06d840d7b6f84c9b169f9b363b595916a
wanlibing <646770026@qq.com>
2019-02-27 10:21:25 +0800

    Update go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -short -v -coverprofile=profile.out -covermode=count "$d" | go2xunit > /tmp/test_report_upload/coverage.xml
+    go test -short -v -coverprofile=profile.out -covermode=count "$d" | go2xunit >> /tmp/test_report_upload/coverage.xml
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
