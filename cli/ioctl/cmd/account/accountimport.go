80b35a882884bf2618345b99badd4ae6e7170a90
Frank <35358771+Frankonly@users.noreply.github.com>
2019-03-18 11:29:54 -0700

    Support windows for ioctl (#753)

--- cli/ioctl/cmd/account/accountimport.go
+++ cli/ioctl/cmd/account/accountimport.go
@@ -47,7 +47,7 @@ func accountImport(args []string) string {
 	}
 	wallet := cfg.Wallet
 	fmt.Printf("#%s: Enter your private key, which will not be exposed on the screen.\n", name)
-	privateKeyBytes, err := terminal.ReadPassword(syscall.Stdin)
+	privateKeyBytes, err := terminal.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		log.L().Error("fail to get private key", zap.Error(err))
 		return err.Error()
