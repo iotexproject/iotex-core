0b742650617d9ad7041465ce16a373341bca2aed
Zhijie Shen <zjshen14@gmail.com>
2019-03-21 17:23:38 -0700

    IOTEX-355 Return action hash and block hash with the action core from API

--- api/api.go
+++ api/api.go
@@ -154,7 +154,7 @@ func (api *Server) GetActions(ctx context.Context, in *iotexapi.GetActionsReques
 		return api.getActions(request.Start, request.Count)
 	case in.GetByHash() != nil:
 		request := in.GetByHash()
-		return api.getAction(request.ActionHash, request.CheckPending)
+		return api.getSingleAction(request.ActionHash, request.CheckPending)
 	case in.GetByAddr() != nil:
 		request := in.GetByAddr()
 		return api.getActionsByAddress(request.Address, request.Start, request.Count)
@@ -418,7 +418,7 @@ func (api *Server) readState(ctx context.Context, in *iotexapi.ReadStateRequest)
 
 // GetActions returns actions within the range
 func (api *Server) getActions(start uint64, count uint64) (*iotexapi.GetActionsResponse, error) {
-	var res []*iotextypes.Action
+	var res []*iotexapi.Action
 	var actionCount uint64
 
 	tipHeight := api.bc.TipHeight()
@@ -438,7 +438,11 @@ func (api *Server) getActions(start uint64, count uint64) (*iotexapi.GetActionsR
 			if uint64(len(res)) >= count {
 				return &iotexapi.GetActionsResponse{Actions: res}, nil
 			}
-			res = append(res, selps[i].Proto())
+			act, err := api.convertToAction(selps[i], true)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, act)
 		}
 	}
 
@@ -446,21 +450,21 @@ func (api *Server) getActions(start uint64, count uint64) (*iotexapi.GetActionsR
 }
 
 // getAction returns action by action hash
-func (api *Server) getAction(actionHash string, checkPending bool) (*iotexapi.GetActionsResponse, error) {
+func (api *Server) getSingleAction(actionHash string, checkPending bool) (*iotexapi.GetActionsResponse, error) {
 	actHash, err := toHash256(actionHash)
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-	actPb, err := getAction(api.bc, api.ap, actHash, checkPending)
+	act, err := api.getAction(actHash, checkPending)
 	if err != nil {
 		return nil, status.Error(codes.Unavailable, err.Error())
 	}
-	return &iotexapi.GetActionsResponse{Actions: []*iotextypes.Action{actPb}}, nil
+	return &iotexapi.GetActionsResponse{Actions: []*iotexapi.Action{act}}, nil
 }
 
 // getActionsByAddress returns all actions associated with an address
 func (api *Server) getActionsByAddress(address string, start uint64, count uint64) (*iotexapi.GetActionsResponse, error) {
-	var res []*iotextypes.Action
+	var res []*iotexapi.Action
 	var actions []hash.Hash256
 	if api.cfg.UseRDS {
 		actionHistory, err := api.idx.Indexer().GetIndexHistory(config.IndexAction, address)
@@ -495,12 +499,12 @@ func (api *Server) getActionsByAddress(address string, start uint64, count uint6
 			break
 		}
 
-		actPb, err := getAction(api.bc, api.ap, actions[i], false)
+		act, err := api.getAction(actions[i], false)
 		if err != nil {
 			return nil, status.Error(codes.NotFound, err.Error())
 		}
 
-		res = append(res, actPb)
+		res = append(res, act)
 	}
 
 	return &iotexapi.GetActionsResponse{Actions: res}, nil
@@ -508,7 +512,7 @@ func (api *Server) getActionsByAddress(address string, start uint64, count uint6
 
 // getUnconfirmedActionsByAddress returns all unconfirmed actions in actpool associated with an address
 func (api *Server) getUnconfirmedActionsByAddress(address string, start uint64, count uint64) (*iotexapi.GetActionsResponse, error) {
-	var res []*iotextypes.Action
+	var res []*iotexapi.Action
 	var actionCount uint64
 
 	selps := api.ap.GetUnconfirmedActs(address)
@@ -522,8 +526,11 @@ func (api *Server) getUnconfirmedActionsByAddress(address string, start uint64, 
 		if uint64(len(res)) >= count {
 			break
 		}
-
-		res = append(res, selps[i].Proto())
+		act, err := api.convertToAction(selps[i], false)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, act)
 	}
 
 	return &iotexapi.GetActionsResponse{Actions: res}, nil
@@ -531,7 +538,7 @@ func (api *Server) getUnconfirmedActionsByAddress(address string, start uint64, 
 
 // getActionsByBlock returns all actions in a block
 func (api *Server) getActionsByBlock(blkHash string, start uint64, count uint64) (*iotexapi.GetActionsResponse, error) {
-	var res []*iotextypes.Action
+	var res []*iotexapi.Action
 	hash, err := toHash256(blkHash)
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
@@ -555,7 +562,11 @@ func (api *Server) getActionsByBlock(blkHash string, start uint64, count uint64)
 			break
 		}
 
-		res = append(res, selps[i].Proto())
+		act, err := api.convertToAction(selps[i], true)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, act)
 	}
 	return &iotexapi.GetActionsResponse{Actions: res}, nil
 }
@@ -630,6 +641,36 @@ func (api *Server) getBlockMeta(blkHash string) (*iotexapi.GetBlockMetasResponse
 	return &iotexapi.GetBlockMetasResponse{BlkMetas: []*iotextypes.BlockMeta{blockMeta}}, nil
 }
 
+func (api *Server) convertToAction(selp action.SealedEnvelope, pullBlkHash bool) (*iotexapi.Action, error) {
+	actHash := selp.Hash()
+	blkHash := hash.ZeroHash256
+	var err error
+	if pullBlkHash {
+		if blkHash, err = api.bc.GetBlockHashByActionHash(actHash); err != nil {
+			return nil, err
+		}
+	}
+	return &iotexapi.Action{
+		Action:  selp.Proto(),
+		ActHash: hex.EncodeToString(actHash[:]),
+		BlkHash: hex.EncodeToString(blkHash[:]),
+	}, nil
+}
+func (api *Server) getAction(actHash hash.Hash256, checkPending bool) (*iotexapi.Action, error) {
+	var selp action.SealedEnvelope
+	var err error
+	if selp, err = api.bc.GetActionByActionHash(actHash); err != nil {
+		if checkPending {
+			// Try to fetch pending action from actpool
+			selp, err = api.ap.GetActionByHash(actHash)
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+	return api.convertToAction(selp, !checkPending)
+}
+
 func toHash256(hashString string) (hash.Hash256, error) {
 	bytes, err := hex.DecodeString(hashString)
 	if err != nil {
@@ -638,21 +679,6 @@ func toHash256(hashString string) (hash.Hash256, error) {
 	var hash hash.Hash256
 	copy(hash[:], bytes)
 	return hash, nil
-}
-
-func getAction(bc blockchain.Blockchain, ap actpool.ActPool, actHash hash.Hash256, checkPending bool) (*iotextypes.Action, error) {
-	var selp action.SealedEnvelope
-	var err error
-	if selp, err = bc.GetActionByActionHash(actHash); err != nil {
-		if checkPending {
-			// Try to fetch pending action from actpool
-			selp, err = ap.GetActionByHash(actHash)
-		}
-	}
-	if err != nil {
-		return nil, err
-	}
-	return selp.Proto(), nil
 }
 
 func getTranferAmountInBlock(blk *block.Block) *big.Int {
