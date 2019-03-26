iotexproject/iotex-core

commit 0280e43e037632c2f05d77b29eea5e819c9d8777

rebroadcast precommit endorsement on timeout (#835)

consensus/consensusfsm/fsm.go
@@ -54,6 +54,7 @@ const (
 	eReceiveLockEndorsement           fsm.EventType = "E_RECEIVE_LOCK_ENDORSEMENT"
 	eStopReceivingLockEndorsement     fsm.EventType = "E_STOP_RECEIVING_LOCK_ENDORSEMENT"
 	eReceivePreCommitEndorsement      fsm.EventType = "E_RECEIVE_PRECOMMIT_ENDORSEMENT"
+	eBroadcastPreCommitEndorsement    fsm.EventType = "E_BROADCAST_PRECOMMIT_ENDORSEMENT"
 
 	// BackdoorEvent indicates a backdoor event type
 	BackdoorEvent fsm.EventType = "E_BACKDOOR"
@@ -87,6 +88,7 @@ type Config struct {
 	AcceptBlockTTL               time.Duration `yaml:"acceptBlockTTL"`
 	AcceptProposalEndorsementTTL time.Duration `yaml:"acceptProposalEndorsementTTL"`
 	AcceptLockEndorsementTTL     time.Duration `yaml:"acceptLockEndorsementTTL"`
+	CommitTTL                    time.Duration `yaml:"commitTTL"`
 }
 
 // ConsensusFSM wraps over the general purpose FSM and implements the consensus logic
@@ -158,6 +160,13 @@ func NewConsensusFSM(cfg Config, ctx Context, clock clock.Clock) (*ConsensusFSM,
 			[]fsm.State{
 				sAcceptLockEndorsement,      // not enough endorsements
 				sAcceptPreCommitEndorsement, // reach commit agreement, jump to next step
+			}).
+		AddTransition(
+			sAcceptPreCommitEndorsement,
+			eBroadcastPreCommitEndorsement,
+			cm.onBroadcastPreCommitEndorsement,
+			[]fsm.State{
+				sAcceptPreCommitEndorsement,
 			}).
 		AddTransition(
 			sAcceptLockEndorsement,
@@ -453,7 +462,18 @@ func (m *ConsensusFSM) onReceiveLockEndorsement(evt fsm.Event) (fsm.State, error
 		return sAcceptLockEndorsement, nil
 	}
 	m.ProduceReceivePreCommitEndorsementEvent(preCommitEndorsement)
-	m.ctx.Broadcast(preCommitEndorsement)
+	m.produce(m.ctx.NewConsensusEvent(eBroadcastPreCommitEndorsement, preCommitEndorsement), 0)
+
+	return sAcceptPreCommitEndorsement, nil
+}
+
+func (m *ConsensusFSM) onBroadcastPreCommitEndorsement(evt fsm.Event) (fsm.State, error) {
+	cEvt, ok := evt.(*ConsensusEvent)
+	if !ok {
+		return sAcceptPreCommitEndorsement, errors.Wrap(ErrEvtCast, "failed to cast to consensus event")
+	}
+	m.ctx.Broadcast(cEvt.Data())
+	m.produce(cEvt, m.cfg.CommitTTL)
 
 	return sAcceptPreCommitEndorsement, nil
 }

consensus/consensusfsm/fsm_test.go
@@ -80,6 +80,7 @@ func TestStateTransitions(t *testing.T) {
 		AcceptBlockTTL:               4 * time.Second,
 		AcceptProposalEndorsementTTL: 2 * time.Second,
 		AcceptLockEndorsementTTL:     2 * time.Second,
+		CommitTTL:                    2 * time.Second,
 	}, mockCtx, mockClock)
 	require.Nil(err)
 	require.NotNil(cfsm)
@@ -117,13 +118,13 @@ func TestStateTransitions(t *testing.T) {
 				require.Nil(evt.Data())
 				time.Sleep(100 * time.Millisecond)
 				// garbage collection
-				mockClock.Add(4 * time.Second)
+				mockClock.Add(cfsm.cfg.AcceptBlockTTL)
 				evt = <-cfsm.evtq
 				require.Equal(eFailedToReceiveBlock, evt.Type())
-				mockClock.Add(2 * time.Second)
+				mockClock.Add(cfsm.cfg.AcceptProposalEndorsementTTL)
 				evt = <-cfsm.evtq
 				require.Equal(eStopReceivingProposalEndorsement, evt.Type())
-				mockClock.Add(2 * time.Second)
+				mockClock.Add(cfsm.cfg.AcceptLockEndorsementTTL)
 				evt = <-cfsm.evtq
 				require.Equal(eStopReceivingLockEndorsement, evt.Type())
 			})
@@ -284,7 +285,6 @@ func TestStateTransitions(t *testing.T) {
 		})
 		t.Run("ready-to-pre-commit", func(t *testing.T) {
 			mockCtx.EXPECT().NewPreCommitEndorsement(gomock.Any()).Return(NewMockEndorsement(ctrl), nil).Times(1)
-			mockCtx.EXPECT().Broadcast(gomock.Any()).Return().Times(1)
 			state, err := cfsm.onReceiveLockEndorsement(&ConsensusEvent{
 				eventType: eReceiveLockEndorsement,
 				data:      NewMockEndorsement(ctrl),
@@ -293,6 +293,8 @@ func TestStateTransitions(t *testing.T) {
 			require.Equal(sAcceptPreCommitEndorsement, state)
 			evt := <-cfsm.evtq
 			require.Equal(eReceivePreCommitEndorsement, evt.Type())
+			evt = <-cfsm.evtq
+			require.Equal(eBroadcastPreCommitEndorsement, evt.Type())
 		})
 	})
 	t.Run("onStopReceivingLockEndorsement", func(t *testing.T) {
@@ -301,6 +303,27 @@ func TestStateTransitions(t *testing.T) {
 		require.Equal(sPrepare, state)
 		evt := <-cfsm.evtq
 		require.Equal(ePrepare, evt.Type())
+	})
+	t.Run("onBroadcastPreCommitEndorsement", func(t *testing.T) {
+		t.Run("invalid-fsm-event", func(t *testing.T) {
+			state, err := cfsm.onBroadcastPreCommitEndorsement(nil)
+			require.Error(err)
+			require.Equal(sAcceptPreCommitEndorsement, state)
+		})
+		t.Run("success", func(t *testing.T) {
+			mockCtx.EXPECT().Broadcast(gomock.Any()).Return().Times(1)
+			mockEndorsement := NewMockEndorsement(ctrl)
+			state, err := cfsm.onBroadcastPreCommitEndorsement(&ConsensusEvent{
+				eventType: eBroadcastPreCommitEndorsement,
+				data:      mockEndorsement,
+			})
+			require.NoError(err)
+			require.Equal(sAcceptPreCommitEndorsement, state)
+			time.Sleep(100 * time.Millisecond)
+			mockClock.Add(cfsm.cfg.CommitTTL)
+			evt := <-cfsm.evtq
+			require.Equal(eBroadcastPreCommitEndorsement, evt.Type())
+		})
 	})
 	t.Run("onReceivePreCommitEndorsement", func(t *testing.T) {
 		t.Run("invalid-fsm-event", func(t *testing.T) {

config/config.go
@@ -125,6 +125,7 @@ var (
 					AcceptBlockTTL:               4 * time.Second,
 					AcceptProposalEndorsementTTL: 2 * time.Second,
 					AcceptLockEndorsementTTL:     2 * time.Second,
+					CommitTTL:                    2 * time.Second,
 					EventChanSize:                10000,
 				},
 				ToleratedOvertime: 2 * time.Second,

consensus/scheme/rolldpos/rolldpos_test.go
@@ -294,6 +294,7 @@ func TestRollDPoSConsensus(t *testing.T) {
 		cfg.Consensus.RollDPoS.FSM.AcceptBlockTTL = 400 * time.Millisecond
 		cfg.Consensus.RollDPoS.FSM.AcceptProposalEndorsementTTL = 200 * time.Millisecond
 		cfg.Consensus.RollDPoS.FSM.AcceptLockEndorsementTTL = 200 * time.Millisecond
+		cfg.Consensus.RollDPoS.FSM.CommitTTL = 200 * time.Millisecond
 		cfg.Consensus.RollDPoS.FSM.UnmatchedEventTTL = time.Second
 		cfg.Consensus.RollDPoS.FSM.UnmatchedEventInterval = 10 * time.Millisecond
 		cfg.Consensus.RollDPoS.ToleratedOvertime = 200 * time.Millisecond

consensus/scheme/rolldpos/rolldposctx.go
@@ -413,7 +413,7 @@ func (ctx *rollDPoSCtx) IsStaleEvent(evt *consensusfsm.ConsensusEvent) bool {
 	ctx.mutex.RLock()
 	defer ctx.mutex.RUnlock()
 
-	return ctx.round.IsStale(evt.Height(), ctx.round.Number())
+	return ctx.round.IsStale(evt.Height(), evt.Round(), evt.Data())
 }
 
 func (ctx *rollDPoSCtx) IsFutureEvent(evt *consensusfsm.ConsensusEvent) bool {
@@ -505,7 +505,7 @@ func (ctx *rollDPoSCtx) newConsensusEvent(
 			data,
 			ed.Height(),
 			roundNum,
-			ed.Endorsement().Timestamp(),
+			ctx.clock.Now(),
 		)
 	default:
 		return consensusfsm.NewConsensusEvent(

consensus/scheme/rolldpos/roundctx.go
@@ -129,8 +129,26 @@ func (ctx *roundCtx) ProofOfLock() []*endorsement.Endorsement {
 	return ctx.proofOfLock
 }
 
-func (ctx *roundCtx) IsStale(height uint64, num uint32) bool {
-	return height < ctx.height || height == ctx.height && num < ctx.roundNum
+func (ctx *roundCtx) IsStale(height uint64, num uint32, data interface{}) bool {
+	switch {
+	case height < ctx.height:
+		return true
+	case height > ctx.height:
+		return false
+	case num >= ctx.roundNum:
+		return false
+	default:
+		msg, ok := data.(*EndorsedConsensusMessage)
+		if !ok {
+			return true
+		}
+		vote, ok := msg.Document().(*ConsensusVote)
+		if !ok {
+			return true
+		}
+
+		return vote.Topic() != COMMIT
+	}
 }
 
 func (ctx *roundCtx) IsFuture(height uint64, num uint32) bool {

consensus/scheme/rolldpos/roundctx_test.go
@@ -12,6 +12,8 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/iotex-core/endorsement"
 )
 
 func TestRoundCtx(t *testing.T) {
@@ -52,17 +54,43 @@ func TestRoundCtx(t *testing.T) {
 	for _, d := range round.Delegates() {
 		require.True(round.IsDelegate(d))
 	}
-	require.True(round.IsStale(blockHeight, 2))
-	require.True(round.IsStale(blockHeight, 3))
-	require.True(round.IsStale(blockHeight, 4))
-	require.True(round.IsStale(blockHeight+1, 2))
-	require.False(round.IsStale(blockHeight+1, 3))
-	require.False(round.IsStale(blockHeight+1, 4))
-	require.False(round.IsStale(blockHeight+2, 2))
-	require.False(round.IsFuture(blockHeight, 4))
-	require.False(round.IsFuture(blockHeight+1, 2))
-	require.False(round.IsFuture(blockHeight+1, 3))
-	require.True(round.IsFuture(blockHeight+1, 4))
-	require.True(round.IsFuture(blockHeight+2, 2))
+	t.Run("is-stale", func(t *testing.T) {
+		blkHash := []byte("Some block hash")
+		require.True(round.IsStale(blockHeight, 2, nil))
+		require.True(round.IsStale(blockHeight, 3, nil))
+		require.True(round.IsStale(blockHeight, 4, nil))
+		require.True(round.IsStale(blockHeight+1, 2, nil))
+		require.True(round.IsStale(blockHeight+1, 2, NewEndorsedConsensusMessage(
+			blockHeight+1,
+			&blockProposal{},
+			&endorsement.Endorsement{},
+		)))
+		require.True(round.IsStale(blockHeight+1, 2, NewEndorsedConsensusMessage(
+			blockHeight+1,
+			NewConsensusVote(
+				blkHash,
+				PROPOSAL,
+			),
+			&endorsement.Endorsement{},
+		)))
+		require.False(round.IsStale(blockHeight+1, 2, NewEndorsedConsensusMessage(
+			blockHeight+1,
+			NewConsensusVote(
+				blkHash,
+				COMMIT,
+			),
+			&endorsement.Endorsement{},
+		)))
+		require.False(round.IsStale(blockHeight+1, 3, nil))
+		require.False(round.IsStale(blockHeight+1, 4, nil))
+		require.False(round.IsStale(blockHeight+2, 2, nil))
+	})
+	t.Run("is-future", func(t *testing.T) {
+		require.False(round.IsFuture(blockHeight, 4))
+		require.False(round.IsFuture(blockHeight+1, 2))
+		require.False(round.IsFuture(blockHeight+1, 3))
+		require.True(round.IsFuture(blockHeight+1, 4))
+		require.True(round.IsFuture(blockHeight+2, 2))
+	})
 	// TODO: add more unit tests
 }

tools/minicluster/minicluster.go
@@ -372,6 +372,7 @@ func newConfig(
 	cfg.Consensus.RollDPoS.FSM.AcceptBlockTTL = 1800 * time.Millisecond
 	cfg.Consensus.RollDPoS.FSM.AcceptProposalEndorsementTTL = 1800 * time.Millisecond
 	cfg.Consensus.RollDPoS.FSM.AcceptLockEndorsementTTL = 1800 * time.Millisecond
+	cfg.Consensus.RollDPoS.FSM.CommitTTL = 600 * time.Millisecond
 	cfg.Consensus.RollDPoS.FSM.EventChanSize = 100000
 	cfg.Consensus.RollDPoS.ToleratedOvertime = 1200 * time.Millisecond
 	cfg.Consensus.RollDPoS.Delay = 6 * time.Second

commit 9788c99570a74657cf101202fcacffac68c8132e

fix bug introduced in #835

consensus/consensusfsm/fsm.go
@@ -148,6 +148,14 @@ func NewConsensusFSM(cfg Config, ctx Context, clock clock.Clock) (*ConsensusFSM,
 			}).
 		AddTransition(
 			sAcceptProposalEndorsement,
+			eReceivePreCommitEndorsement,
+			cm.onReceiveProposalEndorsement,
+			[]fsm.State{
+				sAcceptProposalEndorsement, // not enough endorsements
+				sAcceptLockEndorsement,     // enough endorsements
+			}).
+		AddTransition(
+			sAcceptProposalEndorsement,
 			eStopReceivingProposalEndorsement,
 			cm.onStopReceivingProposalEndorsement,
 			[]fsm.State{
@@ -156,6 +164,14 @@ func NewConsensusFSM(cfg Config, ctx Context, clock clock.Clock) (*ConsensusFSM,
 		AddTransition(
 			sAcceptLockEndorsement,
 			eReceiveLockEndorsement,
+			cm.onReceiveLockEndorsement,
+			[]fsm.State{
+				sAcceptLockEndorsement,      // not enough endorsements
+				sAcceptPreCommitEndorsement, // reach commit agreement, jump to next step
+			}).
+		AddTransition(
+			sAcceptLockEndorsement,
+			eReceivePreCommitEndorsement,
 			cm.onReceiveLockEndorsement,
 			[]fsm.State{
 				sAcceptLockEndorsement,      // not enough endorsements
@@ -297,11 +313,11 @@ func (m *ConsensusFSM) produce(evt *ConsensusEvent, delay time.Duration) {
 
 func (m *ConsensusFSM) handle(evt *ConsensusEvent) error {
 	if m.ctx.IsStaleEvent(evt) {
-		m.ctx.Logger().Debug("stale event", zap.Any("event", evt))
+		m.ctx.Logger().Debug("stale event", zap.Any("event", evt.Type()))
 		return nil
 	}
 	if m.ctx.IsFutureEvent(evt) {
-		m.ctx.Logger().Debug("future event", zap.Any("event", evt))
+		m.ctx.Logger().Debug("future event", zap.Any("event", evt.Type()))
 		// TODO: find a more appropriate delay
 		m.produce(evt, m.cfg.UnmatchedEventInterval)
 		return nil
@@ -472,6 +488,7 @@ func (m *ConsensusFSM) onBroadcastPreCommitEndorsement(evt fsm.Event) (fsm.State
 	if !ok {
 		return sAcceptPreCommitEndorsement, errors.Wrap(ErrEvtCast, "failed to cast to consensus event")
 	}
+	m.ctx.Logger().Debug("broadcast pre-commit endorsement")
 	m.ctx.Broadcast(cEvt.Data())
 	m.produce(cEvt, m.cfg.CommitTTL)
 

consensus/consensusfsm/fsm_test.go
@@ -60,7 +60,7 @@ func TestBackdoorEvt(t *testing.T) {
 	}
 }
 
-func TestStateTransitions(t *testing.T) {
+func TestStateTransitionFunctions(t *testing.T) {
 	t.Parallel()
 	require := require.New(t)
 	ctrl := gomock.NewController(t)
@@ -231,18 +231,23 @@ func TestStateTransitions(t *testing.T) {
 			require.Equal(sAcceptProposalEndorsement, state)
 		})
 		t.Run("is-not-locked", func(t *testing.T) {
-			mockCtx.EXPECT().NewLockEndorsement(gomock.Any()).Return(nil, nil).Times(1)
-			mockEndorsement := NewMockEndorsement(ctrl)
+			mockCtx.EXPECT().NewLockEndorsement(gomock.Any()).Return(nil, nil).Times(2)
 			state, err := cfsm.onReceiveProposalEndorsement(&ConsensusEvent{
 				eventType: eReceiveProposalEndorsement,
-				data:      mockEndorsement,
+				data:      NewMockEndorsement(ctrl),
+			})
+			require.NoError(err)
+			require.Equal(sAcceptProposalEndorsement, state)
+			state, err = cfsm.onReceiveProposalEndorsement(&ConsensusEvent{
+				eventType: eReceivePreCommitEndorsement,
+				data:      NewMockEndorsement(ctrl),
 			})
 			require.NoError(err)
 			require.Equal(sAcceptProposalEndorsement, state)
 		})
 		t.Run("is-locked", func(t *testing.T) {
-			mockCtx.EXPECT().NewLockEndorsement(gomock.Any()).Return(NewMockEndorsement(ctrl), nil).Times(1)
-			mockCtx.EXPECT().Broadcast(gomock.Any()).Return().Times(1)
+			mockCtx.EXPECT().NewLockEndorsement(gomock.Any()).Return(NewMockEndorsement(ctrl), nil).Times(2)
+			mockCtx.EXPECT().Broadcast(gomock.Any()).Return().Times(2)
 			state, err := cfsm.onReceiveProposalEndorsement(&ConsensusEvent{
 				eventType: eReceiveProposalEndorsement,
 				data:      NewMockEndorsement(ctrl),
@@ -250,6 +255,14 @@ func TestStateTransitions(t *testing.T) {
 			require.NoError(err)
 			require.Equal(sAcceptLockEndorsement, state)
 			evt := <-cfsm.evtq
+			require.Equal(eReceiveLockEndorsement, evt.Type())
+			state, err = cfsm.onReceiveProposalEndorsement(&ConsensusEvent{
+				eventType: eReceivePreCommitEndorsement,
+				data:      NewMockEndorsement(ctrl),
+			})
+			require.NoError(err)
+			require.Equal(sAcceptLockEndorsement, state)
+			evt = <-cfsm.evtq
 			require.Equal(eReceiveLockEndorsement, evt.Type())
 		})
 	})
@@ -275,9 +288,15 @@ func TestStateTransitions(t *testing.T) {
 			require.Equal(sAcceptLockEndorsement, state)
 		})
 		t.Run("not-ready-to-pre-commit", func(t *testing.T) {
-			mockCtx.EXPECT().NewPreCommitEndorsement(gomock.Any()).Return(nil, nil).Times(1)
+			mockCtx.EXPECT().NewPreCommitEndorsement(gomock.Any()).Return(nil, nil).Times(2)
 			state, err := cfsm.onReceiveLockEndorsement(&ConsensusEvent{
 				eventType: eReceiveLockEndorsement,
+				data:      NewMockEndorsement(ctrl),
+			})
+			require.NoError(err)
+			require.Equal(sAcceptLockEndorsement, state)
+			state, err = cfsm.onReceiveLockEndorsement(&ConsensusEvent{
+				eventType: eReceivePreCommitEndorsement,
 				data:      NewMockEndorsement(ctrl),
 			})
 			require.NoError(err)
@@ -420,4 +439,8 @@ func TestStateTransitions(t *testing.T) {
 
 		})
 	})
+}
+
+func TestStateTransitionEvents(t *testing.T) {
+	// TODO (zhi): add event tests
 }