614aba91c605351a1f17c5f3e7234b8e2a4cc81c
lzxm160 <lzxm160@sina.com>
2019-03-30 11:21:08 +0800

    merge erc721token to executiontester

--- tools/executiontester/executiontester.go
+++ tools/executiontester/executiontester.go
@@ -20,6 +20,7 @@ import (
 	"github.com/iotexproject/iotex-core/pkg/log"
 	"github.com/iotexproject/iotex-core/pkg/probe"
 	"github.com/iotexproject/iotex-core/server/itx"
+	"github.com/iotexproject/iotex-core/testutil"
 	"github.com/iotexproject/iotex-core/tools/executiontester/assetcontract"
 )
 
@@ -101,6 +102,15 @@ func main() {
 
 	if _, err := fpToken.RiskLock(contractAddr, creditorAddr, creditorPriKey, risk); err != nil {
 		log.L().Fatal("Failed to transfer amount of risk from creditor to contract", zap.Error(err))
+	}
+
+	// Wait until transfer is successfully
+	var block *block.Block
+	if err := testutil.WaitUntil(100*time.Millisecond, 20*time.Second, func() (bool, error) {
+		block, err = itxsvr.ChainService(uint32(1)).Blockchain().GetBlockByHeight(10)
+		return block != nil, nil
+	}); err != nil {
+		log.L().Fatal("Failed to get receipt of execution deployment", zap.Error(err))
 	}
 
 	debtorBalance, err := fpToken.ReadValue(contractAddr, blockchain.BalanceOf, debtorAddr)
