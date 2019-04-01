24350003fc1db5c1db047dd54ab266bbb46ace6b
lzxm160 <lzxm160@sina.com>
2019-04-02 07:10:33 +0800

    Removed hardcoded Tempfile in few more test files for db_test pr6152

--- db/db_test.go
+++ db/db_test.go
@@ -8,6 +8,8 @@ package db
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -56,19 +58,19 @@ func TestKVStorePutGet(t *testing.T) {
 	})
 
 	path := "test-kv-store.bolt"
-	cfg.DbPath = path
+	testFile, _ := ioutil.TempFile(os.TempDir(), path)
+	testPath := testFile.Name()
+	cfg.DbPath = testPath
 	t.Run("Bolt DB", func(t *testing.T) {
-		testutil.CleanupPath(t, path)
-		defer testutil.CleanupPath(t, path)
 		testKVStorePutGet(NewOnDiskDB(cfg), t)
 	})
 
 	path = "test-kv-store.badger"
-	cfg.DbPath = path
+	testFile, _ = ioutil.TempFile(os.TempDir(), path)
+	testPath = testFile.Name()
+	cfg.DbPath = testPath
 	cfg.UseBadgerDB = true
 	t.Run("Badger DB", func(t *testing.T) {
-		testutil.CleanupPath(t, path)
-		defer testutil.CleanupPath(t, path)
 		testKVStorePutGet(NewOnDiskDB(cfg), t)
 	})
 }
