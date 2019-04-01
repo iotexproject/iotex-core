377c1455f8ba67c97893790c8f349ab01c9e8741
lzxm160 <lzxm160@sina.com>
2019-04-01 15:42:37 +0800

    Removed hardcoded Tempfile

--- blockchain/blockdao_test.go
+++ blockchain/blockdao_test.go
@@ -359,18 +359,12 @@ func TestBlockDAO(t *testing.T) {
 	t.Run("Bolt DB for blocks", func(t *testing.T) {
 		testBlockDao(db.NewOnDiskDB(cfg), t)
 	})
-
 	t.Run("In-memory KV Store for actions", func(t *testing.T) {
 		testActionsDao(db.NewMemKVStore(), t)
 	})
-	//pathBolt := "test-kv-store-bolt"
-	//testBoltFile, _ := ioutil.TempFile(os.TempDir(), pathBolt)
-	//testBoltPath := testBoltFile.Name()
-	//cfg.DbPath = testBoltPath
 	t.Run("Bolt DB for actions", func(t *testing.T) {
 		testActionsDao(db.NewOnDiskDB(cfg), t)
 	})
-
 	t.Run("In-memory KV Store deletions", func(t *testing.T) {
 		testDeleteDao(db.NewMemKVStore(), t)
 	})
