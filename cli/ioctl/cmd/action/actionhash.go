15e1591c83d027cafa8e21e28e25c80ba836f1ce
Frank <35358771+Frankonly@users.noreply.github.com>
2019-04-02 10:03:25 -0700

    Add multisend bytecode generator (#896)

--- cli/ioctl/cmd/action/actionhash.go
+++ cli/ioctl/cmd/action/actionhash.go
@@ -134,15 +134,6 @@ func printActionProto(action *iotextypes.Action) (string, error) {
 }
 
 func printReceiptProto(receipt *iotextypes.Receipt) string {
-	logs := make([]string, 0)
-	for _, l := range receipt.Logs {
-		log := fmt.Sprintf("#%d block:%d txHash:%s address:%s data:%s\n",
-			l.Index, l.BlockNumber, l.TxnHash, l.Address, l.Data)
-		for _, t := range l.Topics {
-			log += fmt.Sprintf("  %s\n", t)
-		}
-		logs = append(logs, log)
-	}
 	output := ""
 	if len(receipt.ReturnValue) != 0 {
 		output += fmt.Sprintf("returnValue: %x\n", receipt.ReturnValue)
@@ -151,13 +142,11 @@ func printReceiptProto(receipt *iotextypes.Receipt) string {
 		Match(strconv.Itoa(int(receipt.Status)), "status")) +
 		fmt.Sprintf("actHash: %x\n", receipt.ActHash) +
 		// TODO: blkHash
-		fmt.Sprintf("gasConsumed: %d", receipt.GasConsumed)
+		fmt.Sprintf("gasConsumed: %d\n", receipt.GasConsumed) +
+		fmt.Sprintf("logs: %d", len(receipt.Logs))
 	if len(receipt.ContractAddress) != 0 {
 		output += fmt.Sprintf("\ncontractAddress: %s %s", receipt.ContractAddress,
 			Match(receipt.ContractAddress, "address"))
-	}
-	if len(logs) != 0 {
-		output += fmt.Sprintf("\nlogs:\n%s", logs)
 	}
 	return output
 }
