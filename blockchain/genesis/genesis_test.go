397034c3b9145f6efbd9616fe96e5d01f1435754
Yutong Pei <yutong@iotex.io>
2019-04-23 16:02:22 -0700

    make sure testing identity set address will not be merged with yaml

--- blockchain/genesis/genesis_test.go
+++ blockchain/genesis/genesis_test.go
@@ -22,14 +22,6 @@ func TestDefaultConfig(t *testing.T) {
 	assert.Equal(t, Default.ActionGasLimit, cfg.ActionGasLimit)
 	assert.Equal(t, Default.NumSubEpochs, cfg.NumSubEpochs)
 	assert.Equal(t, Default.NumDelegates, cfg.NumDelegates)
-	// Validate poll protocol
-	assert.Equal(t, Default.Delegates, cfg.Delegates)
-	assert.Equal(t, cfg.NumDelegates, uint64(len(cfg.Delegates)))
-	// Validate account protocol
-	eAddrs, eBalances := Default.InitBalances()
-	aAddrs, aBalances := cfg.InitBalances()
-	assert.Equal(t, eAddrs, aAddrs)
-	assert.Equal(t, eBalances, aBalances)
 	// Validate rewarding protocol)
 	assert.Equal(t, Default.BlockReward(), cfg.BlockReward())
 	assert.Equal(t, Default.EpochReward(), cfg.EpochReward())
