d547b6fddba97cb27646a352a050f24f2a22f291
lzxm160 <lzxm160@sina.com>
2019-04-25 16:47:52 +0800

    Add unit tests for action/protocol/poll/protocol_test.go #638

--- action/protocol/poll/protocol_test.go
+++ action/protocol/poll/protocol_test.go
@@ -8,14 +8,21 @@ package poll
 
 import (
 	"context"
+	"math/big"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
-	"github.com/iotexproject/iotex-election/test/mock/mock_committee"
-	"github.com/iotexproject/iotex-election/types"
 	"github.com/stretchr/testify/require"
 
+	"github.com/iotexproject/iotex-election/test/mock/mock_committee"
+	"github.com/iotexproject/iotex-election/types"
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/action/protocol"
+	"github.com/iotexproject/iotex-core/db"
+	"github.com/iotexproject/iotex-core/test/mock/mock_chainmanager"
+	"github.com/iotexproject/iotex-core/test/testaddress"
 	"github.com/iotexproject/iotex-address/address"
 	"github.com/iotexproject/iotex-core/action/protocol/vote/candidatesutil"
 	"github.com/iotexproject/iotex-core/config"
@@ -24,12 +31,18 @@ import (
 	"github.com/iotexproject/iotex-core/state/factory"
 )
 
-func TestInitialize(t *testing.T) {
+func initConstruct(t *testing.T)(Protocol,context.Context,factory.WorkingSet,*types.ElectionResult){
 	require := require.New(t)
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	cfg := config.Default
-	ctx := context.Background()
+	ctx := protocol.WithRunActionsCtx(
+		context.Background(),
+		protocol.RunActionsCtx{
+			BlockHeight: 123456,
+		},
+	)
+
 	sf, err := factory.NewFactory(cfg, factory.InMemTrieOption())
 	require.NoError(err)
 	require.NoError(sf.Start(ctx))
@@ -40,7 +53,8 @@ func TestInitialize(t *testing.T) {
 	require.NoError(err)
 	committee := mock_committee.NewMockCommittee(ctrl)
 	r := types.NewElectionResultForTest(time.Now())
-	committee.EXPECT().ResultByHeight(uint64(123456)).Return(r, nil).Times(1)
+	committee.EXPECT().ResultByHeight(uint64(123456)).Return(r, nil).AnyTimes()
+	committee.EXPECT().HeightByTime(gomock.Any()).Return(uint64(123456),nil).AnyTimes()
 	p, err := NewGovernanceChainCommitteeProtocol(
 		nil,
 		committee,
@@ -52,6 +66,11 @@ func TestInitialize(t *testing.T) {
 		cfg.Genesis.NumDelegates,
 	)
 	require.NoError(err)
+	return p,ctx,ws,r
+}
+func TestInitialize(t *testing.T) {
+	require := require.New(t)
+	p,ctx,ws,r:=initConstruct(t)
 	require.NoError(p.Initialize(ctx, ws))
 	var sc state.CandidateList
 	require.NoError(ws.State(candidatesutil.ConstructKey(1), &sc))
@@ -69,7 +88,185 @@ func TestInitialize(t *testing.T) {
 }
 
 func TestHandle(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	sm := mock_chainmanager.NewMockStateManager(ctrl)
+	cb := db.NewCachedBatch()
+	sm.EXPECT().GetCachedBatch().Return(cb).AnyTimes()
+	sm.EXPECT().State(gomock.Any(), gomock.Any()).Return(state.ErrStateNotExist).AnyTimes()
+	sm.EXPECT().PutState(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+	sm.EXPECT().Snapshot().Return(1).AnyTimes()
+	p,ctx,ws,_:=initConstruct(t)
+	require.NoError(p.Initialize(ctx, ws))
+
+	// wrong action
+	recipientAddr := testaddress.Addrinfo["alfa"]
+	senderKey := testaddress.Keyinfo["producer"]
+	tsf, err := action.NewTransfer(0, big.NewInt(10), recipientAddr.String(), []byte{}, uint64(100000), big.NewInt(10))
+	require.NoError(err)
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(tsf).Build()
+	selp, err := action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp)
+	// Case 1: wrong action type
+	receipt,err:=p.Handle(ctx,selp.Action(),nil)
+	require.Nil(err)
+	require.Nil(receipt)
+	// Case 2: right action type,setCandidates error
+	var sc state.CandidateList
+	require.NoError(ws.State(candidatesutil.ConstructKey(1), &sc))
+	act := action.NewPutPollResult(1, 123456, sc)
+	elp = bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act).Build()
+	selp, err = action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp)
+	receipt,err=p.Handle(ctx,selp.Action(),sm)
+	require.Error(err)
+	require.Nil(receipt)
+	// Case 3: all right
+	p3,ctx3,ws3,_:=initConstruct(t)
+	require.NoError(p3.Initialize(ctx3, ws3))
+	var sc3 state.CandidateList
+	require.NoError(ws3.State(candidatesutil.ConstructKey(1), &sc3))
+	act3 := action.NewPutPollResult(1, 1, sc3)
+	elp = bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act3).Build()
+	selp3, err := action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp3)
+	receipt,err=p.Handle(ctx3,selp3.Action(),sm)
+	require.NoError(err)
+	require.NotNil(receipt)
 }
 
 func TestProtocol_Validate(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	p,ctx,ws,_:=initConstruct(t)
+	require.NoError(p.Initialize(ctx, ws))
+
+	// wrong action
+	recipientAddr := testaddress.Addrinfo["alfa"]
+	senderKey := testaddress.Keyinfo["producer"]
+	tsf, err := action.NewTransfer(0, big.NewInt(10), recipientAddr.String(), []byte{}, uint64(100000), big.NewInt(10))
+	require.NoError(err)
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(tsf).Build()
+	selp, err := action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp)
+	// Case 1: wrong action type
+	err=p.Validate(ctx,selp.Action())
+	require.Nil(err)
+	// Case 2: Only producer could create this protocol
+	p2,ctx2,ws2,_:=initConstruct(t)
+	require.NoError(p2.Initialize(ctx2, ws2))
+	var sc2 state.CandidateList
+	require.NoError(ws2.State(candidatesutil.ConstructKey(1), &sc2))
+	act2 := action.NewPutPollResult(1, 1, sc2)
+	elp = bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act2).Build()
+	selp2, err := action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp2)
+	caller, err := address.FromBytes(selp.SrcPubkey().Hash())
+	require.NoError(err)
+	ctx2 = protocol.WithValidateActionsCtx(
+		context.Background(),
+		protocol.ValidateActionsCtx{
+			BlockHeight:  1,
+			ProducerAddr: recipientAddr.String(),
+			Caller:       caller,
+		},
+	)
+	err=p.Validate(ctx2,selp2.Action())
+	require.True(strings.Contains(err.Error(), "Only producer could create this protocol"))
+	// Case 3: duplicate candidate
+	p3,ctx3,ws3,_:=initConstruct(t)
+	require.NoError(p3.Initialize(ctx3, ws3))
+	var sc3 state.CandidateList
+	require.NoError(ws3.State(candidatesutil.ConstructKey(1), &sc3))
+	sc3=append(sc3,&state.Candidate{"1",big.NewInt(10),"2"})
+	sc3=append(sc3,&state.Candidate{"1",big.NewInt(10),"2"})
+	act3 := action.NewPutPollResult(1, 1, sc3)
+	elp = bd.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act3).Build()
+	selp3, err := action.Sign(elp, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp3)
+	ctx3 = protocol.WithValidateActionsCtx(
+		context.Background(),
+		protocol.ValidateActionsCtx{
+			BlockHeight:  1,
+			ProducerAddr: testaddress.Addrinfo["producer"].String(),
+			Caller:       caller,
+		},
+	)
+	err=p.Validate(ctx3,selp3.Action())
+	require.True(strings.Contains(err.Error(), "duplicate candidate"))
+
+	// Case 4: delegate's length is not equal
+	p4,ctx4,ws4,_:=initConstruct(t)
+	require.NoError(p4.Initialize(ctx4, ws4))
+	var sc4 state.CandidateList
+	require.NoError(ws4.State(candidatesutil.ConstructKey(1), &sc4))
+	sc4=append(sc4,&state.Candidate{"1",big.NewInt(10),"2"})
+	act4 := action.NewPutPollResult(1, 1, sc4)
+	bd4 := &action.EnvelopeBuilder{}
+	elp4 := bd4.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act4).Build()
+	selp4, err := action.Sign(elp4, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp4)
+	ctx4 = protocol.WithValidateActionsCtx(
+		context.Background(),
+		protocol.ValidateActionsCtx{
+			BlockHeight:  1,
+			ProducerAddr: testaddress.Addrinfo["producer"].String(),
+			Caller:       caller,
+		},
+	)
+	err=p4.Validate(ctx4,selp4.Action())
+	require.True(strings.Contains(err.Error(), "the proposed delegate list length"))
+
+	// Case 5: all good
+	p5,ctx5,ws5,_:=initConstruct(t)
+	require.NoError(p5.Initialize(ctx5, ws5))
+	var sc5 state.CandidateList
+	require.NoError(ws5.State(candidatesutil.ConstructKey(1), &sc5))
+	act5 := action.NewPutPollResult(1, 1, sc5)
+	bd5 := &action.EnvelopeBuilder{}
+	elp5 := bd5.SetGasLimit(uint64(100000)).
+		SetGasPrice(big.NewInt(10)).
+		SetAction(act5).Build()
+	selp5, err := action.Sign(elp5, senderKey.PriKey)
+	require.NoError(err)
+	require.NotNil(selp5)
+	caller5, err := address.FromBytes(selp5.SrcPubkey().Hash())
+	require.NoError(err)
+	ctx5 = protocol.WithValidateActionsCtx(
+		context.Background(),
+		protocol.ValidateActionsCtx{
+			BlockHeight:  1,
+			ProducerAddr: testaddress.Addrinfo["producer"].String(),
+			Caller:       caller5,
+		},
+	)
+	err=p5.Validate(ctx5,selp5.Action())
+	require.NoError(err)
 }
