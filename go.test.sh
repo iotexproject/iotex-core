7cb6b9d703b89e41a3374d721c933c0a22236ac2
feiren.kuang <feigu_1984@qq.com>
2019-04-26 16:50:51 +0800

    Update go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -2,11 +2,11 @@
 
 set -e
 echo "" > coverage.txt
-go test -v $(go list ./... |grep -v vendor | circleci tests split)
-# for d in $(go list ./... | grep -v vendor); do
-#     go test -short -v -coverprofile=profile.out -covermode=count "$d"
-#     if [ -f profile.out ]; then
-#         cat profile.out >> coverage.txt
-#         rm profile.out
-#     fi
-# done
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -short -v -coverprofile=profile.out -covermode=count "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
