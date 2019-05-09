79e76c3062713eb10ea4f6986d678f4f55ec8e85
lzxm160 <lzxm160@sina.com>
2019-05-09 08:40:38 +0800

    fix test

--- state/account.go
+++ state/account.go
@@ -27,13 +27,12 @@ var (
 type Account struct {
 	// 0 is reserved from actions in genesis block and coinbase transfers nonces
 	// other actions' nonces start from 1
-	Nonce    uint64
-	Balance  *big.Int
-	Root     hash.Hash256 // storage trie root for contract account
-	CodeHash []byte       // hash of the smart contract byte-code for contract account
+	Nonce        uint64
+	Balance      *big.Int
+	Root         hash.Hash256 // storage trie root for contract account
+	CodeHash     []byte       // hash of the smart contract byte-code for contract account
 	IsCandidate  bool
 	VotingWeight *big.Int
-	Votee        string
 }
 
 // ToProto converts to protobuf's Account
@@ -51,7 +50,6 @@ func (st *Account) ToProto() *accountpb.Account {
 	if st.VotingWeight != nil {
 		acPb.VotingWeight = st.VotingWeight.Bytes()
 	}
-	acPb.Votee = st.Votee
 	return acPb
 }
 
@@ -78,7 +76,6 @@ func (st *Account) FromProto(acPb *accountpb.Account) {
 	if acPb.VotingWeight != nil {
 		st.VotingWeight.SetBytes(acPb.VotingWeight)
 	}
-	st.Votee = acPb.Votee
 }
 
 // Deserialize deserializes bytes into account state
@@ -118,7 +115,9 @@ func (st *Account) Clone() *Account {
 	s.Balance = nil
 	s.Balance = new(big.Int).Set(st.Balance)
 	s.VotingWeight = nil
-	s.VotingWeight = new(big.Int).Set(st.VotingWeight)
+	if st.VotingWeight != nil {
+		s.VotingWeight = new(big.Int).Set(st.VotingWeight)
+	}
 	if st.CodeHash != nil {
 		s.CodeHash = nil
 		s.CodeHash = make([]byte, len(st.CodeHash))
@@ -130,7 +129,7 @@ func (st *Account) Clone() *Account {
 // EmptyAccount returns an empty account
 func EmptyAccount() Account {
 	return Account{
-		Balance: big.NewInt(0),
+		Balance:      big.NewInt(0),
 		VotingWeight: big.NewInt(0),
 	}
 }
