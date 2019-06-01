8b68805a1fe42c0a8080fe0920b724f2ba3a760c
Jiuqi Wang <wangjiuqi@wustl.edu>
2019-06-01 13:59:25 -0700

    address comment2

--- gasstation/gasstattion.go
+++ gasstation/gasstattion.go
@@ -63,7 +63,7 @@ func (gs *GasStation) SuggestGasPrice() (uint64, error) {
 		if len(blk.Actions) == 0 {
 			continue
 		}
-		if gs.IsSystemAction(blk.Actions[0]) && len(blk.Actions) == 1 {
+		if len(blk.Actions) == 1 && gs.IsSystemAction(blk.Actions[0]) {
 			continue
 		}
 		smallestPrice := blk.Actions[0].GasPrice()
