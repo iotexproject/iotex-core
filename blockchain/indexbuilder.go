31008d53b1dd1cb9c967926a1c1cf8a2736584a0
lzxm160 <lzxm160@sina.com>
2019-07-22 09:11:58 +0800

    x

--- blockchain/indexbuilder.go
+++ blockchain/indexbuilder.go
@@ -7,8 +7,6 @@
 package blockchain
 
 import (
-	"encoding/hex"
-	"fmt"
 	"strconv"
 
 	"github.com/iotexproject/go-pkgs/hash"
@@ -303,8 +301,8 @@ func putActions(store db.KVStore, blk *block.Block, batch db.KVStoreBatch, actDe
 			}
 			senderDelta[callerAddrBytes] = senderActionCount
 		}
-		str := hex.EncodeToString(callerAddrBytes[:]) + "::::" + fmt.Sprintf("%d", senderActionCount)
-		zap.L().Info(str)
+		//str := hex.EncodeToString(callerAddrBytes[:]) + "::::" + fmt.Sprintf("%d", senderActionCount)
+		//zap.L().Info(str)
 		// put new action to sender
 		senderKey := append(actionFromPrefix, callerAddrBytes[:]...)
 		senderKey = append(senderKey, byteutil.Uint64ToBytes(senderActionCount)...)
