ccebd55bbae63e62e2e7cdc30a4d02716df50990
Frank <35358771+Frankonly@users.noreply.github.com>
2019-11-06 15:59:42 +0800

    rename variable

--- ioctl/cmd/action/stake.go
+++ ioctl/cmd/action/stake.go
@@ -22,8 +22,8 @@ import (
 	"github.com/iotexproject/iotex-core/pkg/log"
 )
 
-// NativeStakingAddress stores native staking address as string
-const NativeStakingAddress = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
+// MainnetStakingAddress stores native staking address as string
+const MainnetStakingAddress = "io1xpq62aw85uqzrccg9y5hnryv8ld2nkpycc3gza"
 
 var autoRestake bool
 var stakingContractAddress string
@@ -43,7 +43,7 @@ func init() {
 	StakeCmd.AddCommand(stakeWithdrawCmd)
 
 	StakeCmd.PersistentFlags().StringVarP(&stakingContractAddress, "staking-contract-address", "c",
-		NativeStakingAddress, "set staking contract address")
+		MainnetStakingAddress, "set staking contract address")
 	StakeCmd.PersistentFlags().StringVar(&config.ReadConfig.Endpoint, "endpoint",
 		config.ReadConfig.Endpoint, "set endpoint for once")
 	StakeCmd.PersistentFlags().BoolVar(&config.Insecure, "insecure", config.Insecure,
