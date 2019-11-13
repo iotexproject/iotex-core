54c0436a0b79104f614b00192ccfb4afb6d58a55
Madongming <madongming@huami.com>
2019-11-13 17:56:05 +0800

    Simplified format 	modified:   api/listener_test.go

--- api/listener_test.go
+++ api/listener_test.go
@@ -10,69 +10,31 @@ var MOCKChainListener Listener
 
 // Mock ChainListener
 func TestNewChainListener(t *testing.T) {
-	test := struct {
-		name string
-	}{
-		name: "New Chain listener.",
-	}
-
-	t.Run(test.name, func(t *testing.T) {
+	t.Run("New Chain listener.", func(t *testing.T) {
 		MOCKChainListener = NewChainListener()
 	})
 }
 
 func Test_chainListener_Start(t *testing.T) {
-	test := struct {
-		name    string
-		wantErr bool
-	}{
-		name:    "Test start listener.",
-		wantErr: false,
-	}
-
-	t.Run(test.name, func(t *testing.T) {
-		if err := MOCKChainListener.Start(); (err != nil) != test.wantErr {
-			t.Errorf("chainListener.Start() error = %v, wantErr %v", err, test.wantErr)
+	t.Run("Test start Chain listener.", func(t *testing.T) {
+		if err := MOCKChainListener.Start(); err != nil {
+			t.Errorf("chainListener.Start() error = %v,", err)
 		}
 	})
 }
 
 func Test_chainListener_Stop(t *testing.T) {
-	test := struct {
-		name    string
-		wantErr bool
-	}{
-
-		name:    "Stop lisntener",
-		wantErr: false,
-	}
-
-	t.Run(test.name, func(t *testing.T) {
-		if err := MOCKChainListener.Stop(); (err != nil) != test.wantErr {
-			t.Errorf("chainListener.Stop() error = %v, wantErr %v", err, test.wantErr)
+	t.Run("Test Stop lisntener", func(t *testing.T) {
+		if err := MOCKChainListener.Stop(); err != nil {
+			t.Errorf("chainListener.Stop() error = %v.", err)
 		}
 	})
 }
 
 func Test_chainListener_HandleBlock(t *testing.T) {
-	type args struct {
-		blk *block.Block
-	}
-	test := struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		name: "Test HandleBlock.",
-		args: args{
-			blk: new(block.Block),
-		},
-		wantErr: false,
-	}
-
-	t.Run(test.name, func(t *testing.T) {
-		if err := MOCKChainListener.HandleBlock(test.args.blk); (err != nil) != test.wantErr {
-			t.Errorf("chainListener.HandleBlock() error = %v, wantErr %v", err, test.wantErr)
+	t.Run("Test HandleBlock.", func(t *testing.T) {
+		if err := MOCKChainListener.HandleBlock(new(block.Block)); err != nil {
+			t.Errorf("chainListener.HandleBlock() error = %v.", err)
 		}
 	})
 }
@@ -88,23 +50,9 @@ func (cr ChainResponder) Exit() {
 var MOCKChainResponder ChainResponder
 
 func Test_chainListener_AddResponder(t *testing.T) {
-	type args struct {
-		r Responder
-	}
-	test := struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		name: "Test AddResponder",
-		args: args{
-			r: MOCKChainResponder,
-		},
-		wantErr: false,
-	}
-	t.Run(test.name, func(t *testing.T) {
-		if err := MOCKChainListener.AddResponder(test.args.r); (err != nil) != test.wantErr {
-			t.Errorf("chainListener.AddResponder() error = %v, wantErr %v", err, test.wantErr)
+	t.Run("Test AddResponder", func(t *testing.T) {
+		if err := MOCKChainListener.AddResponder(MOCKChainResponder); err != nil {
+			t.Errorf("chainListener.AddResponder() error = %v.", err)
 		}
 	})
 }
