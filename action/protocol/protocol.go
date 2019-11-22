b40055e6d8afd66c74346a47adaceb5a7a51572a
CoderZhi <thecoderzhi@gmail.com>
2019-11-21 16:06:04 -0800

    mock protocol (#1678)

--- action/protocol/protocol.go
+++ action/protocol/protocol.go
@@ -8,15 +8,10 @@ package protocol
 
 import (
 	"context"
-	"strings"
 
-	"github.com/iotexproject/iotex-address/address"
 	"github.com/pkg/errors"
 
-	"github.com/iotexproject/go-pkgs/hash"
 	"github.com/iotexproject/iotex-core/action"
-	"github.com/iotexproject/iotex-core/db"
-	"github.com/iotexproject/iotex-core/state"
 )
 
 var (
@@ -46,62 +41,4 @@ type ActionEnvelopeValidator interface {
 // decide if it wants to handle this action or not.
 type ActionHandler interface {
 	Handle(context.Context, action.Action, StateManager) (*action.Receipt, error)
-}
-
-// ChainManager defines the blockchain interface
-type ChainManager interface {
-	// ChainID returns the chain ID
-	ChainID() uint32
-	// CandidatesByHeight returns the candidate list by a given height
-	CandidatesByHeight(height uint64) ([]*state.Candidate, error)
-	// ProductivityByEpoch returns the number of produced blocks per delegate in an epoch
-	ProductivityByEpoch(epochNum uint64) (uint64, map[string]uint64, error)
-	// SimulateExecution simulates a running of smart contract operation
-	SimulateExecution(caller address.Address, ex *action.Execution) ([]byte, *action.Receipt, error)
-}
-
-// StateManager defines the state DB interface atop IoTeX blockchain
-type StateManager interface {
-	// Accounts
-	Height() uint64
-	Snapshot() int
-	Revert(int) error
-	// General state
-	State(hash.Hash160, interface{}) error
-	PutState(hash.Hash160, interface{}) error
-	DelState(pkHash hash.Hash160) error
-	GetDB() db.KVStore
-	GetCachedBatch() db.CachedBatch
-}
-
-// MockChainManager mocks ChainManager interface
-type MockChainManager struct {
-}
-
-// Nonce mocks base method
-func (m *MockChainManager) Nonce(addr string) (uint64, error) {
-	if strings.EqualFold("io1emxf8zzqckhgjde6dqd97ts0y3q496gm3fdrl6", addr) {
-		return 0, errors.New("MockChainManager nonce error")
-	}
-	return 2, nil
-}
-
-// ChainID return chain ID
-func (m *MockChainManager) ChainID() uint32 {
-	return 0
-}
-
-// CandidatesByHeight returns the candidate list by a given height
-func (m *MockChainManager) CandidatesByHeight(height uint64) ([]*state.Candidate, error) {
-	return nil, nil
-}
-
-// ProductivityByEpoch returns the number of produced blocks per delegate in an epoch
-func (m *MockChainManager) ProductivityByEpoch(epochNum uint64) (uint64, map[string]uint64, error) {
-	return 0, nil, nil
-}
-
-// SimulateExecution simulates a running of smart contract operation
-func (m *MockChainManager) SimulateExecution(caller address.Address, ex *action.Execution) ([]byte, *action.Receipt, error) {
-	return nil, nil, nil
 }
