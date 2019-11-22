874b05d9b743c75eec78568ef3843bd2ce994ee3
帅文 <308568175@qq.com>
2019-11-22 15:40:46 +0800

    add action/envelope.go&&envelope_test.go action/sealedenvelope.go&&sealedenvelope_test.go

---
+++ action/envelope_test.go
@@ -0,0 +1,34 @@
+package action
+
+import (
+	"testing"
+
+	"github.com/iotexproject/iotex-core/pkg/unit"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnvelope_LoadProto(t *testing.T) {
+	req := require.New(t)
+	i := 10
+	tsf, _ := NewTransfer(
+		uint64(i),
+		unit.ConvertIotxToRau(1000+int64(i)),
+		identityset.Address(i%identityset.Size()).String(),
+		nil,
+		20000+uint64(i),
+		unit.ConvertIotxToRau(1+int64(i)),
+	)
+	eb := EnvelopeBuilder{}
+	evlp := eb.
+		SetAction(tsf).
+		SetGasLimit(tsf.GasLimit()).
+		SetGasPrice(tsf.GasPrice()).
+		SetNonce(tsf.Nonce()).
+		SetVersion(1).
+		Build()
+
+	proto := evlp.Proto()
+	err := evlp.LoadProto(proto)
+	req.Equal(nil, err)
+}
