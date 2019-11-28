b083ad5b277829dc4e8b717d430da7a67faf685e
Liu Chang <studywiller@gmail.com>
2019-11-28 08:53:06 +0800

    add PrivateNetworkPSK config

--- p2p/agent.go
+++ p2p/agent.go
@@ -111,6 +111,7 @@ func (p *Agent) Start(ctx context.Context) error {
 		p2p.Gossip(),
 		p2p.SecureIO(),
 		p2p.MasterKey(p.cfg.MasterKey),
+		p2p.PrivateNetworkPSK(p.cfg.PrivateNetworkPSK),
 	}
 	if p.cfg.EnableRateLimit {
 		opts = append(opts, p2p.WithRateLimit(p.cfg.RateLimit))
