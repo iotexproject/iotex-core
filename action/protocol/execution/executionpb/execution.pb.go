fe578d57a7a2da12229f2a0d72343944805d0f3b
frank <35358771+Frankonly@users.noreply.github.com>
2020-02-13 17:29:28 +0800

    evm transfer proto

--- action/protocol/execution/executionpb/execution.pb.go
+++ action/protocol/execution/executionpb/execution.pb.go
@@ -75,62 +75,139 @@ func (m *EvmTransfer) GetTo() string {
 	return ""
 }
 
-type EvmTransferList struct {
-	EvmTransfers         []*EvmTransfer `protobuf:"bytes,1,rep,name=evmTransfers,proto3" json:"evmTransfers,omitempty"`
+type ActionEvmTransfers struct {
+	ActionHash           []byte         `protobuf:"bytes,1,opt,name=actionHash,proto3" json:"actionHash,omitempty"`
+	NumEvmTransfer       int32          `protobuf:"varint,2,opt,name=numEvmTransfer,proto3" json:"numEvmTransfer,omitempty"`
+	EvmTransfers         []*EvmTransfer `protobuf:"bytes,3,rep,name=evmTransfers,proto3" json:"evmTransfers,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
 	XXX_unrecognized     []byte         `json:"-"`
 	XXX_sizecache        int32          `json:"-"`
 }
 
-func (m *EvmTransferList) Reset()         { *m = EvmTransferList{} }
-func (m *EvmTransferList) String() string { return proto.CompactTextString(m) }
-func (*EvmTransferList) ProtoMessage()    {}
-func (*EvmTransferList) Descriptor() ([]byte, []int) {
+func (m *ActionEvmTransfers) Reset()         { *m = ActionEvmTransfers{} }
+func (m *ActionEvmTransfers) String() string { return proto.CompactTextString(m) }
+func (*ActionEvmTransfers) ProtoMessage()    {}
+func (*ActionEvmTransfers) Descriptor() ([]byte, []int) {
 	return fileDescriptor_776e2c5022e94aef, []int{1}
 }
 
-func (m *EvmTransferList) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_EvmTransferList.Unmarshal(m, b)
+func (m *ActionEvmTransfers) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ActionEvmTransfers.Unmarshal(m, b)
 }
-func (m *EvmTransferList) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_EvmTransferList.Marshal(b, m, deterministic)
+func (m *ActionEvmTransfers) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ActionEvmTransfers.Marshal(b, m, deterministic)
 }
-func (m *EvmTransferList) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_EvmTransferList.Merge(m, src)
+func (m *ActionEvmTransfers) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ActionEvmTransfers.Merge(m, src)
 }
-func (m *EvmTransferList) XXX_Size() int {
-	return xxx_messageInfo_EvmTransferList.Size(m)
+func (m *ActionEvmTransfers) XXX_Size() int {
+	return xxx_messageInfo_ActionEvmTransfers.Size(m)
 }
-func (m *EvmTransferList) XXX_DiscardUnknown() {
-	xxx_messageInfo_EvmTransferList.DiscardUnknown(m)
+func (m *ActionEvmTransfers) XXX_DiscardUnknown() {
+	xxx_messageInfo_ActionEvmTransfers.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_EvmTransferList proto.InternalMessageInfo
+var xxx_messageInfo_ActionEvmTransfers proto.InternalMessageInfo
 
-func (m *EvmTransferList) GetEvmTransfers() []*EvmTransfer {
+func (m *ActionEvmTransfers) GetActionHash() []byte {
+	if m != nil {
+		return m.ActionHash
+	}
+	return nil
+}
+
+func (m *ActionEvmTransfers) GetNumEvmTransfer() int32 {
+	if m != nil {
+		return m.NumEvmTransfer
+	}
+	return 0
+}
+
+func (m *ActionEvmTransfers) GetEvmTransfers() []*EvmTransfer {
 	if m != nil {
 		return m.EvmTransfers
 	}
 	return nil
 }
 
+type BlockEvmTransfers struct {
+	BlockHeight          uint64                `protobuf:"varint,1,opt,name=blockHeight,proto3" json:"blockHeight,omitempty"`
+	NumEvmTransfer       int32                 `protobuf:"varint,2,opt,name=numEvmTransfer,proto3" json:"numEvmTransfer,omitempty"`
+	ActionEvmTransfers   []*ActionEvmTransfers `protobuf:"bytes,3,rep,name=actionEvmTransfers,proto3" json:"actionEvmTransfers,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}              `json:"-"`
+	XXX_unrecognized     []byte                `json:"-"`
+	XXX_sizecache        int32                 `json:"-"`
+}
+
+func (m *BlockEvmTransfers) Reset()         { *m = BlockEvmTransfers{} }
+func (m *BlockEvmTransfers) String() string { return proto.CompactTextString(m) }
+func (*BlockEvmTransfers) ProtoMessage()    {}
+func (*BlockEvmTransfers) Descriptor() ([]byte, []int) {
+	return fileDescriptor_776e2c5022e94aef, []int{2}
+}
+
+func (m *BlockEvmTransfers) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_BlockEvmTransfers.Unmarshal(m, b)
+}
+func (m *BlockEvmTransfers) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_BlockEvmTransfers.Marshal(b, m, deterministic)
+}
+func (m *BlockEvmTransfers) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_BlockEvmTransfers.Merge(m, src)
+}
+func (m *BlockEvmTransfers) XXX_Size() int {
+	return xxx_messageInfo_BlockEvmTransfers.Size(m)
+}
+func (m *BlockEvmTransfers) XXX_DiscardUnknown() {
+	xxx_messageInfo_BlockEvmTransfers.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_BlockEvmTransfers proto.InternalMessageInfo
+
+func (m *BlockEvmTransfers) GetBlockHeight() uint64 {
+	if m != nil {
+		return m.BlockHeight
+	}
+	return 0
+}
+
+func (m *BlockEvmTransfers) GetNumEvmTransfer() int32 {
+	if m != nil {
+		return m.NumEvmTransfer
+	}
+	return 0
+}
+
+func (m *BlockEvmTransfers) GetActionEvmTransfers() []*ActionEvmTransfers {
+	if m != nil {
+		return m.ActionEvmTransfers
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*EvmTransfer)(nil), "executionpb.EvmTransfer")
-	proto.RegisterType((*EvmTransferList)(nil), "executionpb.EvmTransferList")
+	proto.RegisterType((*ActionEvmTransfers)(nil), "executionpb.ActionEvmTransfers")
+	proto.RegisterType((*BlockEvmTransfers)(nil), "executionpb.BlockEvmTransfers")
 }
 
 func init() { proto.RegisterFile("execution.proto", fileDescriptor_776e2c5022e94aef) }
 
 var fileDescriptor_776e2c5022e94aef = []byte{
-	// 148 bytes of a gzipped FileDescriptorProto
+	// 232 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4f, 0xad, 0x48, 0x4d,
 	0x2e, 0x2d, 0xc9, 0xcc, 0xcf, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x86, 0x0b, 0x14,
 	0x24, 0x29, 0x79, 0x72, 0x71, 0xbb, 0x96, 0xe5, 0x86, 0x14, 0x25, 0xe6, 0x15, 0xa7, 0xa5, 0x16,
 	0x09, 0x89, 0x71, 0xb1, 0x25, 0xe6, 0xe6, 0x97, 0xe6, 0x95, 0x48, 0x30, 0x2a, 0x30, 0x6a, 0x70,
 	0x06, 0x41, 0x79, 0x42, 0x42, 0x5c, 0x2c, 0x69, 0x45, 0xf9, 0xb9, 0x12, 0x4c, 0x60, 0x51, 0x30,
-	0x5b, 0x88, 0x8f, 0x8b, 0xa9, 0x24, 0x5f, 0x82, 0x19, 0x2c, 0xc2, 0x54, 0x92, 0xaf, 0xe4, 0xcf,
-	0xc5, 0x8f, 0x64, 0x94, 0x4f, 0x66, 0x71, 0x89, 0x90, 0x0d, 0x17, 0x4f, 0x2a, 0x42, 0xa8, 0x58,
-	0x82, 0x51, 0x81, 0x59, 0x83, 0xdb, 0x48, 0x42, 0x0f, 0xc9, 0x05, 0x7a, 0x48, 0x7a, 0x82, 0x50,
-	0x54, 0x27, 0xb1, 0x81, 0xdd, 0x6b, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xcb, 0xf5, 0x1d, 0xd2,
-	0xc2, 0x00, 0x00, 0x00,
+	0x5b, 0x88, 0x8f, 0x8b, 0xa9, 0x24, 0x5f, 0x82, 0x19, 0x2c, 0xc2, 0x54, 0x92, 0xaf, 0x34, 0x8b,
+	0x91, 0x4b, 0xc8, 0x31, 0x19, 0x64, 0x2e, 0x92, 0x89, 0xc5, 0x42, 0x72, 0x5c, 0x5c, 0x89, 0x60,
+	0x51, 0x8f, 0xc4, 0xe2, 0x0c, 0xb0, 0xb1, 0x3c, 0x41, 0x48, 0x22, 0x42, 0x6a, 0x5c, 0x7c, 0x79,
+	0xa5, 0xb9, 0x48, 0x5a, 0xc0, 0x96, 0xb0, 0x06, 0xa1, 0x89, 0x0a, 0xd9, 0x70, 0xf1, 0xa4, 0x22,
+	0x99, 0x2b, 0xc1, 0xac, 0xc0, 0xac, 0xc1, 0x6d, 0x24, 0xa1, 0x87, 0xe4, 0x1b, 0x3d, 0x24, 0xf5,
+	0x41, 0x28, 0xaa, 0x95, 0xd6, 0x31, 0x72, 0x09, 0x3a, 0xe5, 0xe4, 0x27, 0x67, 0xa3, 0xb8, 0x4d,
+	0x81, 0x8b, 0x3b, 0x09, 0x24, 0xe8, 0x91, 0x9a, 0x99, 0x9e, 0x01, 0xf1, 0x33, 0x4b, 0x10, 0xb2,
+	0x10, 0xd1, 0xae, 0xf3, 0xe7, 0x12, 0x4a, 0xc4, 0xf0, 0x3b, 0xd4, 0x8d, 0xf2, 0x28, 0x6e, 0xc4,
+	0x0c, 0xa2, 0x20, 0x2c, 0x5a, 0x93, 0xd8, 0xc0, 0x91, 0x65, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff,
+	0x77, 0x21, 0x0d, 0x46, 0xbf, 0x01, 0x00, 0x00,
 }
