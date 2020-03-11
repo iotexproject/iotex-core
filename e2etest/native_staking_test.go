874730310ea172148c7b9d116f4dbb22150e8078
lizhefeng <lizhefeng1993@gmail.com>
2020-03-11 14:57:27 -0700

    Add e2etest for staking protocol (#2022)

---
+++ e2etest/native_staking_test.go
@@ -0,0 +1,347 @@
+package e2etest
+
+import (
+	"context"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/go-pkgs/hash"
+	"github.com/iotexproject/iotex-address/address"
+
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/action/protocol"
+	accountutil "github.com/iotexproject/iotex-core/action/protocol/account/util"
+	"github.com/iotexproject/iotex-core/action/protocol/staking"
+	"github.com/iotexproject/iotex-core/blockchain"
+	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/pkg/unit"
+	"github.com/iotexproject/iotex-core/server/itx"
+	"github.com/iotexproject/iotex-core/state"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/iotexproject/iotex-core/testutil"
+)
+
+const (
+	// TODO: to be removed
+	_const = byte(iota)
+	_bucket
+	_voterIndex
+	_candIndex
+
+	candidate1Name = "candidate1"
+	candidate2Name = "candidate2"
+	selfStake      = "1200000000000000000000000"
+	initVotes      = "1260000000000000053290706"
+	vote           = "100000000000000000000"
+	autoStakeVote  = "103801784016923925869"
+	initBalance    = "100000000000000000000000000"
+)
+
+var (
+	gasPrice = big.NewInt(0)
+	gasLimit = uint64(1000000)
+)
+
+func TestNativeStaking(t *testing.T) {
+	testNativeStaking := func(cfg config.Config, t *testing.T) {
+		require := require.New(t)
+		ctx := context.Background()
+
+		// Create a new blockchain
+		svr, err := itx.NewServer(cfg)
+		require.NoError(err)
+		require.NoError(svr.Start(ctx))
+		defer func() {
+			require.NoError(svr.Stop(ctx))
+		}()
+
+		chainID := cfg.Chain.ID
+		bc := svr.ChainService(chainID).Blockchain()
+		sf := svr.ChainService(chainID).StateFactory()
+		require.NotNil(bc)
+
+		// Create two candidates
+		cand1Addr := identityset.Address(0)
+		cand1PriKey := identityset.PrivateKey(0)
+
+		cand2Addr := identityset.Address(1)
+		cand2PriKey := identityset.PrivateKey(1)
+
+		register1, err := testutil.SignedCandidateRegister(1, candidate1Name, cand1Addr.String(), cand1Addr.String(),
+			cand1Addr.String(), selfStake, 1, false, nil, gasLimit, gasPrice, cand1PriKey)
+		require.NoError(err)
+		register2, err := testutil.SignedCandidateRegister(1, candidate2Name, cand2Addr.String(), cand2Addr.String(),
+			cand2Addr.String(), selfStake, 1, false, nil, gasLimit, gasPrice, cand2PriKey)
+		require.NoError(err)
+
+		fixedTime := time.Unix(cfg.Genesis.Timestamp, 0)
+		require.NoError(createAndCommitBlock(bc, []address.Address{cand1Addr},
+			[]action.SealedEnvelope{register1}, fixedTime))
+		require.NoError(createAndCommitBlock(bc, []address.Address{cand2Addr},
+			[]action.SealedEnvelope{register2}, fixedTime))
+
+		// check candidate state
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), selfStake, initVotes, cand1Addr))
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), selfStake, initVotes, cand1Addr))
+
+		// check candidate account state
+		require.NoError(checkAccountState(cfg, sf, register1, true, initBalance, cand1Addr))
+		require.NoError(checkAccountState(cfg, sf, register2, true, initBalance, cand2Addr))
+
+		// create two stakes from two voters
+		voter1Addr := identityset.Address(2)
+		voter1PriKey := identityset.PrivateKey(2)
+
+		voter2Addr := identityset.Address(3)
+		voter2PriKey := identityset.PrivateKey(3)
+
+		cs1, err := testutil.SignedCreateStake(1, candidate1Name, vote, 1, false,
+			nil, gasLimit, gasPrice, voter1PriKey)
+		require.NoError(err)
+		cs2, err := testutil.SignedCreateStake(1, candidate1Name, vote, 1, false,
+			nil, gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter1Addr, voter2Addr},
+			[]action.SealedEnvelope{cs1, cs2}, fixedTime))
+
+		// check candidate state
+		wv, _ := big.NewInt(0).SetString(vote, 10)
+		expectedVotes, _ := big.NewInt(0).SetString(initVotes, 10)
+		expectedVotes.Add(expectedVotes, big.NewInt(0).Mul(wv, big.NewInt(2)))
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), selfStake, expectedVotes.String(), cand1Addr))
+
+		// check voter account state
+		require.NoError(checkAccountState(cfg, sf, cs1, false, initBalance, voter1Addr))
+		require.NoError(checkAccountState(cfg, sf, cs2, false, initBalance, voter2Addr))
+
+		// TODO: we should get bucket index through receipt
+		var bis staking.BucketIndices
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(voter1Addr, _voterIndex)))
+		require.NoError(err)
+		require.Equal(1, len(bis))
+		voter1BucketIndex := bis[0]
+
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(voter2Addr, _voterIndex)))
+		require.NoError(err)
+		require.Equal(1, len(bis))
+		voter2BucketIndex := bis[0]
+
+		// change candidate
+		cc, err := testutil.SignedChangeCandidate(2, candidate2Name, voter2BucketIndex, nil,
+			gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter2Addr}, []action.SealedEnvelope{cc}, fixedTime))
+
+		// check candidate state
+		expectedVotes, _ = big.NewInt(0).SetString(initVotes, 10)
+		expectedVotes.Add(expectedVotes, wv)
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), selfStake, expectedVotes.String(), cand1Addr))
+		require.NoError(checkCandidateState(sf, candidate2Name, cand2Addr.String(), selfStake, expectedVotes.String(), cand2Addr))
+
+		// transfer stake
+		ts, err := testutil.SignedTransferStake(2, voter2Addr.String(), voter1BucketIndex, nil, gasLimit, gasPrice, voter1PriKey)
+		require.NoError(err)
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter1Addr}, []action.SealedEnvelope{ts}, fixedTime))
+
+		// check buckets
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(voter1Addr, _voterIndex)))
+		require.Error(err)
+		require.Equal(state.ErrStateNotExist, errors.Cause(err))
+
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(voter2Addr, _voterIndex)))
+		require.NoError(err)
+		require.Equal(2, len(bis))
+		require.Equal(voter2BucketIndex, bis[0])
+		require.Equal(voter1BucketIndex, bis[1])
+
+		// deposit to stake
+		ds, err := testutil.SignedDepositToStake(3, voter2BucketIndex, vote, nil, gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+		err = createAndCommitBlock(bc, []address.Address{voter2Addr}, []action.SealedEnvelope{ds}, fixedTime)
+		require.Error(err)
+		require.True(strings.Contains(err.Error(), "deposit is only allowed on auto-stake bucket"))
+
+		// restake
+		rs, err := testutil.SignedRestake(3, voter2BucketIndex, 1, true, nil,
+			gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter2Addr}, []action.SealedEnvelope{rs}, fixedTime))
+
+		// check candidate state
+		expectedVotes, _ = big.NewInt(0).SetString(initVotes, 10)
+		wv, _ = big.NewInt(0).SetString(autoStakeVote, 10)
+		expectedVotes.Add(expectedVotes, wv)
+		require.NoError(checkCandidateState(sf, candidate2Name, cand2Addr.String(), selfStake, expectedVotes.String(), cand2Addr))
+
+		// deposit to stake again
+		ds, err = testutil.SignedDepositToStake(3, voter2BucketIndex, vote, nil, gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter2Addr}, []action.SealedEnvelope{ds}, fixedTime))
+
+		// check voter account state
+		voterBalance, _ := big.NewInt(0).SetString(initBalance, 10)
+		vote, _ := big.NewInt(0).SetString(vote, 10)
+		voterBalance.Sub(voterBalance, vote)
+		require.NoError(checkAccountState(cfg, sf, ds, false, voterBalance.String(), voter2Addr))
+
+		// unstake voter stake
+		us, err := testutil.SignedReclaimStake(false, 4, voter1BucketIndex, nil, gasLimit, gasPrice, voter2PriKey)
+		require.NoError(err)
+		require.NoError(createAndCommitBlock(bc, []address.Address{voter2Addr}, []action.SealedEnvelope{us}, fixedTime))
+
+		// check candidate state
+		expectedVotes, _ = big.NewInt(0).SetString(initVotes, 10)
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), selfStake, expectedVotes.String(), cand1Addr))
+
+		// unstake self stake
+		us, err = testutil.SignedReclaimStake(false, 2, 0, nil, gasLimit, gasPrice, cand1PriKey)
+		require.NoError(err)
+		require.NoError(createAndCommitBlock(bc, []address.Address{cand1Addr}, []action.SealedEnvelope{us}, fixedTime))
+
+		// check candidate state
+		expectedVotes = big.NewInt(0)
+		expectedSelfStake := big.NewInt(0)
+		require.NoError(checkCandidateState(sf, candidate1Name, cand1Addr.String(), expectedSelfStake.String(),
+			expectedVotes.String(), cand1Addr))
+
+		// withdraw stake
+		ws, err := testutil.SignedReclaimStake(true, 3, 0, nil, gasLimit, gasPrice, cand1PriKey)
+		require.NoError(err)
+		err = createAndCommitBlock(bc, []address.Address{cand1Addr}, []action.SealedEnvelope{ws}, fixedTime)
+		require.Error(err)
+		require.True(strings.Contains(err.Error(), "stake is not ready to withdraw"))
+
+		require.NoError(createAndCommitBlock(bc, []address.Address{cand1Addr}, []action.SealedEnvelope{ws}, fixedTime.Add(cfg.Genesis.WithdrawWaitingPeriod)))
+
+		// check buckets
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(cand1Addr, _voterIndex)))
+		require.Error(err)
+		require.Equal(state.ErrStateNotExist, errors.Cause(err))
+
+		_, err = sf.State(&bis, protocol.NamespaceOption(staking.StakingNameSpace),
+			protocol.KeyOption(addrKeyWithPrefix(cand1Addr, _candIndex)))
+		require.NoError(err)
+		require.Equal(1, len(bis))
+
+		// check candidate account state
+		candidateBalance, _ := big.NewInt(0).SetString(initBalance, 10)
+		registrationFee := unit.ConvertIotxToRau(cfg.Genesis.RegistrationConsts.Fee)
+		candidateBalance.Sub(candidateBalance, registrationFee)
+		require.NoError(checkAccountState(cfg, sf, ws, false, candidateBalance.String(), cand1Addr))
+	}
+
+	cfg := config.Default
+	testTrieFile, _ := ioutil.TempFile(os.TempDir(), "trie")
+	testTriePath := testTrieFile.Name()
+	testDBFile, _ := ioutil.TempFile(os.TempDir(), "db")
+	testDBPath := testDBFile.Name()
+	testIndexFile, _ := ioutil.TempFile(os.TempDir(), "index")
+	testIndexPath := testIndexFile.Name()
+	defer func() {
+		testutil.CleanupPath(t, testTriePath)
+		testutil.CleanupPath(t, testDBPath)
+		testutil.CleanupPath(t, testIndexPath)
+		// clear the gateway
+		delete(cfg.Plugins, config.GatewayPlugin)
+	}()
+
+	cfg.Chain.TrieDBPath = testTriePath
+	cfg.Chain.ChainDBPath = testDBPath
+	cfg.Chain.IndexDBPath = testIndexPath
+	cfg.Consensus.Scheme = config.NOOPScheme
+	cfg.Chain.EnableAsyncIndexWrite = false
+
+	t.Run("test native staking", func(t *testing.T) {
+		testNativeStaking(cfg, t)
+	})
+}
+
+// TODO: to be removed
+func addrKeyWithPrefix(voterAddr address.Address, prefix byte) []byte {
+	k := voterAddr.Bytes()
+	key := make([]byte, len(k)+1)
+	key[0] = prefix
+	copy(key[1:], k)
+	return key
+}
+
+func checkCandidateState(
+	sr protocol.StateReader,
+	expectedName,
+	expectedOwnerAddr,
+	expectedSelfStake,
+	expectedVotes string,
+	candidateAddr address.Address,
+) error {
+	var cand staking.Candidate
+	if _, err := sr.State(&cand, protocol.NamespaceOption(staking.CandidateNameSpace), protocol.KeyOption(candidateAddr.Bytes())); err != nil {
+		return err
+	}
+	if expectedName != cand.Name {
+		return errors.New("name does not match")
+	}
+	if expectedOwnerAddr != cand.Owner.String() {
+		return errors.New("Owner address does not match")
+	}
+	if expectedSelfStake != cand.SelfStake.String() {
+		return errors.New("self stake does not match")
+	}
+	if expectedVotes != cand.Votes.String() {
+		return errors.New("votes does not match")
+	}
+	return nil
+}
+
+func checkAccountState(
+	cfg config.Config,
+	sr protocol.StateReader,
+	act action.SealedEnvelope,
+	registrationFee bool,
+	expectedBalance string,
+	accountAddr address.Address,
+) error {
+	cost, err := act.Cost()
+	if err != nil {
+		return err
+	}
+	if registrationFee {
+		cost.Add(cost, unit.ConvertIotxToRau(cfg.Genesis.RegistrationConsts.Fee))
+	}
+	acct1, err := accountutil.LoadAccount(sr, hash.BytesToHash160(accountAddr.Bytes()))
+	if err != nil {
+		return err
+	}
+	if expectedBalance != big.NewInt(0).Add(acct1.Balance, cost).String() {
+		return errors.New("balance does not match")
+	}
+	return nil
+}
+
+func createAndCommitBlock(bc blockchain.Blockchain, callers []address.Address, selps []action.SealedEnvelope, blkTime time.Time) error {
+	if len(callers) != len(selps) {
+		return errors.New("incorrect input")
+	}
+	accMap := make(map[string][]action.SealedEnvelope)
+	for i, caller := range callers {
+		accMap[caller.String()] = []action.SealedEnvelope{selps[i]}
+	}
+	blk, err := bc.MintNewBlock(accMap, blkTime)
+	if err != nil {
+		return err
+	}
+	return bc.CommitBlock(blk)
+}
--- testutil/signedaction.go
+++ testutil/signedaction.go
@@ -50,3 +50,223 @@ func SignedExecution(contractAddr string, executorPriKey crypto.PrivateKey, nonc
 	}
 	return selp, nil
 }
+
+// SignedCandidateRegister returns a signed candidate register
+func SignedCandidateRegister(
+	nonce uint64,
+	name, operatorAddrStr, rewardAddrStr, ownerAddrStr, amountStr string,
+	duration uint32,
+	autoStake bool,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	registererPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	cr, err := action.NewCandidateRegister(nonce, name, operatorAddrStr, rewardAddrStr, ownerAddrStr, amountStr,
+		duration, autoStake, payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(cr).Build()
+	selp, err := action.Sign(elp, registererPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign candidate register %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedCandidateUpdate returns a signed candidate update
+func SignedCandidateUpdate(
+	nonce uint64,
+	name, operatorAddrStr, rewardAddrStr string,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	registererPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	cu, err := action.NewCandidateUpdate(nonce, name, operatorAddrStr, rewardAddrStr, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(cu).Build()
+	selp, err := action.Sign(elp, registererPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign candidate update %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedCreateStake returns a signed create stake
+func SignedCreateStake(nonce uint64,
+	candidateName, amount string,
+	duration uint32,
+	autoStake bool,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	stakerPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	cs, err := action.NewCreateStake(nonce, candidateName, amount, duration, autoStake,
+		payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(cs).Build()
+	selp, err := action.Sign(elp, stakerPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign create stake %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedReclaimStake returns a signed unstake or withdraw stake
+func SignedReclaimStake(
+	withdraw bool,
+	nonce uint64,
+	bucketIndex uint64,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	reclaimerPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	bd := &action.EnvelopeBuilder{}
+	eb := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit)
+	var elp action.Envelope
+	// unstake
+	if !withdraw {
+		us, err := action.NewUnstake(nonce, bucketIndex, payload, gasLimit, gasPrice)
+		if err != nil {
+			return action.SealedEnvelope{}, err
+		}
+		elp = eb.SetAction(us).Build()
+	} else {
+		w, err := action.NewWithdrawStake(nonce, bucketIndex, payload, gasLimit, gasPrice)
+		if err != nil {
+			return action.SealedEnvelope{}, err
+		}
+		elp = eb.SetAction(w).Build()
+	}
+	selp, err := action.Sign(elp, reclaimerPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign reclaim stake %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedChangeCandidate returns a signed change candidate
+func SignedChangeCandidate(
+	nonce uint64,
+	candName string,
+	bucketIndex uint64,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	stakerPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	cc, err := action.NewChangeCandidate(nonce, candName, bucketIndex, payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(cc).Build()
+	selp, err := action.Sign(elp, stakerPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign change candidate %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedTransferStake returns a signed transfer stake
+func SignedTransferStake(
+	nonce uint64,
+	voterAddress string,
+	bucketIndex uint64,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	stakerPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	ts, err := action.NewTransferStake(nonce, voterAddress, bucketIndex, payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(ts).Build()
+	selp, err := action.Sign(elp, stakerPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign transfer stake %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedDepositToStake returns a signed deposit to stake
+func SignedDepositToStake(
+	nonce uint64,
+	index uint64,
+	amount string,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	depositorPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	ds, err := action.NewDepositToStake(nonce, index, amount, payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(ds).Build()
+	selp, err := action.Sign(elp, depositorPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign deposit to stake %v", elp)
+	}
+	return selp, nil
+}
+
+// SignedRestake returns a signed restake
+func SignedRestake(
+	nonce uint64,
+	index uint64,
+	duration uint32,
+	autoStake bool,
+	payload []byte,
+	gasLimit uint64,
+	gasPrice *big.Int,
+	restakerPriKey crypto.PrivateKey,
+) (action.SealedEnvelope, error) {
+	rs, err := action.NewRestake(nonce, index, duration, autoStake, payload, gasLimit, gasPrice)
+	if err != nil {
+		return action.SealedEnvelope{}, err
+	}
+	bd := &action.EnvelopeBuilder{}
+	elp := bd.SetNonce(nonce).
+		SetGasPrice(gasPrice).
+		SetGasLimit(gasLimit).
+		SetAction(rs).Build()
+	selp, err := action.Sign(elp, restakerPriKey)
+	if err != nil {
+		return action.SealedEnvelope{}, errors.Wrapf(err, "failed to sign restake %v", elp)
+	}
+	return selp, nil
+}
