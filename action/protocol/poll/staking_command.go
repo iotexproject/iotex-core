6f974c68a670481f06719780fbdc8cec181395c3
Yutong Pei <yutong@iotex.io>
2020-03-26 16:59:06 -0700

    [staking] support using stakingV2 in poll protocol

--- action/protocol/poll/staking_command.go
+++ action/protocol/poll/staking_command.go
@@ -8,9 +8,12 @@ package poll
 
 import (
 	"context"
+	"math/big"
+	"time"
 
 	"github.com/iotexproject/go-pkgs/hash"
 	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-election/util"
 	"github.com/pkg/errors"
 
 	"github.com/iotexproject/iotex-core/action"
@@ -22,17 +25,19 @@ import (
 )
 
 type stakingCommand struct {
-	addr        address.Address
-	hu          config.HeightUpgrade
-	stakingV1   Protocol
-	stakingV2   *staking.Protocol
-	candIndexer *CandidateIndexer
+	addr           address.Address
+	stakingV1      Protocol
+	stakingV2      *staking.Protocol
+	candIndexer    *CandidateIndexer
+	slasher        *Slasher
+	scoreThreshold *big.Int
 }
 
 // NewStakingCommand creates a staking command center to manage staking committee and new native staking
 func NewStakingCommand(
-	hu config.HeightUpgrade,
 	candIndexer *CandidateIndexer,
+	sh *Slasher,
+	scoreThreshold *big.Int,
 	stkV1 Protocol,
 	stkV2 *staking.Protocol,
 ) (Protocol, error) {
@@ -43,7 +48,6 @@ func NewStakingCommand(
 	}
 
 	sc := stakingCommand{
-		hu:          hu,
 		addr:        addr,
 		stakingV1:   stkV1,
 		stakingV2:   stkV2,
@@ -57,18 +61,18 @@ func NewStakingCommand(
 }
 
 func (sc *stakingCommand) CreateGenesisStates(ctx context.Context, sm protocol.StateManager) error {
+	// if v1 exists, bootstrap from v1 only
 	if sc.stakingV1 != nil {
-		if err := sc.stakingV1.CreateGenesisStates(ctx, sm); err != nil {
-			return err
-		}
+		return sc.stakingV1.CreateGenesisStates(ctx, sm)
 	}
 
-	if sc.stakingV2 != nil {
-		if err := sc.stakingV2.CreateGenesisStates(ctx, sm); err != nil {
-			return err
-		}
+	cands, err := sc.stakingV2.ActiveCandidates(ctx)
+	if err != nil {
+		return err
 	}
-	return nil
+	bcCtx := protocol.MustGetBlockchainCtx(ctx)
+	cands = sc.filterAndSortCandidatesByVoteScore(cands, bcCtx.Tip.Timestamp)
+	return setCandidates(ctx, sm, sc.candIndexer, cands, uint64(1))
 }
 
 func (sc *stakingCommand) Start(ctx context.Context) error {
@@ -81,8 +85,7 @@ func (sc *stakingCommand) Start(ctx context.Context) error {
 }
 
 func (sc *stakingCommand) CreatePreStates(ctx context.Context, sm protocol.StateManager) error {
-	// TODO: handle V2
-	return sc.stakingV1.CreateGenesisStates(ctx, sm)
+	return sc.slasher.CreatePreStates(ctx, sm, sc.candIndexer)
 }
 
 func (sc *stakingCommand) CreatePostSystemActions(ctx context.Context) ([]action.Envelope, error) {
@@ -90,20 +93,14 @@ func (sc *stakingCommand) CreatePostSystemActions(ctx context.Context) ([]action
 }
 
 func (sc *stakingCommand) Handle(ctx context.Context, act action.Action, sm protocol.StateManager) (*action.Receipt, error) {
-	if sc.stakingV1 == nil {
-		return handle(ctx, act, sm, sc.candIndexer, sc.addr.String())
-	}
-
-	if sc.stakingV2 == nil {
-		return sc.stakingV1.Handle(ctx, act, sm)
-	}
-
 	// transition to V2 starting Fairbank
 	height, err := sm.Height()
 	if err != nil {
 		return nil, err
 	}
-	if sc.hu.IsPost(config.Fairbank, height) {
+	bcCtx := protocol.MustGetBlockchainCtx(ctx)
+	hu := config.NewHeightUpgrade(&bcCtx.Genesis)
+	if sc.stakingV1 == nil || hu.IsPost(config.Fairbank, height) {
 		return handle(ctx, act, sm, sc.candIndexer, sc.addr.String())
 	}
 	return sc.stakingV1.Handle(ctx, act, sm)
@@ -114,47 +111,56 @@ func (sc *stakingCommand) Validate(ctx context.Context, act action.Action) error
 }
 
 func (sc *stakingCommand) CalculateCandidatesByHeight(ctx context.Context, height uint64) (state.CandidateList, error) {
-	if sc.stakingV1 == nil {
-		return sc.stakingV2.ActiveCandidates(ctx)
-	}
-
-	if sc.stakingV2 == nil {
-		return sc.stakingV1.CalculateCandidatesByHeight(ctx, height)
-	}
-
 	// transition to V2 starting Fairbank
-	if sc.hu.IsPost(config.Fairbank, height) {
-		return sc.stakingV2.ActiveCandidates(ctx)
+	bcCtx := protocol.MustGetBlockchainCtx(ctx)
+	hu := config.NewHeightUpgrade(&bcCtx.Genesis)
+	if sc.stakingV1 == nil || hu.IsPost(config.Fairbank, height) {
+		cands, err := sc.stakingV2.ActiveCandidates(ctx)
+		if err != nil {
+			return cands, err
+		}
+		bcCtx := protocol.MustGetBlockchainCtx(ctx)
+		return sc.filterAndSortCandidatesByVoteScore(cands, bcCtx.Tip.Timestamp), nil
 	}
 	return sc.stakingV1.CalculateCandidatesByHeight(ctx, height)
 }
 
 // Delegates returns exact number of delegates of current epoch
 func (sc *stakingCommand) Delegates(ctx context.Context, sr protocol.StateReader) (state.CandidateList, error) {
-	// TODO: handle V2
-	return sc.stakingV1.Delegates(ctx, sr)
+	return sc.slasher.GetActiveBlockProducers(ctx, sr, false)
 }
 
 // NextDelegates returns exact number of delegates of next epoch
 func (sc *stakingCommand) NextDelegates(ctx context.Context, sr protocol.StateReader) (state.CandidateList, error) {
-	// TODO: handle V2
-	return sc.stakingV1.Delegates(ctx, sr)
+	return sc.slasher.GetActiveBlockProducers(ctx, sr, true)
 }
 
 // Candidates returns candidate list from state factory of current epoch
 func (sc *stakingCommand) Candidates(ctx context.Context, sr protocol.StateReader) (state.CandidateList, error) {
-	// TODO: handle V2
-	return sc.stakingV1.Candidates(ctx, sr)
+	return sc.slasher.GetCandidates(ctx, sr, false)
 }
 
 // NextCandidates returns candidate list from state factory of next epoch
 func (sc *stakingCommand) NextCandidates(ctx context.Context, sr protocol.StateReader) (state.CandidateList, error) {
-	// TODO: handle V2
-	return sc.stakingV1.NextCandidates(ctx, sr)
+	return sc.slasher.GetCandidates(ctx, sr, true)
 }
 
 func (sc *stakingCommand) ReadState(ctx context.Context, sr protocol.StateReader, method []byte, args ...[]byte) ([]byte, error) {
-	// TODO: handle V2
+	if sc.stakingV1 == nil {
+		return sc.slasher.ReadState(ctx, sr, sc.candIndexer, method, args...)
+	}
+	height, err := sr.Height()
+	if err != nil {
+		return nil, err
+	}
+	bcCtx := protocol.MustGetBlockchainCtx(ctx)
+	hu := config.NewHeightUpgrade(&bcCtx.Genesis)
+	if hu.IsPost(config.Fairbank, height) {
+		res, err := sc.slasher.ReadState(ctx, sr, sc.candIndexer, method, args...)
+		if err == nil {
+			return res, nil
+		}
+	}
 	return sc.stakingV1.ReadState(ctx, sr, method, args...)
 }
 
@@ -166,4 +172,22 @@ func (sc *stakingCommand) Register(r *protocol.Registry) error {
 // ForceRegister registers the protocol with a unique ID and force replacing the previous protocol if it exists
 func (sc *stakingCommand) ForceRegister(r *protocol.Registry) error {
 	return r.ForceRegister(protocolID, sc)
+}
+
+func (sc *stakingCommand) filterAndSortCandidatesByVoteScore(list state.CandidateList, ts time.Time) state.CandidateList {
+	candidates := make(map[string]*state.Candidate)
+	candidateScores := make(map[string]*big.Int)
+	for _, cand := range list {
+		if cand.Votes.Cmp(sc.scoreThreshold) >= 0 {
+			clone := cand.Clone()
+			candidates[string(clone.CanName)] = clone
+			candidateScores[string(clone.CanName)] = clone.Votes
+		}
+	}
+	sorted := util.Sort(candidateScores, uint64(ts.Unix()))
+	res := make(state.CandidateList, 0, len(sorted))
+	for _, name := range sorted {
+		res = append(res, candidates[name])
+	}
+	return res
 }
