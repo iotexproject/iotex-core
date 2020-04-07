873006ebdd1db713b7f752b1b2f24c0a5bc2d413
mhansen <mhs_shiki@163.com>
2020-04-07 16:28:31 +0800

    fix group

--- ioctl/cmd/action/stake2release.go
+++ ioctl/cmd/action/stake2release.go
@@ -9,11 +9,11 @@ import (
 	"encoding/hex"
 	"math/big"
 
-	"github.com/iotexproject/iotex-core/action"
+	"github.com/spf13/cobra"
 
+	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/ioctl/config"
 	"github.com/iotexproject/iotex-core/ioctl/output"
-	"github.com/spf13/cobra"
 )
 
 // Multi-language support
