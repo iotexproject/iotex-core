5d46690bb05a947c293fe655c974e988c16893e9
mhansen <mhs_shiki@163.com>
2020-04-07 16:26:08 +0800

    fix group

--- ioctl/cmd/action/stake2register.go
+++ ioctl/cmd/action/stake2register.go
@@ -9,13 +9,13 @@ package action
 import (
 	"encoding/hex"
 
-	"github.com/iotexproject/iotex-core/ioctl/util"
-	"github.com/iotexproject/iotex-core/ioctl/validator"
+	"github.com/spf13/cobra"
 
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/ioctl/config"
 	"github.com/iotexproject/iotex-core/ioctl/output"
-	"github.com/spf13/cobra"
+	"github.com/iotexproject/iotex-core/ioctl/util"
+	"github.com/iotexproject/iotex-core/ioctl/validator"
 )
 
 // Multi-language support
