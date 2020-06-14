cead8d19d8991eea8337f455ca5c211cf7544d93
frank <35358771+Frankonly@users.noreply.github.com>
2020-06-14 21:18:33 +0800

    group imports

--- ioctl/cmd/contract/contract_test.go
+++ ioctl/cmd/contract/contract_test.go
@@ -12,10 +12,8 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-
 	"github.com/ethereum/go-ethereum/accounts/abi"
-
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
 
