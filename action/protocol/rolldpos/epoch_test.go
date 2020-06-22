ba2c7eeeeea2eb8fed0d76bb14bcbbd8abaec0e7
Charm <qiuguochao001@gmail.com>
2020-06-22 17:55:11 +0800

    feat: Add unit test for rolldpos protocol Issue #1686 Closes #1686

--- action/protocol/rolldpos/epoch_test.go
+++ action/protocol/rolldpos/epoch_test.go
@@ -2,6 +2,7 @@ package rolldpos
 
 import (
 	"context"
+	"errors"
 	"strconv"
 	"testing"
 
@@ -236,5 +237,57 @@ func TestGetSubEpochNum(t *testing.T) {
 	for i, epochHeight := range epochHeights {
 		subEpochNum := p.GetSubEpochNum(epochHeight)
 		require.Equal(expectedSubEpochNums[i], subEpochNum)
+	}
+}
+
+func productivity(epochStartHeight uint64, epochEndHeight uint64) (map[string]uint64, error) {
+	return map[string]uint64{"ret": epochEndHeight - epochStartHeight}, nil
+}
+
+func TestProductivityByEpoch(t *testing.T) {
+	require := require.New(t)
+	p := NewProtocol(23, 4, 3)
+	epochNum := []uint64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	tipHeight := []uint64{1, 1, 12, 25, 38, 53, 59, 80, 90, 93, 120}
+
+	expectedHeights := []uint64{1, 1, 0, 1, 2, 5, 0, 8, 6, 0, 12}
+	var nilMap = map[string]uint64{}
+	nilMap = nil
+	expectedProduces := []map[string]uint64{
+		{"ret": 0},
+		{"ret": 0},
+		nilMap,
+		{"ret": 0},
+		{"ret": 1},
+		{"ret": 4},
+		nilMap,
+		{"ret": 7},
+		{"ret": 5},
+		nilMap,
+		{"ret": 11},
+	}
+	expectedErrors := []error{
+		nil,
+		nil,
+		errors.New("epoch number 2 is larger than current epoch number 1"),
+		nil,
+		nil,
+		nil,
+		errors.New("epoch number 6 is larger than current epoch number 5"),
+		nil,
+		nil,
+		errors.New("epoch number 9 is larger than current epoch number 8"),
+		nil,
+	}
+
+	for i := 0; i < len(epochNum); i++ {
+		retHeight, retProduce, retError := p.ProductivityByEpoch(epochNum[i], tipHeight[i], productivity)
+		require.Equal(retHeight, expectedHeights[i])
+		require.Equal(retProduce, expectedProduces[i])
+		if i == 2 || i == 6 || i == 9 {
+			require.EqualError(retError, expectedErrors[i].Error())
+		} else {
+			require.NoError(retError)
+		}
 	}
 }
