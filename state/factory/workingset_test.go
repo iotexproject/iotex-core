ca8002cb4962c660c466d7782fc6f6796a70bc88
Charm <qiuguochao001@gmail.com>
2020-07-03 17:19:31 +0800

    feat: Add unit test for workingset

---
+++ state/factory/workingset_test.go
@@ -0,0 +1,27 @@
+package factory
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/action/protocol"
+	"github.com/stretchr/testify/require"
+)
+
+
+func TestReadView(t *testing.T){
+	require := require.New(t)
+	sf, err := NewFactory(config.Default, InMemTrieOption())
+	require.NoError(err)
+	ctx := protocol.WithBlockchainCtx(
+		protocol.WithRegistry(context.Background(), protocol.NewRegistry()),
+		protocol.BlockchainCtx{Genesis: config.Default.Genesis},
+	)
+	require.NoError(sf.Start(ctx))
+	ws, err := sf.(workingSetCreator).newWorkingSet(ctx, 1)
+	retrdf,retErr := ws.ReadView("")
+	expectedErrors := errors.New("name : name does not exist")
+	require.EqualError(retErr, expectedErrors.Error())
+	require.Equal(retrdf, nil)
+}
