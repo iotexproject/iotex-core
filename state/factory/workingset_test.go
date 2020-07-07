ac86dea801da6020a0fcf1b9f360c5081fc705a4
Charm <qiuguochao001@gmail.com>
2020-07-07 16:16:24 +0800

    fmt code

--- state/factory/workingset_test.go
+++ state/factory/workingset_test.go
@@ -3,25 +3,23 @@ package factory
 import (
 	"context"
 	"errors"
+	"github.com/iotexproject/go-pkgs/hash"
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/action/protocol"
+	"github.com/iotexproject/iotex-core/blockchain/block"
+	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/pkg/unit"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/iotexproject/iotex-core/testutil"
+	"github.com/stretchr/testify/require"
 	"math/rand"
 	"testing"
-	"github.com/iotexproject/iotex-core/config"
-	"github.com/iotexproject/iotex-core/action/protocol"
-	"github.com/stretchr/testify/require"
-	"github.com/iotexproject/iotex-core/blockchain/block"
-	"github.com/iotexproject/iotex-core/test/identityset"
-	"github.com/iotexproject/go-pkgs/hash"
-	"github.com/iotexproject/iotex-core/testutil"
-	"github.com/iotexproject/iotex-core/action"
 	"time"
-	"github.com/iotexproject/iotex-core/pkg/unit"
-
 	//"github.com/iotexproject/iotex-proto/golang/iotextypes"
 	//"github.com/golang/protobuf/ptypes/timestamp"
 )
 
-
-func TestWriteReadView(t *testing.T){
+func TestWriteReadView(t *testing.T) {
 	require := require.New(t)
 	sf, err := NewFactory(config.Default, InMemTrieOption())
 	require.NoError(err)
@@ -33,35 +31,35 @@ func TestWriteReadView(t *testing.T){
 	ws, err := sf.(workingSetCreator).newWorkingSet(ctx, 1)
 
 	t.Run("ReadView call error", func(t *testing.T) {
-		retrdf,retErr := ws.ReadView("")
+		retrdf, retErr := ws.ReadView("")
 		expectedErrors := errors.New("name : name does not exist")
 		require.EqualError(retErr, expectedErrors.Error())
 		require.Equal(retrdf, nil)
 	})
 
 	t.Run("WriteView ReadView normal call ", func(t *testing.T) {
-		require.NoError(ws.WriteView("test","test"))
-		retrdf,retErr := ws.ReadView("test")
+		require.NoError(ws.WriteView("test", "test"))
+		retrdf, retErr := ws.ReadView("test")
 		require.NoError(retErr)
 		require.Equal(retrdf, "test")
 	})
 
 	t.Run("Unload call error", func(t *testing.T) {
-		retrdf,retErr := ws.Unload("")
+		retrdf, retErr := ws.Unload("")
 		expectedErrors := errors.New("name : name does not exist")
 		require.EqualError(retErr, expectedErrors.Error())
 		require.Equal(retrdf, nil)
 	})
 
 	t.Run("Load Unload normal call ", func(t *testing.T) {
-		require.NoError(ws.Load("test","test"))
-		retrdf,retErr := ws.Unload("test")
+		require.NoError(ws.Load("test", "test"))
+		retrdf, retErr := ws.Unload("test")
 		require.NoError(retErr)
 		require.Equal(retrdf, "test")
 	})
 }
 
-func TestProtocolDirty(t *testing.T){
+func TestProtocolDirty(t *testing.T) {
 	require := require.New(t)
 	sf, err := NewFactory(config.Default, InMemTrieOption())
 	require.NoError(err)
@@ -73,7 +71,7 @@ func TestProtocolDirty(t *testing.T){
 	ws, err := sf.(workingSetCreator).newWorkingSet(ctx, 1)
 	require.False(ws.ProtocolDirty("test"))
 
-	require.NoError(ws.Load("test","test"))
+	require.NoError(ws.Load("test", "test"))
 	require.True(ws.ProtocolDirty("test"))
 }
 
@@ -95,17 +93,17 @@ func TestValidateBlock(t *testing.T) {
 	require.NoError(sf.Start(zctx))
 
 	t.Run("normal call", func(t *testing.T) {
-		blk := makeBlock(t, 0,hash.ZeroHash256,hash.Hash256b([]byte{65,99,99,111,117,110,116,99,117,114,114,101,110,116,72,101,105,103,104,116,1,0,0,0,0,0,0,0}))
+		blk := makeBlock(t, 0, hash.ZeroHash256, hash.Hash256b([]byte{65, 99, 99, 111, 117, 110, 116, 99, 117, 114, 114, 101, 110, 116, 72, 101, 105, 103, 104, 116, 1, 0, 0, 0, 0, 0, 0, 0}))
 		require.NoError(sf.Validate(zctx, blk))
 	})
 
 	t.Run("nonce error", func(t *testing.T) {
-		blk := makeBlock(t, 1,hash.ZeroHash256,hash.Hash256b([]byte{65,99,99,111,117,110,116,99,117,114,114,101,110,116,72,101,105,103,104,116,1,0,0,0,0,0,0,0}))
+		blk := makeBlock(t, 1, hash.ZeroHash256, hash.Hash256b([]byte{65, 99, 99, 111, 117, 110, 116, 99, 117, 114, 114, 101, 110, 116, 72, 101, 105, 103, 104, 116, 1, 0, 0, 0, 0, 0, 0, 0}))
 		require.NoError(sf.Validate(zctx, blk))
 	})
 
 	t.Run("root hash error", func(t *testing.T) {
-		blk := makeBlock(t, 0,hash.Hash256b([]byte("test")),hash.Hash256b([]byte("test")))
+		blk := makeBlock(t, 0, hash.Hash256b([]byte("test")), hash.Hash256b([]byte("test")))
 		expectedErrors := errors.New("failed to validate block with workingset in factory:" +
 			" failed to verify delta state digest: delta state digest doesn't match, " +
 			"expected = 9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658, " +
@@ -114,7 +112,7 @@ func TestValidateBlock(t *testing.T) {
 	})
 
 	t.Run("delta state digest error", func(t *testing.T) {
-		blk := makeBlock(t, 0,hash.ZeroHash256,hash.Hash256b([]byte("test")))
+		blk := makeBlock(t, 0, hash.ZeroHash256, hash.Hash256b([]byte("test")))
 		expectedErrors := errors.New("failed to validate block with workingset in factory: " +
 			"failed to verify delta state digest: delta state digest doesn't match," +
 			" expected = 9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658, " +
@@ -123,7 +121,6 @@ func TestValidateBlock(t *testing.T) {
 	})
 
 }
-
 
 func makeBlock(tb testing.TB, nonce uint64, rootHash hash.Hash256, digest hash.Hash256) *block.Block {
 	rand.Seed(time.Now().Unix())
