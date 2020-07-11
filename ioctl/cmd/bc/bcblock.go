852ad8197d86b7a5727bf2e729d0c6eda6c4e80c
mhansen <mhs_shiki@163.com>
2020-07-11 14:24:24 +0800

    `ioctl bc block` returns all actions with `--verbose` flag

--- ioctl/cmd/bc/bcblock.go
+++ ioctl/cmd/bc/bcblock.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc/status"
@@ -24,6 +25,10 @@ import (
 	"github.com/iotexproject/iotex-core/ioctl/validator"
 )
 
+var (
+	verbose bool
+)
+
 // Multi-language support
 var (
 	bcBlockCmdShorts = map[config.Language]string{
@@ -31,8 +36,12 @@ var (
 		config.Chinese: "获取IoTeX区块链中的区块",
 	}
 	bcBlockCmdUses = map[config.Language]string{
-		config.English: "block [HEIGHT|HASH]",
-		config.Chinese: "block [高度|哈希]",
+		config.English: "block [HEIGHT|HASH] [--verbose]",
+		config.Chinese: "block [高度|哈希] [--verbose]",
+	}
+	flagVerboseUsage = map[config.Language]string{
+		config.English: "returns block info and all actions within this block.",
+		config.Chinese: "返回区块信息和区块内的所有事务",
 	}
 )
 
@@ -40,7 +49,7 @@ var (
 var bcBlockCmd = &cobra.Command{
 	Use:   config.TranslateInLang(bcBlockCmdUses, config.UILanguage),
 	Short: config.TranslateInLang(bcBlockCmdShorts, config.UILanguage),
-	Args:  cobra.MaximumNArgs(1),
+	Args:  cobra.MaximumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cmd.SilenceUsage = true
 		err := getBlock(args)
@@ -48,14 +57,30 @@ var bcBlockCmd = &cobra.Command{
 	},
 }
 
+func init() {
+	bcBlockCmd.Flags().BoolVar(&verbose, "verbose", false, config.TranslateInLang(flagVerboseUsage, config.UILanguage))
+}
+
 type blockMessage struct {
-	Node  string                `json:"node"`
-	Block *iotextypes.BlockMeta `json:"block"`
+	Node    string                `json:"node"`
+	Block   *iotextypes.BlockMeta `json:"block"`
+	Actions []actionInfo          `json:actions`
+}
+
+type actionsInfoMessage struct {
+	ActionInfo []actionInfo `json:"actions"`
+}
+
+type actionInfo struct {
+	ActHash   string               `protobuf:"bytes,2,opt,name=actHash,proto3" json:"actHash,omitempty"`
+	Sender    string               `protobuf:"bytes,6,opt,name=sender,proto3" json:"sender,omitempty"`
+	GasFee    string               `protobuf:"bytes,7,opt,name=gasFee,proto3" json:"gasFee,omitempty"`
+	Timestamp *timestamp.Timestamp `protobuf:"bytes,4,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
 }
 
 func (m *blockMessage) String() string {
 	if output.Format == "" {
-		message := fmt.Sprintf("Blockchain Node: %s\n%s", m.Node, output.JSONString(m.Block))
+		message := fmt.Sprintf("Blockchain Node: %s\n%s\n%s", m.Node, output.JSONString(m.Block), output.JSONString(m.Actions))
 		return message
 	}
 	return output.FormatString(output.Result, m)
@@ -81,6 +106,7 @@ func getBlock(args []string) error {
 		height = chainMeta.Height
 	}
 	var blockMeta *iotextypes.BlockMeta
+	var actionInfos []*iotexapi.ActionInfo
 	if isHeight {
 		blockMeta, err = getBlockMetaByHeight(height)
 	} else {
@@ -89,9 +115,56 @@ func getBlock(args []string) error {
 	if err != nil {
 		return output.NewError(0, "failed to get block meta", err)
 	}
-	message := blockMessage{Node: config.ReadConfig.Endpoint, Block: blockMeta}
-	fmt.Println(message.String())
+	blockInfoMessage := blockMessage{Node: config.ReadConfig.Endpoint, Block: blockMeta, Actions: nil}
+	if verbose {
+		actionInfos, err = getActionInfoByBlock(blockMeta.Hash, blockMeta.Height, uint64(blockMeta.NumActions))
+		if err != nil {
+			return output.NewError(0, "failed to get actions info", err)
+		}
+		for _, ele := range actionInfos {
+			actionInfo := actionInfo{
+				ActHash:   ele.ActHash,
+				Sender:    ele.Sender,
+				GasFee:    ele.GasFee,
+				Timestamp: ele.Timestamp,
+			}
+			blockInfoMessage.Actions = append(blockInfoMessage.Actions, actionInfo)
+		}
+	}
+	fmt.Println(blockInfoMessage.String())
 	return nil
+}
+
+// getActionInfoByBlock gets action info by block hash with start index and action count
+func getActionInfoByBlock(hash string, height uint64, count uint64) ([]*iotexapi.ActionInfo, error) {
+	conn, err := util.ConnectToEndpoint(config.ReadConfig.SecureConnect && !config.Insecure)
+	if err != nil {
+		return nil, output.NewError(output.NetworkError, "failed to connect to endpoint", err)
+	}
+	defer conn.Close()
+	cli := iotexapi.NewAPIServiceClient(conn)
+	//request := iotexapi.GetActionsRequest{Lookup: &iotexapi.GetActionsRequest_ByBlk{ByBlk: &iotexapi.GetActionsByBlockRequest{BlkHash: hash, Start: height, Count: count}}}
+	request := iotexapi.GetActionsRequest{Lookup: &iotexapi.GetActionsRequest_ByIndex{ByIndex: &iotexapi.GetActionsByIndexRequest{Start: height, Count: count}}}
+	ctx := context.Background()
+
+	jwtMD, err := util.JwtAuth()
+	if err == nil {
+		ctx = metautils.NiceMD(jwtMD).ToOutgoing(ctx)
+	}
+
+	response, err := cli.GetActions(ctx, &request)
+	if err != nil {
+		sta, ok := status.FromError(err)
+		if ok {
+			return nil, output.NewError(output.APIError, sta.Message(), nil)
+		}
+		return nil, output.NewError(output.NetworkError, "failed to invoke GetActions api", err)
+	}
+	if len(response.ActionInfo) == 0 {
+		return nil, output.NewError(output.APIError, "no actions returned", err)
+	}
+	return response.ActionInfo, nil
+
 }
 
 // getBlockMetaByHeight gets block metadata by height
