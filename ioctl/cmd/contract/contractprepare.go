6817686323fd7b3d0ed6e06369b854a9ea4a978c
mhansen <mhs_shiki@163.com>
2020-07-12 13:35:20 +0800

    ioctl contract prepare cmd needs to install solidity version 0.5+

--- ioctl/cmd/contract/contractprepare.go
+++ ioctl/cmd/contract/contractprepare.go
@@ -51,15 +51,13 @@ func prepare() error {
 
 		err = cmd.Run()
 		if err != nil {
-			installGuide := "https://solidity.readthedocs.io/en/v0.4.25/installing-solidity.html"
-			return output.NewError(output.UpdateError, fmt.Sprintf("\nfailed to prepare solc\n\n"+
-				"you can install solidity 0.4.25 manually following:\n%s\n", installGuide), nil)
+			return output.NewError(output.UpdateError, "failed to prepare solc", nil)
 		}
 	}
 
 	if !checkCompilerVersion(solc) {
 		return output.NewError(output.CompilerError,
-			fmt.Sprintf("unsupported solc version %d.%d.%d, expects solc version ^0.4.24",
+			fmt.Sprintf("unsupported solc version %d.%d.%d, expects solc version 0.5.17",
 				solc.Major, solc.Minor, solc.Patch), nil)
 	}
 
