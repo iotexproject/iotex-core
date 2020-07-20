4c09c2299c074613a2780be2d898da47da32c363
gyzhang <newyear6565@163.com>
2020-07-20 23:02:32 +0800

    Add test for api/blocklistener.go (#1581)

---
+++ api/blocklistener_test.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2019 IoTeX Foundation
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package api
+
+import (
+	"testing"
+	"time"
+	"errors"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/blockchain/block"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/iotexproject/iotex-core/test/mock/mock_apiserver"
+)
+
+var (
+	sendError error = errors.New("send error")
+)
+
+func TestBlockListener(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	errChan := make(chan error, 10)
+
+	server := mock_apiserver.NewMockAPIService_StreamBlocksServer(ctrl)
+	responder := NewBlockListener(server, errChan)
+
+	receipts := []*action.Receipt{
+		{
+			BlockHeight: 1,
+		},
+		{
+			BlockHeight: 2,
+		},
+	}
+	builder := block.NewTestingBuilder()
+	builder.SetHeight(1)
+	builder.SetVersion(111)
+	builder.SetTimeStamp(time.Now())
+	builder.SetReceipts(receipts)
+	testBlock, _ := builder.SignAndBuild(identityset.PrivateKey(0))
+
+	server.EXPECT().Send(gomock.Any()).Return(nil).Times(1)
+	err := responder.Respond(&testBlock)
+	require.NoError(t, err)
+
+	server.EXPECT().Send(gomock.Any()).Return(sendError).Times(1)
+	err = responder.Respond(&testBlock)
+	require.Error(t, err)
+	require.Equal(t, sendError, err)
+
+	responder.Exit()
+
+	err = <-errChan
+	require.Error(t, err)
+	require.Equal(t, sendError, err)
+
+	err = <-errChan
+	require.NoError(t, err)
+}
+
+
--- misc/scripts/mockgen.sh
+++ misc/scripts/mockgen.sh
@@ -128,3 +128,9 @@ mockgen -destination=./test/mock/mock_apiresponder/mock_apiresponder.go  \
         -source=./api/responder.go \
         -package=mock_apiresponder \
         Responder
+
+mkdir -p ./test/mock/mock_apiserver
+mockgen -destination=./test/mock/mock_apiserver/mock_apiserver.go  \
+        -source=./api/apitestserver.go \
+        -package=mock_apiserver \
+        APIService_StreamBlocksServer
