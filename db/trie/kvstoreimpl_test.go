1715d12d497f1027e1850765b10dd8ea497cec35
JonSnow47 <745539141@qq.com>
2020-07-23 11:44:04 +0800

    test trie package (#1897) (#2308)
    * test leafiterator
    
    test faild
    cause: https://github.com/iotexproject/iotex-core/blob/master/db/trie/mptrie/leafiterator.go#L50
    detail:
    === RUN   TestIterator
        TestIterator: leafiterator_test.go:55:
            	Error Trace:	leafiterator_test.go:55
            	Error:      	Not equal:
            	            	expected: "coin"
            	            	actual  : "coincoin"
    
            	            	Diff:
            	            	--- Expected
            	            	+++ Actual
            	            	@@ -1 +1 @@
            	            	-coin
            	            	+coincoin
            	Test:       	TestIterator
            	Messages:   	key: iotex
    --- FAIL: TestIterator (0.00s)
    
    * test: MPT functions and benchmark
    
    * delete benchmark test data
    
    Co-authored-by: CoderZhi <thecoderzhi@gmail.com>

--- db/trie/kvstoreimpl_test.go
+++ db/trie/kvstoreimpl_test.go
@@ -15,14 +15,79 @@ import (
 	"github.com/iotexproject/iotex-core/db"
 )
 
-func TestKVStore_ErrNotExist(t *testing.T) {
+func TestKVStore_Get(t *testing.T) {
+	var (
+		require = require.New(t)
+		tests   = []struct{ k, v []byte }{
+			{[]byte("key"), []byte("value")},
+			{[]byte("k"), []byte("v")},
+			{[]byte("iotex"), []byte("coin")},
+			{[]byte("puppy"), []byte("dog")},
+		}
+	)
+
 	store, err := NewKVStore("test", db.NewMemKVStore())
-	require.NoError(t, err)
-	require.NoError(t, store.Put([]byte("key"), []byte("value1")))
-	require.NoError(t, store.Delete([]byte("key")))
-	_, err = store.Get([]byte("key"))
-	require.Equal(t, ErrNotExist, errors.Cause(err))
-	// A non-existing key should return ErrNotExist too
-	_, err = store.Get([]byte("key1"))
-	require.Equal(t, ErrNotExist, errors.Cause(err))
+	require.NoError(err)
+
+	for _, test := range tests {
+		require.NoError(store.Put(test.k, test.v))
+	}
+	for _, test := range tests {
+		val, err := store.Get(test.k)
+		require.NoError(err)
+		require.Equal(test.v, val)
+	}
+}
+
+func TestKvStore_Put(t *testing.T) {
+	var (
+		require = require.New(t)
+		key     = []byte("key")
+		tests   = []struct{ k, v []byte }{
+			{key, []byte("value1")},
+			{key, []byte("value2")},
+			{key, []byte("value3")},
+		}
+	)
+
+	store, err := NewKVStore("test", db.NewMemKVStore())
+	require.NoError(err)
+
+	for _, test := range tests {
+		require.NoError(store.Put(test.k, test.v))
+	}
+	val, err := store.Get(key)
+	require.NoError(err)
+	require.Equal(tests[len(tests)-1].v, val)
+}
+
+func TestKvStore_Delete(t *testing.T) {
+	var (
+		require = require.New(t)
+		tests   = []struct{ k, v []byte }{
+			{[]byte("key"), []byte("value")},
+			{[]byte("k"), []byte("v")},
+			{[]byte("iotex"), []byte("coin")},
+			{[]byte("puppy"), []byte("dog")},
+		}
+	)
+
+	store, err := NewKVStore("test", db.NewMemKVStore())
+	require.NoError(err)
+
+	for _, test := range tests {
+		_, err = store.Get(test.k)
+		require.Equal(ErrNotExist, errors.Cause(err))
+
+		require.NoError(store.Put(test.k, test.v))
+	}
+	for _, test := range tests {
+		val, err := store.Get(test.k)
+		require.NoError(err)
+		require.Equal(test.v, val)
+
+		require.NoError(store.Delete(test.k))
+		_, err = store.Get(test.k)
+		require.Equal(ErrNotExist, errors.Cause(err))
+	}
 }
--- db/trie/mptrie/merklepatriciatrie_benchmark_test.go
+++ db/trie/mptrie/merklepatriciatrie_benchmark_test.go
@@ -2,89 +2,92 @@ package mptrie
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"testing"
 
-	"github.com/iotexproject/go-pkgs/hash"
+	"github.com/stretchr/testify/require"
+
 	"github.com/iotexproject/iotex-core/config"
 	"github.com/iotexproject/iotex-core/db"
 	"github.com/iotexproject/iotex-core/db/batch"
 	"github.com/iotexproject/iotex-core/db/trie"
 	"github.com/iotexproject/iotex-core/testutil"
-	"github.com/pkg/errors"
-	"github.com/stretchr/testify/require"
 )
 
-// new version
-func benchmarkTrie(async bool, b *testing.B) {
-	if testing.Short() {
-		b.Skip("Skipping TestPressure in short mode.")
-	}
-	require := require.New(b)
-	testPath, err := testutil.PathOfTempFile(fmt.Sprintf("test-kv-store-%t.bolt", async))
-	require.NoError(err)
-	cfg := config.Default.DB
-	cfg.DbPath = testPath
-	dao := db.NewBoltDB(cfg)
-	flusher, err := db.NewKVStoreFlusher(dao, batch.NewCachedBatch())
-	flusherKV := flusher.KVStoreWithBuffer()
-	kv, err := trie.NewKVStore("test", flusherKV)
-	require.NoError(err)
-	require.NoError(kv.Start(context.Background()))
-	var tr trie.Trie
+func BenchmarkTrie_Get(b *testing.B)            { benchTrieGet(b, false, false) }
+func BenchmarkTrie_GetWithAsync(b *testing.B)   { benchTrieGet(b, true, false) }
+func BenchmarkTrie_GetDB(b *testing.B)          { benchTrieGet(b, false, true) }
+func BenchmarkTrie_GetDBWithAsync(b *testing.B) { benchTrieGet(b, true, true) }
+func BenchmarkTrie_UpsertLE(b *testing.B)       { benchTrieUpsert(b, binary.LittleEndian) }
+func BenchmarkTrie_UpsertBE(b *testing.B)       { benchTrieUpsert(b, binary.BigEndian) }
+
+const (
+	benchElemCount = 20000
+	keyLength      = 32
+)
+
+func benchTrieGet(b *testing.B, async, withDB bool) {
+	var (
+		require = require.New(b)
+		opts    = []Option{KeyLengthOption(keyLength)}
+		flush   func() error
+	)
 	if async {
-		tr, err = New(KVStoreOption(kv), KeyLengthOption(20), AsyncOption())
-	} else {
-		tr, err = New(KVStoreOption(kv), KeyLengthOption(20))
+		opts = append(opts, AsyncOption())
 	}
+	if withDB {
+		testPath, err := testutil.PathOfTempFile(fmt.Sprintf("test-kv-store-%t.bolt", async))
+		require.NoError(err)
+		cfg := config.Default.DB
+		cfg.DbPath = testPath
+		dao := db.NewBoltDB(cfg)
+		flusher, err := db.NewKVStoreFlusher(dao, batch.NewCachedBatch())
+		flusherKV := flusher.KVStoreWithBuffer()
+		flush = flusher.Flush
+		kvStore, err := trie.NewKVStore("test", flusherKV)
+		require.NoError(err)
+		require.NoError(kvStore.Start(context.Background()))
+		opts = append(opts, KVStoreOption(kvStore))
+	}
+	tr, err := New(opts...)
 	require.NoError(err)
 	require.NoError(tr.Start(context.Background()))
-	seed := 0
+	defer require.NoError(tr.Stop(context.Background()))
+
+	key := make([]byte, keyLength)
+	for i := 0; i < benchElemCount; i++ {
+		binary.LittleEndian.PutUint64(key, uint64(i))
+		require.NoError(tr.Upsert(key, key))
+	}
+	binary.LittleEndian.PutUint64(key, uint64(benchElemCount)/2)
+	if withDB {
+		require.NoError(flush())
+	}
+
 	b.ResetTimer()
-	// insert 128k entries
-	var k [32]byte
-	k[0] = byte(seed)
-	c := 0
-	for c = 0; c < b.N<<2; c++ {
-		k = hash.Hash256b(k[:])
-		v := testV[k[0]&7]
-		if _, err := tr.Get(k[:20]); err == nil {
-			b.Logf("Warning: collision on k %x", k[:20])
-			break
-		}
-		require.NoError(tr.Upsert(k[:20], v))
-		vb, err := tr.Get(k[:20])
-		require.NoError(err)
-		require.Equal(v, vb)
+	for i := 0; i < b.N; i++ {
+		tr.Get(key)
 	}
-	_, err = tr.RootHash()
-	require.NoError(err)
-	require.False(tr.IsEmpty())
-	// delete 128k entries
-	var d [32]byte
-	d[0] = byte(seed)
-	for i := 0; i < c; i++ {
-		d = hash.Hash256b(d[:])
-		if i%2 == 0 {
-			require.NoError(tr.Delete(d[:20]))
-			_, err = tr.Get(d[:20])
-			require.Equal(trie.ErrNotExist, errors.Cause(err))
-		}
-	}
-	_, err = tr.RootHash()
-	require.NoError(err)
-	b.Log("upsert times:", b.N<<2, "buffer size:", flusherKV.Size())
 	b.StopTimer()
-	require.NoError(flusher.Flush())
-	require.NoError(tr.Stop(context.Background()))
-	require.NoError(kv.Stop(context.Background()))
+	b.ReportAllocs()
 }
 
-func BenchmarkTrie(b *testing.B) {
-	b.Run("without-async", func(b *testing.B) {
-		benchmarkTrie(false, b)
-	})
-	b.Run("with-async", func(b *testing.B) {
-		benchmarkTrie(true, b)
-	})
+func benchTrieUpsert(b *testing.B, e binary.ByteOrder) {
+	var (
+		require = require.New(b)
+		opts    = []Option{KeyLengthOption(keyLength)}
+	)
+	trie, err := New(opts...)
+	require.NoError(err)
+	require.NoError(trie.Start(context.Background()))
+	defer require.NoError(trie.Stop(context.Background()))
+	k := make([]byte, keyLength)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		e.PutUint64(k, uint64(i))
+		trie.Upsert(k, k)
+	}
+	b.StopTimer()
+	b.ReportAllocs()
 }
