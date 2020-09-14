6614dc4b82b90bfa2e6dc7653e89646e0099fb54
lzxm160 <lzxm160@sina.com>
2020-09-14 22:28:57 +0800

    fix comments

--- action/protocol/staking/protocol.go
+++ action/protocol/staking/protocol.go
@@ -8,9 +8,10 @@ package staking
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 	"time"
+
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/pkg/errors"
@@ -73,7 +74,7 @@ type (
 		hu                 config.HeightUpgrade
 		candBucketsIndexer *CandidatesBucketsIndexer
 		voteReviser        *VoteReviser
-		enableArchiveMode  bool
+		archiveMode        bool
 	}
 
 	// Configuration is the staking protocol configuration.
@@ -90,7 +91,7 @@ type (
 )
 
 // NewProtocol instantiates the protocol of staking
-func NewProtocol(depositGas DepositGas, cfg genesis.Staking, candBucketsIndexer *CandidatesBucketsIndexer, enableArchiveMode bool, reviseHeights ...uint64) (*Protocol, error) {
+func NewProtocol(depositGas DepositGas, cfg genesis.Staking, candBucketsIndexer *CandidatesBucketsIndexer, archiveMode bool, reviseHeights ...uint64) (*Protocol, error) {
 	h := hash.Hash160b([]byte(protocolID))
 	addr, err := address.FromBytes(h[:])
 	if err != nil {
@@ -130,7 +131,7 @@ func NewProtocol(depositGas DepositGas, cfg genesis.Staking, candBucketsIndexer 
 		depositGas:         depositGas,
 		candBucketsIndexer: candBucketsIndexer,
 		voteReviser:        voteReviser,
-		enableArchiveMode:  enableArchiveMode,
+		archiveMode:        archiveMode,
 	}, nil
 }
 
@@ -217,8 +218,10 @@ func (p *Protocol) CreatePreStates(ctx context.Context, sm protocol.StateManager
 	bcCtx := protocol.MustGetBlockchainCtx(ctx)
 	blkCtx := protocol.MustGetBlockCtx(ctx)
 	hu := config.NewHeightUpgrade(&bcCtx.Genesis)
-	if p.enableArchiveMode && p.hu.IsPost(config.Fairbank, blkCtx.BlockHeight) && p.hu.IsPre(config.Greenland, blkCtx.BlockHeight) {
-		p.saveStakingAddressHistory(blkCtx.BlockHeight, sm)
+	if p.archiveMode && p.hu.IsPre(config.Greenland, blkCtx.BlockHeight) {
+		if err := p.saveStakingAddressHistory(blkCtx.BlockHeight, sm); err != nil {
+			return err
+		}
 	}
 	if blkCtx.BlockHeight == hu.GreenlandBlockHeight() {
 		csr, err := ConstructBaseView(sm)
@@ -255,18 +258,19 @@ func (p *Protocol) CreatePreStates(ctx context.Context, sm protocol.StateManager
 	return p.handleStakingIndexer(rp.GetEpochHeight(currentEpochNum-1), sm)
 }
 
-func (p *Protocol) saveStakingAddressHistory(height uint64, sm protocol.StateManager) {
+func (p *Protocol) saveStakingAddressHistory(height uint64, sm protocol.StateManager) error {
 	csr, err := ConstructBaseView(sm)
 	if err != nil {
-		return
+		return err
 	}
 	balance := csr.BaseView().bucketPool.total
 	if balance.amount.Sign() <= 0 {
-		return
+		return nil
 	}
-	hei := []byte(fmt.Sprintf("%d", height))
+	hei := byteutil.Uint64ToBytesBigEndian(height - 1)
 	historyKey := append(bucketPoolAddrKey, hei...)
-	sm.PutState(balance, protocol.NamespaceOption(StakingNameSpace), protocol.KeyOption(historyKey))
+	_, err = sm.PutState(balance, protocol.NamespaceOption(StakingNameSpace), protocol.KeyOption(historyKey))
+	return err
 }
 
 func (p *Protocol) handleStakingIndexer(epochStartHeight uint64, sm protocol.StateManager) error {
@@ -462,8 +466,9 @@ func (p *Protocol) ReadState(ctx context.Context, sr protocol.StateReader, metho
 	case iotexapi.ReadStakingDataMethod_CANDIDATE_BY_ADDRESS:
 		resp, height, err = readStateCandidateByAddress(ctx, csr, r.GetCandidateByAddress())
 	case iotexapi.ReadStakingDataMethod_TOTAL_STAKING_AMOUNT:
-		resp, height, err = readStateTotalStakingAmountFromHeight(csr, r.GetTotalStakingAmount(), inputHeight)
-		if inputHeight == 0 || err != nil {
+		if p.archiveMode && p.hu.IsPre(config.Greenland, height) {
+			resp, height, err = readStateTotalStakingAmountFromHeight(csr, r.GetTotalStakingAmount(), inputHeight)
+		} else {
 			resp, height, err = readStateTotalStakingAmount(ctx, csr, r.GetTotalStakingAmount())
 		}
 		log.L().Info("ReadStakingDataMethod_TOTAL_STAKING_AMOUNT", zap.Uint64("csr height", csr.Height()), zap.Uint64("returned height", height))
