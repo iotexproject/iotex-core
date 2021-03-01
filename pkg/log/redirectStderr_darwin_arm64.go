5c8f449f8a6c8ce746d1c82870d760856985b241
millken <millken@gmail.com>
2021-03-01 00:28:16 -0800

    fixed cant compile in mac m1(go1.16 darwin arm64) (#2576)

--- pkg/log/redirectStderr_darwin_arm64.go
+++ pkg/log/redirectStderr_darwin_arm64.go
@@ -1,10 +1,11 @@
-// Copyright (c) 2019 IoTeX Foundation
+// Copyright (c) 2020 IoTeX Foundation
 // This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
 // warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
-//+build arm arm64,!darwin
+//go:build arm64 && darwin
+// +build arm64,darwin
 
 package log
 
@@ -16,7 +17,7 @@ import (
 
 // redirectStderr to the file passed in
 func redirectStderr(f *os.File) error {
-	err := syscall.Dup3(int(f.Fd()), 2, 0)
+	err := syscall.Dup2(int(f.Fd()), 2)
 	if err != nil {
 		log.Fatalf("Failed to redirect stderr to file: %v", err)
 		return err
