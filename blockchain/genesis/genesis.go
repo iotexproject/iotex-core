f0da604c79dbb19eee81a829a74e94ba111d36a3
dustinxie <dahuaxie@gmail.com>
2021-05-10 23:24:06 -0700

    adjust Hawaii height (#2663)

--- blockchain/genesis/genesis.go
+++ blockchain/genesis/genesis.go
@@ -64,7 +64,7 @@ func defaultConfig() Genesis {
 			FbkMigrationBlockHeight: 5157001,
 			FairbankBlockHeight:     5165641,
 			GreenlandBlockHeight:    6544441,
-			HawaiiBlockHeight:       11271961,
+			HawaiiBlockHeight:       11267641,
 		},
 		Account: Account{
 			InitBalanceMap: make(map[string]string),
