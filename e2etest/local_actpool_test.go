fc40a1deda54c4fbef166e511aba9bfcf1b4bda0
zhi <thecoderzhi@gmail.com>
2021-06-07 13:55:52 -0700

    move network config into p2p

--- e2etest/local_actpool_test.go
+++ e2etest/local_actpool_test.go
@@ -51,7 +51,8 @@ func TestLocalActPool(t *testing.T) {
 	require.NoError(err)
 	cfg.Network.BootstrapNodes = []string{validNetworkAddr(addrs)}
 	cli := p2p.NewAgent(
-		cfg,
+		cfg.Network,
+		cfg.Genesis.Hash(),
 		func(_ context.Context, _ uint32, _ string, _ proto.Message) {
 
 		},
@@ -128,7 +129,8 @@ func TestPressureActPool(t *testing.T) {
 	require.NoError(err)
 	cfg.Network.BootstrapNodes = []string{validNetworkAddr(addrs)}
 	cli := p2p.NewAgent(
-		cfg,
+		cfg.Network,
+		cfg.Genesis.Hash(),
 		func(_ context.Context, _ uint32, _ string, _ proto.Message) {
 
 		},
