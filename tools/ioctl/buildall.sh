9ed9db9f8a609093eaa1b3bdaaf2e4924326db07
xianhuawei <xianhuawei@macair2018.local>
2021-07-20 20:51:31 +0800

    ioctl build script(linux/mac/windows)

---
+++ tools/ioctl/buildall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get install gcc-mingw-w64
+apt-get install gcc-multilib
+
+#windows
+GOOS=windows GOARCH=amd64   CGO_ENABLED=1  CXX_FOR_TARGET=x86_64-w64-mingw64-g++ CC_FOR_TARGET=x86_64-w64-mingw64-gcc  go build -o ioctl-windows-amd64.exe
+
+#mac
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o ioctl-darwin-amd64
+
+#linux
+GOOS=linux GOARCH=amd64 go build -o ioctl-linux-amd64
