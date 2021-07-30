4a9968d9930d0162d76450aefa59982304c2932e
xianhuawei <xianhuawei@macair2018.local>
2021-07-30 19:51:27 +0800

    add linux arm64

--- tools/ioctl/buildall.sh
+++ tools/ioctl/buildall.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get install gcc-mingw-w64
-apt-get install gcc-multilib
+apt install -y gcc-mingw-w64
+apt install -y gcc-multilib
+
 
 #windows
 GOOS=windows GOARCH=amd64   CGO_ENABLED=1  CXX_FOR_TARGET=x86_64-w64-mingw64-g++ CC_FOR_TARGET=x86_64-w64-mingw64-gcc  go build -o ioctl-windows-amd64.exe
@@ -11,3 +12,4 @@ GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o ioctl-darwin-amd64
 
 #linux
 GOOS=linux GOARCH=amd64 go build -o ioctl-linux-amd64
+GOOS=linux GOARCH=arm64 go build -o ioctl-linux-arm64
