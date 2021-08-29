f7551bf066b5302cb2b58ad61dda0e199cbb267a
Ashish Nigam <ashishnigam04@gmail.com>
2021-08-29 21:21:46 +0530

    Updated AccountState() to use address.Address

--- blockchain/block/validator_test.go
+++ blockchain/block/validator_test.go
@@ -8,6 +8,7 @@ package block
 
 import (
 	"context"
+	"github.com/iotexproject/iotex-address/address"
 	"math/big"
 	"strings"
 	"testing"
@@ -31,10 +32,10 @@ func TestValidator(t *testing.T) {
 	v := NewValidator(nil)
 	require.NoError(v.Validate(ctx, blk))
 
-	valid := protocol.NewGenericValidator(nil, func(sr protocol.StateReader, addr string) (*state.Account, error) {
+	valid := protocol.NewGenericValidator(nil, func(sr protocol.StateReader, addr address.Address) (*state.Account, error) {
 		pk := identityset.PrivateKey(27).PublicKey()
 		eAddr := pk.Address()
-		if strings.EqualFold(eAddr.String(), addr) {
+		if strings.EqualFold(eAddr.String(), addr.String()) {
 			return nil, errors.New("MockChainManager nonce error")
 		}
 		return &state.Account{Nonce: 2}, nil
