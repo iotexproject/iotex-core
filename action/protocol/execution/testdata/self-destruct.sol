81bb3cf58a2482993bddf16c07b85860fec695e8
Haaai <haixiang@usc.edu>
2021-09-02 19:58:02 +0800

    add indent

--- action/protocol/execution/testdata/self-destruct.sol
+++ action/protocol/execution/testdata/self-destruct.sol
@@ -1,18 +1,21 @@
 pragma solidity >0.6.0;
 contract Storage {
- address payable private owner;
- uint256 number;
- constructor() {
-  owner = payable(msg.sender);
- }
- function store(uint256 num) public {
-  number = num;
- }
- function retrieve() public view returns (uint256){
-  return number;
- }
+    address payable private owner;
+    uint256 number;
+    
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
  
- function close() public { 
-  selfdestruct(owner); 
- }
+    function close() public { 
+        selfdestruct(owner); 
+    }
 }
