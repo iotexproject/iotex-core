dcf27b4902cebda891b1edbf5d817515f933bb55
Haaai <55118568+Liuhaai@users.noreply.github.com>
2021-09-04 16:04:55 +0800

    add self-destruct contract unit test (#2774)
    * add self-destruct unit test

--- action/protocol/execution/protocol_test.go
+++ action/protocol/execution/protocol_test.go
@@ -868,6 +868,10 @@ func TestProtocol_Handle(t *testing.T) {
 	t.Run("infiniteloop-bering", func(t *testing.T) {
 		NewSmartContractTest(t, "testdata/infiniteloop-bering.json")
 	})
+	// self-destruct
+	t.Run("self-destruct", func(t *testing.T) {
+		NewSmartContractTest(t, "testdata/self-destruct.json")
+	})
 }
 
 func TestMaxTime(t *testing.T) {
@@ -948,6 +952,9 @@ func TestIstanbulEVM(t *testing.T) {
 	})
 	t.Run("wireconnection", func(t *testing.T) {
 		NewSmartContractTest(t, "testdata-istanbul/wireconnection.json")
+	})
+	t.Run("self-destruct", func(t *testing.T) {
+		NewSmartContractTest(t, "testdata-istanbul/self-destruct.json")
 	})
 }
 
