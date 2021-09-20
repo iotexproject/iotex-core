f352015cc7ba228cfda4b2e442c9f241920b2b74
Dustin Xie <dahuaxie@gmail.com>
2021-09-20 09:54:38 -0700

    [evm] enable opCall fix at Jutland height

--- action/protocol/execution/evm/evm_test.go
+++ action/protocol/execution/evm/evm_test.go
@@ -229,6 +229,9 @@ func TestConstantinople(t *testing.T) {
 		require.False(evmChainConfig.IsLondon(evm.Context.BlockNumber))
 		require.False(chainRules.IsBerlin)
 		require.False(chainRules.IsLondon)
+
+		require.Equal(big.NewInt(int64(g.JutlandBlockHeight)), evmChainConfig.JutlandBlock)
+		require.Equal(g.IsJutland(e.height), evmChainConfig.IsJutland(evm.Context.BlockNumber))
 	}
 }
 
