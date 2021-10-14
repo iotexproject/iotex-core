62183b38247e3a714fc57e905f06fb773ff543ec
Ashish Nigam <ashishnigam04@gmail.com>
2021-10-14 19:16:53 +0530

    imports optimized using go-imports for file blockstore.go

--- blockchain/block/blockstore.go
+++ blockchain/block/blockstore.go
@@ -10,9 +10,10 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/pkg/log"
-	"github.com/iotexproject/iotex-proto/golang/iotextypes"
 )
 
 type (
