b27774c689ae7c37be9fc97370017f3aacbe02f1
millken <millken@gmail.com>
2021-10-14 23:18:33 +0800

    update rotatefile, added rotate test

--- pkg/log/rotatefile.go
+++ pkg/log/rotatefile.go
@@ -1,32 +1,44 @@
 package log
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
 var (
-	_location = time.UTC
+	currentTime             = time.Now
+	defaultBackupTimeFormat = "20060102"
 )
 
 type RotateFile struct {
-	// Filename is the file to write logs to.  Backup log files will be retained
-	// in the same directory.
-	// os.TempDir() if empty.
+	// Filename is the file to write logs to.  Backup log files will be retained in the same directory.
+	// It uses <processname>.log in os.TempDir() if empty.
 	Filename string `json:"filename" yaml:"filename"`
-	// MaxBackups is the maximum number of old log files to retain.  The default
-	// is to retain all old log files (though MaxAge may still cause them to get
-	// deleted.)
+
+	// MaxBackups is the maximum number of old log files to retain.
 	MaxBackups int `json:"maxbackups" yaml:"maxbackups"`
 
 	// BackupTimeFormat determines if the time used for formatting the backup file name
 	BackupTimeFormat string `json:"backupTimeFormat" yaml:"backupTimeFormat"`
 
+	// LocalTime determines if the time used for formatting the timestamps in
+	// backup files is the computer's local time.  The default is to use UTC
+	// time.
+	LocalTime bool `json:"localtime" yaml:"localtime"`
+
 	file              *os.File
 	currentBackupName string
 	mu                sync.Mutex
+	wokerOnce         sync.Once
+	workerCh          chan bool
 }
 
 // filename generates the name of the logfile from the current time.
@@ -38,15 +50,24 @@ func (f *RotateFile) filename() string {
 	return filepath.Join(os.TempDir(), name)
 }
 
-// dir returns the directory for the current filename.
 func (f *RotateFile) dir() string {
 	return filepath.Dir(f.filename())
 }
 
+func (f *RotateFile) backupTimeFormat() string {
+	if f.BackupTimeFormat != "" {
+		return f.BackupTimeFormat
+	}
+	return defaultBackupTimeFormat
+}
+
 func (f *RotateFile) open() error {
 	var err error
-	t := time.Now().In(_location)
-	f.currentBackupName = t.Format(f.BackupTimeFormat)
+	t := currentTime()
+	if !f.LocalTime {
+		t = t.UTC()
+	}
+	f.currentBackupName = t.Format(f.backupTimeFormat())
 
 	f.file, err = os.OpenFile(f.filename(), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	return err
@@ -70,37 +91,157 @@ func (f *RotateFile) close() error {
 }
 
 // rotate on new day
-func (f *RotateFile) reopenIfNeeded() error {
+func (f *RotateFile) reopenIfNeeded() (bool, error) {
 	if f.file == nil {
-		return f.open()
+		return false, f.open()
 	}
-	t := time.Now().In(_location)
-	if f.currentBackupName == t.Format(f.BackupTimeFormat) {
-		return nil
+	t := currentTime()
+	if !f.LocalTime {
+		t = t.UTC()
+	}
+	if f.currentBackupName == t.Format(f.backupTimeFormat()) {
+		return false, nil
 	}
 	err := f.close()
 	if err != nil {
-		return err
+		return false, err
 	}
-	return f.open()
+	return true, f.open()
 }
 
 // Write writes data to a file
 func (f *RotateFile) Write(d []byte) (int, error) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
-	err := f.reopenIfNeeded()
+	rotate, err := f.reopenIfNeeded()
 	if err != nil {
 		return 0, err
+	}
+	if rotate {
+		if err := f.rotate(); err != nil {
+			return 0, err
+		}
 	}
 
 	return f.file.Write(d)
 }
 
-// logInfo is a convenience struct to return the filename and its embedded
-// timestamp.
+// Rotate close the existing log file and create a new one.
+func (f *RotateFile) Rotate() error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	return f.rotate()
+}
+
+func (f *RotateFile) rotate() error {
+	if err := f.close(); err != nil {
+		return err
+	}
+	if err := f.openNew(); err != nil {
+		return err
+	}
+	f.wokerOnce.Do(func() {
+		f.workerCh = make(chan bool, 1)
+		go func() {
+			for range f.workerCh {
+				f.doWorker()
+			}
+		}()
+	})
+	select {
+	case f.workerCh <- true:
+	default:
+	}
+	return nil
+}
+
+func (f *RotateFile) doWorker() {
+	if f.MaxBackups == 0 {
+		return
+	}
+	files, err := f.oldLogFiles()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if f.MaxBackups > 0 && f.MaxBackups < len(files) {
+		for _, fi := range files[0 : len(files)-f.MaxBackups] {
+			os.Remove(filepath.Join(f.dir(), fi.Name()))
+		}
+	}
+}
+
+func (f *RotateFile) oldLogFiles() ([]logInfo, error) {
+	files, err := ioutil.ReadDir(f.dir())
+	if err != nil {
+		return nil, fmt.Errorf("can't read log file directory: %s", err)
+	}
+	logFiles := []logInfo{}
+
+	filename := filepath.Base(f.filename())
+	ext := filepath.Ext(filename)
+	prefix := filename[:len(filename)-len(ext)] + "-"
+
+	for _, fi := range files {
+		if fi.IsDir() {
+			continue
+		}
+		filename := fi.Name()
+		if strings.HasPrefix(filename, prefix) {
+			ext := filepath.Ext(filename)
+			// ext include unix timestamp of the logfile
+			if len(ext) > 10 {
+				timestamp, err := strconv.ParseInt(ext[1:], 10, 64)
+				if err == nil {
+					logFiles = append(logFiles, logInfo{timestamp, fi})
+				}
+			}
+		}
+	}
+
+	sort.Sort(byFormatTime(logFiles))
+
+	return logFiles, nil
+}
+
+// openNew opens a new log file for writing, moving any old log file out of the
+// way.  This methods assumes the file has already been closed.
+func (f *RotateFile) openNew() error {
+	err := os.MkdirAll(f.dir(), 0755)
+	if err != nil {
+		return fmt.Errorf("can't make directories for new logfile: %s", err)
+	}
+
+	name := f.filename()
+	fi, err := os.Stat(name)
+	if err != nil {
+		return err
+	}
+	if !fi.IsDir() {
+		// move the existing file
+		newname := f.backupName()
+		if err := os.Rename(name, newname); err != nil {
+			return fmt.Errorf("can't rename log file: %s", err)
+		}
+	}
+	return f.open()
+}
+
+func (f *RotateFile) backupName() string {
+	dir := filepath.Dir(f.filename())
+	filename := filepath.Base(f.filename())
+	ext := filepath.Ext(filename)
+	prefix := filename[:len(filename)-len(ext)]
+	t := currentTime()
+	if !f.LocalTime {
+		t = t.UTC()
+	}
+	return filepath.Join(dir, fmt.Sprintf("%s-%s%s.%d", prefix, t.Format(f.backupTimeFormat()), ext, t.Unix()))
+}
+
+// logInfo is a convenience struct to return the filename and its embedded timestamp.
 type logInfo struct {
-	timestamp time.Time
+	timestamp int64
 	os.FileInfo
 }
 
@@ -108,7 +249,7 @@ type logInfo struct {
 type byFormatTime []logInfo
 
 func (b byFormatTime) Less(i, j int) bool {
-	return b[i].timestamp.After(b[j].timestamp)
+	return b[i].timestamp < b[j].timestamp
 }
 
 func (b byFormatTime) Swap(i, j int) {
--- pkg/log/rotatefile_test.go
+++ pkg/log/rotatefile_test.go
@@ -4,25 +4,103 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
+var fakeCurrentTime = time.Now()
+
+func fakeTime() time.Time {
+	return fakeCurrentTime
+}
+
 func TestNewFile(t *testing.T) {
 	require := require.New(t)
-
+	currentTime = fakeTime
 	dir := makeTempDir("TestNewFile", t)
 	defer os.RemoveAll(dir)
+	filename := logFile(dir)
 	l := &RotateFile{
-		Filename: logFile(dir),
+		Filename: filename,
 	}
 	defer l.Close()
 	b := []byte("boo!")
 	n, err := l.Write(b)
 	require.NoError(err)
 	require.Equal(len(b), n)
-	existsWithContent(logFile(dir), b, t)
+	existsWithContent(filename, b, t)
+	fileCount(dir, 1, t)
+
+	//test maxbackups = 0, can be triggered manually
+	newFakeTime()
+
+	err = l.Rotate()
+	require.NoError(err)
+
+	<-time.After(10 * time.Millisecond)
+
+	filename2 := backupFile(dir)
+	existsWithContent(filename2, b, t)
+	existsWithContent(filename, []byte{}, t)
+	fileCount(dir, 2, t)
+
+}
+
+func TestRotate(t *testing.T) {
+	require := require.New(t)
+	currentTime = fakeTime
+	dir := makeTempDir("TestRotate", t)
+	defer os.RemoveAll(dir)
+
+	filename := logFile(dir)
+
+	l := &RotateFile{
+		Filename:   filename,
+		MaxBackups: 1,
+	}
+	defer l.Close()
+	b := []byte("boo!")
+	n, err := l.Write(b)
+	require.NoError(err)
+	require.Equal(len(b), n)
+	existsWithContent(filename, b, t)
+	fileCount(dir, 1, t)
+
+	newFakeTime()
+
+	err = l.Rotate()
+	require.NoError(err)
+
+	<-time.After(10 * time.Millisecond)
+
+	fileCount(dir, 2, t)
+
+	filename2 := backupFile(dir)
+	existsWithContent(filename2, b, t)
+	existsWithContent(filename, []byte{}, t)
+	fileCount(dir, 2, t)
+
+	newFakeTime()
+
+	err = l.Rotate()
+	require.NoError(err)
+
+	<-time.After(10 * time.Millisecond)
+
+	filename3 := backupFile(dir)
+	existsWithContent(filename3, []byte{}, t)
+	existsWithContent(filename, []byte{}, t)
+	fileCount(dir, 2, t)
+
+	b2 := []byte("foooooo!")
+	n, err = l.Write(b2)
+	require.NoError(err)
+	require.Equal(len(b2), n)
+	existsWithContent(filename, b2, t)
+
 }
 
 func makeTempDir(name string, t testing.TB) string {
@@ -32,20 +110,33 @@ func makeTempDir(name string, t testing.TB) string {
 	return dir
 }
 
-// logFile returns the log file name in the given directory for the current fake
-// time.
 func logFile(dir string) string {
 	return filepath.Join(dir, "foobar.log")
 }
 
-// existsWithContent checks that the given file exists and has the correct content.
 func existsWithContent(path string, content []byte, t testing.TB) {
 	require := require.New(t)
 	info, err := os.Stat(path)
-	require.Nil(err)
+	require.NoError(err)
 	require.Equal(int64(len(content)), info.Size())
 
 	b, err := ioutil.ReadFile(path)
 	require.Nil(err)
 	require.Equal(content, b)
+}
+
+func fileCount(dir string, exp int, t testing.TB) {
+	require := require.New(t)
+	files, err := ioutil.ReadDir(dir)
+	require.NoError(err)
+	require.Equal(exp, len(files))
+}
+
+// newFakeTime sets the fake "current time" to two days later.
+func newFakeTime() {
+	fakeCurrentTime = fakeCurrentTime.Add(time.Hour * 24 * 2)
+}
+
+func backupFile(dir string) string {
+	return filepath.Join(dir, "foobar-"+fakeTime().UTC().Format(defaultBackupTimeFormat)+".log."+strconv.FormatInt(fakeTime().Unix(), 10))
 }
