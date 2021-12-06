1dfb10cd5041c0b79a03713763ac266915ea6113
Liuhaai <haixiang@usc.edu>
2021-12-06 09:25:51 -0800

    add comment for hardcode

--- api/web3server.go
+++ api/web3server.go
@@ -377,6 +377,7 @@ func (svr *Web3Server) call(in interface{}) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+	// the special balance checker contract is checked by metamask periodically
 	if to == "io1k8uw2hrlvnfq8s2qpwwc24ws2ru54heenx8chr" {
 		return nil, nil
 	}
