50c75c20d1a711f7d994ca2b32d7ade850949315
huof6829 <68298506@qq.com>
2022-01-17 23:03:08 +0800

    add unit test for api

---
+++ api/serverV2_test.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+
+	"github.com/iotexproject/iotex-core/config"
+	"github.com/iotexproject/iotex-core/testutil"
+)
+
+func TestServerV2(t *testing.T) {
+	require := require.New(t)
+	cfg := newConfig(t)
+	config.SetEVMNetworkID(1)
+	svr, bfIndexFile, _ := createServerV2(cfg, false)
+	defer func() {
+		testutil.CleanupPath(t, bfIndexFile)
+	}()
+
+	err := svr.Start()
+	require.NoError(err)
+
+	err = svr.Stop()
+	require.NoError(err)
+}
+
+func TestServerV2Error(t *testing.T) {
+	require := require.New(t)
+	cfg := newConfig(t)
+	config.SetEVMNetworkID(1)
+	svr, bfIndexFile, _ := createServerV2(cfg, false)
+	defer func() {
+		testutil.CleanupPath(t, bfIndexFile)
+	}()
+
+	err := svr.GrpcServer.Start()
+	require.NoError(err)
+	err = svr.Start()
+	require.Error(err)
+	require.Contains(err.Error(), "grpc server failed to listen")
+
+	svr.core.chainListener = nil
+	err = svr.Start()
+	require.Error(err)
+	require.Contains(err.Error(), "failed to add chainListener")
+
+	svr.tracer = &tracesdk.TracerProvider{}
+	err = svr.Stop()
+	require.Error(err)
+	require.Contains(err.Error(), "failed to shutdown api tracer")
+}
