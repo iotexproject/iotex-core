a93efdc2fd9645ea25db870c44fad25d4f50a388
Liuhaai <haixiang@iotex.io>
2022-02-04 18:50:39 -0800

    fix comments

--- db/batch/kv_cache.go
+++ db/batch/kv_cache.go
@@ -31,25 +31,25 @@ type (
 
 	// kvCache implements KVStoreCache interface
 	kvCache struct {
-		cache      map[string]map[string][]byte // local cache of batched <k, v> for fast query
-		deletedMap map[string]map[string]struct{}
+		cache   map[string]map[string][]byte // local cache of batched <k, v> for fast query
+		removed map[string]map[string]struct{}
 	}
 )
 
 // NewKVCache returns a KVCache
 func NewKVCache() KVStoreCache {
 	return &kvCache{
-		cache:      make(map[string]map[string][]byte),
-		deletedMap: make(map[string]map[string]struct{}),
+		cache:   make(map[string]map[string][]byte),
+		removed: make(map[string]map[string]struct{}),
 	}
 }
 
 // Read retrieves a record
 func (c *kvCache) Read(key *kvCacheKey) ([]byte, error) {
-	if v := c.cacheGet(key); v != nil {
+	if v, exist := c.cacheGet(key); exist {
 		return v, nil
 	}
-	if c.deletedMapGet(key) {
+	if c.removedGet(key) {
 		return nil, ErrAlreadyDeleted
 	}
 	return nil, ErrNotExist
@@ -58,36 +58,37 @@ func (c *kvCache) Read(key *kvCacheKey) ([]byte, error) {
 // Write puts a record into cache
 func (c *kvCache) Write(key *kvCacheKey, v []byte) {
 	c.cachePut(key, v)
-	c.deletedMapDelete(key)
+	c.removedDelete(key)
 }
 
 // WriteIfNotExist puts a record into cache only if it doesn't exist, otherwise return ErrAlreadyExist
 func (c *kvCache) WriteIfNotExist(key *kvCacheKey, v []byte) error {
-	if v := c.cacheGet(key); v != nil {
+	if _, exist := c.cacheGet(key); exist {
 		return ErrAlreadyExist
 	}
 	c.cachePut(key, v)
-	c.deletedMapDelete(key)
+	c.removedDelete(key)
 	return nil
 }
 
 // Evict deletes a record from cache
 func (c *kvCache) Evict(key *kvCacheKey) {
 	c.cacheDelete(key)
-	c.deletedMapPut(key)
+	c.removedPut(key)
 }
 
 // Clear clear the cache
 func (c *kvCache) Clear() {
-	c.cacheClear()
-	c.deletedMapClear()
+	// Unused mem in 2D map will be cleaned automatically by GC
+	c.cache = make(map[string]map[string][]byte)
+	c.removed = make(map[string]map[string]struct{})
 }
 
 // Clone clones the cache
 func (c *kvCache) Clone() KVStoreCache {
 	clone := kvCache{
-		cache:      make(map[string]map[string][]byte),
-		deletedMap: make(map[string]map[string]struct{}),
+		cache:   make(map[string]map[string][]byte),
+		removed: make(map[string]map[string]struct{}),
 	}
 	// clone entries in map
 	for k1, v1 := range c.cache {
@@ -96,10 +97,10 @@ func (c *kvCache) Clone() KVStoreCache {
 			clone.cache[k1][k2] = v2
 		}
 	}
-	for k1, v1 := range c.deletedMap {
-		clone.deletedMap[k1] = make(map[string]struct{})
+	for k1, v1 := range c.removed {
+		clone.removed[k1] = make(map[string]struct{})
 		for k2, v2 := range v1 {
-			clone.deletedMap[k1][k2] = v2
+			clone.removed[k1][k2] = v2
 		}
 	}
 	return &clone
@@ -112,13 +113,13 @@ func (c *kvCache) cachePut(key *kvCacheKey, v []byte) {
 	c.cache[key.key1][key.key2] = v
 }
 
-func (c *kvCache) cacheGet(key *kvCacheKey) []byte {
+func (c *kvCache) cacheGet(key *kvCacheKey) ([]byte, bool) {
 	if v1, ok := c.cache[key.key1]; ok {
 		if v2, ok := v1[key.key2]; ok {
-			return v2
+			return v2, true
 		}
 	}
-	return nil
+	return nil, false
 }
 
 func (c *kvCache) cacheDelete(key *kvCacheKey) {
@@ -130,24 +131,15 @@ func (c *kvCache) cacheDelete(key *kvCacheKey) {
 	}
 }
 
-func (c *kvCache) cacheClear() {
-	for k1 := range c.cache {
-		for k2 := range c.cache[k1] {
-			delete(c.cache[k1], k2)
-		}
-		delete(c.cache, k1)
+func (c *kvCache) removedPut(key *kvCacheKey) {
+	if _, ok := c.removed[key.key1]; !ok {
+		c.removed[key.key1] = make(map[string]struct{})
 	}
+	c.removed[key.key1][key.key2] = struct{}{}
 }
 
-func (c *kvCache) deletedMapPut(key *kvCacheKey) {
-	if _, ok := c.deletedMap[key.key1]; !ok {
-		c.deletedMap[key.key1] = make(map[string]struct{})
-	}
-	c.deletedMap[key.key1][key.key2] = struct{}{}
-}
-
-func (c *kvCache) deletedMapGet(key *kvCacheKey) bool {
-	if v1, ok := c.deletedMap[key.key1]; ok {
+func (c *kvCache) removedGet(key *kvCacheKey) bool {
+	if v1, ok := c.removed[key.key1]; ok {
 		if _, ok := v1[key.key2]; ok {
 			return true
 		}
@@ -155,20 +147,11 @@ func (c *kvCache) deletedMapGet(key *kvCacheKey) bool {
 	return false
 }
 
-func (c *kvCache) deletedMapDelete(key *kvCacheKey) {
-	if v1, ok := c.deletedMap[key.key1]; ok {
+func (c *kvCache) removedDelete(key *kvCacheKey) {
+	if v1, ok := c.removed[key.key1]; ok {
 		delete(v1, key.key2)
 		if len(v1) == 0 {
-			delete(c.deletedMap, key.key1)
+			delete(c.removed, key.key1)
 		}
-	}
-}
-
-func (c *kvCache) deletedMapClear() {
-	for k1 := range c.deletedMap {
-		for k2 := range c.deletedMap[k1] {
-			delete(c.deletedMap[k1], k2)
-		}
-		delete(c.deletedMap, k1)
 	}
 }
