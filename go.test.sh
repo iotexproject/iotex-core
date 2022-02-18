d00dd840d2bb08498d965b02abe2700c209e827c
huof6829 <68298506@qq.com>
2022-02-18 14:39:54 +0800

    fix profile.out

--- go.test.sh
+++ go.test.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    courtney -short -v -o=coverage.txt "$d"
+    courtney -short -v -o=profile.out "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
 done
-# for d in $(go list ./... | grep -v vendor); do
-#     go test -short -v -coverprofile=profile.out -covermode=count "$d"
-#     if [ -f profile.out ]; then
-#         cat profile.out >> coverage.txt
-#         rm profile.out
-#     fi
-# done
