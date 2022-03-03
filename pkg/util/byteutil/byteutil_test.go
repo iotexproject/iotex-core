5aed39e839677fb2f047d519f365ea7c2973796c
dustinxie <dahuaxie@gmail.com>
2022-03-03 10:50:27 -0800

    Update byteutil_test.go

--- pkg/util/byteutil/byteutil_test.go
+++ pkg/util/byteutil/byteutil_test.go
@@ -14,6 +14,47 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestUint32(t *testing.T) {
+	input := uint32(31415926)
+	t.Run("convert uint32 to bytes", func(t *testing.T) {
+		expectedValue := []uint8([]byte{0x76, 0x5e, 0xdf, 0x1})
+		result := Uint32ToBytes(input)
+		require.Equal(t, expectedValue, result)
+	})
+
+	t.Run("converts a uint32 to 4 bytes in big-endian", func(t *testing.T) {
+		expectedValue := []uint8([]byte{0x1, 0xdf, 0x5e, 0x76})
+		result := Uint32ToBytesBigEndian(input)
+		require.Equal(t, expectedValue, result)
+	})
+}
+
+func TestUint64(t *testing.T) {
+	input := uint64(1844674407370955161)
+	byteInput := []byte{0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x19}
+	t.Run("convert uint64 to bytes", func(t *testing.T) {
+		result := Uint64ToBytes(input)
+		require.Equal(t, byteInput, result)
+	})
+
+	t.Run("convert bytes to unit64", func(t *testing.T) {
+		result := BytesToUint64(byteInput)
+		require.Equal(t, input, result)
+	})
+
+	t.Run("converts a uint64 to 8 bytes in big-endian", func(t *testing.T) {
+		expectedValue := []uint8([]byte{0x19, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99})
+		result := Uint64ToBytesBigEndian(input)
+		require.Equal(t, expectedValue, result)
+	})
+
+	t.Run("converts 8 bytes to uint64 in big-endian", func(t *testing.T) {
+		expectedValue := uint64(11068046444225730841)
+		result := BytesToUint64BigEndian(byteInput)
+		require.Equal(t, expectedValue, result)
+	})
+}
+
 func TestMust(t *testing.T) {
 	t.Run("return identical output when given nil error", func(t *testing.T) {
 		b := []byte{0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x19}
