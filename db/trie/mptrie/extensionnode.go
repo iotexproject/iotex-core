596d1ba006c63734fda975fd3bdb6b1b41654a5e
Liuhaai <haixiang@iotex.io>
2022-03-05 22:22:28 -0800

    remove e.commonPrefixLength()

--- db/trie/mptrie/extensionnode.go
+++ db/trie/mptrie/extensionnode.go
@@ -53,7 +53,7 @@ func newExtensionNodeFromProtoPb(mpt *merklePatriciaTrie, pb *triepb.ExtendPb) *
 
 func (e *extensionNode) Delete(key keyType, offset uint8) (node, error) {
 	trieMtc.WithLabelValues("extensionNode", "delete").Inc()
-	matched := e.commonPrefixLength(key[offset:])
+	matched := commonPrefixLength(e.path, key[offset:])
 	if matched != uint8(len(e.path)) {
 		return nil, trie.ErrNotExist
 	}
@@ -84,7 +84,7 @@ func (e *extensionNode) Delete(key keyType, offset uint8) (node, error) {
 
 func (e *extensionNode) Upsert(key keyType, offset uint8, value []byte) (node, error) {
 	trieMtc.WithLabelValues("extensionNode", "upsert").Inc()
-	matched := e.commonPrefixLength(key[offset:])
+	matched := commonPrefixLength(e.path, key[offset:])
 	if matched == uint8(len(e.path)) {
 		newChild, err := e.child.Upsert(key, offset+matched, value)
 		if err != nil {
@@ -119,7 +119,7 @@ func (e *extensionNode) Upsert(key keyType, offset uint8, value []byte) (node, e
 
 func (e *extensionNode) Search(key keyType, offset uint8) (node, error) {
 	trieMtc.WithLabelValues("extensionNode", "search").Inc()
-	matched := e.commonPrefixLength(key[offset:])
+	matched := commonPrefixLength(e.path, key[offset:])
 	if matched != uint8(len(e.path)) {
 		return nil, trie.ErrNotExist
 	}
@@ -154,10 +154,6 @@ func (e *extensionNode) proto(flush bool) (proto.Message, error) {
 func (e *extensionNode) Child() node {
 	trieMtc.WithLabelValues("extensionNode", "child").Inc()
 	return e.child
-}
-
-func (e *extensionNode) commonPrefixLength(key []byte) uint8 {
-	return commonPrefixLength(e.path, key)
 }
 
 func (e *extensionNode) Flush() error {
