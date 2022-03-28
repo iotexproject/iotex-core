ecc8b480d041776d8ba61a580c0f22d00473fc5b
LuckyPigeon <lucky90322@gmail.com>
2022-03-29 02:57:32 +0800

    prefix unexported globals with _ in ioctl/cmd/contract pkg

--- ioctl/cmd/contract/contract.go
+++ ioctl/cmd/contract/contract.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 IoTeX Foundation
+// Copyright (c) 2022 IoTeX Foundation
 // This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
 // warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
@@ -21,33 +21,33 @@ import (
 	"github.com/iotexproject/iotex-core/ioctl/util"
 )
 
-const solCompiler = "solc"
+const _solCompiler = "solc"
 
 // Flags
 var (
-	initialAmountFlag = flag.NewStringVar("init-amount", "0",
-		config.TranslateInLang(flagInitialAmountUsage, config.UILanguage))
+	_initialAmountFlag = flag.NewStringVar("init-amount", "0",
+		config.TranslateInLang(_flagInitialAmountUsage, config.UILanguage))
 )
 
 // Multi-language support
 var (
-	contractCmdUses = map[config.Language]string{
+	_contractCmdUses = map[config.Language]string{
 		config.English: "contract",
 		config.Chinese: "contract",
 	}
-	contractCmdShorts = map[config.Language]string{
+	_contractCmdShorts = map[config.Language]string{
 		config.English: "Deal with smart contract of IoTeX blockchain",
 		config.Chinese: "处理IoTeX区块链的智能合约",
 	}
-	flagEndpointUsages = map[config.Language]string{
+	_flagEndpointUsages = map[config.Language]string{
 		config.English: "set endpoint for once",
 		config.Chinese: "一次设置端点",
 	}
-	flagInsecureUsages = map[config.Language]string{
+	_flagInsecureUsages = map[config.Language]string{
 		config.English: "insecure connection for once",
 		config.Chinese: "一次不安全的连接",
 	}
-	flagInitialAmountUsage = map[config.Language]string{
+	_flagInitialAmountUsage = map[config.Language]string{
 		config.English: "transfer an initial amount to the new deployed contract",
 		config.Chinese: "为部署的新合约转入一笔初始资金",
 	}
@@ -55,30 +55,30 @@ var (
 
 // ContractCmd represents the contract command
 var ContractCmd = &cobra.Command{
-	Use:   config.TranslateInLang(contractCmdUses, config.UILanguage),
-	Short: config.TranslateInLang(contractCmdShorts, config.UILanguage),
+	Use:   config.TranslateInLang(_contractCmdUses, config.UILanguage),
+	Short: config.TranslateInLang(_contractCmdShorts, config.UILanguage),
 }
 
 func init() {
 	ContractCmd.AddCommand(ContractPrepareCmd)
 	ContractCmd.AddCommand(ContractCompileCmd)
-	ContractCmd.AddCommand(contractDeployCmd)
-	ContractCmd.AddCommand(contractInvokeCmd)
-	ContractCmd.AddCommand(contractTestCmd)
-	ContractCmd.AddCommand(contractShareCmd)
+	ContractCmd.AddCommand(_contractDeployCmd)
+	ContractCmd.AddCommand(_contractInvokeCmd)
+	ContractCmd.AddCommand(_contractTestCmd)
+	ContractCmd.AddCommand(_contractShareCmd)
 	ContractCmd.PersistentFlags().StringVar(&config.ReadConfig.Endpoint, "endpoint",
-		config.ReadConfig.Endpoint, config.TranslateInLang(flagEndpointUsages, config.UILanguage))
+		config.ReadConfig.Endpoint, config.TranslateInLang(_flagEndpointUsages, config.UILanguage))
 	ContractCmd.PersistentFlags().BoolVar(&config.Insecure, "insecure", config.Insecure,
-		config.TranslateInLang(flagInsecureUsages, config.UILanguage))
+		config.TranslateInLang(_flagInsecureUsages, config.UILanguage))
 
-	flag.WithArgumentsFlag.RegisterCommand(contractDeploySolCmd)
-	flag.WithArgumentsFlag.RegisterCommand(contractInvokeFunctionCmd)
-	flag.WithArgumentsFlag.RegisterCommand(contractTestFunctionCmd)
+	flag.WithArgumentsFlag.RegisterCommand(_contractDeploySolCmd)
+	flag.WithArgumentsFlag.RegisterCommand(_contractInvokeFunctionCmd)
+	flag.WithArgumentsFlag.RegisterCommand(_contractTestFunctionCmd)
 }
 
 // Compile compiles smart contract from source code
 func Compile(sourceFiles ...string) (map[string]*compiler.Contract, error) {
-	solc, err := compiler.SolidityVersion(solCompiler)
+	solc, err := compiler.SolidityVersion(_solCompiler)
 	if err != nil {
 		return nil, output.NewError(output.CompilerError, "solidity compiler not ready", err)
 	}
@@ -87,7 +87,7 @@ func Compile(sourceFiles ...string) (map[string]*compiler.Contract, error) {
 			fmt.Sprintf("unsupported solc version %d.%d.%d", solc.Major, solc.Minor, solc.Patch), nil)
 	}
 
-	contracts, err := compiler.CompileSolidity(solCompiler, sourceFiles...)
+	contracts, err := compiler.CompileSolidity(_solCompiler, sourceFiles...)
 	if err != nil {
 		return nil, output.NewError(output.CompilerError, "failed to compile", err)
 	}
