99d15544dbb7517efc07831c57181507cc077a8c
LuckyPigeon <lucky90322@gmail.com>
2022-03-29 01:20:26 +0800

    replace parseBlockRange by ParseInt

--- api/coreservice_test.go
+++ api/coreservice_test.go
@@ -29,32 +29,38 @@ func TestLogsInRange(t *testing.T) {
 		testData := &filterObject{FromBlock: "1", ToBlock: "4"}
 		filter, err := getTopicsAddress(testData.Address, testData.Topics)
 		require.NoError(err)
-		from, to, err := svr.web3Server.parseBlockRange(testData.FromBlock, testData.ToBlock)
+		from, err := strconv.ParseUint(testData.FromBlock, 10, 64)
+		require.NoError(err)
+		to, err := strconv.ParseUint(testData.ToBlock, 10, 64)
 		require.NoError(err)
 
-		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), uint64(from), uint64(to), uint64(0))
+		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), from, to, uint64(0))
 		require.NoError(err)
 		require.Equal(4, len(logs))
 	})
 	t.Run("empty log", func(t *testing.T) {
-		testData := &filterObject{Address: []string{"0x8ce313ab12bf7aed8136ab36c623ff98c8eaad34"}}
+		testData := &filterObject{FromBlock: "1", ToBlock: "4", Address: []string{"0x8ce313ab12bf7aed8136ab36c623ff98c8eaad34"}}
 		filter, err := getTopicsAddress(testData.Address, testData.Topics)
 		require.NoError(err)
-
-		from, to, err := svr.web3Server.parseBlockRange(testData.FromBlock, testData.ToBlock)
+		from, err := strconv.ParseUint(testData.FromBlock, 10, 64)
 		require.NoError(err)
-		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), uint64(from), uint64(to), uint64(0))
+		to, err := strconv.ParseUint(testData.ToBlock, 10, 64)
+		require.NoError(err)
+
+		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), from, to, uint64(0))
 		require.NoError(err)
 		require.Equal(0, len(logs))
 	})
 	t.Run("over 5000 pagenation size", func(t *testing.T) {
-		testData := &filterObject{FromBlock: "0"}
+		testData := &filterObject{FromBlock: "1", ToBlock: "4"}
 		filter, err := getTopicsAddress(testData.Address, testData.Topics)
 		require.NoError(err)
-		from, to, err := svr.web3Server.parseBlockRange(testData.FromBlock, testData.ToBlock)
+		from, err := strconv.ParseUint(testData.FromBlock, 10, 64)
+		require.NoError(err)
+		to, err := strconv.ParseUint(testData.ToBlock, 10, 64)
 		require.NoError(err)
 
-		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), uint64(from), uint64(to), uint64(5001))
+		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), from, to, uint64(5001))
 		require.NoError(err)
 		require.Equal(4, len(logs))
 	})
@@ -62,10 +68,12 @@ func TestLogsInRange(t *testing.T) {
 		testData := &filterObject{FromBlock: "2", ToBlock: "1"}
 		filter, err := getTopicsAddress(testData.Address, testData.Topics)
 		require.NoError(err)
-		from, to, err := svr.web3Server.parseBlockRange(testData.FromBlock, testData.ToBlock)
+		from, err := strconv.ParseUint(testData.FromBlock, 10, 64)
+		require.NoError(err)
+		to, err := strconv.ParseUint(testData.ToBlock, 10, 64)
 		require.NoError(err)
 
-		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), uint64(from), uint64(to), uint64(0))
+		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), from, to, uint64(0))
 		expectedErr := errors.New("invalid start and end height")
 		expectedValue := []*iotextypes.Log(nil)
 		require.Error(err)
@@ -76,10 +84,12 @@ func TestLogsInRange(t *testing.T) {
 		testData := &filterObject{FromBlock: "5", ToBlock: "5"}
 		filter, err := getTopicsAddress(testData.Address, testData.Topics)
 		require.NoError(err)
-		from, to, err := svr.web3Server.parseBlockRange(testData.FromBlock, testData.ToBlock)
+		from, err := strconv.ParseUint(testData.FromBlock, 10, 64)
+		require.NoError(err)
+		to, err := strconv.ParseUint(testData.ToBlock, 10, 64)
 		require.NoError(err)
 
-		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), uint64(from), uint64(to), uint64(0))
+		logs, err := svr.web3Server.coreService.LogsInRange(logfilter.NewLogFilter(&filter, nil, nil), from, to, uint64(0))
 		expectedErr := errors.New("start block > tip height")
 		expectedValue := []*iotextypes.Log(nil)
 		require.Error(err)
