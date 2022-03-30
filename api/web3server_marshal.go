52e9f901ae1106b266bc989f3d62d49c1a2b6e23
Liuhaai <haixiang@iotex.io>
2022-03-29 20:58:38 -0700

    add test

--- api/web3server_marshal.go
+++ api/web3server_marshal.go
@@ -8,11 +8,27 @@ import (
 	"github.com/iotexproject/go-pkgs/crypto"
 	"github.com/iotexproject/go-pkgs/hash"
 	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"github.com/pkg/errors"
 
 	"github.com/iotexproject/iotex-core/action"
 )
 
 type (
+	blockObjectV2 struct {
+		blkMeta      *iotextypes.BlockMeta
+		logsBloom    string
+		transactions []interface{}
+	}
+
+	transactionObjectV2 struct {
+		blockHash hash.Hash256
+		to        *string
+		ethTx     *types.Transaction
+		receipt   *action.Receipt
+		pubkey    crypto.PublicKey
+		signature []byte
+	}
+
 	receiptObjectV2 struct {
 		blockHash       hash.Hash256
 		from            string
@@ -26,138 +42,16 @@ type (
 		blockHash hash.Hash256
 		log       *action.Log
 	}
-
-	transactionObjectV2 struct {
-		blockHash hash.Hash256
-		to        *string
-		ethTx     types.Transaction
-		receipt   *action.Receipt
-		pubkey    crypto.PublicKey
-		signature []byte
-	}
-
-	blockObjectV2 struct {
-		blkMeta      *iotextypes.BlockMeta
-		logsBloom    string
-		transactions []interface{}
-	}
 )
 
-func (obj *receiptObjectV2) MarshalJSON() ([]byte, error) {
-	from, err := ioAddrToEthAddr(obj.from)
-	if err != nil {
-		return nil, err
-	}
-
-	logs := make([]*logsObjectV2, 0, len(obj.receipt.Logs()))
-	for i := range obj.receipt.Logs() {
-		logs[i] = &logsObjectV2{obj.blockHash, obj.receipt.Logs()[i]}
-	}
-
-	return json.Marshal(&struct {
-		TransactionIndex  string          `json:"transactionIndex"`
-		TransactionHash   string          `json:"transactionHash"`
-		BlockHash         string          `json:"blockHash"`
-		BlockNumber       string          `json:"blockNumber"`
-		From              string          `json:"from"`
-		To                *string         `json:"to"`
-		CumulativeGasUsed string          `json:"cumulativeGasUsed"`
-		GasUsed           string          `json:"gasUsed"`
-		ContractAddress   *string         `json:"contractAddress"`
-		LogsBloom         string          `json:"logsBloom"`
-		Logs              []*logsObjectV2 `json:"logs"`
-		Status            string          `json:"status"`
-	}{
-		TransactionIndex:  uint64ToHex(uint64(obj.receipt.TxIndex)),
-		TransactionHash:   "0x" + hex.EncodeToString(obj.receipt.ActionHash[:]),
-		BlockHash:         "0x" + hex.EncodeToString(obj.blockHash[:]),
-		BlockNumber:       uint64ToHex(obj.receipt.BlockHeight),
-		From:              from,
-		To:                obj.to,
-		CumulativeGasUsed: uint64ToHex(obj.receipt.GasConsumed),
-		GasUsed:           uint64ToHex(obj.receipt.GasConsumed),
-		ContractAddress:   obj.contractAddress,
-		LogsBloom:         obj.logsBloom,
-		Logs:              logs,
-		Status:            uint64ToHex(obj.receipt.Status),
-	})
-}
-
-func (obj *logsObjectV2) MarshalJSON() ([]byte, error) {
-	addr, err := ioAddrToEthAddr(obj.log.Address)
-	if err != nil {
-		return nil, err
-	}
-	topics := make([]string, 0, len(obj.log.Topics))
-	for _, tpc := range obj.log.Topics {
-		topics = append(topics, "0x"+hex.EncodeToString(tpc[:]))
-	}
-	return json.Marshal(&struct {
-		Removed          bool     `json:"removed"`
-		LogIndex         string   `json:"logIndex"`
-		TransactionIndex string   `json:"transactionIndex"`
-		TransactionHash  string   `json:"transactionHash"`
-		BlockHash        string   `json:"blockHash"`
-		BlockNumber      string   `json:"blockNumber"`
-		Address          string   `json:"address"`
-		Data             string   `json:"data"`
-		Topics           []string `json:"topics"`
-	}{
-		Removed:          false,
-		LogIndex:         uint64ToHex(uint64(obj.log.Index)),
-		TransactionIndex: uint64ToHex(uint64(obj.log.TxIndex)),
-		TransactionHash:  "0x" + hex.EncodeToString(obj.log.ActionHash[:]),
-		BlockHash:        "0x" + hex.EncodeToString(obj.blockHash[:]),
-		BlockNumber:      uint64ToHex(uint64(obj.log.BlockHeight)),
-		Address:          addr,
-		Data:             "0x" + hex.EncodeToString(obj.log.Data),
-		Topics:           topics,
-	})
-}
-
-func (obj *transactionObjectV2) MarshalJSON() ([]byte, error) {
-	value, _ := intStrToHex(obj.ethTx.Value().String())
-	gasPrice, _ := intStrToHex(obj.ethTx.GasPrice().String())
-
-	vVal := uint64(obj.signature[64])
-	if vVal < 27 {
-		vVal += 27
-	}
-
-	return json.Marshal(&struct {
-		Hash             string  `json:"hash"`
-		Nonce            string  `json:"nonce"`
-		BlockHash        string  `json:"blockHash"`
-		BlockNumber      string  `json:"blockNumber"`
-		TransactionIndex string  `json:"transactionIndex"`
-		From             string  `json:"from"`
-		To               *string `json:"to"`
-		Value            string  `json:"value"`
-		GasPrice         string  `json:"gasPrice"`
-		Gas              string  `json:"gas"`
-		Input            string  `json:"input"`
-		R                string  `json:"r"`
-		S                string  `json:"s"`
-		V                string  `json:"v"`
-	}{
-		Hash:             "0x" + hex.EncodeToString(obj.receipt.ActionHash[:]),
-		Nonce:            uint64ToHex(obj.ethTx.Nonce()),
-		BlockHash:        "0x" + hex.EncodeToString(obj.blockHash[:]),
-		BlockNumber:      uint64ToHex(obj.receipt.BlockHeight),
-		TransactionIndex: uint64ToHex(uint64(obj.receipt.TxIndex)),
-		From:             obj.pubkey.Address().Hex(),
-		To:               obj.to,
-		Value:            value,
-		GasPrice:         gasPrice,
-		Gas:              uint64ToHex(obj.ethTx.Gas()),
-		Input:            byteToHex(obj.ethTx.Data()),
-		R:                byteToHex(obj.signature[:32]),
-		S:                byteToHex(obj.signature[32:64]),
-		V:                uint64ToHex(vVal),
-	})
-}
+var (
+	errInvalidObject = errors.New("invalid object")
+)
 
 func (obj *blockObjectV2) MarshalJSON() ([]byte, error) {
+	if obj.blkMeta == nil {
+		return nil, errInvalidObject
+	}
 	producerAddr, err := ioAddrToEthAddr(obj.blkMeta.ProducerAddress)
 	if err != nil {
 		return nil, err
@@ -204,5 +98,127 @@ func (obj *blockObjectV2) MarshalJSON() ([]byte, error) {
 		Transactions:     obj.transactions,
 		Step:             "373422302",
 		Uncles:           []string{},
+	})
+}
+
+func (obj *transactionObjectV2) MarshalJSON() ([]byte, error) {
+	if obj.receipt == nil || obj.pubkey == nil || obj.ethTx == nil {
+		return nil, errInvalidObject
+	}
+	value, _ := intStrToHex(obj.ethTx.Value().String())
+	gasPrice, _ := intStrToHex(obj.ethTx.GasPrice().String())
+
+	vVal := uint64(obj.signature[64])
+	if vVal < 27 {
+		vVal += 27
+	}
+
+	return json.Marshal(&struct {
+		Hash             string  `json:"hash"`
+		Nonce            string  `json:"nonce"`
+		BlockHash        string  `json:"blockHash"`
+		BlockNumber      string  `json:"blockNumber"`
+		TransactionIndex string  `json:"transactionIndex"`
+		From             string  `json:"from"`
+		To               *string `json:"to"`
+		Value            string  `json:"value"`
+		GasPrice         string  `json:"gasPrice"`
+		Gas              string  `json:"gas"`
+		Input            string  `json:"input"`
+		R                string  `json:"r"`
+		S                string  `json:"s"`
+		V                string  `json:"v"`
+	}{
+		Hash:             "0x" + hex.EncodeToString(obj.receipt.ActionHash[:]),
+		Nonce:            uint64ToHex(obj.ethTx.Nonce()),
+		BlockHash:        "0x" + hex.EncodeToString(obj.blockHash[:]),
+		BlockNumber:      uint64ToHex(obj.receipt.BlockHeight),
+		TransactionIndex: uint64ToHex(uint64(obj.receipt.TxIndex)),
+		From:             obj.pubkey.Address().Hex(),
+		To:               obj.to,
+		Value:            value,
+		GasPrice:         gasPrice,
+		Gas:              uint64ToHex(obj.ethTx.Gas()),
+		Input:            byteToHex(obj.ethTx.Data()),
+		R:                byteToHex(obj.signature[:32]),
+		S:                byteToHex(obj.signature[32:64]),
+		V:                uint64ToHex(vVal),
+	})
+}
+
+func (obj *receiptObjectV2) MarshalJSON() ([]byte, error) {
+	if obj.receipt == nil {
+		return nil, errInvalidObject
+	}
+	from, err := ioAddrToEthAddr(obj.from)
+	if err != nil {
+		return nil, err
+	}
+	logs := make([]*logsObjectV2, 0, len(obj.receipt.Logs()))
+	for _, v := range obj.receipt.Logs() {
+		logs = append(logs, &logsObjectV2{obj.blockHash, v})
+	}
+
+	return json.Marshal(&struct {
+		TransactionIndex  string          `json:"transactionIndex"`
+		TransactionHash   string          `json:"transactionHash"`
+		BlockHash         string          `json:"blockHash"`
+		BlockNumber       string          `json:"blockNumber"`
+		From              string          `json:"from"`
+		To                *string         `json:"to"`
+		CumulativeGasUsed string          `json:"cumulativeGasUsed"`
+		GasUsed           string          `json:"gasUsed"`
+		ContractAddress   *string         `json:"contractAddress"`
+		LogsBloom         string          `json:"logsBloom"`
+		Logs              []*logsObjectV2 `json:"logs"`
+		Status            string          `json:"status"`
+	}{
+		TransactionIndex:  uint64ToHex(uint64(obj.receipt.TxIndex)),
+		TransactionHash:   "0x" + hex.EncodeToString(obj.receipt.ActionHash[:]),
+		BlockHash:         "0x" + hex.EncodeToString(obj.blockHash[:]),
+		BlockNumber:       uint64ToHex(obj.receipt.BlockHeight),
+		From:              from,
+		To:                obj.to,
+		CumulativeGasUsed: uint64ToHex(obj.receipt.GasConsumed),
+		GasUsed:           uint64ToHex(obj.receipt.GasConsumed),
+		ContractAddress:   obj.contractAddress,
+		LogsBloom:         obj.logsBloom,
+		Logs:              logs,
+		Status:            uint64ToHex(obj.receipt.Status),
+	})
+}
+
+func (obj *logsObjectV2) MarshalJSON() ([]byte, error) {
+	if obj.log == nil {
+		return nil, errInvalidObject
+	}
+	addr, err := ioAddrToEthAddr(obj.log.Address)
+	if err != nil {
+		return nil, err
+	}
+	topics := make([]string, 0, len(obj.log.Topics))
+	for _, tpc := range obj.log.Topics {
+		topics = append(topics, "0x"+hex.EncodeToString(tpc[:]))
+	}
+	return json.Marshal(&struct {
+		Removed          bool     `json:"removed"`
+		LogIndex         string   `json:"logIndex"`
+		TransactionIndex string   `json:"transactionIndex"`
+		TransactionHash  string   `json:"transactionHash"`
+		BlockHash        string   `json:"blockHash"`
+		BlockNumber      string   `json:"blockNumber"`
+		Address          string   `json:"address"`
+		Data             string   `json:"data"`
+		Topics           []string `json:"topics"`
+	}{
+		Removed:          false,
+		LogIndex:         uint64ToHex(uint64(obj.log.Index)),
+		TransactionIndex: uint64ToHex(uint64(obj.log.TxIndex)),
+		TransactionHash:  "0x" + hex.EncodeToString(obj.log.ActionHash[:]),
+		BlockHash:        "0x" + hex.EncodeToString(obj.blockHash[:]),
+		BlockNumber:      uint64ToHex(uint64(obj.log.BlockHeight)),
+		Address:          addr,
+		Data:             "0x" + hex.EncodeToString(obj.log.Data),
+		Topics:           topics,
 	})
 }
