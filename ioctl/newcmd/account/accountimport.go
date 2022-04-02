31dc55c4706dd4a58450aeb9a9eab4d9bb7bd388
huof6890 <68298506@qq.com>
2022-04-02 22:54:30 +0800

    delete output and config.Reading, use ioctl.Client

--- ioctl/newcmd/account/accountimport.go
+++ ioctl/newcmd/account/accountimport.go
@@ -8,14 +8,13 @@ package account
 
 import (
 	"fmt"
-	"github.com/iotexproject/iotex-core/ioctl"
 	"os"
-	"sigs.k8s.io/yaml"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
+	"github.com/iotexproject/iotex-core/ioctl"
 	"github.com/iotexproject/iotex-core/ioctl/config"
-	"github.com/iotexproject/iotex-core/ioctl/output"
 	"github.com/iotexproject/iotex-core/ioctl/util"
 	"github.com/iotexproject/iotex-core/ioctl/validator"
 )
@@ -56,56 +55,7 @@ var (
 	}
 )
 
-func NewAccountImportKeyCmd(client ioctl.Client) *cobra.Command {
-	importKeyUses, _ := client.SelectTranslation(importKeyCmdUses)
-	importKeyShorts, _ := client.SelectTranslation(importKeyCmdShorts)
-	accountImportKeyCmd := &cobra.Command{
-		Use:   importKeyUses,
-		Short: importKeyShorts,
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.SilenceUsage = true
-			err := accountImportKey(client, cmd, args)
-			return output.PrintError(err)
-		},
-	}
-	return accountImportKeyCmd
-}
-
-func NewAccountImportKeyStoreCmd(client ioctl.Client) *cobra.Command {
-	// accountImportKeyStoreCmd represents the account import keystore command
-	importKeyStoreUses, _ := client.SelectTranslation(importKeyStoreCmdUses)
-	importKeyStoreShorts, _ := client.SelectTranslation(importKeyStoreCmdShorts)
-	accountImportKeyStoreCmd := &cobra.Command{
-		Use:   importKeyStoreUses,
-		Short: importKeyStoreShorts,
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.SilenceUsage = true
-			err := accountImportKeyStore(client, cmd, args)
-			return output.PrintError(err)
-		},
-	}
-	return accountImportKeyStoreCmd
-}
-
-func NewAccountImportPemCmd(client ioctl.Client) *cobra.Command {
-	// accountImportPemCmd represents the account import pem command
-	importPemUses, _ := client.SelectTranslation(importPemCmdUses)
-	importPemShorts, _ := client.SelectTranslation(importPemCmdShorts)
-	accountImportPemCmd := &cobra.Command{
-		Use:   importPemUses,
-		Short: importPemShorts,
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.SilenceUsage = true
-			err := accountImportPem(client, cmd, args)
-			return output.PrintError(err)
-		},
-	}
-	return accountImportPemCmd
-}
-
+// NewAccountImportCmd combines three account import command
 func NewAccountImportCmd(client ioctl.Client) *cobra.Command {
 	importUses, _ := client.SelectTranslation(importCmdUses)
 	importShorts, _ := client.SelectTranslation(importCmdShorts)
@@ -121,105 +71,111 @@ func NewAccountImportCmd(client ioctl.Client) *cobra.Command {
 	return accountImportCmd
 }
 
-func validateAlias(alias string) error {
+// NewAccountImportKeyStoreCmd represents the account import key command
+func NewAccountImportKeyCmd(client ioctl.Client) *cobra.Command {
+	importKeyUses, _ := client.SelectTranslation(importKeyCmdUses)
+	importKeyShorts, _ := client.SelectTranslation(importKeyCmdShorts)
+	return &cobra.Command{
+		Use:   importKeyUses,
+		Short: importKeyShorts,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			alias := args[0]
+
+			if err := validateAlias(client, alias); err != nil {
+				return errors.Wrap(err, "invalid alias")
+			}
+			cmd.Println(fmt.Sprintf("#%s: Enter your private key, "+
+				"which will not be exposed on the screen.", alias))
+			privateKey, err := client.ReadSecret()
+			privateKey = util.TrimHexPrefix(privateKey)
+			if err != nil {
+				return errors.Wrap(err, "failed to get password")
+			}
+			addr, err := newAccountByKey(client, cmd, alias, privateKey)
+			if err != nil {
+				return err
+			}
+			return client.SetAlias(alias, addr)
+		},
+	}
+}
+
+// NewAccountImportKeyStoreCmd represents the account import keystore command
+func NewAccountImportKeyStoreCmd(client ioctl.Client) *cobra.Command {
+	importKeyStoreUses, _ := client.SelectTranslation(importKeyStoreCmdUses)
+	importKeyStoreShorts, _ := client.SelectTranslation(importKeyStoreCmdShorts)
+	return &cobra.Command{
+		Use:   importKeyStoreUses,
+		Short: importKeyStoreShorts,
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			alias := args[0]
+
+			if err := validateAlias(client, alias); err != nil {
+				return errors.Wrap(err, "invalid alias")
+			}
+			if _, err := os.Stat(args[1]); err != nil {
+				return err
+			}
+
+			cmd.Println(fmt.Sprintf("#%s: Enter your password of keystore, "+
+				"which will not be exposed on the screen.", alias))
+			password, err := client.ReadSecret()
+			if err != nil {
+				return errors.Wrap(err, "failed to get password")
+			}
+			addr, err := newAccountByKeyStore(client, cmd, alias, password, args[1])
+			if err != nil {
+				return err
+			}
+			return client.SetAlias(alias, addr)
+		},
+	}
+}
+
+// NewAccountImportPemCmd represents the account import pem command
+func NewAccountImportPemCmd(client ioctl.Client) *cobra.Command {
+	importPemUses, _ := client.SelectTranslation(importPemCmdUses)
+	importPemShorts, _ := client.SelectTranslation(importPemCmdShorts)
+	return &cobra.Command{
+		Use:   importPemUses,
+		Short: importPemShorts,
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			alias := args[0]
+
+			if err := validateAlias(client, alias); err != nil {
+				return errors.Wrap(err, "invalid alias")
+			}
+			if _, err := os.Stat(args[1]); err != nil {
+				return err
+			}
+
+			cmd.Println(fmt.Sprintf("#%s: Enter your password of pem file, "+
+				"which will not be exposed on the screen.", alias))
+			password, err := client.ReadSecret()
+			if err != nil {
+				return errors.Wrap(err, "failed to get password")
+			}
+			addr, err := newAccountByPem(client, cmd, alias, password, args[1])
+			if err != nil {
+				return err
+			}
+			return client.SetAlias(alias, addr)
+		},
+	}
+}
+
+func validateAlias(client ioctl.Client, alias string) error {
 	if err := validator.ValidateAlias(alias); err != nil {
 		return err
 	}
-	if addr, ok := config.ReadConfig.Aliases[alias]; ok {
+	if addr, ok := client.Config().Aliases[alias]; ok {
 		return fmt.Errorf("alias \"%s\" has already used for %s", alias, addr)
 	}
 	return nil
-}
-
-func writeToFile(alias, addr string) error {
-	config.ReadConfig.Aliases[alias] = addr
-	out, err := yaml.Marshal(&config.ReadConfig)
-	if err != nil {
-		return output.NewError(output.SerializationError, "failed to marshal config", err)
-	}
-	if err := os.WriteFile(config.DefaultConfigFile, out, 0600); err != nil {
-		return output.NewError(output.WriteFileError,
-			fmt.Sprintf("failed to write to config file %s", config.DefaultConfigFile), err)
-	}
-	output.PrintResult(fmt.Sprintf("New account #%s is created. Keep your password, "+
-		"or you will lose your private key.", alias))
-	return nil
-}
-
-func readPasswordFromStdin() (string, error) {
-	password, err := util.ReadSecretFromStdin()
-	if err != nil {
-		return "", fmt.Errorf("failed to get password")
-	}
-	return password, nil
-}
-func accountImportKey(client ioctl.Client, cmd *cobra.Command, args []string) error {
-	// Validate inputs
-	alias := args[0]
-	err := validateAlias(alias)
-	if err != nil {
-		return output.NewError(output.ValidationError, "invalid alias", err)
-	}
-	output.PrintQuery(fmt.Sprintf("#%s: Enter your private key, "+
-		"which will not be exposed on the screen.", alias))
-	privateKey, err := readPasswordFromStdin()
-	privateKey = util.TrimHexPrefix(privateKey)
-	if err != nil {
-		return output.NewError(output.InputError, "failed to get password", err)
-	}
-	addr, err := newAccountByKey(client, cmd, alias, privateKey)
-	if err != nil {
-		return output.NewError(0, "", err)
-	}
-	return writeToFile(alias, addr)
-}
-
-func accountImportKeyStore(client ioctl.Client, cmd *cobra.Command, args []string) error {
-	// Validate inputs
-	alias := args[0]
-	err := validateAlias(alias)
-	if err != nil {
-		return output.NewError(output.ValidationError, "invalid alias", err)
-	}
-	_, err = os.Stat(args[1])
-	if err != nil {
-		return output.NewError(output.ReadFileError, "", err)
-	}
-
-	output.PrintQuery(fmt.Sprintf("#%s: Enter your password of keystore, "+
-		"which will not be exposed on the screen.", alias))
-	password, err := util.ReadSecretFromStdin()
-	if err != nil {
-		return output.NewError(output.InputError, "failed to get password", err)
-	}
-	addr, err := newAccountByKeyStore(client, cmd, alias, password, args[1])
-	if err != nil {
-		return output.NewError(0, "", err)
-	}
-	return writeToFile(alias, addr)
-}
-
-func accountImportPem(client ioctl.Client, cmd *cobra.Command, args []string) error {
-	// Validate inputs
-	alias := args[0]
-	err := validateAlias(alias)
-	if err != nil {
-		return output.NewError(output.ValidationError, "invalid alias", err)
-	}
-	_, err = os.Stat(args[1])
-	if err != nil {
-		return output.NewError(output.ReadFileError, "", err)
-	}
-
-	output.PrintQuery(fmt.Sprintf("#%s: Enter your password of pem file, "+
-		"which will not be exposed on the screen.", alias))
-	password, err := util.ReadSecretFromStdin()
-	if err != nil {
-		return output.NewError(output.InputError, "failed to get password", err)
-	}
-	addr, err := newAccountByPem(client, cmd, alias, password, args[1])
-	if err != nil {
-		return output.NewError(0, "", err)
-	}
-	return writeToFile(alias, addr)
 }
--- ioctl/newcmd/account/accountimport_test.go
+++ ioctl/newcmd/account/accountimport_test.go
@@ -7,18 +7,18 @@
 package account
 
 import (
-	"github.com/iotexproject/iotex-core/testutil"
-	"os"
-	"path/filepath"
+	"crypto/ecdsa"
 	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/iotexproject/go-pkgs/crypto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotexproject/iotex-core/ioctl/config"
 	"github.com/iotexproject/iotex-core/ioctl/util"
 	"github.com/iotexproject/iotex-core/test/mock/mock_ioctlclient"
+	"github.com/iotexproject/iotex-core/testutil"
 )
 
 func TestNewAccountImportCmd(t *testing.T) {
@@ -26,99 +26,116 @@ func TestNewAccountImportCmd(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_ioctlclient.NewMockClient(ctrl)
-	client.EXPECT().SelectTranslation(gomock.Any()).Return("", config.English).AnyTimes()
+	client.EXPECT().SelectTranslation(gomock.Any()).Return("key", config.English).AnyTimes()
 	client.EXPECT().Config().Return(config.Config{}).AnyTimes()
 
 	cmd := NewAccountImportCmd(client)
-	result, err := util.ExecuteCmd(cmd, "hh")
-
-	require.NotNil(result)
-	require.Error(err)
+	_, err := util.ExecuteCmd(cmd, "key", "-h")
+	require.NoError(err)
+	_, err = util.ExecuteCmd(cmd, "hhcmd", "-h")
+	require.Contains(err.Error(), "unknown command")
 }
 
 func TestNewAccountImportKeyCmd(t *testing.T) {
 	require := require.New(t)
+	testWallet, ks, _, _, err := newTestAccount()
+	require.NoError(err)
+	defer testutil.CleanupPath(testWallet)
+
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_ioctlclient.NewMockClient(ctrl)
 	client.EXPECT().SelectTranslation(gomock.Any()).Return("", config.English).AnyTimes()
-	client.EXPECT().Config().Return(config.Config{}).AnyTimes()
+
+	prvKey, err := crypto.GenerateKey()
+	require.NoError(err)
+
+	client.EXPECT().ReadSecret().Return(prvKey.HexString(), nil).AnyTimes()
+	client.EXPECT().NewKeyStore().Return(ks).Times(1)
+	client.EXPECT().Config().Return(config.Config{Wallet: testWallet}).Times(1)
+	client.EXPECT().SetAlias(gomock.Any(), gomock.Any()).Return(nil).Times(1)
 
 	cmd := NewAccountImportKeyCmd(client)
-	result, err := util.ExecuteCmd(cmd, "hh")
-
-	require.NotNil(result)
-	require.Error(err)
+	result, err := util.ExecuteCmd(cmd, "hhalias")
+	require.NoError(err)
+	require.Contains(result, "hhalias: Enter your private key")
 }
 
 func TestNewAccountImportKeyStoreCmd(t *testing.T) {
 	require := require.New(t)
+	testWallet, ks, passwd, _, err := newTestAccount()
+	require.NoError(err)
+	defer testutil.CleanupPath(testWallet)
+
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_ioctlclient.NewMockClient(ctrl)
 	client.EXPECT().SelectTranslation(gomock.Any()).Return("", config.English).AnyTimes()
-	client.EXPECT().Config().Return(config.Config{}).AnyTimes()
+	client.EXPECT().ReadSecret().Return(passwd, nil).AnyTimes()
+	client.EXPECT().NewKeyStore().Return(ks).Times(1)
+	client.EXPECT().Config().Return(config.Config{Wallet: testWallet}).Times(1)
+	client.EXPECT().SetAlias(gomock.Any(), gomock.Any()).Return(nil).Times(1)
 
-	testAccountFolder := filepath.Join(os.TempDir(), "testAccountImportKeyStore")
-	require.NoError(os.MkdirAll(testAccountFolder, os.ModePerm))
-	defer func() {
-		require.NoError(os.RemoveAll(testAccountFolder))
-	}()
+	client.EXPECT().DecryptPrivateKey(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(passwordOfKeyStore, keyStorePath string) (*ecdsa.PrivateKey, error) {
+			sk, err := crypto.GenerateKey()
+			require.NoError(err)
+			p256k1, ok := sk.EcdsaPrivateKey().(*ecdsa.PrivateKey)
+			require.True(ok)
+			return p256k1, nil
+		})
 
 	cmd := NewAccountImportKeyStoreCmd(client)
-	result, err := util.ExecuteCmd(cmd, []string{"hh", "./testAccountImportKeyStoreCmd"}...)
-
-	require.NotNil(result)
-	require.Error(err)
+	result, err := util.ExecuteCmd(cmd, "hhalias", testWallet)
+	require.NoError(err)
+	require.Contains(result, "hhalias: Enter your password of keystore")
 }
 
 func TestNewAccountImportPemCmd(t *testing.T) {
 	require := require.New(t)
+	testWallet, _, passwd, _, err := newTestAccount()
+	require.NoError(err)
+	defer testutil.CleanupPath(testWallet)
+
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_ioctlclient.NewMockClient(ctrl)
 	client.EXPECT().SelectTranslation(gomock.Any()).Return("", config.English).AnyTimes()
-	client.EXPECT().Config().Return(config.Config{}).AnyTimes()
+	client.EXPECT().Config().Return(config.Config{Wallet: testWallet}).Times(3)
+
+	priKey2, err := crypto.GenerateKeySm2()
+	require.NoError(err)
+	addr2 := priKey2.PublicKey().Address()
+	pemFilePath := sm2KeyPath(client, addr2)
+	require.NoError(crypto.WritePrivateKeyToPem(pemFilePath, priKey2.(*crypto.P256sm2PrvKey), passwd))
+
+	client.EXPECT().ReadSecret().Return(passwd, nil).AnyTimes()
+	client.EXPECT().SetAlias(gomock.Any(), gomock.Any()).Return(nil).Times(1)
 
 	cmd := NewAccountImportPemCmd(client)
-	result, err := util.ExecuteCmd(cmd, []string{"hh", "./testAccountImportPemCmd"}...)
-
-	require.NotNil(result)
-	require.Error(err)
+	result, err := util.ExecuteCmd(cmd, "hhalias", pemFilePath)
+	require.NoError(err)
+	require.Contains(result, "hhalias: Enter your password of pem file")
 }
 
 func TestValidateAlias(t *testing.T) {
 	require := require.New(t)
-	testFilePath := filepath.Join(os.TempDir(), testPath)
-	defer testutil.CleanupPath(testFilePath)
-	alias := "aaa"
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	client := mock_ioctlclient.NewMockClient(ctrl)
+	client.EXPECT().Config().Return(config.Config{
+		Aliases: map[string]string{
+			"aaa": "io1uwnr55vqmhf3xeg5phgurlyl702af6eju542sx",
+			"bbb": "io1uwnr55vqmhf3xeg5phgurlyl702af6eju542s1",
+		},
+	}).Times(2)
 
-	config.ReadConfig.Aliases = map[string]string{}
-	err := validateAlias(alias)
+	err := validateAlias(client, "ccc")
 	require.NoError(err)
 
-	config.ReadConfig.Aliases[alias] = "a"
-	err = validateAlias(alias)
-	require.Error(err)
+	err = validateAlias(client, "aaa")
+	require.Contains(err.Error(), `alias "aaa" has already used for`)
 
-	alias = strings.Repeat("a", 50)
-	err = validateAlias(alias)
-	require.Error(err)
-}
-
-func TestWriteToFile(t *testing.T) {
-	require := require.New(t)
-	testFilePath := filepath.Join(os.TempDir(), testPath)
-	defer testutil.CleanupPath(testFilePath)
-	alias := "aaa"
-	addr := "bbb"
-
-	err := writeToFile(alias, addr)
-	require.NoError(err)
-}
-
-func TestReadPasswordFromStdin(t *testing.T) {
-	require := require.New(t)
-	_, err := readPasswordFromStdin()
-	require.Error(err)
+	err = validateAlias(client, strings.Repeat("a", 50))
+	require.Contains(err.Error(), "invalid long alias that is more than 40 characters")
 }
