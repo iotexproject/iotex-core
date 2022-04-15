394a777f8ca4746ba436e4f8ecc9081bff257257
LuckyPigeon <lucky90322@gmail.com>
2022-04-15 13:52:50 +0800

    mock response data

--- ioctl/newcmd/account/accountactions_test.go
+++ ioctl/newcmd/account/accountactions_test.go
@@ -9,10 +9,15 @@ package account
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
@@ -47,17 +52,45 @@ func TestNewAccountAction(t *testing.T) {
 		require.Contains(err.Error(), "failed to send request")
 	})
 
-	client.EXPECT().QueryAnalyser(gomock.Any()).DoAndReturn(func(reqData interface{}) (*http.Response, error) {
-		jsonData, err := json.Marshal(reqData)
-		require.NoError(err)
-		resp, err := http.Post("https://iotex-analyser-api-mainnet.chainanalytics.org/api.ActionsService.GetActionsByAddress", "application/json",
-			bytes.NewBuffer(jsonData))
-		require.NoError(err)
-		return resp, nil
-	}).Times(3)
-
 	t.Run("get account action", func(t *testing.T) {
 		client.EXPECT().Address(gomock.Any()).Return(accAddr, nil).Times(2)
+
+		reqData := map[string]string{
+			"address": accAddr,
+			"offset":  fmt.Sprint(0),
+		}
+
+		client.EXPECT().QueryAnalyser(reqData).DoAndReturn(func(reqData interface{}) (*http.Response, error) {
+			jsonData, err := json.Marshal(reqData)
+			require.NoError(err)
+			resp, err := http.Post("https://iotex-analyser-api-mainnet.chainanalytics.org/api.ActionsService.GetActionsByAddress", "application/json",
+				bytes.NewBuffer(jsonData))
+			require.NoError(err)
+
+			timestamp := strconv.Itoa(int(timestamp.Timestamp{Seconds: 10, Nanos: 10}.Seconds))
+			sender := reqData.(map[string]string)["address"]
+			testData := `
+				{
+					"Count": "1",
+					"Results": [{
+						"ActHash":    "9b1d77d8b8902e8d4e662e7cd07d8a74179e032f030d92441ca7fba1ca68e0f4",
+						"Timestamp":  "%s",
+						"BlkHeight":  "1",
+						"RecordType": "1",
+						"ActType":    "1",
+						"Sender":     "%s",
+						"Recipient":  "io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+						"Amount":     "0.020000000132432"
+					}]
+				}
+			`
+			testData = fmt.Sprintf(testData, timestamp, sender)
+			stringReader := strings.NewReader(testData)
+			stringReadCloser := io.NopCloser(stringReader)
+			resp.Body = stringReadCloser
+
+			return resp, nil
+		}).Times(2)
 		cmd := NewAccountActions(client)
 		result, err := util.ExecuteCmd(cmd, accAddr, "0")
 		require.NoError(err)
@@ -68,6 +101,15 @@ func TestNewAccountAction(t *testing.T) {
 	})
 
 	t.Run("empty address", func(t *testing.T) {
+		client.EXPECT().QueryAnalyser(gomock.Any()).DoAndReturn(func(reqData interface{}) (*http.Response, error) {
+			jsonData, err := json.Marshal(reqData)
+			require.NoError(err)
+			resp, err := http.Post("https://iotex-analyser-api-mainnet.chainanalytics.org/api.ActionsService.GetActionsByAddress", "application/json",
+				bytes.NewBuffer(jsonData))
+			require.NoError(err)
+
+			return resp, nil
+		}).Times(1)
 		client.EXPECT().Address(gomock.Any()).Return("", nil)
 		cmd := NewAccountActions(client)
 		_, err := util.ExecuteCmd(cmd, "", "0")
