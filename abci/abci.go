7c7ad93cf79eb591925501108012ce93bb87c78f
huof6890 <68298506@qq.com>
2022-05-24 22:09:52 +0800

    add checktx interface and proto

--- abci/abci.go
+++ abci/abci.go
@@ -1,4 +1,13 @@
 package abci
 
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	"github.com/iotexproject/iotex-core/api"
+)
+
 type Abci interface {
+	CheckTx(ctx context.Context, in *api.RequestCheckTx, opts ...grpc.CallOption) (*api.ResponseCheckTx, error)
 }
