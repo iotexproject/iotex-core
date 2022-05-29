1b5cc021fe518653128af3450994db18a835665b
LuckyPigeon <lucky90322@gmail.com>
2022-05-29 12:59:18 +0800

    [ioctl] build hdwallet import command line into new ioctl

--- ioctl/newcmd/hdwallet/hdwallet.go
+++ ioctl/newcmd/hdwallet/hdwallet.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 IoTeX Foundation
+// Copyright (c) 2022 IoTeX Foundation
 // This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
 // warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
 // permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
@@ -6,6 +6,19 @@
 
 package hdwallet
 
+import (
+	"errors"
+
+	"github.com/iotexproject/iotex-core/ioctl/config"
+)
+
+// Errors
+var (
+	ErrPasswdNotMatch = errors.New("password doesn't match")
+)
+
 // DefaultRootDerivationPath for iotex
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 const DefaultRootDerivationPath = "m/44'/304'"
+
+var _hdWalletConfigFile = config.ReadConfig.Wallet + "/hdwallet"
---
+++ ioctl/newcmd/hdwallet/hdwalletimport.go
@@ -0,0 +1,95 @@
+// Copyright (c) 2022 IoTeX Foundation
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package hdwallet
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/tyler-smith/go-bip39"
+
+	"github.com/iotexproject/iotex-core/ioctl"
+	"github.com/iotexproject/iotex-core/ioctl/config"
+	"github.com/iotexproject/iotex-core/ioctl/util"
+	"github.com/iotexproject/iotex-core/pkg/util/fileutil"
+)
+
+// Multi-language support
+var (
+	_importCmdShorts = map[config.Language]string{
+		config.English: "import hdwallet using mnemonic",
+		config.Chinese: "通过助记词导入钱包",
+	}
+	_importCmdUses = map[config.Language]string{
+		config.English: "import",
+		config.Chinese: "import 导入",
+	}
+)
+
+// NewHdwalletImportCmd represents the hdwallet import command
+func NewHdwalletImportCmd(client ioctl.Client) *cobra.Command {
+	use, _ := client.SelectTranslation(_importCmdUses)
+	short, _ := client.SelectTranslation(_importCmdShorts)
+
+	cmd := &cobra.Command{
+		Use:   use,
+		Short: short,
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			if fileutil.FileExists(_hdWalletConfigFile) {
+				cmd.Println("Please run 'ioctl hdwallet delete' before import")
+				return nil
+			}
+
+			cmd.Println("Enter 12 mnemonic words you saved, separated by space")
+
+			line, err := client.ReadSecret()
+			if err != nil {
+				return err
+			}
+			mnemonic := strings.TrimSpace(line)
+			if _, err = bip39.MnemonicToByteArray(mnemonic); err != nil {
+				return err
+			}
+
+			cmd.Println("Set password")
+			password, err := util.ReadSecretFromStdin()
+			if err != nil {
+				return errors.New("failed to get password")
+			}
+			cmd.Println("Enter password again")
+			passwordAgain, err := client.ReadSecret()
+			if err != nil {
+				return errors.New("failed to get password")
+			}
+			if password != passwordAgain {
+				return ErrPasswdNotMatch
+			}
+
+			enctxt := append([]byte(mnemonic), util.HashSHA256([]byte(mnemonic))...)
+			enckey := util.HashSHA256([]byte(password))
+			out, err := util.Encrypt(enctxt, enckey)
+			if err != nil {
+				return errors.New("failed to encrypting mnemonic")
+			}
+
+			if err := os.WriteFile(_hdWalletConfigFile, out, 0600); err != nil {
+				return fmt.Errorf(fmt.Sprintf("failed to write to config file %s", _hdWalletConfigFile))
+			}
+
+			cmd.Println(fmt.Sprintf("Mnemonic phrase: %s\n"+
+				"It is used to recover your wallet in case you forgot the password. Write them down and store it in a safe place.", mnemonic))
+			return nil
+		},
+	}
+	return cmd
+}
