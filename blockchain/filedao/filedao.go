bb7207df9d2b12bdfa3c3640c37f2571cc35968f
dustinxie <dahuaxie@gmail.com>
2022-06-24 12:16:33 -0700

    Update filedao.go

--- blockchain/filedao/filedao.go
+++ blockchain/filedao/filedao.go
@@ -79,7 +79,6 @@ type (
 		currFd       BaseFileDAO
 		legacyFd     FileDAO
 		v2Fd         *FileV2Manager // a collection of v2 db files
-		deser        *block.Deserializer
 		evmNetworkID uint32
 	}
 )
