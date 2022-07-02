bb3f8ad53a52bfa38e27b2f37b87b6ebe1ebad5c
LuckyPigeon <lucky90322@gmail.com>
2022-07-02 15:53:07 +0800

    [api] implement TestGrpcServer_GetActions by gomock CoreService interface

--- api/grpcserver_test.go
+++ api/grpcserver_test.go
@@ -27,7 +27,37 @@ func TestGrpcServer_GetAccount(t *testing.T) {
 }
 
 func TestGrpcServer_GetActions(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	core := mock_apicoreservice.NewMockCoreService(ctrl)
+	grpcSvr := newGRPCHandler(core)
 
+	for _, test := range _getActionsTests {
+		actInfo := &iotexapi.ActionInfo{
+			Index:   0,
+			ActHash: "test",
+		}
+		response := []*iotexapi.ActionInfo{}
+		request := &iotexapi.GetActionsRequest{
+			Lookup: &iotexapi.GetActionsRequest_ByIndex{
+				ByIndex: &iotexapi.GetActionsByIndexRequest{
+					Start: test.start,
+					Count: test.count,
+				},
+			},
+		}
+
+		for i := 1; i <= test.numActions; i++ {
+			response = append(response, actInfo)
+		}
+
+		core.EXPECT().Actions(gomock.Any(), gomock.Any()).Return(response, nil)
+
+		res, err := grpcSvr.GetActions(context.Background(), request)
+		require.NoError(err)
+		require.Equal(uint64(test.numActions), res.Total)
+	}
 }
 
 func TestGrpcServer_GetAction(t *testing.T) {
