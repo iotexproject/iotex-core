c146701992a3022c803e8ea28481b93afd136d6b
millken <millken@gmail.com>
2022-07-13 19:46:47 +0800

    use runtime.fastrand as random source

--- pkg/util/randutil/randutil.go
+++ pkg/util/randutil/randutil.go
@@ -2,8 +2,7 @@ package randutil
 
 import (
 	"crypto/rand"
-	"math"
-	"math/big"
+	_ "unsafe" // For go:linkname
 )
 
 var (
@@ -11,25 +10,19 @@ var (
 	Read = rand.Read
 )
 
-// Intn returns a random int in [0, n) use cryto random source
-func Intn(n int) int {
-	if n <= 0 {
-		panic("invalid argument to Intn")
-	}
-	rnd, err := rand.Int(rand.Reader, big.NewInt(int64(n)))
-	if err != nil {
-		panic(err)
-	}
-	return int(rnd.Int64())
+// Uint64 returns a pseudo-random 64-bit value as a uint64.
+func Uint64() uint64 {
+	return uint64(fastrand())<<32 | uint64(fastrand())
 }
 
-// Int63 returns a random int in [0, math.MaxInt64) use cryto random source
+// Intn returns a random int in [0, n) use cryto random source
+func Intn(n int) int {
+	return int(Int63n(int64(n)))
+}
+
+// Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
 func Int63() int64 {
-	rnd, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	if err != nil {
-		panic(err)
-	}
-	return rnd.Int64()
+	return int64(Uint64() & ((1 << 63) - 1))
 }
 
 // Int63n returns, as an int64, a non-negative pseudo-random number in the half-open interval [0,n).
@@ -54,3 +47,9 @@ func Int() int {
 	u := uint(Int63())
 	return int(u << 1 >> 1) // clear sign bit if int == int32
 }
+
+// fastrand is a fast thread local random function built into the Go runtime
+// but not normally exposed.  On Linux x86_64, this is aesrand seeded by
+// /dev/urandom.
+//go:linkname fastrand runtime.fastrand
+func fastrand() uint32
--- pkg/util/randutil/randutil_test.go
+++ pkg/util/randutil/randutil_test.go
@@ -7,21 +7,59 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestInt(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		max  int64
+		fn   func() int64
+	}{
+		{"Int63", math.MaxInt64, Int63},
+		{"Int", int64(math.MaxInt), func() int64 { return int64(Int()) }},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			var min, max, set, unset int64
+			unset = math.MaxInt64
+			for i := 0; i < 100; i++ {
+				v := test.fn()
+				if v < min {
+					min = v
+				}
+				if v > max {
+					max = v
+				}
+				set |= v
+				unset &= v
+			}
+			assert.GreaterOrEqual(max, test.max-(test.max>>2), "no output near expected max")
+			assert.Less(max, test.max, "shouldn't be less than test.max")
+			assert.LessOrEqual(min, test.max>>2, "no output near expected min")
+			assert.GreaterOrEqual(int64(0), min, "shouldn't be less than 0")
+			assert.Equal(test.max, set, "all bits should've been set at least once")
+			assert.Empty(unset, "all bit should've been unset at least once")
+		})
+	}
+}
+
+func TestUint64(t *testing.T) {
+	assert := assert.New(t)
+	n := Uint64()
+	assert.GreaterOrEqual(n, uint64(0))
+	assert.LessOrEqual(n, uint64(math.MaxUint64))
+}
+
 func TestIntn(t *testing.T) {
 	assert := assert.New(t)
 	n := Intn(10)
 	assert.GreaterOrEqual(n, 0)
 	assert.Panics(func() { Intn(0) }, "The code did not panic")
-}
-
-func TestInt63(t *testing.T) {
-	assert := assert.New(t)
-	for i := int64(0); i < 10; i++ {
-		n := Int63()
-		assert.GreaterOrEqual(n, int64(0))
-		assert.LessOrEqual(n, int64(math.MaxInt64))
-
-	}
 }
 
 func TestInt63n(t *testing.T) {
@@ -32,13 +70,6 @@ func TestInt63n(t *testing.T) {
 		assert.LessOrEqual(n, int64(10))
 	}
 	assert.Panics(func() { Int63n(0) }, "The code did not panic")
-}
-
-func TestInt(t *testing.T) {
-	assert := assert.New(t)
-	n := Int()
-	assert.GreaterOrEqual(n, 0)
-	assert.LessOrEqual(n, math.MaxInt)
 }
 
 func TestRand(t *testing.T) {
