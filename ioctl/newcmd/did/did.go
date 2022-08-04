6ef58187d219e035a5fc730d3b3a304978a5ecce
LuckyPigeon <lucky90322@gmail.com>
2022-08-05 04:25:15 +0800

    update short description

--- ioctl/newcmd/did/did.go
+++ ioctl/newcmd/did/did.go
@@ -7,23 +7,33 @@
 package did
 
 import (
+	"github.com/spf13/cobra"
+
 	"github.com/iotexproject/iotex-core/ioctl"
 	"github.com/iotexproject/iotex-core/ioctl/config"
-	"github.com/spf13/cobra"
 )
 
 // Multi-language support
 var (
 	_dIDCmdShorts = map[config.Language]string{
-		config.English: "DID command",
-		config.Chinese: "DID command",
+		config.English: "Manage Decentralized Identity of IoTeX blockchain",
+		config.Chinese: "管理IoTeX区块链上的去中心化数字身份",
 	}
+)
+
+const (
+	_registerDIDName   = "registerDID"
+	_getHashName       = "getHash"
+	_getURIName        = "getURI"
+	_updateDIDName     = "updateDID"
+	_deregisterDIDName = "deregisterDID"
+	// DIDABI is the did abi
+	DIDABI = `[{"constant": false,"inputs": [],"name": "deregisterDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"internalType": "bytes","name": "did","type": "bytes"}],"name": "getHash","outputs": [{"internalType": "bytes32","name": "","type": "bytes32"}],"payable": false,"stateMutability": "view","type": "function"},   {"constant": true,"inputs": [{"internalType": "bytes","name": "did","type": "bytes"}],"name": "getURI","outputs": [{"internalType": "bytes","name": "","type": "bytes"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": false,"inputs": [{"internalType": "bytes32","name": "h","type": "bytes32"},{"internalType": "bytes","name": "uri","type": "bytes"}],"name": "registerDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [{"internalType": "bytes32","name": "h","type": "bytes32"},{"internalType": "bytes","name": "uri","type": "bytes"}],"name": "updateDID","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"}]`
 )
 
 // NewDidCmd represents the did command
 func NewDidCmd(client ioctl.Client) *cobra.Command {
 	short, _ := client.SelectTranslation(_dIDCmdShorts)
-
 	cmd := &cobra.Command{
 		Use:   "did",
 		Short: short,
