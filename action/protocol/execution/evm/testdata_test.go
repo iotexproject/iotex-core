8a3e37a9088f7f823f314646744b86090d4dde8a
dustinxie <dahuaxie@gmail.com>
2022-09-20 17:36:16 -0500

    [evm] snapshot gas refund (#3637)

--- action/protocol/execution/evm/testdata_test.go
+++ action/protocol/execution/evm/testdata_test.go
@@ -56,6 +56,7 @@ type (
 		balance            []bal
 		codes              []code
 		states             []evmSet
+		refund             uint64
 		suicide            []sui
 		preimage           []image
 		accessList         []access
