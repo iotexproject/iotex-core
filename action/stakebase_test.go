24e9239f3b4d68d639f312852adf0f4e3f4f6c53
ququzone <xueping.yang@gmail.com>
2022-11-03 10:41:29 +0800

    add test for stake base

---
+++ action/stakebase_test.go
@@ -0,0 +1,54 @@
+package action
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCallDataToStakeStateContext_Error(t *testing.T) {
+	r := require.New(t)
+
+	data, _ := hex.DecodeString("8ae8a8a4")
+	req, err := CallDataToStakeStateContext(data)
+
+	r.Nil(req)
+	r.EqualValues("invalid call sig", err.Error())
+}
+
+func TestCallDataToStakeStateContext_Invalid(t *testing.T) {
+	r := require.New(t)
+
+	data, _ := hex.DecodeString("8ae8a8")
+	req, err := CallDataToStakeStateContext(data)
+
+	r.Nil(req)
+	r.EqualValues("invalid call binary data", err.Error())
+}
+
+func TestEncodeCandidateToEth(t *testing.T) {
+	r := require.New(t)
+
+	candidate := &iotextypes.CandidateV2{
+		OwnerAddress:       "io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqps833xv",
+		OperatorAddress:    "io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqz75y8gn",
+		RewardAddress:      "io1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrrzsj4p",
+		Name:               "hello",
+		TotalWeightedVotes: "10000000000000000000",
+		SelfStakeBucketIdx: 100,
+		SelfStakingTokens:  "5000000000000000000",
+	}
+
+	cand, err := encodeCandidateToEth(candidate)
+
+	r.Nil(err)
+	r.EqualValues("0x0000000000000000000000000000000000000001", cand.OwnerAddress.Hex())
+	r.EqualValues("0x0000000000000000000000000000000000000002", cand.OperatorAddress.Hex())
+	r.EqualValues("0x0000000000000000000000000000000000000003", cand.RewardAddress.Hex())
+	r.EqualValues("hello", cand.Name)
+	r.EqualValues("10000000000000000000", cand.TotalWeightedVotes.String())
+	r.EqualValues(100, cand.SelfStakeBucketIdx)
+	r.EqualValues("5000000000000000000", cand.SelfStakingTokens.String())
+}
