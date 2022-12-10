b15f483725eb73af3fed0e9a794c2494882fcce3
LuckyPigeon <lucky90322@gmail.com>
2022-12-11 03:02:34 +0800

    [ioctl] build contract invoke command line into new ioctl

---
+++ ioctl/newcmd/contract/contractinvoke.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2022 IoTeX Foundation
+// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
+// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
+// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package contract
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/iotexproject/iotex-core/ioctl"
+	"github.com/iotexproject/iotex-core/ioctl/config"
+)
+
+// Multi-language support
+var (
+	_invokeCmdShorts = map[config.Language]string{
+		config.English: "Invoke smart contract on IoTeX blockchain",
+		config.Chinese: "调用IoTeX区块链上的智能合约",
+	}
+)
+
+// NewContractInvokeCmd represents the contract invoke command
+func NewContractInvokeCmd(client ioctl.Client) *cobra.Command {
+	short, _ := client.SelectTranslation(_invokeCmdShorts)
+
+	cmd := &cobra.Command{
+		Use:   "invoke",
+		Short: short,
+	}
+	// cmd.AddCommand(NewContractInvokeFunctionCmd)
+	// cmd.AddCommand(NewContractInvokeBytecodeCmd)
+	// action.RegisterWriteCommand(NewContractInvokeFunctionCmd)
+	// action.RegisterWriteCommand(NewContractInvokeBytecodeCmd)
+	return cmd
+}
