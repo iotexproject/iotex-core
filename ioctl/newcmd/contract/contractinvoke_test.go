7acb6bdb7dfbad6ba1592e91cbb92eb7ecb3225d
LuckyPigeon <lucky90322@gmail.com>
2022-12-18 15:30:32 +0800

    remove stale comments

--- ioctl/newcmd/contract/contractinvoke_test.go
+++ ioctl/newcmd/contract/contractinvoke_test.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 IoTeX Foundation
-// This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
-// warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
-// permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
+// This source code is provided 'as is' and no warranties are given as to title or non-infringement,
+// merchantability or fitness for purpose and, to the extent permitted by law,
+// all liability for your use of the code is disclaimed. This source code is governed by Apache
 // License 2.0 that can be found in the LICENSE file.
 
 package contract
@@ -22,7 +22,6 @@ func TestNewContractInvokeCmd(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_ioctlclient.NewMockClient(ctrl)
-
 	client.EXPECT().SelectTranslation(gomock.Any()).Return("contract", config.English).AnyTimes()
 
 	t.Run("compile contract", func(t *testing.T) {
