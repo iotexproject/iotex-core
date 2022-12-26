53dc4c10dc47e08dbe7085dc9145801b092d39d4
LuckyPigeon <lucky90322@gmail.com>
2022-12-26 21:21:15 +0800

    remove stale comments

---
+++ ioctl/newcmd/contract/contractcompile_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 IoTeX Foundation
+// This source code is provided 'as is' and no warranties are given as to title or non-infringement,
+// merchantability or fitness for purpose and, to the extent permitted by law,
+// all liability for your use of the code is disclaimed. This source code is governed by Apache
+// License 2.0 that can be found in the LICENSE file.
+
+package contract
+
+import (
+	"testing"
+)
+
+func TestNewContractCompileCmd(t *testing.T) {
+	// require := require.New(t)
+	// ctrl := gomock.NewController(t)
+	// defer ctrl.Finish()
+	// client := mock_ioctlclient.NewMockClient(ctrl)
+
+	// client.EXPECT().SelectTranslation(gomock.Any()).Return("contract", config.English).AnyTimes()
+	// client.EXPECT().SetEndpointWithFlag(gomock.Any())
+	// client.EXPECT().SetInsecureWithFlag(gomock.Any())
+
+	// t.Run("compile contract", func(t *testing.T) {
+	// 	cmd := NewContractCompileCmd(client)
+	// 	_, err := util.ExecuteCmd(cmd, "", "", "")
+	// 	require.NoError(err)
+	// })
+}
