819227b1c5b347a46b4af99289256f11b5819aa6
Haaai <55118568+Liuhaai@users.noreply.github.com>
2023-02-15 10:48:50 -0800

    Update coreservice.go

--- api/coreservice.go
+++ api/coreservice.go
@@ -491,9 +491,9 @@ func (core *coreService) ReadContract(ctx context.Context, callerAddr address.Ad
 		return "", nil, err
 	}
 	sc.SetNonce(state.PendingNonce())
-	actGasLimit := core.bc.Genesis().ActionGasLimit
-	if sc.GasLimit() == 0 || actGasLimit < sc.GasLimit() {
-		sc.SetGasLimit(actGasLimit)
+	blockGasLimit := core.bc.Genesis().BlockGasLimit
+	if sc.GasLimit() == 0 || blockGasLimit < sc.GasLimit() {
+		sc.SetGasLimit(blockGasLimit)
 	}
 	sc.SetGasPrice(big.NewInt(0)) // ReadContract() is read-only, use 0 to prevent insufficient gas
 
