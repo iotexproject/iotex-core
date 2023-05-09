d3bcabe14f3f0b122761bff6799843f90adfc3e4
millken <millken@gmail.com>
2023-05-09 09:41:53 +0800

    update contract and test

--- action/protocol/execution/testdata-istanbul/iip15-manager.sol
+++ action/protocol/execution/testdata-istanbul/iip15-manager.sol
@@ -8,7 +8,6 @@ contract IIP15Manager is Ownable {
 
     struct ContractInfo {
         address recipient;
-        bool isRegistered;
         bool isApproved;
     }
 
@@ -23,13 +22,13 @@ contract IIP15Manager is Ownable {
         require(_contractAddress != address(0), "Contract address cannot be zero");
         require(_recipient != address(0), "Recipient address cannot be zero");
         require(!contractRegistry[_contractAddress].isApproved, "Contract is already approved");
-        contractRegistry[_contractAddress] = ContractInfo(_recipient, true, false);
+        contractRegistry[_contractAddress] = ContractInfo(_recipient, false);
         emit ContractRegistered(_contractAddress, _recipient);
     }
 
     function approveContract(address _contractAddress) public onlyOwner {
         ContractInfo storage contractInfo = contractRegistry[_contractAddress];
-        require(contractInfo.isRegistered, "Contract is not registered");
+        require(contractInfo.recipient != address(0), "Contract is not registered");
         require(!contractInfo.isApproved, "Contract is already approved");
         contractInfo.isApproved = true;
         emit ContractApproved(_contractAddress);
@@ -37,14 +36,15 @@ contract IIP15Manager is Ownable {
 
     function disapproveContract(address _contractAddress) public onlyOwner {
         ContractInfo storage contractInfo = contractRegistry[_contractAddress];
-        require(contractInfo.isRegistered, "Contract is not registered");
+        require(contractInfo.recipient != address(0), "Contract is not registered");
         require(contractInfo.isApproved, "Contract is not approved");
         contractInfo.isApproved = false;
         emit ContractUnapproved(_contractAddress);
     }
 
     function removeContract(address _contractAddress) public onlyOwner {
-        require(contractRegistry[_contractAddress].isRegistered, "Contract is not registered");
+        ContractInfo storage contractInfo = contractRegistry[_contractAddress];
+        require(contractInfo.recipient != address(0), "Contract is not registered");
         delete contractRegistry[_contractAddress];
         emit ContractRemoved(_contractAddress);
     }
