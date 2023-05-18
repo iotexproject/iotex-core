c62fc489a188fef021402ffaa5eae85bb78b6c9d
envestcc <chen1233216@hotmail.com>
2023-05-18 10:32:40 +0800

    fix panic

--- action/protocol/staking/protocol.go
+++ action/protocol/staking/protocol.go
@@ -471,11 +471,11 @@ func (p *Protocol) ActiveCandidates(ctx context.Context, sr protocol.StateReader
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get ActiveCandidates")
 	}
-	featureCtx := protocol.MustGetFeatureCtx(ctx)
+	featureCtx, ok := protocol.GetFeatureCtx(ctx)
 	list := c.AllCandidates()
 	cand := make(CandidateList, 0, len(list))
 	for i := range list {
-		if featureCtx.AddLSDVotes {
+		if ok && featureCtx.AddLSDVotes {
 			list[i].Votes.Add(list[i].Votes, p.liquidIndexer.CandidateVotes(list[i].Owner))
 		}
 		if list[i].SelfStake.Cmp(p.config.RegistrationConsts.MinSelfStake) >= 0 {
