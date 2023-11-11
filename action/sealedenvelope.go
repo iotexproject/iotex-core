7e93d6913b696fa3041e32f494734f3579a7b523
dustinxie <dahuaxie@gmail.com>
2023-11-10 22:06:30 -0800

    [action] add NewEthSigner() and pass Signer as input to rlpRawHash() and rlpSignedHash() (#3967)

--- action/sealedenvelope.go
+++ action/sealedenvelope.go
@@ -39,7 +39,11 @@ func (sealed *SealedEnvelope) envelopeHash() (hash.Hash256, error) {
 		if err != nil {
 			return hash.ZeroHash256, err
 		}
-		return rlpRawHash(tx, sealed.evmNetworkID)
+		signer, err := NewEthSigner(sealed.encoding, sealed.evmNetworkID)
+		if err != nil {
+			return hash.ZeroHash256, err
+		}
+		return rlpRawHash(tx, signer)
 	case iotextypes.Encoding_IOTEX_PROTOBUF:
 		return hash.Hash256b(byteutil.Must(proto.Marshal(sealed.Envelope.Proto()))), nil
 	default:
@@ -71,7 +75,11 @@ func (sealed *SealedEnvelope) calcHash() (hash.Hash256, error) {
 		if err != nil {
 			return hash.ZeroHash256, err
 		}
-		return rlpSignedHash(tx, sealed.evmNetworkID, sealed.Signature())
+		signer, err := NewEthSigner(sealed.encoding, sealed.evmNetworkID)
+		if err != nil {
+			return hash.ZeroHash256, err
+		}
+		return rlpSignedHash(tx, signer, sealed.Signature())
 	case iotextypes.Encoding_IOTEX_PROTOBUF:
 		return hash.Hash256b(byteutil.Must(proto.Marshal(sealed.Proto()))), nil
 	default:
@@ -146,7 +154,11 @@ func (sealed *SealedEnvelope) loadProto(pbAct *iotextypes.Action, evmID uint32) 
 		if err != nil {
 			return err
 		}
-		if _, err = rlpSignedHash(tx, evmID, pbAct.GetSignature()); err != nil {
+		signer, err := NewEthSigner(encoding, evmID)
+		if err != nil {
+			return err
+		}
+		if _, err = rlpSignedHash(tx, signer, pbAct.GetSignature()); err != nil {
 			return err
 		}
 		sealed.evmNetworkID = evmID
