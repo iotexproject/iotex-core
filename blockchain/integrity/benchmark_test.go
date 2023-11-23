874618f33a05412802d65c79ecb5f69b0a05523c
envestcc <chen1233216@hotmail.com>
2023-11-23 12:40:47 +0800

    address comments

--- blockchain/integrity/benchmark_test.go
+++ blockchain/integrity/benchmark_test.go
@@ -277,7 +277,7 @@ func newChainInDB() (blockchain.Blockchain, actpool.ActPool, error) {
 	if bc == nil {
 		return nil, nil, errors.New("pointer is nil")
 	}
-	ep := execution.NewProtocol(dao.GetBlockHash, rewarding.DepositGasWithSGD, nil, func(u uint64) (time.Time, error) { return time.Time{}, nil })
+	ep := execution.NewProtocol(dao.GetBlockHash, rewarding.DepositGasWithSGD, nil, fakeGetBlockTime)
 	if err = ep.Register(registry); err != nil {
 		return nil, nil, err
 	}
