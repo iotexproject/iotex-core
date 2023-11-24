3170059580dc39ca2f104c96fadbc0264d1b2132
saito <birdyfj@gmail.com>
2023-11-24 14:39:37 +0800

    feat(ioctl): support ws configurations (#3986)

--- ioctl/config/config.go
+++ ioctl/config/config.go
@@ -67,6 +67,8 @@ type Config struct {
 	Language         string            `json:"language" yaml:"language"`
 	Nsv2height       uint64            `json:"nsv2height" yaml:"nsv2height"`
 	AnalyserEndpoint string            `json:"analyserEndpoint" yaml:"analyserEndpoint"`
+	// WsEndpint w3bstream endpoint
+	WsEndpoint string `json:"wsEndpoint" yaml:"wsEndpoint"`
 }
 
 var (
