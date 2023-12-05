1d330a715d0e69786171bb85cb4ae07cca9ca1d5
envestcc <chen1233216@hotmail.com>
2023-12-05 16:02:29 +0800

    handle CandidateSelfStake action

---
+++ action/protocol/staking/handler_candidate_selfstake.go
@@ -0,0 +1,150 @@
+package staking
+
+import (
+	"context"
+	"math"
+	"math/big"
+
+	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"github.com/pkg/errors"
+
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/action/protocol"
+)
+
+const (
+	handleCandidateSelfStake = "candidateSelfStake"
+
+	candidateNoSelfStakeBucketIndex = math.MaxUint64
+)
+
+func (p *Protocol) handleCandidateSelfStake(ctx context.Context, act *action.CandidateActivate, csm CandidateStateManager,
+) (*receiptLog, []*action.TransactionLog, error) {
+	var (
+		bucket     *VoteBucket
+		prevBucket *VoteBucket
+		err        error
+		rErr       ReceiptError
+		txLogs     []*action.TransactionLog
+		cand       *Candidate
+		bucketCand *Candidate
+
+		actCtx     = protocol.MustGetActionCtx(ctx)
+		featureCtx = protocol.MustGetFeatureCtx(ctx)
+		log        = newReceiptLog(p.addr.String(), handleCandidateSelfStake, featureCtx.NewStakingReceiptFormat)
+	)
+	// caller must be the owner of a candidate
+	cand = csm.GetByOwner(actCtx.Caller)
+	if cand == nil {
+		return log, nil, errCandNotExist
+	}
+	if cand.SelfStakeBucketIdx != candidateNoSelfStakeBucketIndex {
+		prevBucket, err = p.fetchBucket(csm, cand.SelfStakeBucketIdx)
+		if err != nil {
+			return log, nil, err
+		}
+	}
+
+	bucket, rErr = p.fetchBucket(csm, act.BucketID())
+	if rErr != nil {
+		return log, nil, rErr
+	}
+	if bucketCand = csm.GetByOwner(bucket.Candidate); bucketCand == nil {
+		return log, nil, &handleError{
+			err:           errors.New("bucket candidate does not exist"),
+			failureStatus: iotextypes.ReceiptStatus_ErrInvalidBucketType,
+		}
+	}
+	esm := NewEndorsementStateManager(csm.SM())
+	if err = p.validateBucketSelfStake(ctx, csm, esm, bucket, cand); err != nil {
+		return log, nil, err
+	}
+
+	// unbind previous bucket
+	if prevBucket != nil {
+		cand.SubVote(p.calculateVoteWeight(prevBucket, true))
+		cand.AddVote(p.calculateVoteWeight(prevBucket, false))
+		cand.SelfStake = big.NewInt(0)
+	}
+	// change bucket candidate
+	if !address.Equal(bucket.Candidate, cand.Owner) {
+		if err = p.changeBucketCandidate(csm, bucket, bucketCand, cand); err != nil {
+			return log, nil, err
+		}
+	}
+	// bind new bucket
+	cand.SelfStakeBucketIdx = bucket.Index
+	cand.SelfStake = big.NewInt(0).SetBytes(bucket.StakedAmount.Bytes())
+	cand.SubVote(p.calculateVoteWeight(bucket, false))
+	cand.AddVote(p.calculateVoteWeight(bucket, true))
+
+	if err = csm.Upsert(cand); err != nil {
+		return log, nil, csmErrorToHandleError(cand.Owner.String(), err)
+	}
+
+	return log, txLogs, nil
+}
+
+func (p *Protocol) validateBucketSelfStake(ctx context.Context, csm CandidateStateManager, esm *EndorsementStateManager, bucket *VoteBucket, cand *Candidate) ReceiptError {
+	blkCtx := protocol.MustGetBlockCtx(ctx)
+	if err := validateBucketMinAmount(bucket, p.config.RegistrationConsts.MinSelfStake); err != nil {
+		return err
+	}
+	if err := validateBucketStake(bucket, true); err != nil {
+		return err
+	}
+	if err := validateBucketSelfStake(csm, bucket, false); err != nil {
+		return err
+	}
+	if err := validateBucketCandidate(bucket, cand.Owner); err != nil {
+		return err
+	}
+	if validateBucketOwner(bucket, cand.Owner) != nil &&
+		validateBucketEndorsement(esm, bucket, true, blkCtx.BlockHeight) != nil {
+		return &handleError{
+			err:           errors.New("bucket is not a self-owned or endorsed bucket"),
+			failureStatus: iotextypes.ReceiptStatus_ErrUnauthorizedOperator,
+		}
+	}
+	return nil
+}
+
+func (p *Protocol) changeBucketCandidate(csm CandidateStateManager, bucket *VoteBucket, prevCand, cand *Candidate) error {
+	// update bucket index
+	if err := csm.delCandBucketIndex(bucket.Candidate, bucket.Index); err != nil {
+		return err
+	}
+	if err := csm.putCandBucketIndex(cand.Owner, bucket.Index); err != nil {
+		return err
+	}
+	// update bucket candidate
+	bucket.Candidate = cand.Owner
+	if err := csm.updateBucket(bucket.Index, bucket); err != nil {
+		return err
+	}
+	// update previous candidate
+	weightedVotes := p.calculateVoteWeight(bucket, false)
+	if err := prevCand.SubVote(weightedVotes); err != nil {
+		return &handleError{
+			err:           errors.Wrapf(err, "failed to subtract vote for previous candidate %s", prevCand.Owner.String()),
+			failureStatus: iotextypes.ReceiptStatus_ErrNotEnoughBalance,
+		}
+	}
+	if err := csm.Upsert(prevCand); err != nil {
+		return csmErrorToHandleError(prevCand.Owner.String(), err)
+	}
+
+	// update current candidate
+	if err := cand.AddVote(weightedVotes); err != nil {
+		return &handleError{
+			err:           errors.Wrapf(err, "failed to add vote for candidate %s", cand.Owner.String()),
+			failureStatus: iotextypes.ReceiptStatus_ErrInvalidBucketAmount,
+		}
+	}
+	if err := csm.Upsert(cand); err != nil {
+		return csmErrorToHandleError(cand.Owner.String(), err)
+	}
+
+	return nil
+}
---
+++ action/protocol/staking/handler_candidate_selfstake_test.go
@@ -0,0 +1,390 @@
+package staking
+
+import (
+	"context"
+	"math"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/iotex-core/action"
+	"github.com/iotexproject/iotex-core/action/protocol"
+	accountutil "github.com/iotexproject/iotex-core/action/protocol/account/util"
+	"github.com/iotexproject/iotex-core/blockchain/genesis"
+	"github.com/iotexproject/iotex-core/pkg/unit"
+	"github.com/iotexproject/iotex-core/test/identityset"
+	"github.com/iotexproject/iotex-core/testutil/testdb"
+)
+
+var (
+	timeBeforeBlockI  = time.Unix(1502044560, 0)
+	timeBeforeBlockII = time.Unix(1602044560, 0)
+	timeBlock         = time.Unix(1702044560, 0)
+)
+
+type (
+	bucketConfig struct {
+		Candidate       address.Address
+		Owner           address.Address
+		StakedAmountStr string
+		StakedDuration  uint32
+		AutoStake       bool
+		SelfStake       bool
+		UnstakeTime     *time.Time
+	}
+	candidateConfig struct {
+		Owner    address.Address
+		Operator address.Address
+		Reward   address.Address
+		Name     string
+	}
+	expectCandidate struct {
+		owner                  address.Address
+		candSelfStakeIndex     uint64
+		candSelfStakeAmountStr string
+		candVoteStr            string
+	}
+	expectBucket struct {
+		id        uint64
+		candidate address.Address
+	}
+)
+
+func initTestState(t *testing.T, ctrl *gomock.Controller, bucketCfgs []*bucketConfig, candidateCfgs []*candidateConfig) (protocol.StateManager, *Protocol, []*VoteBucket, []*Candidate) {
+	require := require.New(t)
+	sm := testdb.NewMockStateManager(ctrl)
+	csm := newCandidateStateManager(sm)
+	_, err := sm.PutState(
+		&totalBucketCount{count: 0},
+		protocol.NamespaceOption(_stakingNameSpace),
+		protocol.KeyOption(TotalBucketKey),
+	)
+	require.NoError(err)
+
+	// create protocol
+	p, err := NewProtocol(depositGas, &BuilderConfig{
+		Staking:                  genesis.Default.Staking,
+		PersistStakingPatchBlock: math.MaxUint64,
+	}, nil, nil, genesis.Default.GreenlandBlockHeight)
+	require.NoError(err)
+
+	// set up bucket
+	buckets := []*VoteBucket{}
+	candVotesMap := make(map[string]*big.Int)
+	selfStakeMap := make(map[string]uint64)
+	for _, bktCfg := range bucketCfgs {
+		amount, _ := big.NewInt(0).SetString(bktCfg.StakedAmountStr, 10)
+		// bkt := NewVoteBucket(bktCfg.Candidate, bktCfg.Owner, amount, bktCfg.StakedDuration, time.Now(), bktCfg.AutoStake)
+		bkt := &VoteBucket{
+			Candidate:        bktCfg.Candidate,
+			Owner:            bktCfg.Owner,
+			StakedAmount:     amount,
+			StakedDuration:   time.Duration(bktCfg.StakedDuration) * 24 * time.Hour,
+			CreateTime:       timeBeforeBlockI,
+			StakeStartTime:   timeBeforeBlockI,
+			UnstakeStartTime: time.Unix(0, 0).UTC(),
+			AutoStake:        bktCfg.AutoStake,
+		}
+		if bktCfg.UnstakeTime != nil {
+			bkt.UnstakeStartTime = bktCfg.UnstakeTime.UTC()
+		}
+		_, err = csm.putBucketAndIndex(bkt)
+		require.NoError(err)
+		buckets = append(buckets, bkt)
+		if _, ok := candVotesMap[bkt.Candidate.String()]; !ok {
+			candVotesMap[bkt.Candidate.String()] = big.NewInt(0)
+		}
+		candVotesMap[bkt.Candidate.String()].Add(candVotesMap[bkt.Candidate.String()], p.calculateVoteWeight(bkt, bktCfg.SelfStake))
+		if bktCfg.SelfStake {
+			selfStakeMap[bkt.Candidate.String()] = bkt.Index
+		}
+	}
+
+	// set up candidate
+	candidates := []*Candidate{}
+	for _, candCfg := range candidateCfgs {
+		selfStakeAmount := big.NewInt(0)
+		if selfStakeMap[candCfg.Owner.String()] != 0 {
+			selfStakeAmount = selfStakeAmount.SetBytes(buckets[selfStakeMap[candCfg.Owner.String()]].StakedAmount.Bytes())
+		}
+		votes := big.NewInt(0)
+		if candVotesMap[candCfg.Owner.String()] != nil {
+			votes = votes.Add(votes, candVotesMap[candCfg.Owner.String()])
+		}
+		cand := &Candidate{
+			Owner:              candCfg.Owner,
+			Operator:           candCfg.Operator,
+			Reward:             candCfg.Reward,
+			Name:               candCfg.Name,
+			Votes:              votes,
+			SelfStakeBucketIdx: selfStakeMap[candCfg.Owner.String()],
+			SelfStake:          selfStakeAmount,
+		}
+		require.NoError(csm.putCandidate(cand))
+		candidates = append(candidates, cand)
+	}
+
+	ctx := genesis.WithGenesisContext(context.Background(), genesis.Default)
+	ctx = protocol.WithFeatureWithHeightCtx(ctx)
+	v, err := p.Start(ctx, sm)
+	require.NoError(err)
+	cc, ok := v.(*ViewData)
+	require.True(ok)
+	require.NoError(sm.WriteView(_protocolID, cc))
+
+	return sm, p, buckets, candidates
+}
+
+func TestProtocol_HandleCandidateSelfStake(t *testing.T) {
+	require := require.New(t)
+	ctrl := gomock.NewController(t)
+	// NOT change existed items in initBucketCfgs and initCandidateCfgs
+	// only append new items to the end of the list if needed
+	initBucketCfgs := []*bucketConfig{
+		{identityset.Address(1), identityset.Address(1), "1", 1, true, false, nil},
+		{identityset.Address(1), identityset.Address(1), "1200000000000000000000000", 30, true, false, nil},
+		{identityset.Address(1), identityset.Address(1), "1200000000000000000000000", 30, true, false, &timeBeforeBlockII},
+		{identityset.Address(2), identityset.Address(1), "1200000000000000000000000", 30, true, true, nil},
+		{identityset.Address(1), identityset.Address(2), "1200000000000000000000000", 30, true, false, nil},
+		{identityset.Address(2), identityset.Address(1), "1200000000000000000000000", 30, true, false, nil},
+		{identityset.Address(2), identityset.Address(2), "1200000000000000000000000", 30, true, true, nil},
+	}
+	initCandidateCfgs := []*candidateConfig{
+		{identityset.Address(1), identityset.Address(7), identityset.Address(1), "test1"},
+		{identityset.Address(2), identityset.Address(8), identityset.Address(1), "test2"},
+	}
+	initTestStateFromIds := func(bucketCfgIdx, candCfgIds []uint64) (protocol.StateManager, *Protocol, []*VoteBucket, []*Candidate) {
+		bucketCfgs := []*bucketConfig{}
+		for _, idx := range bucketCfgIdx {
+			bucketCfgs = append(bucketCfgs, initBucketCfgs[idx])
+		}
+		candCfgs := []*candidateConfig{}
+		for _, idx := range candCfgIds {
+			candCfgs = append(candCfgs, initCandidateCfgs[idx])
+		}
+		return initTestState(t, ctrl, bucketCfgs, candCfgs)
+	}
+	sm, p, _, _ := initTestState(t, ctrl, initBucketCfgs, initCandidateCfgs)
+
+	tests := []struct {
+		name string
+		// params
+		initBucketCfgIds    []uint64
+		initCandidateCfgIds []uint64
+		initBalance         int64
+		caller              address.Address
+		nonce               uint64
+		gasLimit            uint64
+		blkGasLimit         uint64
+		gasPrice            *big.Int
+		bucketID            uint64
+		newProtocol         bool
+		// expect
+		err              error
+		status           iotextypes.ReceiptStatus
+		expectCandidates []expectCandidate
+		expectBuckets    []expectBucket
+	}{
+		{
+			"selfstake for unstaked candidate",
+			[]uint64{0, 1},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_Success,
+			[]expectCandidate{
+				{identityset.Address(1), 1, "1200000000000000000000000", "1469480667073232815766915"},
+			},
+			nil,
+		},
+		{
+			"selfstake bucket amount is unsufficient",
+			[]uint64{0, 1},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			0,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_ErrInvalidBucketAmount,
+			nil,
+			nil,
+		},
+		{
+			"selfstake bucket is unstaked",
+			[]uint64{0, 2},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_ErrInvalidBucketType,
+			nil,
+			nil,
+		},
+		{
+			"bucket is already selfstaked",
+			[]uint64{0, 3},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_ErrInvalidBucketType,
+			nil,
+			nil,
+		},
+		{
+			"other candidate's bucket is unauthorized",
+			[]uint64{0, 4},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_ErrUnauthorizedOperator,
+			nil,
+			nil,
+		},
+		{
+			"bucket has been voted to other candidate",
+			[]uint64{0, 5, 6},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_Success,
+			[]expectCandidate{
+				{identityset.Address(1), 1, "1200000000000000000000000", "1469480667073232815766915"},
+				{identityset.Address(2), 2, "1200000000000000000000000", "1469480667073232815766914"},
+			},
+			[]expectBucket{
+				{1, identityset.Address(1)},
+			},
+		},
+		{
+			"bucket has been voted to other candidate II",
+			[]uint64{0, 5},
+			[]uint64{0, 1},
+			1300000,
+			identityset.Address(1),
+			1,
+			uint64(1000000),
+			uint64(1000000),
+			big.NewInt(1000),
+			1,
+			true,
+			nil,
+			iotextypes.ReceiptStatus_Success,
+			[]expectCandidate{
+				{identityset.Address(1), 1, "1200000000000000000000000", "1469480667073232815766915"},
+				{identityset.Address(2), 0, "0", "0"},
+			},
+			[]expectBucket{
+				{1, identityset.Address(1)},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			nonce := test.nonce
+			if test.newProtocol {
+				sm, p, _, _ = initTestStateFromIds(test.initBucketCfgIds, test.initCandidateCfgIds)
+			}
+			require.NoError(setupAccount(sm, test.caller, test.initBalance))
+			act := action.NewCandidateActivate(nonce, test.gasLimit, test.gasPrice, test.bucketID)
+			IntrinsicGas, _ := act.IntrinsicGas()
+			ctx := protocol.WithActionCtx(context.Background(), protocol.ActionCtx{
+				Caller:       test.caller,
+				GasPrice:     test.gasPrice,
+				IntrinsicGas: IntrinsicGas,
+				Nonce:        nonce,
+			})
+			ctx = protocol.WithBlockCtx(ctx, protocol.BlockCtx{
+				BlockHeight:    1,
+				BlockTimeStamp: timeBlock,
+				GasLimit:       test.blkGasLimit,
+			})
+			ctx = genesis.WithGenesisContext(ctx, genesis.Default)
+			ctx = protocol.WithFeatureCtx(protocol.WithFeatureWithHeightCtx(ctx))
+			require.Equal(test.err, errors.Cause(p.Validate(ctx, act, sm)))
+			if test.err != nil {
+				return
+			}
+			r, err := p.Handle(ctx, act, sm)
+			require.NoError(err)
+			if r != nil {
+				require.Equal(uint64(test.status), r.Status)
+			} else {
+				require.Equal(test.status, iotextypes.ReceiptStatus_Failure)
+			}
+
+			if test.err == nil && test.status == iotextypes.ReceiptStatus_Success {
+				// check candidate
+				csm, err := NewCandidateStateManager(sm, false)
+				require.NoError(err)
+				for _, expectCand := range test.expectCandidates {
+					candidate := csm.GetByOwner(expectCand.owner)
+					require.NotNil(candidate)
+					require.Equal(expectCand.candSelfStakeIndex, candidate.SelfStakeBucketIdx)
+					require.Equal(expectCand.candSelfStakeAmountStr, candidate.SelfStake.String())
+					require.Equal(expectCand.candVoteStr, candidate.Votes.String())
+				}
+				// check buckets
+				for _, expectBkt := range test.expectBuckets {
+					bkt, err := csm.getBucket(expectBkt.id)
+					require.NoError(err)
+					require.Equal(expectBkt.candidate, bkt.Candidate)
+				}
+
+				// test staker's account
+				caller, err := accountutil.LoadAccount(sm, test.caller)
+				require.NoError(err)
+				actCost, err := act.Cost()
+				require.NoError(err)
+				total := big.NewInt(0)
+				require.Equal(unit.ConvertIotxToRau(test.initBalance), total.Add(total, caller.Balance).Add(total, actCost))
+				require.Equal(nonce+1, caller.PendingNonce())
+			}
+		})
+	}
+}
