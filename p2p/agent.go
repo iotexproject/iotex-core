a871bc02e07344c4d1d98c071128cf0575a3bccd
envestcc <chen1233216@hotmail.com>
2023-12-22 11:05:32 +0800

    reduce conns to 30-60

--- p2p/agent.go
+++ p2p/agent.go
@@ -94,6 +94,8 @@ type (
 		EnableRateLimit   bool                `yaml:"enableRateLimit"`
 		PrivateNetworkPSK string              `yaml:"privateNetworkPSK"`
 		MaxPeers          int                 `yaml:"maxPeers"`
+		ConnLowWater      int                 `yaml:"connLowWater"`
+		ConnHighWater     int                 `yaml:"connHighWater"`
 	}
 
 	// Agent is the agent to help the blockchain node connect into the P2P networks and send/receive messages
@@ -143,6 +145,8 @@ var DefaultConfig = Config{
 	EnableRateLimit:   true,
 	PrivateNetworkPSK: "",
 	MaxPeers:          30,
+	ConnLowWater:      30,
+	ConnHighWater:     60,
 }
 
 // NewDummyAgent creates a dummy p2p agent
@@ -214,6 +218,7 @@ func (p *agent) Start(ctx context.Context) error {
 		p2p.DHTProtocolID(p.chainID),
 		p2p.DHTGroupID(p.chainID),
 		p2p.WithMaxPeer(uint32(p.cfg.MaxPeers)),
+		p2p.WithConnectionManagerConfig(p.cfg.ConnLowWater, p.cfg.ConnHighWater, 0),
 	}
 	if p.cfg.EnableRateLimit {
 		opts = append(opts, p2p.WithRateLimit(p.cfg.RateLimit))
