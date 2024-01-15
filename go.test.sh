53e4cb22aea37e57bf8006a84dbb9cbe644c3ba1
envestcc <chen1233216@hotmail.com>
2024-01-15 17:51:14 +0800

    split install tools

--- go.test.sh
+++ go.test.sh
@@ -7,10 +7,10 @@ for dir in $(go list -f '{{.ImportPath}}={{join .TestGoFiles ","}}' ./...);do
     array=(${dir//=/ })
     if [ -n "${array[1]}" ]; then
         go test -short -v -coverprofile=coverage.out -covermode=count "${array[0]}"
-        # courtney -l=coverage.out -o=profile.out
-        # if [ -f profile.out ]; then
-        #     cat profile.out >> coverage.txt
-        #     rm profile.out coverage.out
-        # fi
+        courtney -l=coverage.out -o=profile.out
+        if [ -f profile.out ]; then
+            cat profile.out >> coverage.txt
+            rm profile.out coverage.out
+        fi
     fi    
 done
