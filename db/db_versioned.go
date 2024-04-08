958474ca72ed6a49fad4d4180365c5885bf34186
Dustin Xie <dahuaxie@gmail.com>
2024-04-08 10:16:22 -0700

    address comments

--- db/db_versioned.go
+++ db/db_versioned.go
@@ -8,81 +8,61 @@ package db
 
 import (
 	"context"
+
+	"github.com/iotexproject/iotex-core/pkg/lifecycle"
 )
 
 type (
-	// KvVersioned is a versioned key-value store, where each key can (but doesn't
-	// have to) have multiple versions of value (corresponding to different heights
-	// in a blockchain)
+	// KvVersioned is a versioned key-value store, where each key has multiple
+	// versions of value (corresponding to different heights in a blockchain)
 	//
-	// A namespace/bucket is considered versioned by default, user needs to use the
-	// NonversionedNamespaceOption() to specify non-versioned namespaces at the time
-	// of creating the versioned key-value store
-	//
-	// Versioning is achieved by using (key + 8-byte version) as the actual storage
-	// key in the underlying DB. For buckets containing versioned keys, a metadata
-	// is stored at the special key = []byte{0}. The metadata specifies the bucket's
-	// name and the key length
+	// Versioning is achieved by using (key + 8-byte version) as the actual
+	// storage key in the underlying DB. For each bucket, a metadata is stored
+	// at the special key = []byte{0}. The metadata specifies the bucket's name
+	// and the key length.
 	//
 	// For each versioned key, the special location = key + []byte{0} stores the
 	// key's metadata, which includes the following info:
 	// 1. the version when the key is first created
 	// 2. the version when the key is lastly written
 	// 3. the version when the key is deleted
-	// 4. hash of the key's last written value
+	// 4. hash of the key's last written value (to detect/avoid same write)
 	// If the location does not store a value, the key has never been written.
 	//
-	// Here's an example of a versioned DB which has 3 buckets:
-	// 1. "mta" -- regular bucket storing metadata, key is not versioned
-	// 2. "act" -- versioned namespace, key length = 20
-	// 3. "stk" -- versioned namespace, key length = 8
+	// How to use a versioned DB:
+	//
+	// db := NewBoltDBVersioned(cfg) // creates a versioned DB
+	// db.Start(ctx)
+	// defer func() { db.Stop(ctx) }()
+	//
+	// kv := db.SetVersion(5)
+	// value, err := kv.Get("ns", key) // read 'key' at version 5
+	// kv = db.SetVersion(8)
+	// err := kv.Put("ns", key, value) // write 'key' at version 8
+
 	KvVersioned interface {
-		KVStore
+		lifecycle.StartStopper
 
 		// Version returns the key's most recent version
 		Version(string, []byte) (uint64, error)
 
-		// SetVersion sets the version before calling Put()
-		SetVersion(uint64) KvVersioned
-
-		// CreateVersionedNamespace creates a namespace to store versioned keys
-		CreateVersionedNamespace(string, uint32) error
+		// SetVersion sets the version, and returns a KVStore to call Put()/Get()
+		SetVersion(uint64) KVStoreBasic
 	}
 
 	// BoltDBVersioned is KvVersioned implementation based on bolt DB
 	BoltDBVersioned struct {
-		*BoltDB
-		version      uint64          // version for Get/Put()
-		versioned    map[string]int  // buckets for versioned keys
-		nonversioned map[string]bool // buckets for non-versioned keys
+		db *BoltDB
 	}
 )
 
 // Option sets an option
 type Option func(b *BoltDBVersioned)
 
-// NonversionedNamespaceOption sets non-versioned namespace
-func NonversionedNamespaceOption(ns ...string) Option {
-	return func(b *BoltDBVersioned) {
-		for _, v := range ns {
-			b.nonversioned[v] = true
-		}
-	}
-}
-
-// VersionedNamespaceOption sets versioned namespace
-func VersionedNamespaceOption(ns string, n int) Option {
-	return func(b *BoltDBVersioned) {
-		b.versioned[ns] = n
-	}
-}
-
-// NewBoltDBVersioned instantiates an BoltDB with implements KvVersioned
+// NewBoltDBVersioned instantiates an BoltDB which implements KvVersioned
 func NewBoltDBVersioned(cfg Config, opts ...Option) *BoltDBVersioned {
 	b := &BoltDBVersioned{
-		BoltDB:       NewBoltDB(cfg),
-		versioned:    make(map[string]int),
-		nonversioned: make(map[string]bool),
+		db: NewBoltDB(cfg),
 	}
 	for _, opt := range opts {
 		opt(b)
@@ -92,16 +72,17 @@ func NewBoltDBVersioned(cfg Config, opts ...Option) *BoltDBVersioned {
 
 // Start starts the DB
 func (b *BoltDBVersioned) Start(ctx context.Context) error {
-	if err := b.BoltDB.Start(ctx); err != nil {
-		return err
-	}
-	// TODO: check non-versioned and versioned namespace
-	return nil
+	return b.db.Start(ctx)
+}
+
+// Stop stops the DB
+func (b *BoltDBVersioned) Stop(ctx context.Context) error {
+	return b.db.Stop(ctx)
 }
 
 // Put writes a <key, value> record
-func (b *BoltDBVersioned) Put(ns string, key, value []byte) error {
-	if !b.IsReady() {
+func (b *BoltDBVersioned) Put(ns string, version uint64, key, value []byte) error {
+	if !b.db.IsReady() {
 		return ErrDBNotStarted
 	}
 	// TODO: implement Put
@@ -109,8 +90,8 @@ func (b *BoltDBVersioned) Put(ns string, key, value []byte) error {
 }
 
 // Get retrieves the most recent version
-func (b *BoltDBVersioned) Get(ns string, key []byte) ([]byte, error) {
-	if !b.IsReady() {
+func (b *BoltDBVersioned) Get(ns string, version uint64, key []byte) ([]byte, error) {
+	if !b.db.IsReady() {
 		return nil, ErrDBNotStarted
 	}
 	// TODO: implement Get
@@ -119,7 +100,7 @@ func (b *BoltDBVersioned) Get(ns string, key []byte) ([]byte, error) {
 
 // Delete deletes a record,if key is nil,this will delete the whole bucket
 func (b *BoltDBVersioned) Delete(ns string, key []byte) error {
-	if !b.IsReady() {
+	if !b.db.IsReady() {
 		return ErrDBNotStarted
 	}
 	// TODO: implement Delete
@@ -128,24 +109,48 @@ func (b *BoltDBVersioned) Delete(ns string, key []byte) error {
 
 // Version returns the key's most recent version
 func (b *BoltDBVersioned) Version(ns string, key []byte) (uint64, error) {
-	if !b.IsReady() {
+	if !b.db.IsReady() {
 		return 0, ErrDBNotStarted
 	}
 	// TODO: implement Version
 	return 0, nil
 }
 
-// SetVersion sets the version which a Get()/Put() wants to access
-func (b *BoltDBVersioned) SetVersion(v uint64) KvVersioned {
-	b.version = v
-	return b
+// SetVersion sets the version, and returns a KVStore to call Put()/Get()
+func (b *BoltDBVersioned) SetVersion(v uint64) KVStoreBasic {
+	return &KvWithVersion{
+		db:      b,
+		version: v,
+	}
 }
 
-// CreateVersionedNamespace creates a namespace to store versioned keys
-func (b *BoltDBVersioned) CreateVersionedNamespace(ns string, n uint32) error {
-	if !b.IsReady() {
-		return ErrDBNotStarted
-	}
-	// TODO: implement CreateVersionedNamespace
-	return nil
+// KvWithVersion wraps the BoltDBVersioned with a certain version
+type KvWithVersion struct {
+	db      *BoltDBVersioned
+	version uint64 // version for Get/Put()
+}
+
+// Start starts the DB
+func (b *KvWithVersion) Start(context.Context) error {
+	panic("should call BoltDBVersioned's Start method")
+}
+
+// Stop stops the DB
+func (b *KvWithVersion) Stop(context.Context) error {
+	panic("should call BoltDBVersioned's Stop method")
+}
+
+// Put writes a <key, value> record
+func (b *KvWithVersion) Put(ns string, key, value []byte) error {
+	return b.db.Put(ns, b.version, key, value)
+}
+
+// Get retrieves a key's value
+func (b *KvWithVersion) Get(ns string, key []byte) ([]byte, error) {
+	return b.db.Get(ns, b.version, key)
+}
+
+// Delete deletes a key
+func (b *KvWithVersion) Delete(ns string, key []byte) error {
+	return b.db.Delete(ns, key)
 }
