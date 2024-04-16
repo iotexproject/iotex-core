67ee5e1680ce3eca69228dbaea480d3922a820ab
millken <millken@gmail.com>
2024-04-16 16:25:08 +0800

    Add CandidateTransferOwnership

---
+++ action/candidate_transfer_ownership.go
@@ -0,0 +1,220 @@
+package action
+
+import (
+	"bytes"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
+	"github.com/iotexproject/iotex-core/pkg/version"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	// CandidateTransferOwnershipBaseIntrinsicGas represents the base intrinsic gas for CandidateTransferOwnership
+	CandidateTransferOwnershipBaseIntrinsicGas = uint64(10000)
+
+	_candidateTransferOwnershipInterfaceABI = `[
+		{
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "name",
+					"type": "string"
+				},
+				{
+					"internalType": "address",
+					"name": "newOwner",
+					"type": "address"
+				},
+				{
+					"internalType": "uint8[]",
+					"name": "payload",
+					"type": "uint8[]"
+				}
+			],
+			"name": "candidateTransferOwnership",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		}
+	]`
+)
+
+var (
+	// _candidateTransferOwnershipMethod is the interface of the abi encoding of candidate transfer ownership action
+	_candidateTransferOwnershipMethod abi.Method
+)
+
+func init() {
+	candidateTransferOwnershipInterface, err := abi.JSON(strings.NewReader(_candidateTransferOwnershipInterfaceABI))
+	if err != nil {
+		panic(err)
+	}
+	var ok bool
+	_candidateTransferOwnershipMethod, ok = candidateTransferOwnershipInterface.Methods["candidateTransferOwnership"]
+	if !ok {
+		panic("fail to load the method")
+	}
+}
+
+// CandidateTransferOwnership is the action to transfer ownership of a candidate
+type CandidateTransferOwnership struct {
+	AbstractAction
+
+	name     string
+	newOwner address.Address
+	payload  []byte
+}
+
+// NewCandidateTransferOwnership returns a CandidateTransferOwnership action
+func NewCandidateTransferOwnership(nonce, gasLimit uint64, gasPrice *big.Int, name string,
+	newOwnerStr string, payload []byte) (*CandidateTransferOwnership, error) {
+	newOwner, err := address.FromString(newOwnerStr)
+	if err != nil {
+		return nil, err
+	}
+	return &CandidateTransferOwnership{
+		AbstractAction: AbstractAction{
+			version:  version.ProtocolVersion,
+			nonce:    nonce,
+			gasLimit: gasLimit,
+			gasPrice: gasPrice,
+		},
+		name:     name,
+		newOwner: newOwner,
+		payload:  payload,
+	}, nil
+}
+
+func NewCandidateTransferOwnershipFromABIBinary(data []byte) (*CandidateTransferOwnership, error) {
+	var (
+		paramsMap = map[string]any{}
+		ok        bool
+		err       error
+		cr        CandidateTransferOwnership
+	)
+	// sanity check
+	if len(data) <= 4 || !bytes.Equal(_candidateTransferOwnershipMethod.ID, data[:4]) {
+		return nil, errDecodeFailure
+	}
+	if err = _candidateTransferOwnershipMethod.Inputs.UnpackIntoMap(paramsMap, data[4:]); err != nil {
+		return nil, err
+	}
+	if cr.name, ok = paramsMap["name"].(string); !ok {
+		return nil, errDecodeFailure
+	}
+	if cr.newOwner, err = ethAddrToNativeAddr(paramsMap["newOwner"]); err != nil {
+		return nil, err
+	}
+	if cr.payload, ok = paramsMap["payload"].([]byte); !ok {
+		return nil, errDecodeFailure
+	}
+	return &cr, nil
+}
+
+// Payload returns the payload bytes
+func (act *CandidateTransferOwnership) Payload() []byte { return act.payload }
+
+// Name returns candidate name to transfer ownership
+func (act *CandidateTransferOwnership) Name() string { return act.name }
+
+// NewOwner returns the new owner address
+func (act *CandidateTransferOwnership) NewOwner() address.Address { return act.newOwner }
+
+// IntrinsicGas returns the intrinsic gas of a CandidateTransferOwnership
+func (act *CandidateTransferOwnership) IntrinsicGas() (uint64, error) {
+	return CandidateTransferOwnershipBaseIntrinsicGas, nil
+}
+
+// Cost returns the total cost of a CandidateTransferOwnership
+func (act *CandidateTransferOwnership) Cost() (*big.Int, error) {
+	intrinsicGas, _ := act.IntrinsicGas()
+
+	fee := big.NewInt(0).Mul(act.GasPrice(), big.NewInt(0).SetUint64(intrinsicGas))
+	return fee, nil
+}
+
+// Serialize returns a raw byte stream of the CandidateTransferOwnership struct
+func (act *CandidateTransferOwnership) Serialize() []byte {
+	return byteutil.Must(proto.Marshal(act.Proto()))
+}
+
+// Proto converts to protobuf CandidateRegister Action
+func (act *CandidateTransferOwnership) Proto() *iotextypes.CandidateTransferOwnership {
+	ac := iotextypes.CandidateTransferOwnership{
+		CandidateName:   act.Name(),
+		NewOwnerAddress: act.newOwner.String(),
+	}
+
+	if len(act.payload) > 0 {
+		ac.Payload = make([]byte, len(act.payload))
+		copy(ac.Payload, act.payload)
+	}
+	return &ac
+}
+
+// LoadProto loads the CandidateTransferOwnership Action from protobuf
+func (act *CandidateTransferOwnership) LoadProto(pbAct *iotextypes.CandidateTransferOwnership) error {
+	if pbAct == nil {
+		return ErrNilProto
+	}
+	act.name = pbAct.GetCandidateName()
+	newOwner, err := address.FromString(pbAct.GetNewOwnerAddress())
+	if err != nil {
+		return err
+	}
+	act.newOwner = newOwner
+	act.payload = nil
+	if len(pbAct.GetPayload()) > 0 {
+		act.payload = make([]byte, len(pbAct.GetPayload()))
+		copy(act.payload, pbAct.GetPayload())
+	}
+	return nil
+}
+
+// EncodeABIBinary encodes data in abi encoding
+func (act *CandidateTransferOwnership) EncodeABIBinary() ([]byte, error) {
+	return act.encodeABIBinary()
+}
+
+func (act *CandidateTransferOwnership) encodeABIBinary() ([]byte, error) {
+	if act.newOwner == nil {
+		return nil, ErrAddress
+	}
+	data, err := _candidateTransferOwnershipMethod.Inputs.Pack(act.name, common.BytesToAddress(act.newOwner.Bytes()), act.payload)
+	if err != nil {
+		return nil, err
+	}
+	return append(_candidateTransferOwnershipMethod.ID, data...), nil
+}
+
+// SanityCheck validates the variables in the action
+func (act *CandidateTransferOwnership) SanityCheck() error {
+	if !IsValidCandidateName(act.Name()) {
+		return ErrInvalidCanName
+	}
+
+	return act.AbstractAction.SanityCheck()
+}
+
+// ToEthTx returns an Ethereum transaction which corresponds to this action
+func (act *CandidateTransferOwnership) ToEthTx(_ uint32) (*types.Transaction, error) {
+	data, err := act.encodeABIBinary()
+	if err != nil {
+		return nil, err
+	}
+	return types.NewTx(&types.LegacyTx{
+		Nonce:    act.Nonce(),
+		GasPrice: act.GasPrice(),
+		Gas:      act.GasLimit(),
+		To:       &_stakingProtocolEthAddr,
+		Value:    big.NewInt(0),
+		Data:     data,
+	}), nil
+}
---
+++ action/candidate_transfer_ownership_test.go
@@ -0,0 +1,159 @@
+package action
+
+import (
+	"encoding/hex"
+	"math/big"
+	"testing"
+
+	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCandidateTransferOwnership(t *testing.T) {
+	require := require.New(t)
+	tests := []struct {
+		nonce       uint64
+		gasLimit    uint64
+		gasPrice    *big.Int
+		name        string
+		newOwner    string
+		payload     []byte
+		cost        string
+		serialize   string
+		expected    error
+		sanityCheck error
+	}{
+		// valid test
+		{
+			1,
+			1000000,
+			big.NewInt(1000),
+			"test",
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			[]byte("payload"),
+			"10000000",
+			"0a04746573741229696f3130613239387a6d7a7672743467757137396139663478377165646a35397937657279383468651a077061796c6f6164",
+			nil,
+			nil,
+		},
+		//invalid address
+		{
+			1,
+			1000000,
+			big.NewInt(1000),
+			"test",
+			"ab-10",
+			[]byte("payload"),
+			"",
+			"",
+			address.ErrInvalidAddr,
+			nil,
+		},
+		//invalid gas price
+		{
+			1,
+			1000000,
+			big.NewInt(-1000),
+			"test",
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			nil,
+			"",
+			"",
+			nil,
+			ErrNegativeValue,
+		},
+		//invalid name
+		{
+			1,
+			1000000,
+			big.NewInt(1000),
+			"aaaaaaaaaaaaa",
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			nil,
+			"",
+			"",
+			nil,
+			ErrInvalidCanName,
+		},
+	}
+	for _, test := range tests {
+		cr, err := NewCandidateTransferOwnership(test.nonce, test.gasLimit, test.gasPrice, test.name, test.newOwner, test.payload)
+		require.Equal(test.expected, errors.Cause(err))
+		if err != nil {
+			continue
+		}
+		err = cr.SanityCheck()
+		require.Equal(test.sanityCheck, errors.Cause(err))
+		if err != nil {
+			continue
+		}
+
+		require.Equal(test.serialize, hex.EncodeToString(cr.Serialize()))
+
+		require.NoError(err)
+		require.Equal(test.gasLimit, cr.GasLimit())
+		require.Equal(test.gasPrice, cr.GasPrice())
+		require.Equal(test.nonce, cr.Nonce())
+
+		require.Equal(test.name, cr.Name())
+		require.Equal(test.newOwner, cr.NewOwner().String())
+
+		require.Equal(test.payload, cr.Payload())
+
+		gas, err := cr.IntrinsicGas()
+		require.NoError(err)
+		require.Equal(CandidateTransferOwnershipBaseIntrinsicGas, gas)
+		cost, err := cr.Cost()
+		require.NoError(err)
+		require.Equal(test.cost, cost.Text(10))
+
+		cr2 := &CandidateTransferOwnership{}
+		require.NoError(cr2.LoadProto(cr.Proto()))
+		require.Equal(test.name, cr2.Name())
+		require.Equal(test.newOwner, cr2.NewOwner().String())
+		require.Equal(test.payload, cr2.Payload())
+
+	}
+}
+
+func TestCandidateTransferOwnershipABIEncodeAndDecode(t *testing.T) {
+	require := require.New(t)
+	cr, err := NewCandidateTransferOwnership(1, 1000000, big.NewInt(1000), "test", "io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he", []byte("payload"))
+	require.NoError(err)
+	enc, err := cr.EncodeABIBinary()
+	require.NoError(err)
+
+	cr2, err := NewCandidateTransferOwnershipFromABIBinary(enc)
+	require.NoError(err)
+	require.Equal(cr.Name(), cr2.Name())
+	require.Equal(cr.NewOwner().String(), cr2.NewOwner().String())
+	require.Equal(cr.Payload(), cr2.Payload())
+
+	cr2.newOwner = nil
+	enc, err = cr2.EncodeABIBinary()
+	require.Equal(ErrAddress, errors.Cause(err))
+	require.Nil(enc)
+
+	//invalid data
+	data := []byte{1, 2, 3, 4}
+	cr2, err = NewCandidateTransferOwnershipFromABIBinary(data)
+	require.Equal(errDecodeFailure, err)
+	require.Nil(cr2)
+}
+
+func TestCandidateTransferOwnershipToEthTx(t *testing.T) {
+	require := require.New(t)
+	cr, err := NewCandidateTransferOwnership(1, 1000000, big.NewInt(1000), "test", "io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he", []byte("payload"))
+	require.NoError(err)
+	ethTx, err := cr.ToEthTx(0)
+	require.NoError(err)
+	require.NotNil(ethTx)
+	require.Equal(byteutil.Must(cr.EncodeABIBinary()), ethTx.Data())
+	require.Equal(cr.GasPrice(), ethTx.GasPrice())
+	require.Equal(cr.GasLimit(), ethTx.Gas())
+	require.Equal(big.NewInt(0), ethTx.Value())
+	require.Equal(_stakingProtocolEthAddr.Hex(), ethTx.To().Hex())
+
+}
