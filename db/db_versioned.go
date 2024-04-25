fdff680a935a0b2f7e5b2e7ebd2c021778b8effe
Dustin Xie <dahuaxie@gmail.com>
2024-04-25 14:37:01 -0700

    [db] implement Get/Put/Delete/Version() for BoltDBVersioned

--- db/db_versioned.go
+++ db/db_versioned.go
@@ -7,9 +7,21 @@
 package db
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 
+	"github.com/pkg/errors"
+	bolt "go.etcd.io/bbolt"
+
+	"github.com/iotexproject/iotex-core/db/batch"
 	"github.com/iotexproject/iotex-core/pkg/lifecycle"
+	"github.com/iotexproject/iotex-core/pkg/util/byteutil"
+)
+
+var (
+	ErrDeleted = errors.New("deleted in DB")
+	_minKey    = []byte{0} // the minimum key, used to store namespace's metadata
 )
 
 type (
@@ -26,7 +38,7 @@ type (
 	// 1. the version when the key is first created
 	// 2. the version when the key is lastly written
 	// 3. the version when the key is deleted
-	// 4. hash of the key's last written value (to detect/avoid same write)
+	// 4. the key's last written value (to fast-track read of current version)
 	// If the location does not store a value, the key has never been written.
 	//
 	// How to use a versioned DB:
@@ -43,11 +55,14 @@ type (
 	KvVersioned interface {
 		lifecycle.StartStopper
 
+		// Base returns the underlying KVStore
+		Base() KVStore
+
 		// Version returns the key's most recent version
 		Version(string, []byte) (uint64, error)
 
 		// SetVersion sets the version, and returns a KVStore to call Put()/Get()
-		SetVersion(uint64) KVStoreBasic
+		SetVersion(uint64) KVStore
 	}
 
 	// BoltDBVersioned is KvVersioned implementation based on bolt DB
@@ -80,31 +95,115 @@ func (b *BoltDBVersioned) Stop(ctx context.Context) error {
 	return b.db.Stop(ctx)
 }
 
+// Base returns the underlying KVStore
+func (b *BoltDBVersioned) Base() KVStore {
+	return b.db
+}
+
 // Put writes a <key, value> record
-func (b *BoltDBVersioned) Put(ns string, version uint64, key, value []byte) error {
+func (b *BoltDBVersioned) Put(version uint64, ns string, key, value []byte) error {
 	if !b.db.IsReady() {
 		return ErrDBNotStarted
 	}
-	// TODO: implement Put
-	return nil
+	// check namespace
+	vn, err := b.checkNamespace(ns)
+	if err != nil {
+		return err
+	}
+	buf := batch.NewBatch()
+	if vn == nil {
+		// namespace not yet created
+		buf.Put(ns, _minKey, (&versionedNamespace{
+			keyLen: uint32(len(key)),
+		}).serialize(), "failed to create metadata")
+	} else {
+		if len(key) != int(vn.keyLen) {
+			return errors.Wrapf(ErrInvalid, "invalid key length, expecting %d, got %d", vn.keyLen, len(key))
+		}
+	}
+	// check key's metadata
+	km, err := b.checkKey(ns, key)
+	if err != nil {
+		return err
+	}
+	km, exit := km.updateWrite(version, value)
+	if exit {
+		return nil
+	}
+	buf.Put(ns, append(key, 0), km.serialize(), fmt.Sprintf("failed to put key %x's metadata", key))
+	buf.Put(ns, versionedKey(key, version), value, fmt.Sprintf("failed to put key %x", key))
+	return b.db.WriteBatch(buf)
 }
 
 // Get retrieves the most recent version
-func (b *BoltDBVersioned) Get(ns string, version uint64, key []byte) ([]byte, error) {
+func (b *BoltDBVersioned) Get(version uint64, ns string, key []byte) ([]byte, error) {
 	if !b.db.IsReady() {
 		return nil, ErrDBNotStarted
 	}
-	// TODO: implement Get
-	return nil, nil
+	// check key's metadata
+	km, err := b.checkNamespaceAndKey(ns, key)
+	if err != nil {
+		return nil, err
+	}
+	hitLast, err := km.updateRead(version)
+	if err != nil {
+		return nil, errors.Wrapf(err, "key = %x", key)
+	}
+	if hitLast {
+		return km.lastWrite, nil
+	}
+	return b.get(version, ns, key)
 }
 
-// Delete deletes a record,if key is nil,this will delete the whole bucket
-func (b *BoltDBVersioned) Delete(ns string, key []byte) error {
+func (b *BoltDBVersioned) get(version uint64, ns string, key []byte) ([]byte, error) {
+	// construct the actual key = key + version (represented in 8-bytes)
+	// and read from DB
+	key = versionedKey(key, version)
+	var value []byte
+	err := b.db.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(ns))
+		if bucket == nil {
+			return errors.Wrapf(ErrBucketNotExist, "bucket = %x doesn't exist", []byte(ns))
+		}
+		c := bucket.Cursor()
+		k, v := c.Seek(key)
+		if k == nil || bytes.Compare(k, key) == 1 {
+			k, v = c.Prev()
+			if k == nil || bytes.Compare(k, append(key[:len(key)-8], 0)) <= 0 {
+				// cursor is at the beginning/end of the bucket or smaller than minimum key
+				panic(fmt.Sprintf("BoltDBVersioned.get(), invalid key = %x", key))
+			}
+		}
+		value = make([]byte, len(v))
+		copy(value, v)
+		return nil
+	})
+	if err == nil {
+		return value, nil
+	}
+	if cause := errors.Cause(err); cause == ErrNotExist || cause == ErrBucketNotExist {
+		return nil, err
+	}
+	return nil, errors.Wrap(ErrIO, err.Error())
+}
+
+// Delete deletes a record, if key does not exist, it returns nil
+func (b *BoltDBVersioned) Delete(version uint64, ns string, key []byte) error {
 	if !b.db.IsReady() {
 		return ErrDBNotStarted
 	}
-	// TODO: implement Delete
-	return nil
+	// check key's metadata
+	km, err := b.checkNamespaceAndKey(ns, key)
+	if err != nil {
+		if cause := errors.Cause(err); cause != ErrNotExist && cause != ErrInvalid {
+			return err
+		}
+	}
+	if km == nil || version < km.lastVersion || version <= km.deleteVersion {
+		return nil
+	}
+	km.deleteVersion = version
+	return b.db.Put(ns, append(key, 0), km.serialize())
 }
 
 // Version returns the key's most recent version
@@ -112,16 +211,77 @@ func (b *BoltDBVersioned) Version(ns string, key []byte) (uint64, error) {
 	if !b.db.IsReady() {
 		return 0, ErrDBNotStarted
 	}
-	// TODO: implement Version
-	return 0, nil
+	// check key's metadata
+	km, err := b.checkNamespaceAndKey(ns, key)
+	if err != nil {
+		return 0, err
+	}
+	if km == nil {
+		// key not yet written
+		return 0, errors.Wrapf(ErrNotExist, "key = %x doesn't exist", key)
+	}
+	if km.deleteVersion != 0 {
+		err = errors.Wrapf(ErrDeleted, "key = %x already deleted", key)
+	}
+	return km.lastVersion, err
 }
 
 // SetVersion sets the version, and returns a KVStore to call Put()/Get()
-func (b *BoltDBVersioned) SetVersion(v uint64) KVStoreBasic {
+func (b *BoltDBVersioned) SetVersion(v uint64) KVStore {
 	return &KvWithVersion{
 		db:      b,
 		version: v,
 	}
+}
+
+func versionedKey(key []byte, v uint64) []byte {
+	return append(key, byteutil.Uint64ToBytesBigEndian(v)...)
+}
+
+func (b *BoltDBVersioned) checkNamespace(ns string) (*versionedNamespace, error) {
+	data, err := b.db.Get(ns, _minKey)
+	switch errors.Cause(err) {
+	case nil:
+		vn, err := deserializeVersionedNamespace(data)
+		if err != nil {
+			return nil, err
+		}
+		return vn, nil
+	case ErrNotExist, ErrBucketNotExist:
+		return nil, nil
+	default:
+		return nil, err
+	}
+}
+
+func (b *BoltDBVersioned) checkKey(ns string, key []byte) (*keyMeta, error) {
+	data, err := b.db.Get(ns, append(key, 0))
+	switch errors.Cause(err) {
+	case nil:
+		km, err := deserializeKeyMeta(data)
+		if err != nil {
+			return nil, err
+		}
+		return km, nil
+	case ErrNotExist, ErrBucketNotExist:
+		return nil, nil
+	default:
+		return nil, err
+	}
+}
+
+func (b *BoltDBVersioned) checkNamespaceAndKey(ns string, key []byte) (*keyMeta, error) {
+	vn, err := b.checkNamespace(ns)
+	if err != nil {
+		return nil, err
+	}
+	if vn == nil {
+		return nil, errors.Wrapf(ErrNotExist, "key = %x doesn't exist", key)
+	}
+	if len(key) != int(vn.keyLen) {
+		return nil, errors.Wrapf(ErrInvalid, "invalid key length, expecting %d, got %d", vn.keyLen, len(key))
+	}
+	return b.checkKey(ns, key)
 }
 
 // KvWithVersion wraps the BoltDBVersioned with a certain version
@@ -142,15 +302,26 @@ func (b *KvWithVersion) Stop(context.Context) error {
 
 // Put writes a <key, value> record
 func (b *KvWithVersion) Put(ns string, key, value []byte) error {
-	return b.db.Put(ns, b.version, key, value)
+	return b.db.Put(b.version, ns, key, value)
 }
 
 // Get retrieves a key's value
 func (b *KvWithVersion) Get(ns string, key []byte) ([]byte, error) {
-	return b.db.Get(ns, b.version, key)
+	return b.db.Get(b.version, ns, key)
 }
 
 // Delete deletes a key
 func (b *KvWithVersion) Delete(ns string, key []byte) error {
-	return b.db.Delete(ns, key)
+	return b.db.Delete(b.version, ns, key)
+}
+
+// Filter returns <k, v> pair in a bucket that meet the condition
+func (b *KvWithVersion) Filter(ns string, cond Condition, minKey, maxKey []byte) ([][]byte, [][]byte, error) {
+	panic("Filter not supported for versioned DB")
+}
+
+// WriteBatch commits a batch
+func (b *KvWithVersion) WriteBatch(kvsb batch.KVStoreBatch) error {
+	// TODO: implement WriteBatch
+	return nil
 }
--- db/db_versioned_test.go
+++ db/db_versioned_test.go
@@ -7,21 +7,235 @@
 package db
 
 import (
-	"encoding/hex"
+	"context"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
+
+	"github.com/iotexproject/iotex-core/testutil"
 )
 
-func TestPb(t *testing.T) {
-	r := require.New(t)
+type versionTest struct {
+	ns     string
+	k, v   []byte
+	height uint64
+	err    error
+}
 
-	vn := &versionedNamespace{
-		name:   "3jfsp5@(%)EW*#)_#¡ªº–ƒ˚œade∆…",
-		keyLen: 5}
-	data := vn.serialize()
-	r.Equal("0a29336a667370354028252945572a23295f23c2a1c2aac2bae28093c692cb9ac593616465e28886e280a61005", hex.EncodeToString(data))
-	vn1, err := deserializeVersionedNamespace(data)
+func TestVersionedDB(t *testing.T) {
+	r := require.New(t)
+	testPath, err := testutil.PathOfTempFile("test-version")
 	r.NoError(err)
-	r.Equal(vn, vn1)
+	defer func() {
+		testutil.CleanupPath(testPath)
+	}()
+
+	cfg := DefaultConfig
+	cfg.DbPath = testPath
+	db := NewBoltDBVersioned(cfg)
+	ctx := context.Background()
+	r.NoError(db.Start(ctx))
+	defer func() {
+		db.Stop(ctx)
+	}()
+
+	// namespace and key does not exist
+	vn, err := db.checkNamespace(_bucket1)
+	r.Nil(vn)
+	r.Nil(err)
+	km, err := db.checkKey(_bucket1, _k1)
+	r.Nil(km)
+	r.Nil(err)
+	// write first key, namespace and key now exist
+	r.NoError(db.Put(0, _bucket1, _k2, _v2))
+	vn, err = db.checkNamespace(_bucket1)
+	r.NoError(err)
+	r.EqualValues(len(_k2), vn.keyLen)
+	km, err = db.checkKey(_bucket1, _k2)
+	r.Zero(km.firstVersion)
+	r.Zero(km.lastVersion)
+	r.Zero(km.deleteVersion)
+	r.Equal(_v2, km.lastWrite)
+	r.NoError(err)
+	// check more Put/Get
+	var (
+		_k5  = []byte("key_5")
+		_k10 = []byte("key_10")
+	)
+	err = db.Put(1, _bucket1, _k10, _v1)
+	r.Equal("invalid key length, expecting 5, got 6: invalid input", err.Error())
+	r.NoError(db.Put(1, _bucket1, _k2, _v1))
+	r.NoError(db.Put(3, _bucket1, _k2, _v3))
+	r.NoError(db.Put(6, _bucket1, _k2, _v2))
+	r.NoError(db.Put(2, _bucket1, _k4, _v2))
+	r.NoError(db.Put(4, _bucket1, _k4, _v1))
+	r.NoError(db.Put(7, _bucket1, _k4, _v3))
+	for _, e := range []versionTest{
+		{_bucket2, _k1, nil, 0, ErrNotExist}, // bucket not exist
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, _v2, 0, nil},
+		{_bucket1, _k2, _v1, 1, nil},
+		{_bucket1, _k2, _v1, 2, nil},
+		{_bucket1, _k2, _v3, 3, nil},
+		{_bucket1, _k2, _v3, 5, nil},
+		{_bucket1, _k2, _v2, 6, nil},
+		{_bucket1, _k2, _v2, 7, nil}, // after last write version
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 1, ErrNotExist}, // before first write version
+		{_bucket1, _k4, _v2, 2, nil},
+		{_bucket1, _k4, _v2, 3, nil},
+		{_bucket1, _k4, _v1, 4, nil},
+		{_bucket1, _k4, _v1, 6, nil},
+		{_bucket1, _k4, _v3, 7, nil},
+		{_bucket1, _k4, _v3, 8, nil}, // larger than last key in bucket
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Get(e.height, e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.v, value)
+	}
+	// overwrite the same height again
+	r.NoError(db.Put(6, _bucket1, _k2, _v4))
+	r.NoError(db.Put(7, _bucket1, _k4, _v4))
+	// write to earlier version again does nothing
+	r.NoError(db.Put(3, _bucket1, _k2, _v4))
+	r.NoError(db.Put(4, _bucket1, _k4, _v4))
+	// write with same value
+	r.NoError(db.Put(9, _bucket1, _k2, _v4))
+	r.NoError(db.Put(10, _bucket1, _k4, _v4))
+	for _, e := range []versionTest{
+		{_bucket2, _k1, nil, 0, ErrNotExist}, // bucket not exist
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, _v2, 0, nil},
+		{_bucket1, _k2, _v1, 1, nil},
+		{_bucket1, _k2, _v1, 2, nil},
+		{_bucket1, _k2, _v3, 3, nil},
+		{_bucket1, _k2, _v3, 5, nil},
+		{_bucket1, _k2, _v4, 6, nil},
+		{_bucket1, _k2, _v4, 8, nil},
+		{_bucket1, _k2, _v4, 9, nil},
+		{_bucket1, _k2, _v4, 10, nil}, // after last write version
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 1, ErrNotExist}, // before first write version
+		{_bucket1, _k4, _v2, 2, nil},
+		{_bucket1, _k4, _v2, 3, nil},
+		{_bucket1, _k4, _v1, 4, nil},
+		{_bucket1, _k4, _v1, 6, nil},
+		{_bucket1, _k4, _v4, 7, nil},
+		{_bucket1, _k4, _v4, 9, nil},
+		{_bucket1, _k4, _v4, 10, nil},
+		{_bucket1, _k4, _v4, 11, nil}, // larger than last key in bucket
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Get(e.height, e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.v, value)
+	}
+	// check version
+	for _, e := range []versionTest{
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, nil, 9, nil},
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 10, nil},
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Version(e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.height, value)
+	}
+	// test delete
+	r.NoError(db.Delete(10, _bucket2, _k1))
+	for _, k := range [][]byte{_k1, _k2, _k3, _k4, _k5, _k10} {
+		r.NoError(db.Delete(10, _bucket1, k))
+	}
+	// key still can be read before delete version
+	for _, e := range []versionTest{
+		{_bucket2, _k1, nil, 0, ErrNotExist}, // bucket not exist
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, _v2, 0, nil},
+		{_bucket1, _k2, _v1, 1, nil},
+		{_bucket1, _k2, _v1, 2, nil},
+		{_bucket1, _k2, _v3, 3, nil},
+		{_bucket1, _k2, _v3, 5, nil},
+		{_bucket1, _k2, _v4, 6, nil},
+		{_bucket1, _k2, _v4, 8, nil},
+		{_bucket1, _k2, _v4, 9, nil},         // before delete version
+		{_bucket1, _k2, nil, 10, ErrDeleted}, // after delete version
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 1, ErrNotExist}, // before first write version
+		{_bucket1, _k4, _v2, 2, nil},
+		{_bucket1, _k4, _v2, 3, nil},
+		{_bucket1, _k4, _v1, 4, nil},
+		{_bucket1, _k4, _v1, 6, nil},
+		{_bucket1, _k4, _v4, 7, nil},
+		{_bucket1, _k4, _v4, 9, nil},         // before delete version
+		{_bucket1, _k4, nil, 10, ErrDeleted}, // after delete version
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Get(e.height, e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.v, value)
+	}
+	// write before delete version does nothing
+	r.NoError(db.Put(9, _bucket1, _k2, _k2))
+	r.NoError(db.Put(9, _bucket1, _k4, _k4))
+	for _, e := range []versionTest{
+		{_bucket1, _k2, _v4, 9, nil},         // before delete version
+		{_bucket1, _k2, nil, 10, ErrDeleted}, // after delete version
+		{_bucket1, _k4, _v4, 9, nil},         // before delete version
+		{_bucket1, _k4, nil, 10, ErrDeleted}, // after delete version
+	} {
+		value, err := db.Get(e.height, e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.v, value)
+	}
+	// write after delete version
+	r.NoError(db.Put(10, _bucket1, _k2, _k2))
+	r.NoError(db.Put(10, _bucket1, _k4, _k4))
+	for _, e := range []versionTest{
+		{_bucket2, _k1, nil, 0, ErrNotExist}, // bucket not exist
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, _v2, 0, nil},
+		{_bucket1, _k2, _v1, 1, nil},
+		{_bucket1, _k2, _v1, 2, nil},
+		{_bucket1, _k2, _v3, 3, nil},
+		{_bucket1, _k2, _v3, 5, nil},
+		{_bucket1, _k2, _v4, 6, nil},
+		{_bucket1, _k2, _v4, 8, nil},
+		{_bucket1, _k2, _v4, 9, nil},  // before delete version
+		{_bucket1, _k2, _k2, 10, nil}, // after delete version
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 1, ErrNotExist}, // before first write version
+		{_bucket1, _k4, _v2, 2, nil},
+		{_bucket1, _k4, _v2, 3, nil},
+		{_bucket1, _k4, _v1, 4, nil},
+		{_bucket1, _k4, _v1, 6, nil},
+		{_bucket1, _k4, _v4, 7, nil},
+		{_bucket1, _k4, _v4, 9, nil},  // before delete version
+		{_bucket1, _k4, _k4, 10, nil}, // after delete version
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Get(e.height, e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.v, value)
+	}
+	// check version after delete
+	for _, e := range []versionTest{
+		{_bucket1, _k1, nil, 0, ErrNotExist},
+		{_bucket1, _k2, nil, 10, nil},
+		{_bucket1, _k3, nil, 0, ErrNotExist},
+		{_bucket1, _k4, nil, 10, nil},
+		{_bucket1, _k5, nil, 0, ErrNotExist},
+		{_bucket1, _k10, nil, 0, ErrInvalid},
+	} {
+		value, err := db.Version(e.ns, e.k)
+		r.Equal(e.err, errors.Cause(err))
+		r.Equal(e.height, value)
+	}
 }
