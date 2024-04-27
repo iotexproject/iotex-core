004e35abc6d7dd4ee0cdf151949eb1574f2d7efc
Dustin Xie <dahuaxie@gmail.com>
2024-04-26 18:02:35 -0700

    [factory] implement versioned stateDB to support archive mode

--- state/factory/workingsetstore_test.go
+++ state/factory/workingsetstore_test.go
@@ -12,10 +12,23 @@ import (
 	"testing"
 
 	"github.com/iotexproject/iotex-core/v2/action/protocol"
+	"github.com/iotexproject/iotex-core/v2/action/protocol/execution/evm"
 	"github.com/iotexproject/iotex-core/v2/db"
 	"github.com/iotexproject/iotex-core/v2/db/batch"
 	"github.com/iotexproject/iotex-core/v2/pkg/util/byteutil"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	name      = "name"
+	viewValue = "value"
+	namespace = "namespace"
+	key1      = []byte("key1")
+	value1    = []byte("value1")
+	key2      = []byte("key2")
+	value2    = []byte("value2")
+	key3      = []byte("key3")
+	value3    = []byte("value3")
 )
 
 func TestStateDBWorkingSetStore(t *testing.T) {
@@ -25,18 +38,9 @@ func TestStateDBWorkingSetStore(t *testing.T) {
 	inMemStore := db.NewMemKVStore()
 	flusher, err := db.NewKVStoreFlusher(inMemStore, batch.NewCachedBatch())
 	require.NoError(err)
-	store := newStateDBWorkingSetStore(view, flusher, true)
+	store := newStateDBWorkingSetStore(view, flusher, true, AccountKVNamespace)
 	require.NotNil(store)
 	require.NoError(store.Start(ctx))
-	name := "name"
-	viewValue := "value"
-	namespace := "namespace"
-	key1 := []byte("key1")
-	value1 := []byte("value1")
-	key2 := []byte("key2")
-	value2 := []byte("value2")
-	key3 := []byte("key3")
-	value3 := []byte("value3")
 	t.Run("test view", func(t *testing.T) {
 		_, err := store.ReadView(name)
 		require.Error(err)
@@ -107,6 +111,43 @@ func TestStateDBWorkingSetStore(t *testing.T) {
 		require.True(bytes.Equal(heightInStore, byteutil.Uint64ToBytes(height)))
 	})
 	require.NoError(store.Stop(ctx))
+}
+
+func TestVersionedWorkingSetStore(t *testing.T) {
+	r := require.New(t)
+	var (
+		mns    = "mta"
+		stores = []workingSetStore{}
+		digest = [2]string{
+			"e6958faedcc37528dad9ac99f5e6613fbefbf403a06fe962535225d42a27b189",
+			"bb262ac0603e48aa737f5eb42014f481cb54d831c14fe736b8f61b69e5b4924a",
+		}
+	)
+	for _, preEaster := range []bool{false, true} {
+		for _, versioned := range []bool{true, false} {
+			for _, ns := range []string{mns, "test1", "test can pass with any string here"} {
+				sdb := stateDB{
+					versioned: versioned,
+					metaNS:    ns,
+				}
+				flusher, err := db.NewKVStoreFlusher(db.NewMemKVStore(), batch.NewCachedBatch(), sdb.flusherOptions(preEaster)...)
+				r.NoError(err)
+				stores = append(stores, newStateDBWorkingSetStore(nil, flusher, true, sdb.metadataNS()))
+			}
+		}
+	}
+	for i, store := range stores {
+		r.NotNil(store)
+		r.NoError(store.Put(namespace, key1, value1))
+		r.NoError(store.Put(namespace, key2, value2))
+		r.NoError(store.Put(namespace, []byte(CurrentHeightKey), value3))
+		r.NoError(store.Put(mns, key1, value1))
+		r.NoError(store.Delete(namespace, key2))
+		r.NoError(store.Put(evm.CodeKVNameSpace, key3, value1))
+		r.NoError(store.Finalize(3))
+		h := store.Digest()
+		r.Equal(digest[i/6], hex.EncodeToString(h[:]))
+	}
 }
 
 func TestFactoryWorkingSetStore(t *testing.T) {
