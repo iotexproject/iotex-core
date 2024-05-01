6cc55798c3196c06cdbe1416ed53505838cb23db
Dustin Xie <dahuaxie@gmail.com>
2024-05-01 11:54:47 -0700

    [db] KvWithVersion to handle both versioned and non-versioned namespace

--- db/db_versioned.go
+++ db/db_versioned.go
@@ -29,35 +29,17 @@ var (
 )
 
 type (
-	// KvVersioned is a versioned key-value store, where each key has multiple
-	// versions of value (corresponding to different heights in a blockchain)
-	//
-	// Versioning is achieved by using (key + 8-byte version) as the actual
-	// storage key in the underlying DB. For each bucket, a metadata is stored
-	// at the special key = []byte{0}. The metadata specifies the bucket's name
-	// and the key length.
-	//
-	// For each versioned key, the special location = key + []byte{0} stores the
-	// key's metadata, which includes the following info:
-	// 1. the version when the key is first created
-	// 2. the version when the key is lastly written
-	// 3. the version when the key is deleted
-	// 4. the key's last written value (to fast-track read of current version)
-	// If the location does not store a value, the key has never been written.
-	//
-	// How to use a versioned DB:
-	//
-	// db := NewBoltDBVersioned(cfg) // creates a versioned DB
-	// db.Start(ctx)
-	// defer func() { db.Stop(ctx) }()
-	//
-	// kv := db.SetVersion(5)
-	// value, err := kv.Get("ns", key) // read 'key' at version 5
-	// kv = db.SetVersion(8)
-	// err := kv.Put("ns", key, value) // write 'key' at version 8
-
-	KvVersioned interface {
+	VersionedDB interface {
 		lifecycle.StartStopper
+
+		// Put insert or update a record identified by (namespace, key)
+		Put(uint64, string, []byte, []byte) error
+
+		// Get gets a record by (namespace, key)
+		Get(uint64, string, []byte) ([]byte, error)
+
+		// Delete deletes a record by (namespace, key)
+		Delete(uint64, string, []byte) error
 
 		// Base returns the underlying KVStore
 		Base() KVStore
@@ -65,8 +47,8 @@ type (
 		// Version returns the key's most recent version
 		Version(string, []byte) (uint64, error)
 
-		// SetVersion sets the version, and returns a KVStore to call Put()/Get()
-		SetVersion(uint64) KVStore
+		// CommitToDB writes a batch to the underlying DB
+		CommitToDB(uint64, map[string]bool, batch.KVStoreBatch) error
 	}
 
 	// BoltDBVersioned is KvVersioned implementation based on bolt DB
@@ -75,7 +57,7 @@ type (
 	}
 )
 
-// NewBoltDBVersioned instantiates an BoltDB which implements KvVersioned
+// NewBoltDBVersioned instantiates an BoltDB which implements VersionedDB
 func NewBoltDBVersioned(cfg Config) *BoltDBVersioned {
 	b := BoltDBVersioned{
 		db: NewBoltDB(cfg),
@@ -505,47 +487,5 @@ func (b *BoltDBVersioned) checkNamespaceAndKey(ns string, key []byte) error {
 	if len(key) != int(vn.keyLen) {
 		return errors.Wrapf(ErrInvalid, "invalid key length, expecting %d, got %d", vn.keyLen, len(key))
 	}
-	return nil
-}
-
-// KvWithVersion wraps the BoltDBVersioned with a certain version
-type KvWithVersion struct {
-	db      *BoltDBVersioned
-	version uint64 // version for Get/Put()
-}
-
-// Start starts the DB
-func (b *KvWithVersion) Start(context.Context) error {
-	panic("should call BoltDBVersioned's Start method")
-}
-
-// Stop stops the DB
-func (b *KvWithVersion) Stop(context.Context) error {
-	panic("should call BoltDBVersioned's Stop method")
-}
-
-// Put writes a <key, value> record
-func (b *KvWithVersion) Put(ns string, key, value []byte) error {
-	return b.db.Put(b.version, ns, key, value)
-}
-
-// Get retrieves a key's value
-func (b *KvWithVersion) Get(ns string, key []byte) ([]byte, error) {
-	return b.db.Get(b.version, ns, key)
-}
-
-// Delete deletes a key
-func (b *KvWithVersion) Delete(ns string, key []byte) error {
-	return b.db.Delete(b.version, ns, key)
-}
-
-// Filter returns <k, v> pair in a bucket that meet the condition
-func (b *KvWithVersion) Filter(ns string, cond Condition, minKey, maxKey []byte) ([][]byte, [][]byte, error) {
-	panic("Filter not supported for versioned DB")
-}
-
-// WriteBatch commits a batch
-func (b *KvWithVersion) WriteBatch(kvsb batch.KVStoreBatch) error {
-	// TODO: implement WriteBatch
 	return nil
 }
