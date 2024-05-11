9b1701d39ec1a559c96f10e722940c3c2f948856
zhi <thecoderzhi@gmail.com>
2024-05-11 14:54:48 +0800

    set db to be read only

--- chainservice/builder.go
+++ chainservice/builder.go
@@ -749,7 +749,10 @@ func (builder *Builder) build(forSubChain, forTest bool) (*ChainService, error) 
 	if builder.cs.p2pAgent == nil {
 		builder.cs.p2pAgent = p2p.NewDummyAgent()
 	}
-	_, readonlyMode := builder.cfg.Plugins[config.ReadOnlyGatewayPlugin]
+	_, readOnlyMode := builder.cfg.Plugins[config.ReadOnlyGatewayPlugin]
+	if readOnlyMode {
+		builder.cfg.DB.ReadOnly = true
+	}
 
 	if err := builder.buildFactory(forTest); err != nil {
 		return nil, err
@@ -757,7 +760,7 @@ func (builder *Builder) build(forSubChain, forTest bool) (*ChainService, error) 
 	if err := builder.buildElectionCommittee(); err != nil {
 		return nil, err
 	}
-	if !readonlyMode {
+	if !readOnlyMode {
 		if err := builder.buildActionPool(); err != nil {
 			return nil, err
 		}
@@ -796,7 +799,7 @@ func (builder *Builder) build(forSubChain, forTest bool) (*ChainService, error) 
 	if err := builder.registerRewardingProtocol(); err != nil {
 		return nil, errors.Wrap(err, "failed to register rewarding protocol")
 	}
-	if !readonlyMode {
+	if !readOnlyMode {
 		if err := builder.buildConsensusComponent(); err != nil {
 			return nil, err
 		}
