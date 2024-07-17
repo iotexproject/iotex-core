cd51767736d4613397dc8398c477274a778668a7
envestcc <chen1233216@hotmail.com>
2024-07-17 11:11:19 +0800

    remove number of logs limit

--- api/coreservice.go
+++ api/coreservice.go
@@ -1401,12 +1401,6 @@ func (core *coreService) LogsInRange(filter *logfilter.LogFilter, start, end, pa
 	if err != nil {
 		return nil, nil, err
 	}
-	if paginationSize == 0 {
-		paginationSize = 1000
-	}
-	if paginationSize > 5000 {
-		paginationSize = 5000
-	}
 	// getLogs via range Blooom filter [start, end]
 	blockNumbers, err := core.bfIndexer.FilterBlocksInRange(filter, start, end, paginationSize)
 	if err != nil {
