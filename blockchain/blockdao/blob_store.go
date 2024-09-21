1907d27b12f3266a24f46f5f10756cf57d45134c
dustinxie <dahuaxie@gmail.com>
2024-09-20 19:15:45 -0700

    address comment 3

--- blockchain/blockdao/blob_store.go
+++ blockchain/blockdao/blob_store.go
@@ -22,18 +22,4 @@ type (
 		GetBlobsByHeight(uint64) ([]*types.BlobTxSidecar, []string, error)
 		PutBlock(*block.Block) error
 	}
-
-	// storage for past N-day's blobs, structured as blow:
-	// 1. Storage is divided by hour, with each hour's data stored in a bucket.
-	//    By default N = 18 so we have 432 buckets, plus 1 extra bucket. At each
-	//    new hour, the oldest bucket is found and all data inside it deleted
-	//    (so we still have 18 days after deletion), and blobs in this hour are
-	//    stored into it
-	// 2. Inside each bucket, block height is used as the key to store the blobs
-	//    in this block. Maximum number of blobs is 6 for each block, and maximum
-	//    data size in the bucket is 131kB x 6 x 720 = 566MB. Maximum total size
-	//    of the entire storage is 566MB x 433 = 245GB.
-	// 3. A mapping of action hash --> (bucket, block) is maintained in a separate
-	//    bucket as the storage index. It converts the requested blob hash to the
-	//    bucket and block number, and retrieves the blob object from storage
 )
