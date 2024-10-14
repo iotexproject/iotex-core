58556c253cc70ef53228c251a9e0b2d38016b69c
dustinxie <dahuaxie@gmail.com>
2024-10-14 00:33:00 -0700

    [workingset] ValidateWithState in pickAndRun

--- state/factory/workingset.go
+++ state/factory/workingset.go
@@ -52,20 +52,23 @@ func init() {
 
 type (
 	workingSet struct {
-		height    uint64
-		store     workingSetStore
-		finalized bool
-		dock      protocol.Dock
-		receipts  []*action.Receipt
+		height      uint64
+		store       workingSetStore
+		finalized   bool
+		dock        protocol.Dock
+		txValidator *protocol.GenericValidator
+		receipts    []*action.Receipt
 	}
 )
 
 func newWorkingSet(height uint64, store workingSetStore) *workingSet {
-	return &workingSet{
+	ws := &workingSet{
 		height: height,
 		store:  store,
 		dock:   protocol.NewDock(),
 	}
+	ws.txValidator = protocol.NewGenericValidator(ws, accountutil.AccountState)
+	return ws
 }
 
 func (ws *workingSet) digest() (hash.Hash256, error) {
@@ -669,6 +672,15 @@ func (ws *workingSet) pickAndRunActions(
 						actionIterator.PopAccount()
 						continue
 					}
+				}
+			}
+			if fCtx.ValidateStateWhenMintBlock {
+				if err := ws.txValidator.ValidateWithState(ctxWithBlockContext, nextAction); err != nil {
+					if caller := nextAction.SenderAddress(); caller != nil {
+						ap.DeleteAction(caller)
+					}
+					actionIterator.PopAccount()
+					continue
 				}
 			}
 			actionCtx, err := withActionCtx(ctxWithBlockContext, nextAction)
