00e3fd87b5c4408d0bc33f6258e095efdbc17e3e
dustinxie <dahuaxie@gmail.com>
2024-10-17 18:02:31 -0700

    [workingset] verify correct tx after unfolding

--- action/protocol/context.go
+++ action/protocol/context.go
@@ -147,6 +147,7 @@ type (
 		CorrectValidationOrder                  bool
 		UnstakedButNotClearSelfStakeAmount      bool
 		EnableNewTxTypes                        bool
+		VerifyNotContainerBeforeRun             bool
 	}
 
 	// FeatureWithHeightCtx provides feature check functions.
@@ -307,6 +308,7 @@ func WithFeatureCtx(ctx context.Context) context.Context {
 			CorrectValidationOrder:                  g.IsVanuatu(height),
 			UnstakedButNotClearSelfStakeAmount:      !g.IsVanuatu(height),
 			EnableNewTxTypes:                        g.IsVanuatu(height),
+			VerifyNotContainerBeforeRun:             g.IsVanuatu(height),
 		},
 	)
 }
