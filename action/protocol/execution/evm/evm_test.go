b8b3c4febd970a38265ca20ac36de7b304adc376
dustinxie <dahuaxie@gmail.com>
2024-11-07 20:43:46 -0800

    [genesis] set Vanuatu to activate at Nov-21-2024 01:04:00AM +UTC

--- action/protocol/execution/evm/evm_test.go
+++ action/protocol/execution/evm/evm_test.go
@@ -260,12 +260,12 @@ func TestConstantinople(t *testing.T) {
 		},
 		{
 			"io1pcg2ja9krrhujpazswgz77ss46xgt88afqlk6y",
-			41174200,
+			33281640,
 		},
 		// after Vanuatu
 		{
 			action.EmptyAddress,
-			41174201,
+			33281641,
 		},
 		{
 			"io1pcg2ja9krrhujpazswgz77ss46xgt88afqlk6y",
