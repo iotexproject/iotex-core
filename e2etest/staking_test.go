ef8bedcc14d74e4b89df925f67fd39344a56ca15
Chen Chen <chen1233216@hotmail.com>
2024-11-19 13:00:31 +0800

    [mint] deadline for mint block (#4487)

--- e2etest/staking_test.go
+++ e2etest/staking_test.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/mohae/deepcopy"
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotexproject/go-pkgs/hash"
@@ -181,7 +182,7 @@ func TestStakingContract(t *testing.T) {
 		}
 	}
 
-	cfg := config.Default
+	cfg := deepcopy.Copy(config.Default).(config.Config)
 	initDBPaths(require, &cfg)
 
 	defer func() {
@@ -193,6 +194,7 @@ func TestStakingContract(t *testing.T) {
 	cfg.ActPool.MinGasPriceStr = "0"
 	cfg.Chain.TrieDBPatchFile = ""
 	cfg.Chain.ProducerPrivKey = "a000000000000000000000000000000000000000000000000000000000000000"
+	cfg.Chain.MintTimeout = 0
 	cfg.Consensus.Scheme = config.RollDPoSScheme
 	cfg.Genesis.NumDelegates = 1
 	cfg.Genesis.NumSubEpochs = 10
