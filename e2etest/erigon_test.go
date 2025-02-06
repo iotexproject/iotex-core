af5ced7a02cfb02e4f5de337ea4aa63bf33e93e1
envestcc <chen1233216@hotmail.com>
2025-02-06 17:13:57 +0800

    add test

--- e2etest/erigon_test.go
+++ e2etest/erigon_test.go
@@ -5,6 +5,7 @@ import (
 	_ "embed"
 	"encoding/hex"
 	"math/big"
+	"math/rand"
 	"strings"
 	"testing"
 	"time"
@@ -15,8 +16,10 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotexproject/iotex-address/address"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
 
 	"github.com/iotexproject/iotex-core/v2/action"
+	"github.com/iotexproject/iotex-core/v2/action/protocol/staking"
 	"github.com/iotexproject/iotex-core/v2/blockchain/block"
 	"github.com/iotexproject/iotex-core/v2/config"
 	"github.com/iotexproject/iotex-core/v2/pkg/unit"
@@ -40,7 +43,7 @@ func init() {
 	}
 }
 
-func TestArchiveModeByErigon(t *testing.T) {
+func TestErigonArchiveContract(t *testing.T) {
 	r := require.New(t)
 	sender := identityset.Address(10).String()
 	senderSK := identityset.PrivateKey(10)
@@ -131,6 +134,7 @@ func TestArchiveModeByErigon(t *testing.T) {
 			r.Equal(balance.String(), b.String())
 		},
 	})
+	var code []byte
 	test.runCase(ctx, &testcase{
 		name: "deploy erc20",
 		act: &actionWithTime{
@@ -156,6 +160,8 @@ func TestArchiveModeByErigon(t *testing.T) {
 			consumed := new(big.Int).Mul(gasPrice, big.NewInt(int64(blk.Receipts[0].GasConsumed)))
 			balance.Sub(balance, consumed)
 			r.Equal(balance.String(), b.String())
+			code, err = test.ethcli.CodeAt(ctx, ethContractAddr, nil)
+			r.NoError(err)
 		},
 	})
 	test.runCase(ctx, &testcase{
@@ -182,6 +188,104 @@ func TestArchiveModeByErigon(t *testing.T) {
 			}, nil)
 			r.NoError(err)
 			r.Equal("1", big.NewInt(0).SetBytes(b).String())
+			// check code
+			c, err := test.ethcli.CodeAt(ctx, ethContractAddr, big.NewInt(int64(blk.Height())))
+			r.NoError(err)
+			r.Equal(code, c)
 		},
 	})
+}
+
+func TestErigonArchiveAccountForAllActions(t *testing.T) {
+	r := require.New(t)
+	cfg := initCfg(r)
+	cfg.API.GRPCPort = testutil.RandomPort()
+	cfg.API.HTTPPort = testutil.RandomPort()
+	cfg.API.WebSocketPort = 0
+	cfg.Plugins[config.GatewayPlugin] = true
+	cfg.Genesis.VanuatuBlockHeight = 1
+	cfg.Genesis.SystemStakingContractV2Address = "io1dkqh5mu9djfas3xyrmzdv9frsmmytel4mp7a64"
+	cfg.Genesis.SystemStakingContractV2Height = 1
+	cfg.DardanellesUpgrade.BlockInterval = time.Second * 8640
+	balance := unit.ConvertIotxToRau(2000000)
+	for i := 0; i < 10; i++ {
+		cfg.Genesis.InitBalanceMap[identityset.Address(i).String()] = balance.String()
+	}
+	testutil.NormalizeGenesisHeights(&cfg.Genesis.Blockchain)
+	test := newE2ETest(t, cfg)
+	chainID := cfg.Chain.ID
+	gasPrice := big.NewInt(unit.Qev)
+	newLegacyTx := func(nonce uint64) action.TxCommonInternal {
+		return action.NewLegacyTx(chainID, nonce, gasLimit, gasPrice)
+	}
+	deployERC20, err := hex.DecodeString(erc20Bytecode)
+	r.NoError(err)
+	deploySystemStaking, err := hex.DecodeString(stakingContractV2Bytecode)
+	r.NoError(err)
+	mustCallDataSystemStaking := func(m string, args ...any) []byte {
+		data, err := abiCall(staking.StakingContractABI, m, args...)
+		r.NoError(err)
+		return data
+	}
+	type actionPayload interface {
+		IntrinsicGas() (uint64, error)
+		SanityCheck() error
+		FillAction(*iotextypes.ActionCore)
+	}
+	ctx := context.Background()
+	check := func(act *action.SealedEnvelope) {
+		test.runCase(ctx, &testcase{
+			act: &actionWithTime{act, time.Now()},
+			expect: []actionExpect{&functionExpect{fn: func(test *e2etest, act *action.SealedEnvelope, receipt *action.Receipt, err error) {
+				r.NoError(err)
+				r.EqualValues(iotextypes.ReceiptStatus_Success, receipt.Status)
+				t.Log("contract address", receipt.ContractAddress)
+				sender := act.SrcPubkey().Address()
+				preBalance, err := test.ethcli.BalanceAt(ctx, common.BytesToAddress(sender.Bytes()), big.NewInt(int64(receipt.BlockHeight)))
+				r.NoError(err)
+				t.Log("pre balance:", preBalance, "block height:", receipt.BlockHeight)
+				postBalance, err := test.ethcli.BalanceAt(ctx, common.BytesToAddress(sender.Bytes()), nil)
+				r.NoError(err)
+				t.Log("post balance:", postBalance)
+				postNonce, err := test.ethcli.NonceAt(ctx, common.BytesToAddress(sender.Bytes()), nil)
+				r.NoError(err)
+				t.Log("post nonce:", postNonce)
+				cost := costOfAction(act.SrcPubkey().Address().String(), receipt)
+				r.Equal(postBalance.String(), big.NewInt(0).Sub(preBalance, cost).String(), "balance mismatch")
+				r.Equal(act.Nonce()+1, postNonce, "nonce mismatch")
+			}}},
+		})
+	}
+	type payloadWithSender struct {
+		payload actionPayload
+		sender  int
+	}
+	payloads := []payloadWithSender{
+		{action.NewExecution("", big.NewInt(0), append(deploySystemStaking, mustCallDataSystemStaking("", unit.ConvertIotxToRau(100))...)), 1}, // deploy systemstaking contract
+		{action.NewTransfer(big.NewInt(10), identityset.Address(11).String(), nil), rand.Intn(10)},
+		{action.NewExecution("", big.NewInt(0), deployERC20), rand.Intn(10)},
+		{action.NewDepositToRewardingFund(big.NewInt(10000), nil), rand.Intn(10)},
+		{mustNoErr(action.NewCandidateRegister("cand1", identityset.Address(1).String(), identityset.Address(1).String(), identityset.Address(1).String(), unit.ConvertIotxToRau(1200000).String(), 0, false, nil)), rand.Intn(10)},
+		{mustNoErr(action.NewCreateStake("cand1", unit.ConvertIotxToRau(200).String(), 1, true, nil)), 1},
+		{action.NewMigrateStake(1), 1},
+	}
+
+	for i := range payloads {
+		senderIdx := payloads[i].sender
+		act := mustNoErr(action.Sign(action.NewEnvelope(newLegacyTx(test.nonceMgr.pop(identityset.Address(senderIdx).String())), payloads[i].payload), identityset.PrivateKey(senderIdx)))
+		check(act)
+	}
+}
+
+func costOfAction(sender string, receipt *action.Receipt) *big.Int {
+	cost := big.NewInt(0)
+	for _, l := range receipt.TransactionLogs() {
+		if l.Sender == sender {
+			cost.Add(cost, l.Amount)
+		}
+		if l.Recipient == sender {
+			cost.Sub(cost, l.Amount)
+		}
+	}
+	return cost
 }
