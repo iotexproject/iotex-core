9e64e9e42612b16ab18cf98046715b991db2448d
envestcc <chen1233216@hotmail.com>
2025-02-26 08:58:16 +0800

    stop and run at 13693961

--- blockchain/blockdao/blockindexer.go
+++ blockchain/blockdao/blockindexer.go
@@ -71,7 +71,7 @@ func (bic *BlockIndexerChecker) CheckIndexer(ctx context.Context, indexer BlockI
 		return err
 	}
 	if targetHeight == 0 || targetHeight > daoTip {
-		targetHeight = daoTip
+		targetHeight = min(daoTip, 13693961)
 	}
 	startHeight := tipHeight + 1
 	if indexerWS, ok := indexer.(BlockIndexerWithStart); ok {
