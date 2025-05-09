97b6f0b9eeeb2ca93e83088177918d63459df75e
seventeen <315654883@qq.com>
2025-05-09 14:21:15 +0800

    update unstable download

--- install-cli.sh
+++ install-cli.sh
@@ -163,10 +163,12 @@ if [ "$1" = "unstable" ]; then
         echo $(git describe --tags $(git rev-list --tags --max-count=10))
         exit 1
     else
-        cd $(dirname $0)
+        git clone https://github.com/iotexproject/iotex-core.git
+        cd iotex-core
         git checkout $2
         make $INSTALL_NAME
         sudo mv bin/$INSTALL_NAME $INSTALL_DIRECTORY/
+        cd ../ && rm -rf iotex-core
         exit 0
     fi
 else
