4d947975d4f90525d95ad54bacbe8c1aca7520a0
zhi <thecoderzhi@gmail.com>
2025-06-18 23:49:47 +0800

    refactor blockindex

--- blockindex/contractstaking/dirty_cache_test.go
+++ blockindex/contractstaking/dirty_cache_test.go
@@ -62,8 +62,8 @@ func TestContractStakingDirty_getBucketInfo(t *testing.T) {
 	require.Equal(identityset.Address(2), bi.Owner)
 
 	// added bucket info
-	require.NoError(dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2}))
-	require.NoError(dirty.addBucketInfo(2, &bucketInfo{TypeIndex: 2, CreatedAt: 2, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(2), Owner: identityset.Address(3)}))
+	dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2})
+	dirty.addBucketInfo(2, &bucketInfo{TypeIndex: 2, CreatedAt: 2, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(2), Owner: identityset.Address(3)})
 	bi, ok = dirty.getBucketInfo(2)
 	require.True(ok)
 	require.EqualValues(2, bi.TypeIndex)
@@ -74,7 +74,7 @@ func TestContractStakingDirty_getBucketInfo(t *testing.T) {
 	require.Equal(identityset.Address(3), bi.Owner)
 
 	// modified bucket info
-	require.NoError(dirty.updateBucketInfo(1, &bucketInfo{TypeIndex: 2, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)}))
+	dirty.updateBucketInfo(1, &bucketInfo{TypeIndex: 2, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)})
 	bi, ok = dirty.getBucketInfo(1)
 	require.True(ok)
 	require.EqualValues(2, bi.TypeIndex)
@@ -85,7 +85,7 @@ func TestContractStakingDirty_getBucketInfo(t *testing.T) {
 	require.Equal(identityset.Address(4), bi.Owner)
 
 	// removed bucket info
-	require.NoError(dirty.deleteBucketInfo(1))
+	dirty.deleteBucketInfo(1)
 	bi, ok = dirty.getBucketInfo(1)
 	require.False(ok)
 	require.Nil(bi)
@@ -112,7 +112,7 @@ func TestContractStakingDirty_matchBucketType(t *testing.T) {
 	require.EqualValues(1, id)
 
 	// added bucket type
-	require.NoError(dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2}))
+	dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2})
 	id, bt, ok = dirty.matchBucketType(big.NewInt(200), 200)
 	require.True(ok)
 	require.EqualValues(200, bt.Amount.Int64())
@@ -136,7 +136,7 @@ func TestContractStakingDirty_getBucketTypeCount(t *testing.T) {
 	require.EqualValues(1, count)
 
 	// added bucket type
-	require.NoError(dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2}))
+	dirty.addBucketType(2, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2})
 	count = dirty.getBucketTypeCount()
 	require.EqualValues(2, count)
 }
@@ -144,10 +144,12 @@ func TestContractStakingDirty_getBucketTypeCount(t *testing.T) {
 func TestContractStakingDirty_finalize(t *testing.T) {
 	require := require.New(t)
 	clean := newContractStakingCache(Config{CalculateVoteWeight: calculateVoteWeightGen(genesis.TestDefault().VoteWeightCalConsts)})
-	dirty := newContractStakingDirty(clean)
+	dirty := newContractStakingDirty(newWrappedCache(clean))
+	totalCnt := clean.TotalBucketCount()
+	require.EqualValues(0, totalCnt)
 
 	// no dirty data
-	batcher, delta := dirty.finalize()
+	batcher, cache := dirty.finalize()
 	require.EqualValues(1, batcher.Size())
 	info, err := batcher.Entry(0)
 	require.NoError(err)
@@ -155,17 +157,12 @@ func TestContractStakingDirty_finalize(t *testing.T) {
 	require.EqualValues(batch.Put, info.WriteType())
 	require.EqualValues(_stakingTotalBucketCountKey, info.Key())
 	require.EqualValues(byteutil.Uint64ToBytesBigEndian(0), info.Value())
-	for _, d := range delta.BucketTypeDelta() {
-		require.Len(d, 0)
-	}
-	for _, d := range delta.BucketTypeDelta() {
-		require.Len(d, 0)
-	}
+	require.Equal(0, cache.BucketTypeCount())
 
 	// added bucket type
 	bt := &BucketType{Amount: big.NewInt(100), Duration: 100, ActivatedAt: 1}
-	require.NoError(dirty.addBucketType(1, bt))
-	batcher, delta = dirty.finalize()
+	dirty.addBucketType(1, bt)
+	batcher, cache = dirty.finalize()
 	require.EqualValues(2, batcher.Size())
 	info, err = batcher.Entry(1)
 	require.NoError(err)
@@ -173,17 +170,12 @@ func TestContractStakingDirty_finalize(t *testing.T) {
 	require.EqualValues(batch.Put, info.WriteType())
 	require.EqualValues(byteutil.Uint64ToBytesBigEndian(1), info.Key())
 	require.EqualValues(bt.Serialize(), info.Value())
-	btDelta := delta.BucketTypeDelta()
-	require.NotNil(btDelta[deltaStateAdded])
-	require.Len(btDelta[deltaStateAdded], 1)
-	require.EqualValues(100, btDelta[deltaStateAdded][1].Amount.Int64())
-	require.EqualValues(100, btDelta[deltaStateAdded][1].Duration)
-	require.EqualValues(1, btDelta[deltaStateAdded][1].ActivatedAt)
+	require.Equal(1, cache.BucketTypeCount())
 
 	// add bucket info
 	bi := &bucketInfo{TypeIndex: 1, CreatedAt: 2, UnlockedAt: 3, UnstakedAt: 4, Delegate: identityset.Address(1), Owner: identityset.Address(2)}
-	require.NoError(dirty.addBucketInfo(1, bi))
-	batcher, delta = dirty.finalize()
+	dirty.addBucketInfo(1, bi)
+	batcher, cache = dirty.finalize()
 	require.EqualValues(3, batcher.Size())
 	info, err = batcher.Entry(2)
 	require.NoError(err)
@@ -191,46 +183,38 @@ func TestContractStakingDirty_finalize(t *testing.T) {
 	require.EqualValues(batch.Put, info.WriteType())
 	require.EqualValues(byteutil.Uint64ToBytesBigEndian(1), info.Key())
 	require.EqualValues(bi.Serialize(), info.Value())
-	biDelta := delta.BucketInfoDelta()
-	require.NotNil(biDelta[deltaStateAdded])
-	require.Len(biDelta[deltaStateAdded], 1)
-	require.EqualValues(1, biDelta[deltaStateAdded][1].TypeIndex)
-	require.EqualValues(2, biDelta[deltaStateAdded][1].CreatedAt)
-	require.EqualValues(3, biDelta[deltaStateAdded][1].UnlockedAt)
-	require.EqualValues(4, biDelta[deltaStateAdded][1].UnstakedAt)
-	require.EqualValues(identityset.Address(1).String(), biDelta[deltaStateAdded][1].Delegate.String())
-	require.EqualValues(identityset.Address(2).String(), biDelta[deltaStateAdded][1].Owner.String())
-
+	totalCnt = cache.TotalBucketCount()
+	require.EqualValues(2, totalCnt)
 }
 
 func TestContractStakingDirty_noSideEffectOnClean(t *testing.T) {
 	require := require.New(t)
 	clean := newContractStakingCache(Config{CalculateVoteWeight: calculateVoteWeightGen(genesis.TestDefault().VoteWeightCalConsts)})
-	dirty := newContractStakingDirty(clean)
+	dirty := newContractStakingDirty(newWrappedCache(clean))
 
 	// add bucket type to dirty cache
-	require.NoError(dirty.addBucketType(1, &BucketType{Amount: big.NewInt(100), Duration: 100, ActivatedAt: 1}))
+	dirty.addBucketType(1, &BucketType{Amount: big.NewInt(100), Duration: 100, ActivatedAt: 1})
 	// check that clean cache is not affected
 	bt, ok := clean.getBucketType(1)
 	require.False(ok)
 	require.Nil(bt)
 
 	// add bucket info to dirty cache
-	require.NoError(dirty.addBucketInfo(1, &bucketInfo{TypeIndex: 1, CreatedAt: 1, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(1), Owner: identityset.Address(2)}))
+	dirty.addBucketInfo(1, &bucketInfo{TypeIndex: 1, CreatedAt: 1, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(1), Owner: identityset.Address(2)})
 	// check that clean cache is not affected
 	bi, ok := clean.getBucketInfo(1)
 	require.False(ok)
 	require.Nil(bi)
 
 	// update bucket type in dirty cache
-	require.NoError(dirty.updateBucketType(1, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2}))
+	dirty.updateBucketType(1, &BucketType{Amount: big.NewInt(200), Duration: 200, ActivatedAt: 2})
 	// check that clean cache is not affected
 	bt, ok = clean.getBucketType(1)
 	require.False(ok)
 	require.Nil(bt)
 
 	// update bucket info in dirty cache
-	require.NoError(dirty.updateBucketInfo(1, &bucketInfo{TypeIndex: 2, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)}))
+	dirty.updateBucketInfo(1, &bucketInfo{TypeIndex: 2, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)})
 	// check that clean cache is not affected
 	bi, ok = clean.getBucketInfo(1)
 	require.False(ok)
@@ -239,7 +223,7 @@ func TestContractStakingDirty_noSideEffectOnClean(t *testing.T) {
 	// update bucket info existed in clean cache
 	clean.PutBucketInfo(2, &bucketInfo{TypeIndex: 1, CreatedAt: 1, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(1), Owner: identityset.Address(2)})
 	// update bucket info in dirty cache
-	require.NoError(dirty.updateBucketInfo(2, &bucketInfo{TypeIndex: 1, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)}))
+	dirty.updateBucketInfo(2, &bucketInfo{TypeIndex: 1, CreatedAt: 3, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(3), Owner: identityset.Address(4)})
 	// check that clean cache is not affected
 	bi, ok = clean.getBucketInfo(2)
 	require.True(ok)
@@ -253,7 +237,7 @@ func TestContractStakingDirty_noSideEffectOnClean(t *testing.T) {
 	// remove bucket info existed in clean cache
 	clean.PutBucketInfo(3, &bucketInfo{TypeIndex: 1, CreatedAt: 1, UnlockedAt: maxBlockNumber, UnstakedAt: maxBlockNumber, Delegate: identityset.Address(1), Owner: identityset.Address(2)})
 	// remove bucket info from dirty cache
-	require.NoError(dirty.deleteBucketInfo(3))
+	dirty.deleteBucketInfo(3)
 	// check that clean cache is not affected
 	bi, ok = clean.getBucketInfo(3)
 	require.True(ok)
