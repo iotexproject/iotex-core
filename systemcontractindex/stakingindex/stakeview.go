ad0c950f474bf31c50534c33527e946e80337f6a
envestcc <chen1233216@hotmail.com>
2025-06-20 15:50:10 +0800

    build staking view from blockdao if indexer height behind

--- systemcontractindex/stakingindex/stakeview.go
+++ systemcontractindex/stakingindex/stakeview.go
@@ -5,10 +5,13 @@ import (
 	"sync"
 
 	"github.com/iotexproject/iotex-address/address"
+	"github.com/pkg/errors"
 
 	"github.com/iotexproject/iotex-core/v2/action"
 	"github.com/iotexproject/iotex-core/v2/action/protocol"
 	"github.com/iotexproject/iotex-core/v2/action/protocol/staking"
+	"github.com/iotexproject/iotex-core/v2/blockchain/block"
+	"github.com/iotexproject/iotex-core/v2/blockchain/genesis"
 )
 
 type stakeView struct {
@@ -63,3 +66,33 @@ func (s *stakeView) Handle(ctx context.Context, receipt *action.Receipt) error {
 }
 
 func (s *stakeView) Commit() {}
+
+func (s *stakeView) BuildWithBlock(ctx context.Context, blk *block.Block) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if blk.Height() != s.height+1 {
+		return errors.Errorf("block height %d does not match stake view height %d", blk.Height(), s.height+1)
+	}
+	g, ok := genesis.ExtractGenesisContext(ctx)
+	if !ok {
+		return errors.New("failed to extract genesis context")
+	}
+	blkCtx := protocol.BlockCtx{
+		BlockHeight:    blk.Height(),
+		BlockTimeStamp: blk.Timestamp(),
+		GasLimit:       g.BlockGasLimitByHeight(blk.Height()),
+		Producer:       blk.PublicKey().Address(),
+		BaseFee:        blk.BaseFee(),
+		ExcessBlobGas:  blk.BlobGasUsed(),
+	}
+	ctx = protocol.WithBlockCtx(ctx, blkCtx)
+	muted := s.helper.muteHeight > 0 && blk.Height() >= s.helper.muteHeight
+	handler := newEventHandler(s.helper.bucketNS, s.cache, blkCtx, s.helper.timestamped, muted)
+	for _, receipt := range blk.Receipts {
+		if err := s.helper.handleReceipt(ctx, handler, receipt); err != nil {
+			return errors.Wrapf(err, "failed to handle receipt at height %d", blk.Height())
+		}
+	}
+	s.height = blk.Height()
+	return nil
+}
