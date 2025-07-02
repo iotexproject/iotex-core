af5ed0be0db806af0a3f525867cd889ff31a9d0f
zhi <thecoderzhi@gmail.com>
2025-07-02 11:37:24 +0800

    Revert "cow cache for staking view (#4655)"
    This reverts commit 7d5c499f2b27793dd940e7ab6e92e3d2c63a8308.

--- blockindex/contractstaking/stakeview.go
+++ blockindex/contractstaking/stakeview.go
@@ -2,96 +2,59 @@ package contractstaking
 
 import (
 	"context"
-	"sync"
 
 	"github.com/iotexproject/iotex-address/address"
-	"github.com/iotexproject/iotex-proto/golang/iotextypes"
-
 	"github.com/iotexproject/iotex-core/v2/action"
 	"github.com/iotexproject/iotex-core/v2/action/protocol"
 	"github.com/iotexproject/iotex-core/v2/action/protocol/staking"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
 )
 
 type stakeView struct {
 	helper *Indexer
-	clean  *contractStakingCache
-	dirty  *contractStakingCache
+	cache  *contractStakingCache
 	height uint64
-	mu     sync.RWMutex
 }
 
 func (s *stakeView) Clone() staking.ContractStakeView {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	clone := &stakeView{
+	return &stakeView{
 		helper: s.helper,
-		clean:  s.clean,
-		dirty:  nil,
+		cache:  s.cache.Clone(),
 		height: s.height,
 	}
-	if s.dirty != nil {
-		clone.clean = s.dirty.Clone()
-	}
-	return clone
 }
 
 func (s *stakeView) BucketsByCandidate(candidate address.Address) ([]*Bucket, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if s.dirty != nil {
-		return s.dirty.bucketsByCandidate(candidate, s.height)
-	}
-	return s.clean.bucketsByCandidate(candidate, s.height)
+	return s.cache.bucketsByCandidate(candidate, s.height)
 }
 
 func (s *stakeView) CreatePreStates(ctx context.Context) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
 	blkCtx := protocol.MustGetBlockCtx(ctx)
 	s.height = blkCtx.BlockHeight
 	return nil
 }
 
 func (s *stakeView) Handle(ctx context.Context, receipt *action.Receipt) error {
+	blkCtx := protocol.MustGetBlockCtx(ctx)
+	// new event handler for this receipt
+	handler := newContractStakingEventHandler(s.cache)
+
+	// handle events of receipt
 	if receipt.Status != uint64(iotextypes.ReceiptStatus_Success) {
 		return nil
 	}
-	var (
-		blkCtx  = protocol.MustGetBlockCtx(ctx)
-		handler *contractStakingEventHandler
-	)
 	for _, log := range receipt.Logs() {
 		if log.Address != s.helper.config.ContractAddress {
 			continue
-		}
-		if handler == nil {
-			s.mu.Lock()
-			// new event handler for this receipt
-			if s.dirty == nil {
-				s.dirty = s.clean.Clone()
-			}
-			handler = newContractStakingEventHandler(s.dirty)
-			s.mu.Unlock()
 		}
 		if err := handler.HandleEvent(ctx, blkCtx.BlockHeight, log); err != nil {
 			return err
 		}
 	}
-	if handler == nil {
-		return nil
-	}
 	_, delta := handler.Result()
 	// update cache
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.dirty.Merge(delta, blkCtx.BlockHeight)
-}
-
-func (s *stakeView) Commit() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	if s.dirty != nil {
-		s.clean = s.dirty
-		s.dirty = nil
+	if err := s.cache.Merge(delta, blkCtx.BlockHeight); err != nil {
+		return err
 	}
+	return nil
 }
