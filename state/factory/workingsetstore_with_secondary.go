155af2d98458e1775980373db2b2793556f72781
envestcc <chen1233216@hotmail.com>
2025-07-16 10:20:08 +0800

    commit erigon db first instead of trie

--- state/factory/workingsetstore_with_secondary.go
+++ state/factory/workingsetstore_with_secondary.go
@@ -118,14 +118,17 @@ func (store *workingSetStoreWithSecondary) DeleteObject(ns string, key []byte, o
 }
 
 func (store *workingSetStoreWithSecondary) Commit(ctx context.Context, retention uint64) error {
+	// Commit to secondary store first, then commit to main store
+	// This ensures that if the secondary store fails, the main store is not committed
+	// the main store can be committed independently, but the secondary store cannot
 	t1 := time.Now()
-	if err := store.writer.Commit(ctx, retention); err != nil {
+	if err := store.writerSecondary.Commit(ctx, retention); err != nil {
 		return err
 	}
-	perfMtc.WithLabelValues("commitstore").Set(float64(time.Since(t1).Nanoseconds()))
+	perfMtc.WithLabelValues("commiterigon").Set(float64(time.Since(t1).Nanoseconds()))
 	t2 := time.Now()
-	err := store.writerSecondary.Commit(ctx, retention)
-	perfMtc.WithLabelValues("commiterigon").Set(float64(time.Since(t2).Nanoseconds()))
+	err := store.writer.Commit(ctx, retention)
+	perfMtc.WithLabelValues("commitstore").Set(float64(time.Since(t2).Nanoseconds()))
 	return err
 }
 
