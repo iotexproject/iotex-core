678383b0ffca0130fff59d05b7be4d6efcd1c326
envestcc <chen1233216@hotmail.com>
2025-08-11 12:34:18 +0800

    fix account protocol contract abi

--- action/account_contract_interface.sol
+++ action/account_contract_interface.sol
@@ -5,13 +5,18 @@ pragma solidity ^0.8.24;
 interface IAccountProtocolContract {
     // enum representing different types of transfer events
     enum TransactionLogType {
-        NATIVE_TRANSFER,
         IN_CONTRACT_TRANSFER,
+        WITHDRAW_BUCKET,
+        CREATE_BUCKET,
+        DEPOSIT_TO_BUCKET,
+        CANDIDATE_SELF_STAKE,
+        CANDIDATE_REGISTRATION_FEE,
         GAS_FEE,
+        NATIVE_TRANSFER,
         DEPOSIT_TO_REWARDING_FUND,
         CLAIM_FROM_REWARDING_FUND,
-        PRIORITY_FEE,
-        BLOB_FEE
+        BLOB_FEE,
+        PRIORITY_FEE
     }
 
     // Event emitted when a transfer occurs
