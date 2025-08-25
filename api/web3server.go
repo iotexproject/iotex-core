e192e8d421d1acc64b9509054b63816618500ade
envestcc <chen1233216@hotmail.com>
2025-08-25 10:33:28 +0800

    fix test

--- api/web3server.go
+++ api/web3server.go
@@ -1260,7 +1260,7 @@ func (svr *web3Handler) traceTransaction(ctx context.Context, in *gjson.Result) 
 	if err != nil {
 		return nil, err
 	}
-	switch tracer := tracer.(*evmTracer).EVMLogger.(type) {
+	switch tracer := tracer.(type) {
 	case *logger.StructLogger:
 		return &debugTraceTransactionResult{
 			Failed:      receipt.Status != uint64(iotextypes.ReceiptStatus_Success),
@@ -1296,7 +1296,7 @@ func (svr *web3Handler) traceCall(ctx context.Context, in *gjson.Result) (interf
 	if err != nil {
 		return nil, err
 	}
-	switch tracer := tracer.(*evmTracer).EVMLogger.(type) {
+	switch tracer := tracer.(type) {
 	case *logger.StructLogger:
 		return &debugTraceTransactionResult{
 			Failed:      receipt.Status != uint64(iotextypes.ReceiptStatus_Success),
